fd0ef2bfe8f66ec6c87aa3fd9fa3255244b5a524
Starcev <starcev_misha@mail.ru>
2017-11-25 12:38:16 +0500

    add TagCloudVisualization

---
+++ TagsCloudVisualization/CloudTagDrawer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TagsCloudVisualization
+{
+    public static class CloudTagDrawer
+    {
+        public static void DrawTagsToFile(Point cloudCenter, Dictionary<Rectangle, (string, Font)> tagsDict, string name, int width, int height)
+        {
+            using (var bitmap = new Bitmap(width, height))
+            {
+                DrawTagsOnBitmap(cloudCenter, tagsDict, bitmap);
+                bitmap.Save(name);
+            }
+        }
+
+        public static void DrawTagsToForm(Point cloudCenter, Dictionary<Rectangle, (string, Font)> tagsDict, int width, int height)
+        {
+            using (var bitmap = new Bitmap(width, height))
+            {
+                DrawTagsOnBitmap(cloudCenter, tagsDict, bitmap);
+                Form aForm = new Form();
+                aForm.Width = 800;
+                aForm.Height = 800;
+                aForm.BackgroundImage = bitmap;
+                aForm.ShowDialog();
+            }
+        }
+
+        private static void DrawTagsOnBitmap(Point cloudCenter, Dictionary<Rectangle, (string, Font)> tagsDict, Bitmap bitmap)
+        {
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                var selPen = new Pen(Color.Blue);
+                var selBrush = new SolidBrush(Color.Black);
+                g.DrawRectangle(new Pen(Color.Red), (int) cloudCenter.X, (int) cloudCenter.Y, 1, 1);
+                foreach (var tag in tagsDict)
+                {
+                    g.DrawRectangle(selPen, tag.Key);
+                    g.DrawString(tag.Value.Item1, tag.Value.Item2, selBrush, tag.Key.X, tag.Key.Y);
+                }
+            }
+        }
+        private static void DrawRectanglesOnBitmap(Point cloudCenter, List<Rectangle> rectangles, Bitmap bitmap)
+        {
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                var selPen = new Pen(Color.Blue);
+                var selBrush = new SolidBrush(Color.Black);
+                g.DrawRectangle(new Pen(Color.Red), (int)cloudCenter.X, (int)cloudCenter.Y, 1, 1);
+                foreach (var rectangle in rectangles)
+                    g.DrawRectangle(selPen, rectangle);
+
+            }
+        }
+
+        public static void DrawRectanglesToFile(Point cloudCenter,List<Rectangle> rectangles, string name, int width, int height)
+        {
+            using (var bitmap = new Bitmap(width, height))
+            {
+                DrawRectanglesOnBitmap(cloudCenter, rectangles, bitmap);
+                bitmap.Save(name);
+            }
+        }
+
+    }
+}
---
+++ TagsCloudVisualization/Tests/RectangleExtensions_Tests.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    public class RectangleExtensions_Tests
+    {
+        [TestFixture]
+        public class RectangleExteinsions_Should
+        {
+
+
+            [Test]
+            public void IntersectWithAny_ShouldBeTrue_OnIntersectingWithAllRectangles()
+            {
+                var rectangles = new List<Rectangle>
+                {
+                    new Rectangle(5, 5, 10, 10),
+                    new Rectangle(15, 15, 10, 10)
+                };
+                var myRectangle = new Rectangle(12, 12, 10, 10);
+                myRectangle.IntersectWithAny(rectangles).Should().BeTrue();
+            }
+
+            [Test]
+            public void IntersectWithAny_ShouldBeTrue_OnIntersectingWithOneRectangle()
+            {
+                var rectangles = new List<Rectangle>
+                {
+                    new Rectangle(5, 5, 10, 10),
+                    new Rectangle(15, 15, 10, 10)
+                };
+                var myRectangle = new Rectangle(15, 23, 10, 10);
+                myRectangle.IntersectWithAny(rectangles).Should().BeTrue();
+            }
+
+            [Test]
+            public void IntersectWithAny_ShouldBeFalse_WhenDontIntersect()
+            {
+                var rectangles = new List<Rectangle>
+                {
+                    new Rectangle(5, 5, 10, 10),
+                    new Rectangle(15, 15, 10, 10)
+                };
+                var myRectangle = new Rectangle(0, 0, 2, 2);
+                myRectangle.IntersectWithAny(rectangles).Should().BeFalse();
+            }
+
+
+            [Test]
+            public void DistanceTo_Test()
+            {
+                var myRectangle = new Rectangle(0, 0, 10, 10);
+                var center = new Vector(9, 8);
+                myRectangle.DistanceTo(center).Should().Be(5);
+            }
+
+            [Test]
+            public void TopLeft_Test()
+            {
+                var rectangle = new Rectangle(10, 10, 10, 10);
+                var expectedTopLeft = new Vector(10, 10);
+                rectangle.TopLeft().Should().Be(expectedTopLeft);
+            }
+
+            [Test]
+            public void Center_Test()
+            {
+                var rectangle = new Rectangle(0, 0, 10, 10);
+                var expectedCenter = new Vector(5, 5);
+                rectangle.Center().Should().Be(expectedCenter);
+            }
+        }
+    }
+}
