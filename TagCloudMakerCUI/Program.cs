cfacff3002b0584150c1c7a69ad54b64a02e79f4
AntonMikheev666 <AntonMikheev666@gmail.com>
2017-12-06 17:15:46 +0500

    Check

--- TagCloudMakerCUI/Program.cs
+++ TagCloudMakerCUI/Program.cs
@@ -14,29 +14,6 @@ using CommandLine.Text;
 
 namespace TagCloudMakerCUI
 {
-    class Option
-    {
-        [Option('i', "inputFile", Required = true, HelpText = "Input file path.")]
-        public string InputFilePath { get; set; }
-
-        [Option('e', "excludFile", Required = false, HelpText = "File with words to exclude.")]
-        public string ExcludingFilePath { get; set; }
-
-        [Option('f', "fintSize", Required = true, HelpText = "Font size in pixels.")]
-        public int FontSize { get; set; }
-
-        [Option('b', "background", Required = true, HelpText = "Background color.")]
-        public string BackColor { get; set; }
-
-        [Option('c', "textColor", Required = true, HelpText = "Text color.")]
-        public string TextColor { get; set; }
-
-        [Option('w', "width", Required = true, HelpText = "Image width.")]
-        public int Width { get; set; }
-
-        [Option('h', "height", Required = true, HelpText = "Image height.")]
-        public int Height { get; set; }
-    }
     class Program
     {
         static void Main(string[] args)
@@ -44,9 +21,9 @@ namespace TagCloudMakerCUI
             var option = new Option();
             var isValid = Parser.Default.ParseArguments(args, option);
             
-            if (!isValid)
+            if (!isValid || !option.AllParamsSet())
             {
-                Console.WriteLine("Not all required arguments was passed.");
+                Console.WriteLine("Not all required arguments was passed correctly.");
                 return;
             }
 
@@ -56,9 +33,9 @@ namespace TagCloudMakerCUI
             using (var scope = GetContainer(excludingWords).BeginLifetimeScope())
             {
                 var maker = scope.Resolve<ITagCloudMaker>();
-                var path = maker.CreateTagCloud(option.InputFilePath, option.FontSize,
+                var path = maker.CreateTagCloud(option.InputFilePath, (int)option.FontSize,
                     new DrawingSettings(Color.FromName(option.BackColor), Color.FromName(option.TextColor), 
-                    FontFamily.GenericMonospace, new Size(option.Width, option.Height), ImageFormat.Png));
+                    FontFamily.GenericMonospace, new Size((int)option.Width, (int)option.Height), ImageFormat.Png));
                 Console.WriteLine(path);
             }
         }
