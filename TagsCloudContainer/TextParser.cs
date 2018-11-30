2d39ec5b4fc586bf40229985f41f927756ebfa5e
vaspahomov <vas2142553@gmail.com>
2018-11-30 10:35:45 +0700

    Add parser transit words to lowercase

--- TagsCloudContainer/TextParser.cs
+++ TagsCloudContainer/TextParser.cs
@@ -1,14 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TagsCloudContainer
 {
-    public class TextParser:ITextParser
+    public class TextParser : ITextParser
     {
         public IEnumerable<string> GetWords(string text)
         {
-            var separators = new[]{' ', '\n'};
+            var separators = new[] { ' ', '\n' };
+            var letters = 
+                string.Join("", text.
+                    Where(c => char.IsLetter(c) || separators.Contains(c)));
 
-            return text.Split(separators);
+            return letters
+                .Split(separators)
+                .Select(w => w.ToLower());
         }
     }
 }
