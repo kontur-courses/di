15edd997cd6b6e42dca8c7c853c89eea61a11d33
Artemiy <38810090+CGOptimum@users.noreply.github.com>
2018-12-03 19:08:45 +0500

    rectangle layouter moved to package

--- TagCloud/RectanglesLayouter/CircularCloudLayouter.cs
+++ TagCloud/RectanglesLayouter/CircularCloudLayouter.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using TagCloud.RectanglesLayouter.PointsGenerator;
 
-namespace TagCloud
+namespace TagCloud.RectanglesLayouter
 {
-    public class CircularCloudLayouter : ILayouter
+    public class CircularCloudLayouter : IRectangleLayouter
     {
         private readonly Point center;
         private readonly IEnumerator<Point> pointsEnumerator;
