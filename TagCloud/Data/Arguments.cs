d92ef35d34711e22dcb62969e0a7dc99d96f3aa3
Artemiy <38810090+CGOptimum@users.noreply.github.com>
2018-12-03 19:07:31 +0500

    tagCloudGenerator added

---
+++ TagCloud/Data/Arguments.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace TagCloud.Data
+{
+    public class Arguments
+    {
+        public string WordsFileName;
+        public string BoringWordsFileName;
+        public string ImageFileName;
+        public int Multiplier = 10;
+        public Brush WordsBrush = Brushes.Black;
+        public Brush BackgroundBrush = Brushes.White;
+        public FontFamily FontFamily = FontFamily.GenericMonospace;
+    }
+}
---
+++ TagCloud/TagCloudGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.Hosting;
+using TagCloud.Data;
+using TagCloud.Drawer;
+using TagCloud.Parser;
+using TagCloud.WordsLayouter;
+
+namespace TagCloud
+{
+    public class TagCloudGenerator
+    {
+        private readonly IWordsParser wordsParser;
+        private readonly IWordsLayouter wordsLayouter;
+        private readonly IWordsDrawer wordsDrawer;
+
+        public TagCloudGenerator(IWordsParser wordsParser, IWordsLayouter wordsLayouter, IWordsDrawer wordsDrawer)
+        {
+            this.wordsParser = wordsParser;
+            this.wordsLayouter = wordsLayouter;
+            this.wordsDrawer = wordsDrawer;
+        }
+
+        public Bitmap Generate(IEnumerable<string> words, IEnumerable<string> boringWords, Arguments arguments)
+        {
+            var wordInfos = wordsParser.Parse(words, new HashSet<string>(boringWords));
+            var layout = wordsLayouter.GenerateLayout(wordInfos, arguments.FontFamily, arguments.Multiplier);
+            var image = wordsDrawer.CreateImage(layout, arguments.WordsBrush, arguments.BackgroundBrush);
+            return image;
+        }
+    }
+}
