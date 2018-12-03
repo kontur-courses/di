148b19aa6da4b0849ce0347ec43e2f3e47e90fe3
Artemiy <38810090+CGOptimum@users.noreply.github.com>
2018-12-03 19:04:06 +0500

    parser added

---
+++ TagCloud/Data/WordInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace TagCloud.Data
+{
+    public class WordInfo
+    {
+        public readonly string Word;
+        public readonly int Occurrences;
+
+        public WordInfo(string word, int occurrences)
+        {
+            Word = word;
+            Occurrences = occurrences;
+        }
+    }
+}
---
+++ TagCloud/Parser/IWordsParser.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using TagCloud.Data;
+
+namespace TagCloud.Parser
+{
+    public interface IWordsParser
+    {
+        IEnumerable<WordInfo> Parse(IEnumerable<string> words, HashSet<string> boringWords);
+    }
+}
