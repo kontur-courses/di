a812a772ca49f183fd6798e48bf6e7de846772b0
Dmitriy Klopov <klopov@skbkontur.ru>
2018-12-03 12:32:14 +0500

    Исправил ошибку в контейнере

--- TagsCloudVisualization/TagsCloudVisualization/Program.cs
+++ TagsCloudVisualization/TagsCloudVisualization/Program.cs
@@ -1,36 +1,43 @@
 using System;
 using System.Windows.Forms;
 using Autofac;
-using FluentAssertions;
 using TagsCloudVisualization.Curves;
 
 namespace TagsCloudVisualization
 {
     public class CloudWordsForm
     {
+        private static IContainer container;
+
         private static void Main(string[] args)
         {
-            var kernel = new ContainerBuilder();
-            kernel.RegisterType<CloudParametersParser>().As<ICloudParametersParser>();
-            kernel.RegisterType<WordDataHandler>().As<IWordDataHandler>();
-            kernel.RegisterType<CircularCloudLayouter>().UsingConstructor(typeof(ICurve)).As<ICloudLayouter>();
-            kernel.RegisterType<Spiral>().As<ICurve>();
-            kernel.RegisterType<Astroid>().As<ICurve>();
-            kernel.RegisterType<Heart>().As<ICurve>();
-            var container = kernel.Build();
-
+            var builder = new ContainerBuilder();
+            Load(builder);
+            container = builder.Build();
             var cloudParametersParser = container.Resolve<ICloudParametersParser>();
             var parameters = cloudParametersParser.Parse(args);
 
             if (!parameters.IsCorrect())
                 return;
 
-            var cloud = container.Resolve<ICloudLayouter>();
+            var cloud = container.Resolve<ICloudLayouter>(new TypedParameter(typeof(ICurve), parameters.Curve));
             var wordDataHandler = container.Resolve<IWordDataHandler>();
             var data = wordDataHandler.GetDatas(cloud);
             var picture = TagsCloudVisualizer.GetPicture(data, parameters);
             picture.Save($"{Application.StartupPath}\\CloudTags.png");
             Console.WriteLine($"Picture saved in {Application.StartupPath}\\CloudTags.png");
+        }
+
+        protected static void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CloudParametersParser>().As<ICloudParametersParser>();
+            builder.RegisterType<WordDataHandler>().As<IWordDataHandler>();
+            builder.RegisterType<CircularCloudLayouter>()
+                .As<ICloudLayouter>()
+                .WithParameter(new TypedParameter(typeof(ICurve), "curve"));
+            builder.RegisterType<Spiral>().As<ICurve>();
+            builder.RegisterType<Astroid>().As<ICurve>();
+            builder.RegisterType<Heart>().As<ICurve>();
         }
     }
 }
