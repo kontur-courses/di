6bb6887f09648e1f67a7e8b42f2171887e00c49f
Andrew <avural-k@mail.ru>
2018-12-07 00:41:44 +0500

    Add console application class

---
+++ TagCloud/ConsoleAplication.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using TagsCloudVisualization;
+
+namespace TagCloud
+{
+    public class ConsoleAplication : IAplication
+    {
+        private readonly ITextParcer parser;
+        private readonly CircularCloudLayouter cloud;
+        private readonly Visualiser visualiser;
+
+        public ConsoleAplication(ITextParcer textParser, CircularCloudLayouter cloud,
+            Visualiser visualiser)
+        {
+            this.visualiser = visualiser;
+            this.cloud = cloud;
+            this.parser = textParser;
+        }
+
+        public void Run()
+        {
+            var words = parser.TryGetWordsFromText();
+            if (words == null)
+                return;
+            var parsedWords = parser.ParseWords(words);
+            cloud.AddWordsFromDictionary(parsedWords);
+            visualiser.RenderCurrentConfig(cloud);
+        }
+    }
+}
