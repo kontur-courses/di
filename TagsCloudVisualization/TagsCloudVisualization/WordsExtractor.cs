c791ecb1c58ae6b203f8a045a44ce0bafcd9dda8
Dmitriy Klopov <klopov@skbkontur.ru>
2018-12-07 12:59:12 +0500

    Добавил возможность чтения ворд файлов

--- TagsCloudVisualization/TagsCloudVisualization/WordsExtractor.cs
+++ TagsCloudVisualization/TagsCloudVisualization/WordsExtractor.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 using TagsCloudVisualization.Interfaces;
 using TagsCloudVisualization.Settings;
 
@@ -11,7 +13,10 @@ namespace TagsCloudVisualization
     {
         public List<string> Extract(string path, IWordsExtractorSettings settings)
         {
-            var text = File.ReadAllText(path, Encoding.Default)
+            var format = path.Substring(path.IndexOf(".", StringComparison.Ordinal) + 1);
+            var text = GetTotalText(path, format);
+
+            text = text
                 .Replace("\n", " ")
                 .Replace("\r", " ")
                 .Replace("\t", " ");
@@ -20,6 +25,31 @@ namespace TagsCloudVisualization
                 .Where(w => w.Length >= 3 && w != string.Empty && !settings.StopWords.Contains(w))
                 .Select(w => w.Trim().ToLowerInvariant()).ToList();
             return words;
+        }
+
+        private static string GetTotalText(string path, string format)
+        {
+            if (format.StartsWith("doc"))
+            {
+                var textBuilder = new StringBuilder();
+                var word = new Microsoft.Office.Interop.Word.Application();
+                object miss = System.Reflection.Missing.Value;
+                object fileName = Path.IsPathRooted(path) ? path : $"{Application.StartupPath}\\{path}";
+                var docs = word.Documents.Open(ref fileName, ref miss, ref miss, ref miss, ref miss, ref miss,
+                    ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss);
+
+                for (var i = 0; i < docs.Paragraphs.Count; i++)
+                    textBuilder.Append(docs.Paragraphs[i + 1].Range.Text);
+
+                docs.Close();
+                word.Quit();
+                return textBuilder.ToString();
+            }
+
+            if (format.Equals("txt"))
+                return File.ReadAllText(path, Encoding.Default);
+
+            throw new ArgumentException($"Unknown format: {format}");
         }
     }
 }
