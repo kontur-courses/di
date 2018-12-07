f635649e45ce20b15df8c9d2121f1887ed97c766
Ivan Plaksin <zero.netto.o@gmail.com>
2018-12-07 13:35:11 +0500

    Added TagsCloudBuilder class

---
+++ Homework/TagsCloudContainer/TagsCloudBuilder/Drawer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using TagsCloudContainer;
+
+namespace TagsCloudBuilder
+{
+    public static class Drawer
+    {
+        public static void DrawAndSaveWords(Size canvasSize, List<ContainerInfo> containers, string name,
+            ImageFormat imageFormat, bool debug = false, string path = "")
+        {
+            var stringFormat = new StringFormat
+            {
+                LineAlignment = StringAlignment.Center,
+                Alignment = StringAlignment.Center
+            };
+            using (var bitmap = new Bitmap(canvasSize.Height, canvasSize.Width))
+            {
+                using (var graphics = Graphics.FromImage(bitmap))
+                {
+                    graphics.FillRectangle(Brushes.White, 0, 0, canvasSize.Height, canvasSize.Width);
+                    foreach (var container in containers)
+                    {
+                        if (debug)
+                            graphics.DrawRectangle(Pens.Black, container.Rectangle);
+                        graphics.DrawString(
+                            container.Text,
+                            container.TextFont,
+                            new SolidBrush(container.TextColor),
+                            container.Rectangle,
+                            stringFormat);
+                    }
+
+                    try
+                    {
+                        bitmap.Save($"{path}{name}", ImageFormat.Png);
+                    }
+                    catch (ExternalException e)
+                    {
+                        Console.Error.WriteLine("Something gone wrong. Check the path correctness.", e);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Homework/TagsCloudContainer/TagsCloudBuilder/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagsCloudContainer;
+using TagsCloudVisualization;
+
+namespace TagsCloudBuilder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var wordsWithFrequency = new Dictionary<string, int>();
+            var withDebug = true;
+            var startPoint = new Point(1000, 1000);
+            var canvasSize = new Size(2000, 2000);
+
+            foreach (var word in TxtReader.ReadAllLines("text.txt"))
+            {
+                var lowerWord = word.ToLower();
+                if (!wordsWithFrequency.ContainsKey(lowerWord))
+                    wordsWithFrequency.Add(lowerWord, 1);
+                else
+                    wordsWithFrequency[lowerWord] += 1;
+            }
+
+            wordsWithFrequency = wordsWithFrequency
+                .RemoveWords(new List<string> { "Консультация", "Кадров", "Оценить" })
+                .RemoveWordsOutOfLengthRange(leftBound: 4);
+
+            var cloudLayouter = new CircularCloudLayouter(startPoint);
+            var containerFormatter = new ContainerFormatter(wordsWithFrequency, cloudLayouter);
+
+            Drawer.DrawAndSaveWords(canvasSize, containerFormatter.ContainersInfo, "sample.png", ImageFormat.Png, withDebug);
+        }
+    }
+}
