c5a0e8327f157d2569cca502d52bb499ec89cddd
Alexander Debelyi <debelyy@yandex.ru>
2018-12-08 00:19:56 +0500

    fixed mystem wrapper problem with cyrillic path

---
+++ TagsCloudContainer.Tests/BoringWordsRemoverTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudContainer.WordsPreprocessors;
+
+namespace TagsCloudContainer.Tests
+{
+    [TestFixture]
+    public class BoringWordsRemoverTests
+    {
+        private IWordsPreprocessor boringWordsRemover;
+
+        [SetUp]
+        public void DoBeforeAnyTest()
+        {
+            boringWordsRemover = new BoringWordsRemover();
+        }
+
+        [Test]
+        public void Preprocess_RemovesBoringWords()
+        {
+            var data = "Заходи в дом. Иди на ту сторону".ToLower();
+            var expected = "Заходи дом иди сторону".ToLower().Split();
+
+            var actual = boringWordsRemover.Preprocess(data.Split());
+
+            actual.Should()
+                .BeEquivalentTo(expected);
+
+        }
+    }
+}
