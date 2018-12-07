918db71d7ccc2fa280d719deb38a526b3645c360
Svetlana <shvetsovasvev@gmail.com>
2018-12-07 11:59:53 +0500

    added implementation for console

---
+++ TagCloudConsole/EntryPoint.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Autofac;
+using CommandLine;
+using TagsCloud;
+using Graphics = TagsCloud.Graphics;
+
+namespace TagCloudConsole
+{
+    public static class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(Run)
+                .WithNotParsed(HandleErrors);
+        }
+
+        private static void Run(Options options)
+        {
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterType<TagCloudRender>().AsSelf();
+            containerBuilder.RegisterInstance(
+                new WithoutBoringWordCollection(
+                    new LowerWordCollection(
+                        new WordsFromFile(options.BoringWordsPath)
+                    ),
+                    new LowerWordCollection(
+                        new WordsFromFile(options.TextPath)
+                    )
+                )
+            ).As<IWordCollection>();
+            containerBuilder.RegisterInstance(new Graphics(options.Name, options.FontFamily,
+                Color.FromName(options.Color),
+                new Size(options.Height, options.Width))).AsSelf();
+            containerBuilder.RegisterType<CreateLayout>().AsSelf();
+            containerBuilder.RegisterType<FrequencyDictionary>().AsSelf();
+            containerBuilder.RegisterInstance(new CircularCloudLayouter(new Point(0, 0))).As<ICloudLayouter>();
+            using (var container = containerBuilder.Build())
+            {
+                var render = container.Resolve<TagCloudRender>();
+                render.Render();
+            }
+        }
+
+        private static void HandleErrors(IEnumerable<Error> errors)
+        {
+            Console.WriteLine(
+                string.Join(
+                    Environment.NewLine,
+                    errors
+                )
+            );
+        }
+    }
+}
---
+++ TagCloudConsole/Options.cs
@@ -0,0 +1,28 @@
+﻿using CommandLine;
+
+namespace TagCloudConsole
+{
+    public class Options
+    {
+        [Option('t', "text", Required = true, HelpText = "Input text path")]
+        public string TextPath { get; set; }
+
+        [Option('b', "boring", Default = "BoringWords.txt", HelpText = "Input boring words path")]
+        public string BoringWordsPath { get; set; }
+
+        [Option('f', "font", Default = "Arial", HelpText = "Font family")]
+        public string FontFamily { get; set; }
+
+        [Option('c', "color", Default = "Black", HelpText = "Text color")]
+        public string Color { get; set; }
+
+        [Option('w', "width", Default = 1000, HelpText = "Image width")]
+        public int Width { get; set; }
+
+        [Option('h', "height", Default = 1000, HelpText = "Image height")]
+        public int Height { get; set; }
+
+        [Option('n', "name", Default = "result", HelpText = "Image name")]
+        public string Name { get; set; }
+    }
+}
