9cf327bf547ad68aed56d46a9d425bcf5b859614
Arina <pada190814@gmail.com>
2018-12-09 23:10:27 +0500

    moved extension method to class for extension methods

--- TagsCloudVisualization/Layouter/PointExtension.cs
+++ TagsCloudVisualization/Layouter/PointExtension.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace TagsCloudVisualization.Layouter
 {
@@ -6,5 +7,8 @@ namespace TagsCloudVisualization.Layouter
     {
         public static Point Add(this Point point1, Point point2) =>
             new Point(point1.X + point2.X, point1.Y + point2.Y);
+
+        public static int GetDistanceTo(this Point point1, Point point2) =>
+            (int)Math.Sqrt(Math.Pow(point1.X - point2.X, 2) + Math.Pow(point1.Y - point2.Y, 2));
     }
 }
