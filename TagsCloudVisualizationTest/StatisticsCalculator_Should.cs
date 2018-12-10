d30de92fb48037bd54853171d26ea9f8c831cc2a
devmentality <nikita.volnov.1999@mail.ru>
2018-12-10 22:20:54 +0500

    Tests on statistics calculator

---
+++ TagsCloudVisualizationTest/StatisticsCalculator_Should.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudVisualization.Utils;
+
+
+namespace TagsCloudVisualizationTest
+{
+    [TestFixture]
+    class StatisticsCalculator_Should
+    {
+        private readonly StatisticsCalculator statisticsCalculator = new StatisticsCalculator();
+
+        [Test]
+        public void ReturnEmptyStatistics_WhenNoWords()
+        {
+            var statistics = statisticsCalculator.CalculateStatistics(new string[] { });
+
+            statistics.OrderedWordsStatistics.Should().BeEmpty();
+            statistics.AllWordsCount.Should().BeGreaterOrEqualTo(0);
+        }
+
+        [Test]
+        public void ReturnRightAllWordsCount_WhenAllWordsUnique()
+        {
+            var statistics = statisticsCalculator.CalculateStatistics(new [] { "car", "phone" });
+            statistics.AllWordsCount.Should().Be(2);
+        }
+
+        [Test]
+        public void ReturnRightAllWordsCount_WhenSomeWordsAreTheSame()
+        {
+            var statistics = statisticsCalculator.CalculateStatistics(new[] { "car", "phone", "car" });
+            statistics.AllWordsCount.Should().Be(3);
+        }
+
+        [Test]
+        public void CountWordsCorrectly()
+        {
+            var statistics = statisticsCalculator.CalculateStatistics(new[] { "car", "phone", "car" });
+            statistics.OrderedWordsStatistics.Should().BeEquivalentTo(
+                new WordStatistics("car", 2), new WordStatistics("phone", 1));
+
+        }
+
+        [Test]
+        public void OrderWordStatistics()
+        {
+            var statistics = statisticsCalculator.CalculateStatistics(new[] { "car", "phone", "car" });
+            statistics.OrderedWordsStatistics.Select(ws => ws.Count).Should().BeInDescendingOrder();
+        }
+    }}
--- TagsCloudVisualizationTest/TextFileReaderTest.cs
+++ TagsCloudVisualizationTest/TextFileReaderTest.cs
@@ -2,23 +2,24 @@
 using System.IO;
 using System.Reflection;
 using NUnit.Framework;
-using TagsCloudVisualization;
 using FluentAssertions;
+using TagsCloudVisualization.WordsFileReading;
 
 
 namespace TagsCloudVisualizationTest
 {
     [TestFixture]
-    public class SimpleFileReaderTest
+    public class TextFileReaderTest
     {
         [Test]
         public void Should_ReadFile_Correctly()
         {
-            var sr = new TextFileReader(
-                Path.Combine(
-                    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
-                    "Resources", "words.txt"));
-            var words = sr.ReadAllWords();
+            var pathToWords = Path.Combine(
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                "Resources", "words.txt");
+            var reader = new TextFileReader();
+
+            var words = reader.ReadAllWords(pathToWords, "txt");
             words.Should().BeEquivalentTo(new List<string> {"машина", "человек", "робот"});
         }
     }
