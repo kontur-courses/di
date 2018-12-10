1552661b30f7986d27d8e11d97ef80ab6adf92c3
kadukm <mk.shark25@gmail.com>
2018-12-11 02:28:41 +0500

    add almost empty CUI project with simple DI

---
+++ TagCloud.CUI/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using Autofac;
+using Autofac.Core;
+using TagCloud.Core.Layouters;
+using TagCloud.Core.Painters;
+using TagCloud.Core.Settings;
+using TagCloud.Core.TextWorking;
+using TagCloud.Core.TextWorking.WordsProcessing;
+using TagCloud.Core.TextWorking.WordsProcessing.ProcessingUtilities;
+using TagCloud.Core.TextWorking.WordsReading;
+using TagCloud.Core.TextWorking.WordsReading.WordsReadersForFiles;
+using TagCloud.Core.Visualizers;
+
+namespace TagCloud.CUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Core.TagCloud>().AsSelf();
+
+            builder.RegisterType<TextWorker>().AsSelf();
+            builder.RegisterType<TxtWordsReader>().As<IWordsReaderForFile>();
+            builder.RegisterType<GeneralWordsReader>().As<IWordsReader>();
+            builder.RegisterType<LowerCaseUtility>().As<IProcessingUtility>();
+            builder.RegisterType<SimpleWordsProcessor>().As<IWordsProcessor>();
+            builder.RegisterType<TextWorkingSettings>().AsSelf();
+
+            builder.RegisterType<SimpleTagCloudVisualizer>().As<ITagCloudVisualizer>();
+            builder.RegisterType<VisualizingSettings>().AsSelf();
+            builder.Register(ctx => new CircularCloudLayouter(new PointF(400, 300))).As<ICloudLayouter>();
+            builder.RegisterType<OneColorPainter>().As<IPainter>();
+            builder.RegisterType<PaintingSettings>().AsSelf();
+
+            var container = builder.Build();
+            
+            var tagCloud = container.Resolve<Core.TagCloud>();
+        }
+    }
+}
