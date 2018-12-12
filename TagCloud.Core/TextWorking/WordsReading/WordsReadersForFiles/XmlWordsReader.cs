74e0acde9903d6c0448bcdcad9fdc776df8f5257
kadukm <mk.shark25@gmail.com>
2018-12-12 13:20:26 +0500

    add XmlWordsReader

---
+++ TagCloud.Core/TextWorking/WordsReading/WordsReadersForFiles/XmlWordsReader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace TagCloud.Core.TextWorking.WordsReading.WordsReadersForFiles
+{
+    public class XmlWordsReader : IWordsReaderForFile
+    {
+        public string ReadingFileExtension { get; }
+        private readonly XmlSerializer serializer;
+
+        public XmlWordsReader()
+        {
+            ReadingFileExtension = ".xml";
+            serializer = new XmlSerializer(typeof(string[]));
+        }
+
+        public IEnumerable<string> ReadFrom(string path)
+        {
+            using (var r = new StreamReader(path))
+            {
+                return (string[])serializer.Deserialize(r);
+            }
+        }
+    }
+}
