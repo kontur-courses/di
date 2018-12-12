6506f5631c26352d5a7fcbaa8c1076edf79e236a
kadukm <mk.shark25@gmail.com>
2018-12-12 11:02:15 +0500

    fix bug and remove image' format resolving from settings

--- TagCloud.Core/Settings/TagCloudSettings.cs
+++ TagCloud.Core/Settings/TagCloudSettings.cs
@@ -1,17 +1,10 @@
-﻿using System.Drawing.Imaging;
-using TagCloud.Core.Util;
-
-namespace TagCloud.Core.Settings
+﻿namespace TagCloud.Core.Settings
 {
     public class TagCloudSettings : ISettings
     {
         public string PathToWords { get; set; }
         public string PathToBoringWords { get; set; }
         public string PathForResultImage { get; set; } = "result.png";
-
-        public ImageFormat ImageFormat => ImageFormatResolver.TryResolveFromFileName(PathForResultImage, out var res)
-            ? ImageFormat.Png
-            : res;
 
         public string GetSettingsName()
         {
--- TagCloud.Core/TagCloud.cs
+++ TagCloud.Core/TagCloud.cs
@@ -1,6 +1,8 @@
 ﻿using System.Drawing;
+using System.Drawing.Imaging;
 using TagCloud.Core.Settings;
 using TagCloud.Core.TextWorking;
+using TagCloud.Core.Util;
 using TagCloud.Core.Visualizers;
 
 namespace TagCloud.Core
@@ -21,13 +23,21 @@ namespace TagCloud.Core
         public void MakeTagCloudAndSave()
         {
             var bitmap = MakeTagCloud();
-            bitmap.Save(settings.PathForResultImage, settings.ImageFormat);
+            Save(bitmap);
         }
 
         public Bitmap MakeTagCloud()
         {
             var tagStats = textWorker.GetTagStats(settings.PathToWords, settings.PathToBoringWords);
             return visualizer.Render(tagStats);
+        }
+
+        public void Save(Image image)
+        {
+            var imageFormat = ImageFormatResolver.TryResolveFromFileName(settings.PathForResultImage, out var res)
+                ? res
+                : ImageFormat.Png;
+            image.Save(settings.PathForResultImage, imageFormat);
         }
     }
 }
