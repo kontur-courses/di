0ea46906e00aa4e5ed64a463eabd0983cce158bf
Vicheslav <rozentor2@gmail.com>
2018-12-12 21:06:23 +0500

    Helper refactor

--- TagCloudUtility/Helper.cs
+++ TagCloudUtility/Helper.cs
@@ -1,41 +1,12 @@
 ﻿using System;
-using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
+using TagCloud.Utility.Container;
 
 namespace TagCloud.Utility
 {
     public static class Helper
     {
-        public static Bitmap ResizeImage(Image image, string size)
-        {
-            var width = int.Parse(size.Split('x').First());
-            var height = int.Parse(size.Split('x').Last());
-            var destRect = new Rectangle(0, 0, width, height);
-            var destImage = new Bitmap(width, height);
-
-            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-
-            using (var graphics = Graphics.FromImage(destImage))
-            {
-                graphics.CompositingMode = CompositingMode.SourceCopy;
-                graphics.CompositingQuality = CompositingQuality.HighQuality;
-                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                graphics.SmoothingMode = SmoothingMode.HighQuality;
-                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-
-                using (var wrapMode = new ImageAttributes())
-                {
-                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
-                }
-            }
-
-            return destImage;
-        }
-
         public static string GetExtension(string path)
         {
             return Path.GetExtension(path);
@@ -43,38 +14,36 @@ namespace TagCloud.Utility
 
         public static void CheckPaths(Options options)
         {
-            if (!Path.HasExtension(options.PathToWords))
-                throw new ArgumentException(
-                    $"Path to words should contain file type, but was {options.PathToWords}");
             if (!File.Exists(options.PathToWords))
                 throw new ArgumentException(
                     $"File {options.PathToWords} doesn't exists!");
+            if (!Path.HasExtension(options.PathToWords))
+                throw new ArgumentException(
+                    $"Path to words should contain file type, but was {options.PathToWords}");
 
             if (!Path.HasExtension(options.PathToPicture))
                 throw new ArgumentException(
                     $"Path to picture should contain picture type, but was {options.PathToPicture}");
-            if (!File.Exists(options.PathToPicture))
-                throw new ArgumentException(
-                    $"File {options.PathToPicture} doesn't exists!");
+
 
             if (options.PathToTags != null)
             {
-                if (!Path.HasExtension(options.PathToTags))
-                    throw new ArgumentException(
-                        $"Path to tags should contain file type, but was {options.PathToTags}");
                 if (!File.Exists(options.PathToTags))
                     throw new ArgumentException(
                         $"File {options.PathToTags} doesn't exists!");
+                if (!Path.HasExtension(options.PathToTags))
+                    throw new ArgumentException(
+                        $"Path to tags should contain file type, but was {options.PathToTags}");
             }
 
             if (options.PathToStopWords != null)
             {
-                if (!Path.HasExtension(options.PathToStopWords))
-                    throw new ArgumentException(
-                        $"Path to stopwords should contain file type, but was {options.PathToStopWords}");
                 if (!File.Exists(options.PathToStopWords))
                     throw new ArgumentException(
                         $"File {options.PathToStopWords} doesn't exists!");
+                if (!Path.HasExtension(options.PathToStopWords))
+                    throw new ArgumentException(
+                        $"Path to stopwords should contain file type, but was {options.PathToStopWords}");
             }
         }
 
