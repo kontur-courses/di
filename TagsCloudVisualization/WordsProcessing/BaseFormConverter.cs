61e47f686691ed3f3292653350f87fabea933010
Arina <pada190814@gmail.com>
2018-12-13 16:45:51 +0500

    base form converter

---
+++ TagsCloudVisualization/WordsProcessing/BaseFormConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NHunspell;
+
+namespace TagsCloudVisualization.WordsProcessing
+{
+    public class BaseFormConverter : IWordsChanger
+    {
+        private readonly string affFileName;
+        private readonly string dicFileName;
+
+        public BaseFormConverter(string affFileName, string dicFileName)
+        {
+            this.affFileName = affFileName;
+            this.dicFileName = dicFileName;
+        }
+
+        public IEnumerable<string> ChangeWords(IEnumerable<string> words)
+        {
+
+            using (var hunspell = new Hunspell(affFileName, dicFileName))
+            {
+                foreach (var word in words)
+                {
+                    var stems = hunspell.Stem(word);
+                    yield return stems.Count != 0 ? stems.First() : word;
+                }
+            }
+        }
+    }
+}
