cae6375088d573293628aaee45c77a22ec0203e5
Andrew <avural-k@mail.ru>
2018-12-13 14:10:44 +0500

    Добавил возможность чтения файлов других форматов

---
+++ TagCloud/DocTextReader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Word;
+
+namespace TagCloud
+{
+    public class DocTextReader : ITextReader
+    {
+        public string TryReadText(string fileName)
+        {
+            object filename = fileName;
+            object missed = Type.Missing;
+            var application = new Application();
+            try
+            {
+                application.Documents.Open(ref filename,
+                    ref missed,
+                    ref missed,
+                    ref missed,
+                    ref missed,
+                    ref missed,
+                    ref missed,
+                    ref missed,
+                    ref missed,
+                    ref missed,
+                    ref missed,
+                    ref missed,
+                    ref missed,
+                    ref missed,
+                    ref missed,
+                    ref missed);
+                var document = application.Documents.Application.ActiveDocument;
+                object start = 0;
+                object stop = document.Characters.Count;
+                var rng = document.Range(ref start, ref stop);
+                var text = rng.Text;
+                application.Quit(ref missed, ref missed, ref missed);
+                return text;
+            }
+            catch (COMException)
+            {
+                Console.WriteLine("File not found. Write full name to file.");
+                return null;
+            }
+            
+        }
+    }
+}
