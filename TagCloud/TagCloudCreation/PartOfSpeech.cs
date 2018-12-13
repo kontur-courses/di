59a5d8394a054da32ded81c751eb047aed844397
Anton Voitsishevskii <grygry0@gmail.com>
2018-12-13 17:49:03 +0300

    reformat; move POS enum to outer scope;

---
+++ TagCloud/TagCloudCreation/PartOfSpeech.cs
@@ -0,0 +1,13 @@
+﻿namespace TagCloudCreation
+{
+    public enum PartOfSpeech
+    {
+        Verb,
+        Noun,
+        Adjective,
+        Modal,
+        Untagged,
+        Interjection,
+        Preposition
+    }
+}
