289bc444a80d1a04c6f653546ff914be89e24184
Andrew <avural-k@mail.ru>
2018-12-13 14:11:13 +0500

    Добовил возможность изменения цветов раскладки

--- TagCloud/Program.cs
+++ TagCloud/Program.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using DocoptNet;
 using System.Drawing;
+using System.Linq;
 using Autofac;
 using TagsCloudVisualization;
 
@@ -9,35 +11,55 @@ namespace TagCloud
     class Program
     {
         private static readonly Point CloudCenter = new Point(300, 300);
-        private static readonly Size PictureSize = new Size(5000, 5000);
+        private static readonly Size PictureSize = new Size(10000, 10000);
         private const string Usage = @"
     Usage:
-      TagCloud.exe --input=<file> --output=<file>
+      TagCloud.exe --input=<file> --output=<file> [--bgColor=<color>] [--textColor=<color>] [--sqrt | --trgl]
       TagCloud.exe (-h | --help)
 
     Options:
-      -h --help           Show this screen.
-      --input file        Chose input text file.
-      --output file       Chose output image File.
+      -h --help           Показывает подсказку по использованию.
+      --input file        Выбор файла для обрабоки.(Поддерживает текстовые форматы: txt, doc, docx).
+      --output file       Выбор файла для вывода результата..
+      --bgColor color     Выбор цвета фона в шестнадцатиричном формате.
+      --textColor color   Выбор цвета текста в шестнадцатирочном формате.
+      --sqrt              Форма облака - квадрат.
+      --trgl              Форма облака - треугольник.
 
     ";
 
         static void Main(string[] args)
         {
             var arguments = ParseArguments(args);
+            var bgColor = arguments["--bgColor"];
+            var textColor = arguments["--textColor"];
+            IPointsMaker pointsMaker;
+            if (arguments["--form"] == "sqrt") 
+                pointsMaker = new SquarePointsMaker();
+            else if (arguments["--form"] == "trgl")
+                pointsMaker = new TrianglePointsMaker();
+            else pointsMaker = new ArchimedesSpiralPointsMaker();
+
+            ITextReader textReader;
+            if (arguments["--fileFormat"] == "doc" 
+                || arguments["--fileFormat"] == "docx")
+                textReader = new DocTextReader();
+            else textReader = new TxtTextReader();
+
             var containerBuilder = new ContainerBuilder();
-            containerBuilder.RegisterInstance(new TxtTextReader()).As<ITextReader>();
+            containerBuilder.RegisterInstance(textReader).As<ITextReader>();
             containerBuilder.RegisterType<SimpleWordParser>().As<IWordParser>();
             containerBuilder.RegisterType<SimpleWordChanger>().As<IWordChanger>();
             containerBuilder.RegisterType<TextParser>().As<ITextParcer>();
-            containerBuilder.RegisterInstance(new CircularCloudLayouter(CloudCenter, 0.1))
+            containerBuilder.RegisterInstance(new CloudLayouter(CloudCenter, 1, pointsMaker))
                 .As<ICloudLayouter>()
                 .SingleInstance();
-            containerBuilder.RegisterInstance(new Visualizer(PictureSize))
+            containerBuilder.RegisterInstance(new Visualizer(PictureSize, bgColor, textColor))
                 .As<Visualizer>()
                 .SingleInstance();
             containerBuilder.RegisterType<ConsoleApplication>();
             var container = containerBuilder.Build();
+
             var app = container.Resolve<ConsoleApplication>();
             var input = arguments["--input"];
             var output = arguments["--output"];
@@ -50,6 +72,18 @@ namespace TagCloud
             var arguments = new Docopt().Apply(Usage, args, exit: true);
             parsedArguments.Add("--input", arguments["--input"].ToString());
             parsedArguments.Add("--output", arguments["--output"].ToString());
+            var inputFormat = parsedArguments["--input"]
+                .Split(new char[] {'.'}, StringSplitOptions.RemoveEmptyEntries).Last();
+            parsedArguments.Add("--fileFormat", inputFormat);
+            if (arguments["--sqrt"].IsTrue)
+                parsedArguments.Add("--form", "sqrt");
+            else if (arguments["--trgl"].IsTrue)
+                parsedArguments.Add("--form", "trgl");
+            else parsedArguments.Add("--form", "circle");
+            parsedArguments.Add("--bgColor",
+                arguments["--bgColor"] != null ? arguments["--bgColor"].ToString() : "#FFFFFF");
+            parsedArguments.Add("--textColor",
+                arguments["--textColor"] != null ? arguments["--textColor"].ToString() : "#000000");
             return parsedArguments;
         }
     }
--- TagCloud/Visualizer.cs
+++ TagCloud/Visualizer.cs
@@ -8,11 +8,13 @@ namespace TagCloud
         private readonly Size bitmapSize;
         private readonly Pen pen;
         private readonly Point center;
+        private readonly Color backGroundColor;
 
-        public Visualizer(Size bitmapSize)
+        public Visualizer(Size bitmapSize, string bgColor, string textColor)
         {
             this.bitmapSize = bitmapSize;
-            pen = new Pen(Color.Black);
+            this.backGroundColor = ColorTranslator.FromHtml(bgColor);
+            pen = new Pen(ColorTranslator.FromHtml(textColor));
             center = new Point(bitmapSize.Width / 2, bitmapSize.Height / 2);
         }
 
@@ -20,6 +22,10 @@ namespace TagCloud
         {
             var bitmap = new Bitmap(bitmapSize.Width, bitmapSize.Height);
             var graphics = Graphics.FromImage(bitmap);
+            using (var brush = new SolidBrush(backGroundColor))
+            {
+                graphics.FillRectangle(brush, 0, 0, bitmapSize.Width, bitmapSize.Height);
+            }
             foreach (var rect in cloud.Rectangles)
             {
                 var replacedRect = new Rectangle(rect.Key.Location.X + center.X,
