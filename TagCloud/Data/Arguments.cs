9b9c180bacf99fd4c378521e157e8885547e1937
Artemiy <38810090+CGOptimum@users.noreply.github.com>
2018-12-13 23:17:38 +0500

    null arguments exception fixed

--- TagCloud/Data/Arguments.cs
+++ TagCloud/Data/Arguments.cs
@@ -4,9 +4,9 @@ namespace TagCloud.Data
 {
     public class Arguments
     {
-        public string WordsFileName;
-        public string BoringWordsFileName;
-        public string ImageFileName;
+        public string WordsFileName = "";
+        public string BoringWordsFileName = "";
+        public string ImageFileName = "";
         public int Multiplier = 10;
         public string WordsColorName = "Black";
         public string BackgroundColorName = "White";
