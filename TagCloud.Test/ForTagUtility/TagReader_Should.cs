7f61e047bdb6b9aa79b4ef8da40eb01aab0f2185
Vicheslav <rozentor2@gmail.com>
2018-12-16 01:11:03 +0500

    Refactor tag reader test

--- TagCloud.Test/ForTagUtility/TagReader_Should.cs
+++ TagCloud.Test/ForTagUtility/TagReader_Should.cs
@@ -3,18 +3,23 @@ using FluentAssertions;
 using NUnit.Framework;
 using TagCloud.Utility.Container;
 using TagCloud.Utility.Models.Tag;
+using TagCloud.Utility.Models.Tag.Container;
 
 namespace TagCloud.Tests.ForTagUtility
 {
     [TestFixture]
-    public class TagReader_Should
+    public class TagReader_Should 
     {
         private readonly IContainer container = ContainerConfig.StandartContainer;
 
         [Test]
         public void ReadTags()
         {
+            var tagContainer = container.Resolve<TagContainer>();
             var reader = container.Resolve<TagReader>();
+            var expectBigSize = tagContainer.GetTagGroupFor(10 / 17d).FontSize;
+            var expectSizeOfAverage = tagContainer.GetTagGroupFor(6 / 17d).FontSize;
+            var expectSizOfSmall = tagContainer.GetTagGroupFor(1 / 17d).FontSize;
 
             var tags = reader.ReadTags(new[]
             {
@@ -24,9 +29,12 @@ namespace TagCloud.Tests.ForTagUtility
             });
 
             tags.Should().HaveCount(3)
-                .And.Contain(item => item.Word == "Big" && item.FontSize == 35)
-                .And.Contain(item => item.Word == "Average" && item.FontSize == 25)
-                .And.Contain(item => item.Word == "Small" && item.FontSize == 15);
+                .And.Contain(item =>
+                    item.Word == "Big" && item.FontSize == expectBigSize)
+                .And.Contain(item =>
+                    item.Word == "Average" && item.FontSize == expectSizeOfAverage)
+                .And.Contain(item =>
+                    item.Word == "Small" && item.FontSize == expectSizOfSmall);
         }
     }
 }
