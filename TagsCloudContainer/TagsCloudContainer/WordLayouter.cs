5364e7aba0c990fe4cfa24a32b66d37dc2d3281d
Turin_nowadays <k.dobrovolsky@gmail.com>
2018-12-17 18:32:58 +0500

    small fixes

--- TagsCloudContainer/TagsCloudContainer/WordLayouter.cs
+++ TagsCloudContainer/TagsCloudContainer/WordLayouter.cs
@@ -7,15 +7,14 @@ namespace TagsCloudContainer
 {
     internal class WordLayouter
     {
-        private readonly IWordStorage _wordStorage;
         private readonly Func<Word, Size> _getWordSize;
         private readonly IRectangleLayout _layout;
-        private List<Word> _wordsToDraw;
+        private readonly List<Word> _wordsToDraw;
         private List<Rectangle> _coordinates;
 
         public WordLayouter(IWordStorage wordStorage, Func<Word, Size> getWordSize, IRectangleLayout layout)
         {
-            _wordStorage = wordStorage;
+            _wordsToDraw = wordStorage.ToList();
             _getWordSize = getWordSize;
             _layout = layout;
         }
@@ -35,8 +34,6 @@ namespace TagsCloudContainer
 
         private void PlaceWords()
         {
-            _wordsToDraw = _wordStorage.ToList();
-
             var wordSizes = _wordsToDraw
                 .Select(w => _getWordSize(w))
                 .ToList();
