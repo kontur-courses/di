541deb7b939c7c3b0869725bfa5609e3b0444d29
Turin_nowadays <k.dobrovolsky@gmail.com>
2018-12-19 20:03:23 +0500

    [Типа] Консольный клиент

--- TagsCloudContainer/TagsCloudContainer/Program.cs
+++ TagsCloudContainer/TagsCloudContainer/Program.cs
@@ -13,9 +13,49 @@ namespace TagsCloudContainer
 
         static void Main(string[] args)
         {
-            var filePath = @"..\..\tmpTextFile";
-            var words = File.ReadAllLines(filePath + ".txt");
+            while (true)
+            {
+                Console.Write("Want to continue?(Y/N): ");
+                var line = Console.ReadLine();
+                if (line == "N" || line == "n" || line.ToLower() == "exit")
+                    break;
+                if (line == "Y" || line == "y")
+                {
+                    ReadData();
+                    Console.WriteLine("Success!\n");
+                }
+                else
+                    Console.WriteLine("Unknown command. Please try again");
+            }
+        }
+
+        private static void ReadData()
+        {
+            Console.Write("Input text file name: ");
+            var filePath = Console.ReadLine();
+            Console.Write("Input angle for customizing creating tag cloud[unnecessary parameter]: ");
+            var angleForDirectionStr = Console.ReadLine();
+
+            int angle;
+            try
+            {
+                angle = int.Parse(angleForDirectionStr);
+            }
+            catch (Exception e)
+            {
+                angle = 1;
+            }
+
+            CreateTagCloudImage(
+                string.IsNullOrEmpty(filePath) ? @"..\..\tmpTextFile.txt" : filePath,
+                angle);
+        }
+
+        private static void CreateTagCloudImage(string filePath, double angleForDirection = 1)
+        {
+            var words = File.ReadAllLines(filePath);
             Func<Word, Size> wordSizeFunc = w => new Size(w.Count * 20 * w.Value.Length, w.Count * 20);
+            var t = GetFileName(filePath);
 
             var builder = new ContainerBuilder();
             builder.RegisterType<Drawer>().As<IDrawer<Word>>();
@@ -24,9 +64,9 @@ namespace TagsCloudContainer
             builder.RegisterType<WordStorage>().As<IWordStorage>().WithParameter("wordsToHandle", words);
             builder.RegisterType<WordsCustomizer>().AsSelf().SingleInstance();
             builder.RegisterType<RectangleStorage>().AsSelf().SingleInstance();
-            builder.RegisterType<Direction>().As<IDirection<double>>();
+            builder.RegisterType<Direction>().As<IDirection<double>>().WithParameter("angleShift", angleForDirection);
             builder.RegisterType<WordLayouter>().WithParameter("getWordSize", wordSizeFunc).AsSelf().SingleInstance();
-            builder.RegisterType<DrawSettings<Word>>().WithParameter("filePath", filePath);
+            builder.RegisterType<DrawSettings<Word>>().WithParameter("filePath", GetFileName(filePath));
             builder.RegisterType<CircularCloudLayout>().As<IRectangleLayout>();
             builder.Register(p => new Point()).As<Point>();
 
@@ -36,6 +76,11 @@ namespace TagsCloudContainer
             drawer.DrawItems();
 
             Container.Dispose();
+        }
+
+        private static string GetFileName(string fullPath)
+        {
+            return fullPath.Substring(0, fullPath.Length - 4);
         }
     }
 }
