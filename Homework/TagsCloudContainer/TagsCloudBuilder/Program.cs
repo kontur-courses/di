20d4b472390f74c41491187ec3cf3e599bd2eecd
Ivan Plaksin <zero.netto.o@gmail.com>
2018-12-20 12:04:59 +0500

    Realisation of Builder now on DI

--- Homework/TagsCloudContainer/TagsCloudBuilder/Program.cs
+++ Homework/TagsCloudContainer/TagsCloudBuilder/Program.cs
@@ -1,42 +1,138 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Autofac;
+using CommandLine;
+using TagsCloudBuilder.Drawer;
 using TagsCloudContainer;
+using TagsCloudContainer.WordsFilter;
+using TagsCloudContainer.WordsFilter.BannedWords;
 using TagsCloudVisualization;
+using TagsCloudVisualization.CloudLayouter;
 
 namespace TagsCloudBuilder
 {
     public class Program
     {
+        private static Dictionary<string, ImageFormat> imageFormatsComparer = new Dictionary<string, ImageFormat>()
+        {
+            { "png", ImageFormat.Png},
+            {"jpeg", ImageFormat.Jpeg },
+            { "bmp", ImageFormat.Bmp}
+        };
+
+        private class Options
+        {
+            [Option('i', "input", Default = "text.txt",
+                HelpText = "Filename with input data.")]
+            public string InputFilename { get; set; }
+
+            [Option('d', "debug", Required = false, Default = false,
+                HelpText = "Draw area around every word.")]
+            public bool Debug { get; set; }
+
+            [Option('s', "max font size", Required = false, Default = 50,
+                HelpText = "Set the max size of the words in px. Can't be less then 10px.")]
+            public int MaxFontSize { get; set; }
+
+            [Option('f', "font", Required = false, Default = "Arial",
+                HelpText = "Set the font for text.")]
+            public string FontFamily { get; set; }
+
+            [Option('b', "banned words", Default = "",
+                HelpText = "Filename with words ignored words.")]
+            public string BannedWordsFilename { get; set; }
+
+            [Option('s', "canvas size", Default = new[] { 2000, 2000 },
+                HelpText = "Set the canvas size of output file.")]
+            public int[] CanvasSize { get; set; }
+
+            [Option('o', "output filename", Default = "sample.png",
+                HelpText = "Set the output file name.")]
+            public string OutputFilename { get; set; }
+
+            [Option('c', "center point", Default = new[] { 1000, 1000 },
+                HelpText = "Set the center of clouds.")]
+            public int[] CenterPoint { get; set; }
+
+            [Option('r', "radius step", Default = 0.00001,
+                HelpText = "Set the radius step.")]
+            public double RadiusStep { get; set; }
+
+            [Option('a', "angle step", Default = 0.01,
+                HelpText = "Set the angle step.")]
+            public double AngleStep { get; set; }
+
+            [Option('l', "words length", Default = new[] { 4, int.MaxValue },
+                HelpText = "Set the bounds of words length")]
+            public int[] WordsLength { get; set; }
+
+            [Option('e', "output file extension", Default = "png", HelpText = "Set the output file extension.")]
+            public string OutpiutFileExtension { get; set; }
+        }
+
         public static void Main(string[] args)
         {
-            var wordsWithFrequency = new Dictionary<string, int>();
-            var withDebug = true;
-            var startPoint = new Point(1000, 1000);
-            var canvasSize = new Size(2000, 2000);
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(BuildAndRun);
+        }
 
-            foreach (var word in TxtReader.ReadAllLines("text.txt"))
+        private static void BuildAndRun(Options options)
+        {
+            using (var container = BuildContainer(options))
             {
-                var lowerWord = word.ToLower();
-                if (!wordsWithFrequency.ContainsKey(lowerWord))
-                    wordsWithFrequency.Add(lowerWord, 1);
+                var wordsFilters = container.Resolve<IWordsFilters>();
+                wordsFilters.RemoveIgnoredWords();
+                if (options.WordsLength.Length == 1)
+                    wordsFilters.RemoveWordsOutOfLengthRange(options.WordsLength[0]);
                 else
-                    wordsWithFrequency[lowerWord] += 1;
+                    wordsFilters.RemoveWordsOutOfLengthRange(options.WordsLength[0], options.WordsLength[1]);
+                container.Resolve<ICloudLayouter>();
+                var containersFormatter = container.Resolve<IContainersCreator>();
+                containersFormatter.InitializeContainers();
+                var drawer = container.Resolve<Drawer.Drawer>();
+                drawer.DrawAndSaveWords();
             }
+        }
 
-            wordsWithFrequency = wordsWithFrequency
-                .RemoveWords(new List<string> { "Консультация", "Кадров", "Оценить" })
-                .RemoveWordsOutOfLengthRange(leftBound: 4);
+        private static IContainer BuildContainer(Options options)
+        {
+            var builder = new ContainerBuilder();
+            var imageFormat = GetImageFormat(options.OutpiutFileExtension);
+            builder.RegisterType<TxtWordsPreparer>()
+                .As<IWordsPreparer>()
+                .WithParameter("fileName", options.InputFilename);
+            builder.RegisterType<BannedWords>()
+                .As<IBannedWords>()
+                .WithParameter("fileName", options.BannedWordsFilename);
+            builder.RegisterType<CircularCloudLayouter>()
+                .As<ICloudLayouter>()
+                .WithParameter("center", new Point(options.CenterPoint[0], options.CenterPoint[1]))
+                .WithParameter("radiusStep", options.RadiusStep)
+                .WithParameter("angleStep", options.AngleStep);
+            builder.RegisterType<ContainersCreator>()
+                .As<IContainersCreator>()
+                .WithParameter("fontName", options.FontFamily)
+                .WithParameter("maxFontSize", options.MaxFontSize);
+            builder.RegisterType<Drawer.Drawer>()
+                .As<IDrawer>()
+                .WithParameter("canvasSize", options.CanvasSize)
+                .WithParameter("fileName", options.OutputFilename)
+                .WithParameter("imageFormat", imageFormat);
+            builder.RegisterType<WordsFilters>()
+                .As<IWordsFilters>();
+            builder.RegisterType<SpiralInfo>()
+                .AsSelf();
+            return builder.Build();
+        }
 
-            var cloudLayouter = new CircularCloudLayouter(startPoint);
-            var containerFormatter = new ContainerFormatter(wordsWithFrequency, cloudLayouter);
+        private static ImageFormat GetImageFormat(string formatName)
+        {
+            var lowerFormatName = formatName.ToLower();
+            if (!imageFormatsComparer.ContainsKey(lowerFormatName))
+                return ImageFormat.Png;
 
-            Drawer.DrawAndSaveWords(canvasSize, containerFormatter.ContainersInfo, "sample.png", ImageFormat.Png, withDebug);
+            return imageFormatsComparer[lowerFormatName];
         }
     }
 }
