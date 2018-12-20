db82d258b1878882d9ab2ad990725c21d6ba071a
Ivan Plaksin <zero.netto.o@gmail.com>
2018-12-20 12:06:08 +0500

    WordsFilters

---
+++ Homework/TagsCloudContainer/TagsCloudContainer/WordsFilter/IWordsFilters.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TagsCloudContainer.WordsFilter
+{
+    public interface IWordsFilters
+    {
+        Dictionary<string, int> FilteredWords { get; }
+        void RemoveIgnoredWords();
+        void RemoveWordsOutOfLengthRange(int leftBound, int rightBound = int.MaxValue);
+    }
+}
