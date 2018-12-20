0a1a64fdd2d42eafe2ebf5dd2ec0f6c4e3df0ac3
Ivan Plaksin <zero.netto.o@gmail.com>
2018-12-20 12:06:28 +0500

    Realisation of text reader

---
+++ Homework/TagsCloudContainer/TagsCloudContainer/WordsPreparer/IWordsPreparer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudBuilder
+{
+    public interface IWordsPreparer
+    {
+        Dictionary<string, int> GetPreparedWords();
+    }
+}
---
+++ Homework/TagsCloudContainer/TagsCloudContainer/WordsPreparer/TxtWordsPreparer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TagsCloudBuilder
+{
+    public class TxtWordsPreparer : IWordsPreparer
+    {
+        private readonly string fileName;
+
+        public TxtWordsPreparer(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public Dictionary<string, int> GetPreparedWords()
+        {
+            try
+            {
+                var wordsWithFrequency = new Dictionary<string, int>();
+
+                foreach (var word in File.ReadAllLines(fileName).Where(word => word.Length > 0))
+                {
+                    var lowerWord = word.ToLower();
+                    if (!wordsWithFrequency.ContainsKey(lowerWord))
+                        wordsWithFrequency.Add(lowerWord, 1);
+                    else
+                        wordsWithFrequency[lowerWord] += 1;
+                }
+
+                return wordsWithFrequency;
+            }
+            catch (IOException e)
+            {
+                throw new IOException($"Something went wrong. Check the correctness of {fileName} path.", e);
+            }
+        }
+
+        public static Dictionary<string, int> ReadAllLines(string fileName)
+        {
+            try
+            {
+                var wordsWithFrequency = new Dictionary<string, int>();
+
+                foreach (var word in File.ReadAllLines(fileName))
+                {
+                    var lowerWord = word.ToLower();
+                    if (!wordsWithFrequency.ContainsKey(lowerWord))
+                        wordsWithFrequency.Add(lowerWord, 1);
+                    else
+                        wordsWithFrequency[lowerWord] += 1;
+                }
+
+                return wordsWithFrequency;
+            }
+            catch (IOException e)
+            {
+                throw new IOException($"Something went wrong. Check the correctness of {fileName} path.", e);
+            }
+        }
+    }
+}
