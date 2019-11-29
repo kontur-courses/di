93fd40aeb10e84820c738e4a27f63c58d43f30c4
Sergey Subachev <56560526+SergeySubachev@users.noreply.github.com>
2019-11-29 20:45:43 +0500

    #

--- FractalPainter/App/Fractals/DragonPainter.cs
+++ FractalPainter/App/Fractals/DragonPainter.cs
@@ -38,7 +38,7 @@ namespace FractalPainting.App.Fractals
                 var p = new PointF(0, 0);
                 foreach (var i in Enumerable.Range(0, settings.IterationsCount))
                 {
-                    graphics.FillRectangle(Brushes.Yellow, imageSize.Width/3f + p.X, imageSize.Height/2f + p.Y, 1, 1);
+                    graphics.FillRectangle(new SolidBrush(palette.PrimaryColor), imageSize.Width/3f + p.X, imageSize.Height/2f + p.Y, 1, 1);
                     if (r.Next(0, 2) == 0)
                         p = new PointF(scale*(p.X*cosa - p.Y*sina), scale*(p.X*sina + p.Y*cosa));
                     else
