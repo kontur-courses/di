caba565b846ee69af5a5e0aaa1fc93ee9073b83f
Данил Панков <mistertimechip@mail.ru>
2019-11-30 14:28:16 +0300

    Added cloud words parser realisations

---
+++ TagsCloudContainer/TextParsing/CloudParsing/CloudWordsParser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace TagsCloudContainer.TextParsing.CloudParsing
+{
+    public class CloudWordsParser
+    {
+        private Dictionary<string, CloudWord> words;
+
+        public CloudWordsParser()
+        {
+            words = new Dictionary<string, CloudWord>();
+        }
+        IEnumerable<CloudWord> ParseFrom(IFileWordsParser fileWordsParser, string path, ICloudWordParsingRule rule)
+        {
+            foreach (var word in fileWordsParser.ParseFrom(path))
+            {
+                if (!rule.Check(word)) continue;
+                var appliedRule = rule.Apply(word);
+                if (words.ContainsKey(appliedRule))
+                    words[appliedRule].AddCount();
+                else
+                    words[appliedRule] = new CloudWord(appliedRule);
+            }
+
+            return words.Values;
+        }
+    }
+}
