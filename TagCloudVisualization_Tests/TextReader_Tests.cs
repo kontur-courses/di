aa879d286cfeeec03d56d2cb1c64c96a125843d6
Olga Bursina <bursina.olya@yandex.ru>
2019-11-30 22:08:10 +0500

    Added TextReader

---
+++ TagCloudVisualization_Tests/TextReader_Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using TextReader = TagsCloudVisualization.TextReader;
+
+namespace TagCloudVisualization_Tests
+{
+    public class TextReader_Tests
+    {
+        [Test]
+        public void TextReaderCtor_FileNotFound_ShouldThrowException()
+        {
+            Action act = () => new TextReader("nonexistentName");
+            act.Should().Throw<ArgumentException>();
+        }
+    }
+}
