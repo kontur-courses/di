d7fd2037e168bf0e8c0d3c9eedae0e65d1a3cc4f
Nicole <mrhomka.com@gmail.com>
2019-12-01 15:29:35 +0500

    Add IVisualizer and SimpleVsualizer

---
+++ TagsCloudContainer/TagToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainer
+{
+    class TagToken
+    {
+        public string Word { get; }
+        public Rectangle Rectangle { get; }
+
+        public TagToken(string word, Rectangle rectangle)
+        {
+            Word = word;
+            Rectangle = rectangle;
+        }
+    }
+}
---
+++ TagsCloudContainer/Visualizers/SimpleVsualizer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainer.Visualizers
+{
+    class SimpleVsualizer : IVisualizer
+    {
+        public Bitmap VisualizeCloud(List<TagToken> tags)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
