1ca78d94de062abaaaa94676efc6b89ebe7614a6
Nicole <mrhomka.com@gmail.com>
2019-12-01 15:37:19 +0500

    Generator for SimpleVsualizer

--- TagsCloudContainer/Visualizers/SimpleVsualizer.cs
+++ TagsCloudContainer/Visualizers/SimpleVsualizer.cs
@@ -4,11 +4,19 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TagsCloudContainer.Palettes;
 
 namespace TagsCloudContainer.Visualizers
 {
     class SimpleVsualizer : IVisualizer
     {
+        private IPalette palette;
+
+        public SimpleVsualizer(IPalette palette)
+        {
+            this.palette = palette;
+        }
+
         public Bitmap VisualizeCloud(List<TagToken> tags)
         {
             throw new NotImplementedException();
