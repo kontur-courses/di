84c27b9b67fc231124bf4e1536a8f3b7373e9162
veolenexe <44974960+veolenexe@users.noreply.github.com>
2019-12-01 17:58:40 +0500

    Cloud layouter realisation

---
+++ TagsCloudContainer/CloudLayouters/CloudLayouter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using TagsCloudContainer.CloudVisualizers;
+using TagsCloudContainer.TextParsing.CloudParsing;
+
+namespace TagsCloudContainer.CloudLayouters
+{
+    public class CloudLayouter
+    {
+        private CloudLayouterSettings settings;
+        
+        public CloudLayouter(CloudLayouterSettings settings)
+        {
+            this.settings = settings;
+        }
+
+        public IEnumerable<CloudVisualizationWord> GetWords(IEnumerable<CloudWord> cloudWords)
+        {
+            var ordered = cloudWords.OrderByDescending(w => w.Count);
+            double maxCount = ordered.First().Count;
+            double minCount = ordered.Last().Count;
+            var countDifference = maxCount - minCount == 0 ? 1 : maxCount - minCount;
+            var sizeMultiplier = settings.RectangleSquareMultiplier/countDifference;
+            foreach (var word in ordered)
+            {
+                var length = word.Word.Length;
+                var ratio = word.Count * sizeMultiplier / length;
+                var width = (int)ratio;
+                var height = (int)(word.Count * sizeMultiplier - ratio);
+                var size = new Size(width, height);
+                var rect = settings.Algorithm.PutNextRectangle(size);
+                yield return new CloudVisualizationWord(rect, word.Word);
+            }
+        }
+    }
+}
