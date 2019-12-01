4283c76573e2b5e14a7e5cff8529bcd59111ec89
veolenexe <44974960+veolenexe@users.noreply.github.com>
2019-12-01 20:59:51 +0500

    Visualization class

---
+++ TagsCloudContainer/CloudVisualizers/CloudVisualizer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudContainer.CloudVisualizers
+{
+    public class CloudVisualizer
+    {
+        private CloudVisualizerSettings settings;
+        public CloudVisualizer(CloudVisualizerSettings settings)
+        {
+            this.settings = settings;
+        }
+
+        public Bitmap GetBitmap(IEnumerable<CloudVisualizationWord> words)
+        {
+            return settings.BitmapMaker.MakeBitmap(words, settings.Palette);
+        }
+    }
+}
