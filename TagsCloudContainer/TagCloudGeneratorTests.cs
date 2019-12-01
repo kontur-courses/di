ddf432a808165dfe92df5730f534c2ee4160d0da
Nicole <mrhomka.com@gmail.com>
2019-12-01 12:33:14 +0500

    add classes TagsCloudGenerator и TagCloudGeneratorTests

---
+++ TagsCloudContainer/TagCloudGeneratorTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework.Interfaces;
+
+namespace TagsCloudContainer
+{
+    [TestFixture]
+    class TagCloudGeneratorTests
+    {
+    }
+}
