fbf96f754cab8d3e2feae9ce0393c8ab7c50dba9
veolenexe <44974960+veolenexe@users.noreply.github.com>
2019-12-01 21:00:41 +0500

    Image saving class

---
+++ TagsCloudContainer/CloudVisualizers/ImageSaving/ImageSaver.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace TagsCloudContainer.CloudVisualizers.ImageSaving
+{
+    public class ImageSaver
+    {
+        private ImageSaverSettings settings;
+        public ImageSaver(ImageSaverSettings settings)
+        {
+            this.settings = settings;
+        }
+
+        public void Save(Bitmap bitmap)
+        {
+            bitmap.Save(settings.Path, settings.Format);
+        }
+    }
+}
