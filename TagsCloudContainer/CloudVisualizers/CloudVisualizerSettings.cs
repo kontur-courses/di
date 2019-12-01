6d09556e8ee7b42630a97d5b1c191adfefe79abf
veolenexe <44974960+veolenexe@users.noreply.github.com>
2019-12-01 20:59:37 +0500

    Settings container for visualization

---
+++ TagsCloudContainer/CloudVisualizers/CloudVisualizerSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+
+namespace TagsCloudContainer.CloudVisualizers
+{
+    public class CloudVisualizerSettings
+    {
+        public Palette Palette { get; set; }
+        public IBitmapMaker BitmapMaker { get; set; }
+        
+    }
+}
