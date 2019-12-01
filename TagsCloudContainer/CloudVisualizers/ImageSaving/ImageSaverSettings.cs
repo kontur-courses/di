c2ebd01e275cb55bc4367d7781cdbf3ae3654428
veolenexe <44974960+veolenexe@users.noreply.github.com>
2019-12-01 21:00:49 +0500

    Image saving settings

---
+++ TagsCloudContainer/CloudVisualizers/ImageSaving/ImageSaverSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing.Imaging;
+
+namespace TagsCloudContainer.CloudVisualizers.ImageSaving
+{
+    public class ImageSaverSettings
+    {
+        public ImageFormat Format { get; set; }
+        public string Path { get; set; }
+    }
+}
