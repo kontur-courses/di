e7bd836a6e1ef1bd5c99ac7b78349f24b820ef27
FacelessLord <skyres21@yandex.ru>
2019-12-01 14:31:35 +0500

    Decomposition

---
+++ TagCloudContainer/WordCloudLayouter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace TagCloudContainer
+{
+    public class WordCloudLayouter
+    {
+        public CircularCloudLayouter RectangleLayouter;
+        
+        public WordCloudLayouter()
+        {
+            RectangleLayouter = new CircularCloudLayouter();
+        }
+
+
+        public void AddWords(IEnumerable<(string word, int occurrenceCount)> words)
+        {
+            //todo sort tuples by occurenceCount and start adding words to layouter from biggest to smallest
+        }
+    }
+}
---
+++ TagCloudContainer/WordProcessor.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TagCloudContainer
+{
+    public class WordProcessor
+    {
+        public IEnumerable<(string word, int occurrenceCount)> CountWordOccurrences(IEnumerable<string> words)
+        {
+            //todo use dictionary<word, wordOccurrenceCount> to count
+            //todo words and then process it to list of tuples
+            return null;
+        }
+    }
+}
