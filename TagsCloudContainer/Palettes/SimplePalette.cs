32ea8264f125b64c3438bd74b0dd3deda4404b03
Nicole <mrhomka.com@gmail.com>
2019-12-01 15:33:19 +0500

    add IPalette and SimplePalette

---
+++ TagsCloudContainer/Palettes/SimplePalette.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainer.Palettes
+{
+    class SimplePalette : IPalette
+    {
+        public Font Font => throw new NotImplementedException();
+
+        public Brush Brush => throw new NotImplementedException();
+    }
+}
