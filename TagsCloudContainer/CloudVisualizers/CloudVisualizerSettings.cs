2fb5de92d99845a261bcfeb76c4f38fa3de81c98
veolenexe <44974960+veolenexe@users.noreply.github.com>
2019-12-02 01:02:03 +0500

    Added bitmap size setting

--- TagsCloudContainer/CloudVisualizers/CloudVisualizerSettings.cs
+++ TagsCloudContainer/CloudVisualizers/CloudVisualizerSettings.cs
@@ -1,13 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Drawing.Imaging;
-
-namespace TagsCloudContainer.CloudVisualizers
+﻿namespace TagsCloudContainer.CloudVisualizers
 {
     public class CloudVisualizerSettings
     {
         public Palette Palette { get; set; }
         public IBitmapMaker BitmapMaker { get; set; }
-        
+        public int Width { get; set; }
+        public int Height { get; set; }
     }
 }
