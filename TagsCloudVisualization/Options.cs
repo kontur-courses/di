fe9ab47c404f6bfc6740c2c9cff4ac05b33e0619
Olga Bursina <bursina.olya@yandex.ru>
2019-12-01 23:10:44 +0500

    Added Options

---
+++ TagsCloudVisualization/Options.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class Options
+    {
+        public readonly Font Font;
+        public readonly Size ImageSize;
+        public readonly Color BackgroundColor;
+        public readonly Color TextColor;
+
+        public Options(Font font, Size imageSize, Color backgroundColor, Color textColor)
+        {
+            Font = font;
+            ImageSize = imageSize;
+            BackgroundColor = backgroundColor;
+            TextColor = textColor;
+        }
+    }
+}
