cf99ac2eb73377a585667520821a0ba61c16ae22
veolenexe <44974960+veolenexe@users.noreply.github.com>
2019-12-01 16:59:29 +0500

    Settings for cloud words parser

---
+++ TagsCloudContainer/TextParsing/CloudParsing/CloudWordsParserSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace TagsCloudContainer.TextParsing.CloudParsing
+{
+    public class CloudWordsParserSettings
+    {
+        public IFileWordsParser FileWordsParser { get; set; }
+        public string Path { get; set; }
+        public ICloudWordParsingRule Rule { get; set; }
+    }
+}
