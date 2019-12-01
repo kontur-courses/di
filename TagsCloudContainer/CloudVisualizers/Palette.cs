41c567e81e9ad0fb4750bd9ef7462ee80520a53a
veolenexe <44974960+veolenexe@users.noreply.github.com>
2019-12-01 20:57:04 +0500

    Added palette class for visualisation settings

---
+++ TagsCloudContainer/CloudVisualizers/Palette.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace TagsCloudContainer.CloudVisualizers
+{
+    public class Palette
+    {
+        public bool IsGradient { get; set; } = false;
+        public Color PrimaryColor { get; set; } = Color.Red;
+        public Color SecondaryColor { get; set; } = Color.Plum;
+        public Color BackgroundColor { get; set; } = Color.DarkBlue;
+    }
+}
