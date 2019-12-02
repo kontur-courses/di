6ff9cd87bda83aa4405a5358e7f7c60a06906c6f
Данил Панков <mistertimechip@mail.ru>
2019-12-02 23:40:30 +0300

    Refactored default bitmapmaker

--- TagsCloudContainer/CloudVisualizers/BitmapMakers/DefaultBitmapMaker.cs
+++ TagsCloudContainer/CloudVisualizers/BitmapMakers/DefaultBitmapMaker.cs
@@ -10,50 +10,87 @@ namespace TagsCloudContainer.CloudVisualizers.BitmapMakers
         public Bitmap MakeBitmap(IEnumerable<CloudVisualizationWord> words, CloudVisualizerSettings settings)
         {
             var bitmap = new Bitmap(settings.Width, settings.Height);
-            var wordsEnum = words.ToList();
-            var enumerable = words.Select(w => w.Rectangle).ToList();
-            var minX = enumerable.OrderBy(rect => rect.X).First().X;
-            var minY = enumerable.OrderBy(rect => rect.Y).First().Y;
-            var maxX = enumerable.OrderBy(rect => rect.Right).Last().Right;
-            var maxY = enumerable.OrderBy(rect => rect.Bottom).Last().Bottom;
-            var xDifference = Math.Abs(maxX - minX);
-            var yDifference = Math.Abs(maxY - minY);
+            var wordsList = words.ToList();
+            var rectangles = wordsList.Select(w => w.Rectangle).ToList();
+            var minX = rectangles.OrderBy(rect => rect.X).First().X;
+            var minY = rectangles.OrderBy(rect => rect.Y).First().Y;
+            var (xRatio, yRatio) = GetSizeRatio(settings, rectangles, minX, minY);
 
             using (var g = Graphics.FromImage(bitmap))
             {
-                var xRatio = 1f;
-                var yRatio = 1f;
-                if (xDifference > yDifference)
-                { 
-                    xRatio = (float)settings.Width / xDifference;
-                    var yRatioDiff = maxY + (float)settings.Height / yDifference;
-                    yRatio = settings.Height / yRatioDiff;
-                }
-
-                if (xDifference < yDifference)
-                {
-                    var xRatioDiff = maxX + (float)settings.Width / xDifference;
-                    xRatio = ((float)settings.Width - xDifference) / xRatioDiff;
-                    yRatio = (float)settings.Height / yDifference;
-                }
-                
                 g.ScaleTransform(xRatio, yRatio);
                 g.TranslateTransform(-minX, -minY);
-                foreach (var word in wordsEnum)
+                foreach (var word in wordsList)
                 {
                     var font = new Font("Arial", 16);
                     var textSize = g.MeasureString(word.Word, font);
                     var brush = new SolidBrush(settings.Palette.PrimaryColor);
-                    var state = g.Save();
-                    g.TranslateTransform(word.Rectangle.Left, word.Rectangle.Top);
-                    g.ScaleTransform(word.Rectangle.Width / textSize.Width, 
-                        word.Rectangle.Height / textSize.Height);
-                    g.DrawString(word.Word, font, brush, PointF.Empty);
-                    g.Restore(state);
+                    WriteWordToRectangle(g, word, textSize, font, brush);
                     g.DrawRectangle(new Pen(settings.Palette.SecondaryColor), word.Rectangle);
                 }
             }
             return bitmap;
+        }
+
+        private static void WriteWordToRectangle(
+            Graphics g, 
+            CloudVisualizationWord word, 
+            SizeF textSize, 
+            Font font,
+            Brush brush)
+        {
+            var state = g.Save();
+            g.TranslateTransform(word.Rectangle.Left, word.Rectangle.Top);
+            g.ScaleTransform(word.Rectangle.Width / textSize.Width,
+                word.Rectangle.Height / textSize.Height);
+            g.DrawString(word.Word, font, brush, PointF.Empty);
+            g.Restore(state);
+        }
+
+        private static (float xRatio, float yRatio) GetSizeRatio(
+            CloudVisualizerSettings settings, 
+            IEnumerable<Rectangle> rectangles, 
+            int minX,
+            int minY)
+        {
+            var maxX = rectangles.OrderBy(rect => rect.Right).Last().Right;
+            var maxY = rectangles.OrderBy(rect => rect.Bottom).Last().Bottom;
+            var xDifference = Math.Abs(maxX - minX);
+            var yDifference = Math.Abs(maxY - minY);
+            var xRatio = 1f;
+            var yRatio = 1f;
+            
+            if (xDifference > yDifference)
+                (xRatio, yRatio) = GetHorizontalLayoutRatio(settings, xDifference, maxY, yDifference);
+
+            if (xDifference < yDifference)
+                (xRatio, yRatio) = GetVerticalLayoutRatio(settings, maxX, xDifference, yDifference);
+
+            return (xRatio, yRatio);
+        }
+
+        private static (float xRatio, float yRatio) GetVerticalLayoutRatio(
+            CloudVisualizerSettings settings, 
+            int maxX,
+            int xDifference, 
+            int yDifference)
+        {
+            var xRatioDiff = maxX + (float) settings.Width / xDifference;
+            var xRatio = ((float) settings.Width - xDifference) / xRatioDiff;
+            var yRatio = (float) settings.Height / yDifference;
+            return (xRatio, yRatio);
+        }
+
+        private static (float xRatio, float yRatio) GetHorizontalLayoutRatio(
+            CloudVisualizerSettings settings, 
+            int xDifference,
+            int maxY, 
+            int yDifference)
+        {
+            var xRatio = (float) settings.Width / xDifference;
+            var yRatioDiff = maxY + (float) settings.Height / yDifference;
+            var yRatio = settings.Height / yRatioDiff;
+            return (xRatio, yRatio);
         }
     }
 }
