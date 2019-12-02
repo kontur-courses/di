3852e5a0c678dd1f648348fad5370803d0129a6c
Данил Панков <mistertimechip@mail.ru>
2019-12-03 00:37:28 +0300

    Added settings manager stub

---
+++ TagsCloudContainer/ApplicationRunning/SettingsManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TagsCloudContainer.CloudLayouters;
+using TagsCloudContainer.CloudVisualizers;
+using TagsCloudContainer.CloudVisualizers.ImageSaving;
+using TagsCloudContainer.TextParsing.CloudParsing;
+
+namespace TagsCloudContainer.ApplicationRunning
+{
+    public class SettingsManager
+    {
+        public CloudLayouterSettings GetLayouterSettings()
+        {
+            return new CloudLayouterSettings();
+        }
+
+        public CloudVisualizerSettings GetVisualizerSettings()
+        {
+            return new CloudVisualizerSettings();
+        }
+
+        public ImageSaverSettings GetImageSaverSettings()
+        {
+            return new ImageSaverSettings();
+        }
+        
+        public CloudWordsParserSettings GetWordsParserSettings()
+        {
+            return new CloudWordsParserSettings();
+        }
+    }
+}
