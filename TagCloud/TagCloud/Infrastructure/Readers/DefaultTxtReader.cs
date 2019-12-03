d215efcac3d561500b19a9d4fa4bce245e2fe868
neoezop <49761942+neoezop@users.noreply.github.com>
2019-12-04 03:52:12 +0500

    default file reader

---
+++ TagCloud/TagCloud/Infrastructure/Readers/DefaultTxtReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace TagCloud
+{
+    public class DefaultTxtReader : IReader
+    {
+        public string ReadAllText(string path)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException();
+            if (path == null)
+                throw new ArgumentNullException();
+
+            return File.ReadAllText(path, Encoding.UTF8);
+        }
+    }
+}
