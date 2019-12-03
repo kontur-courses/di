5b47c6eec5fc35885c23a1fea39c18e814b66951
Данил Панков <mistertimechip@mail.ru>
2019-12-04 00:57:47 +0300

    Added settings configuration

--- TagsCloudContainer/ApplicationRunning/SettingsManager.cs
+++ TagsCloudContainer/ApplicationRunning/SettingsManager.cs
@@ -1,31 +1,87 @@
 ﻿using System;
+using System.Drawing.Imaging;
 using TagsCloudContainer.CloudLayouters;
 using TagsCloudContainer.CloudVisualizers;
 using TagsCloudContainer.CloudVisualizers.ImageSaving;
+using TagsCloudContainer.TextParsing;
 using TagsCloudContainer.TextParsing.CloudParsing;
 
 namespace TagsCloudContainer.ApplicationRunning
 {
     public class SettingsManager
     {
+        private CloudLayouterSettings layouterSettings;
+        private CloudVisualizerSettings visualizerSettings;
+        private ImageSaverSettings imageSaverSettings;
+        private CloudWordsParserSettings cloudWordsParserSettings;
+
+        public SettingsManager()
+        {
+            layouterSettings = new CloudLayouterSettings();
+            visualizerSettings = new CloudVisualizerSettings();
+            imageSaverSettings = new ImageSaverSettings();
+            cloudWordsParserSettings = new CloudWordsParserSettings();
+        }
+        
+        public void ConfigureLayouterSettings(
+            ICloudLayoutingAlgorithm algorithm,
+            int rectangleSquareMultiplier,
+            double rectangleStep,
+            int rectangleBroadness)
+        {
+            var newSettings = new CloudLayouterSettings
+            {
+                Algorithm = algorithm,
+                RectangleSquareMultiplier = rectangleSquareMultiplier,
+                RectangleStep = rectangleStep,
+                RectangleBroadness = rectangleBroadness
+            };
+            layouterSettings = newSettings;
+        }
+        
+        public void ConfigureVisualizerSettings(
+            Palette palette,
+            IBitmapMaker bitmapMaker,
+            int width,
+            int height)
+        {
+            var newSettings = new CloudVisualizerSettings
+            {
+                Palette = palette, BitmapMaker = bitmapMaker, Width = width, Height = height
+            };
+            visualizerSettings = newSettings;
+        }
+        
+        public void ConfigureImageSaverSettings(ImageFormat format, string savePath)
+        {
+            var newSettings = new ImageSaverSettings {Format = format, Path = savePath};
+            imageSaverSettings = newSettings;
+        }
+        
+        public void ConfigureWordsParserSettings(IFileWordsParser wordsParser, string path, ICloudWordParsingRule rule)
+        {
+            var newSettings = new CloudWordsParserSettings {FileWordsParser = wordsParser, Path = path, Rule = rule};
+            cloudWordsParserSettings = newSettings;
+        }
+        
         public CloudLayouterSettings GetLayouterSettings()
         {
-            return new CloudLayouterSettings();
+            return layouterSettings;
         }
 
         public CloudVisualizerSettings GetVisualizerSettings()
         {
-            return new CloudVisualizerSettings();
+            return visualizerSettings;
         }
 
         public ImageSaverSettings GetImageSaverSettings()
         {
-            return new ImageSaverSettings();
+            return imageSaverSettings;
         }
         
         public CloudWordsParserSettings GetWordsParserSettings()
         {
-            return new CloudWordsParserSettings();
+            return cloudWordsParserSettings;
         }
     }
 }
