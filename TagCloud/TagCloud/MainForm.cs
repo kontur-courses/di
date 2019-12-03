ae6de48f695ff0339f5f5444a0cb5f5bb834eb26
neoezop <49761942+neoezop@users.noreply.github.com>
2019-12-04 03:56:24 +0500

    main form and program
    program is anentry point so there i register dependencies. Main form is in anearly stage, so it just shows the picture with cloud

---
+++ TagCloud/TagCloud/MainForm.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace TagCloud
+{
+    public partial class MainForm : Form
+    {
+        public MainForm(ImageSettings imageSettings, IVisualizer visualizer)
+        {
+            ClientSize = new Size(imageSettings.Width, imageSettings.Height);
+            var pictureBox = new PictureBox
+            {
+                Image = visualizer.VisualizeTextFromFile("input.txt"),
+                ClientSize = new Size(imageSettings.Width, imageSettings.Height)
+            };
+            Controls.Add(pictureBox);
+        }
+    }
+}
---
+++ TagCloud/TagCloud/Program.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using System.Windows.Forms;
+
+namespace TagCloud
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var container = BuildContainer();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var mainForm = container.Resolve<MainForm>();
+            Application.Run(mainForm);
+        }
+
+        private static IContainer BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<DefaultExtracter>().SingleInstance().As<IExtracter>();
+            builder.RegisterType<ArchimedeanSpiralLayouter>().As<ILayouter>();
+            builder.RegisterType<DefaultParser>().SingleInstance().As<IParser>();
+            builder.RegisterType<DefaultTxtReader>().SingleInstance().As<IReader>();
+            builder.RegisterType<DefaultVisualizer>().SingleInstance().As<IVisualizer>();
+            builder.Register(context => ImageSettings.GetDefaultSettings()).SingleInstance().As<ImageSettings>();
+            builder.Register(context => FontSettings.GetDefaultSettings()).SingleInstance().As<FontSettings>();
+            builder.Register(context => LayouterSettings.GetDefaultSettings()).SingleInstance().As<LayouterSettings>();
+            builder.RegisterType<MainForm>().SingleInstance().As<MainForm>();
+            return builder.Build();
+        }
+
+    }
+}
