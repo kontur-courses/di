5ba8e668b9686dd321019d399644f215b9d387a1
Данил Панков <mistertimechip@mail.ru>
2019-12-04 00:57:09 +0300

    Removed center settings, as it didn't make sense

--- TagsCloudContainer/CloudLayouters/CloudLayouterSettings.cs
+++ TagsCloudContainer/CloudLayouters/CloudLayouterSettings.cs
@@ -5,7 +5,6 @@ namespace TagsCloudContainer.CloudLayouters
     public class CloudLayouterSettings
     {
         public ICloudLayoutingAlgorithm Algorithm { get; set; }
-        public Point Center { get; set; }
         public int RectangleSquareMultiplier { get; set; } = 100;
         public double RectangleStep { get; set; } = 0.1;
         public int RectangleBroadness { get; set; } = 1;
