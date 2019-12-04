aefdcf93fe14316363816928abc00cf8e7fbae66
neoezop <49761942+neoezop@users.noreply.github.com>
2019-12-05 03:33:07 +0500

    Helper

---
+++ TagCloud/TagCloud/Infrastructure/HelperMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace TagCloud
+{
+    public static class HelperMethods
+    {
+        public static string GetProjectDirectory()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
+            return projectDirectory;
+        }
+    }
+}
