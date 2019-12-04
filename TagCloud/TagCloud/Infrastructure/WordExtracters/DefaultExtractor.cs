62ff5b8c7c09c7ed764dd8c60031ae00ec8459fd
neoezop <49761942+neoezop@users.noreply.github.com>
2019-12-05 03:21:47 +0500

    deleted old extractor, made new one
    now extractor just gets words from text

---
+++ TagCloud/TagCloud/Infrastructure/WordExtracters/DefaultExtractor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TagCloud
+{
+    public class DefaultExtractor : IExtractor
+    {
+        public string[] ExtractWords(string text)
+        {
+            if (text == null)
+                throw new ArgumentNullException();
+            var words = text.Split('\r', '\n');
+            return words;
+        }
+    }
+}
