c5f05ca1c9554e03466778c4cdaf9df583b02d74
neoezop <49761942+neoezop@users.noreply.github.com>
2019-12-05 03:30:42 +0500

    Added static Tokenizer
    also moved WordToken to different folder

---
+++ TagCloud/TagCloud/Infrastructure/WordToken/WordTokenizer.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace TagCloud
+{
+    public static class WordTokenizer
+    {
+        public static WordToken[] Tokenize(string[] words)
+        {
+            var frequencyDictionary = words
+                .GroupBy(word => word)
+                .ToDictionary(group => group.Key, group => group.Count());
+            var wordTokens = frequencyDictionary
+                .Select(pair => new WordToken(pair.Key, pair.Value))
+                .ToArray();
+            return wordTokens;
+        }
+    }
+}
