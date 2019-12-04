a9f297a3578bb16308caa44921dffe2dee2697cc
neoezop <49761942+neoezop@users.noreply.github.com>
2019-12-05 03:30:09 +0500

    changed parser

--- TagCloud/TagCloud/Infrastructure/Parsers/DefaultParser.cs
+++ TagCloud/TagCloud/Infrastructure/Parsers/DefaultParser.cs
@@ -1,8 +1,11 @@
-﻿namespace TagCloud
+﻿using System.Linq;
+
+namespace TagCloud
 {
     public class DefaultParser : IParser
     {
-        public string Parse(string word) =>
-            word.ToLower();
+        public string[] ParseWords(string[] words) => words
+            .Select(word => word.ToLower())
+            .ToArray();
     }
 }
