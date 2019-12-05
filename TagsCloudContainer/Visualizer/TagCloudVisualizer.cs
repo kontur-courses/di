6ec2ab5953ed4ef55361b068911864fa44e64fc1
Timur <dragrayraptor@yandex.ru>
2019-12-05 06:55:54 +0500

    Unnecessary comments removed

--- TagsCloudContainer/Visualizer/TagCloudVisualizer.cs
+++ TagsCloudContainer/Visualizer/TagCloudVisualizer.cs
@@ -27,48 +27,9 @@ namespace TagsCloudContainer.Visualizer
                 var size = wordRectangle.Rectangle.Height;
                 var font = new Font(settings.FontFamily, size, settings.FontStyle, GraphicsUnit.Pixel);
                 var brush = new SolidBrush(settings.TextColor);
-                //graphics.DrawRectangle(new Pen(Color.Green), wordRectangle.Rectangle);
                 graphics.DrawString(wordRectangle.Word, font, brush, wordRectangle.Rectangle);
             }
         }
-
-        //private void DrawCenter(Point center)
-        //{
-        //    var brush = Brushes.Red;
-        //    var location = center - new Size(1, 1);
-        //    graphics.FillRectangle(brush, new Rectangle(location, new Size(2, 2)));
-        //}
-
-        //private void DrawRectangles()
-        //{
-        //    var brush = Brushes.Green;
-        //    graphics.FillRectangles(brush, rectangles.ToArray());
-        //    var pen = new Pen(Color.Black);
-        //    graphics.DrawRectangles(pen, rectangles.ToArray());
-        //}
-
-        //private void Save(string directoryName, string filename)
-        //{
-        //    var directoryInfo = Directory.GetParent(Environment.CurrentDirectory);
-        //    while (directoryInfo != null && directoryInfo.Name != "TagsCloudVisualization")
-        //        directoryInfo = directoryInfo.Parent;
-        //    if (directoryInfo == null)
-        //        throw new DirectoryNotFoundException("Directory with the name TagsCloudVisualization not found");
-        //    Directory.CreateDirectory(directoryInfo.FullName + @"\" + directoryName);
-        //    bitmap.Save(directoryInfo.FullName + $@"\{directoryName}\{filename}");
-        //}
-
-        //public static void SaveNewRectanglesLayout(List<Rectangle> rectangles, string directoryName, string filename)
-        //{
-        //    var imageSize = VisualizerСalculations.GetOptimalSizeForImage(rectangles, 5);
-        //    var center = VisualizerСalculations.GetCenter(imageSize);
-        //    var offset = new Size(center);
-        //    rectangles = VisualizerСalculations.GetRectanglesWithOptimalLocation(rectangles, offset);
-        //    using var visualizer = new TagCloudVisualizer(imageSize, rectangles);
-        //    visualizer.DrawRectangles();
-        //    visualizer.DrawCenter(center);
-        //    visualizer.Save(directoryName, filename);
-        //}
 
         public Bitmap GetImage(IList<WordRectangle> wordRectangles)
         {
