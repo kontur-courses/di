bd7b404b8eb4b2e4ca8f66d14cd91751d35d7e0a
Shcherbina99 <ej99@example.com>
2019-12-06 00:01:05 +0500

    add cloudSetting

---
+++ TagsCloudContainer/Visualization/ICloudSetting.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace TagsCloudContainer.Visualization
+{
+    public interface ICloudSetting
+    {
+        Font Font { get; }
+        Size ImageSize { get; }
+        Color TextColor { get; }
+        Color BackgroundColor { get; }
+        Point GetCenterImage();
+    }
+}
---
+++ TagsCloudContainer/Visualization/TagsCloudSetting.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+namespace TagsCloudContainer.Visualization
+{
+    public class TagsCloudSetting : ICloudSetting
+    {
+        public Font Font { get; }
+        public Size ImageSize { get; }
+        public Color TextColor { get; }
+        public Color BackgroundColor { get; }
+        public Point GetCenterImage()
+        {
+            return new Point(ImageSize.Width / 2, ImageSize.Height / 2);
+        }
+
+        public TagsCloudSetting(Font font, Size imageSize, Color textColor, Color backgroundColor)
+        {
+            Font = font;
+            ImageSize = imageSize;
+            TextColor = textColor;
+            BackgroundColor = backgroundColor;
+        }
+
+        public static TagsCloudSetting GetDefault()
+        {
+            return new TagsCloudSetting(new Font(FontFamily.GenericMonospace,60), new Size(1000,1000), Color.Red,Color.Black);
+        }
+    }
+}
