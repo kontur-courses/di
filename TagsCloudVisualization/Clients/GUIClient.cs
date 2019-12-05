4cbf3585e21051a9e0299e4d1bf02e4de216daf3
Olga Bursina <bursina.olya@yandex.ru>
2019-12-06 02:14:36 +0500

    added GUIClient

---
+++ TagsCloudVisualization/Clients/GUIClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TagsCloudVisualization.Clients
+{
+    public class GUIClient
+    {
+        public GUIClient(CloudCreator cloudCreator)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TagsCloudVisualization/CloudPainters/SingleColorCloudPainter.cs
+++ TagsCloudVisualization/CloudPainters/SingleColorCloudPainter.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using TagsCloudVisualization.Visualization;
 
 namespace TagsCloudVisualization.CloudPainters
