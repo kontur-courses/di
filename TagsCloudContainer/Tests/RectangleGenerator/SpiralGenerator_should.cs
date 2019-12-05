360bf22e288825f12bf64edd1b34c200c5608625
Shcherbina99 <ej99@example.com>
2019-12-06 00:06:10 +0500

    add Tests

---
+++ TagsCloudContainer/Tests/RectangleGenerator/SpiralGenerator_should.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudContainer.RectangleGenerator.PointGenerator;
+
+namespace TagsCloudContainer.Tests.RectangleGenerator
+{
+    class SpiralGenerator_Should
+    {
+        private static Point center;
+        private static SpiralGenerator generator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            center = new Point(10, 10);
+            generator = new SpiralGenerator(center);
+        }
+
+        [Test]
+        public void GetNextPoint_WhenGetOnePoint_ReturnCenterPoint()
+        {
+            generator.GetNextPoint().Should().Be(generator.Center);
+        }
+
+        [Test]
+        public void GetNextPoint_WhenGetPoints_ReturnDifferentPoints()
+        {
+            var hash = new HashSet<Point>();
+            var point = new Point();
+            while (point.X>=0 && point.Y >=0)
+            {
+                point = generator.GetNextPoint();
+                hash.Should().NotContain(point);
+                hash.Add(point);
+            }
+        }
+    }
+}
