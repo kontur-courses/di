a71dacf90e3ac3f41b0683dd2268f56fc4297bfe
lgnv <43182296+lgnv@users.noreply.github.com>
2019-12-06 22:47:59 +0500

    Создан интерфейса читателя слов и реализация чтения из файла

---
+++ TagsCloudContainer/Core/Readers/FileReader.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace TagsCloudContainer.Core.Readers
+{
+    class FileReader : IReader
+    {
+        public IEnumerable<string> ReadWords(string path) => File.ReadAllText(path).Split();
+    }
+}
---
+++ TagsCloudContainer/Core/Readers/IReader.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace TagsCloudContainer.Core.Readers
+{
+    interface IReader
+    {
+        IEnumerable<string> ReadWords(string path);
+    }
+}
