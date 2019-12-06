b926277088936f7f159269f2c3e1eb6d5bb0aa5c
lgnv <43182296+lgnv@users.noreply.github.com>
2019-12-06 22:45:03 +0500

    Добавлен частотный словарь и тесты для него

---
+++ TagsCloudContainer/Core/FrequencyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TagsCloudContainer.Core
+{
+    class FrequencyDictionary<T>
+    {
+        private readonly Dictionary<T, int> frequencyDictionary;
+        public FrequencyDictionary()
+        {
+            frequencyDictionary = new Dictionary<T, int>();
+        }
+
+        public FrequencyDictionary(IEnumerable<T> keys) : this()
+        {
+            foreach (var key in keys)
+                Add(key);
+        }
+
+        public void Add(T key)
+        {
+            if (!frequencyDictionary.ContainsKey(key))
+                frequencyDictionary[key] = 0;
+            frequencyDictionary[key]++;
+        }
+
+        public int GetCounter(T key) => frequencyDictionary.TryGetValue(key, out var result) ? result : 0;
+
+        public IEnumerable<(T, int)> Top(int count)
+        {
+            return frequencyDictionary
+                .OrderByDescending(kvp => kvp.Value)
+                .Take(Math.Min(count, frequencyDictionary.Count))
+                .Select(kvp => (kvp.Key, kvp.Value));
+        }
+
+        public int Count() => frequencyDictionary.Count;
+
+    }
+}
---
+++ TagsCloudContainer/Tests/FrequencyDictionaryTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudContainer.Core;
+
+namespace TagsCloudContainer.Tests
+{
+    [TestFixture]
+    class FrequencyDictionaryTests
+    {
+        private FrequencyDictionary<string> frequencyDictionary;
+
+        [SetUp]
+        public void SetUp()
+        {
+            frequencyDictionary = new FrequencyDictionary<string>();;
+        }
+
+        [Test]
+        public void Constructor_DoesNotThrow_WhenWithoutParameters()
+        {
+            Action action = () => new FrequencyDictionary<string>();
+            action.Should().NotThrow();
+        }
+
+        [Test]
+        public void Constructor_DoesNotThrow_WhenWithIEnumerableKeys()
+        {
+            Action action = () => new FrequencyDictionary<string>(new [] {"a", "b", "c"});
+            action.Should().NotThrow();
+        }
+
+        [TestCase(1, "bla", "bla", TestName = "WhenAddedTheseWordsOnce")]
+        [TestCase(5, "a", "a", "a", "a", "a", "a", TestName = "WhenAddedTheseWordsFiveTimes")]
+        [TestCase(1, "a", "a", "b", TestName = "WhenNotOnlyThisWordWasAdded")]
+        public void Add_ShouldIncrementCounter(int expectedCounter, string verifiedKey, params string[] words)
+        {
+            foreach (var word in words)
+                frequencyDictionary.Add(word);
+
+            frequencyDictionary.GetCounter(verifiedKey).Should().Be(expectedCounter);
+        }
+
+        [Test]
+        public void Top_ShouldReturnsMostFrequent()
+        {
+            for(var i = 0; i < 5; i++)
+                frequencyDictionary.Add("a");
+            for(var i = 0; i < 3; i++)
+                frequencyDictionary.Add("b");
+            for (int i = 0; i < 10; i++)
+                frequencyDictionary.Add("c");
+
+            frequencyDictionary.Top(3).Should().BeInDescendingOrder(kvp => kvp.Item2);
+        }
+
+    }
+}
