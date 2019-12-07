999440a1ba5be64c9559c6e5e04b5bb6e20dfcbf
MrTimeChip <mistertimechip@mail.ru>
2019-12-07 16:16:07 +0500

    Added parsing command

--- TagsCloudContainer/ApplicationRunning/ConsoleApp/ConsoleCommands/ParseCommand.cs
+++ TagsCloudContainer/ApplicationRunning/ConsoleApp/ConsoleCommands/ParseCommand.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.IO;
+using TagsCloudContainer.TextParsing.CloudParsing.ParsingRules;
+using TagsCloudContainer.TextParsing.FileWordsParsers;
 
 namespace TagsCloudContainer.ApplicationRunning.ConsoleApp.ConsoleCommands
 {
     public class ParseCommand : IConsoleCommand
     {
+        private TagsCloud cloud;
+        private SettingsManager manager;
+        public ParseCommand(TagsCloud cloud, SettingsManager manager)
+        {
+            this.cloud = cloud;
+            this.manager = manager;
+        }
         public void Act(string[] args)
         {
-            Console.WriteLine("Im parsing");
+            var path = string.Join("", args);
+            if(!File.Exists(path)) throw new ArgumentException($"No file '{path}' found!");
+            var extension = Path.GetExtension(path);
+            var parser = WordsParser.GetParser(extension);
+            manager.ConfigureWordsParserSettings(parser, path, new DefaultParsingRule());
+            cloud.ParseWords();
+            Console.WriteLine($"Successfully parsed words from: '{path}'");
         }
 
         public string Name => "Parse";
---
+++ TagsCloudContainer/TextParsing/FileWordsParsers/WordsParser.cs
@@ -0,0 +1,14 @@
+﻿namespace TagsCloudContainer.TextParsing.FileWordsParsers
+{
+    public static class WordsParser
+    {
+        public static IFileWordsParser GetParser(string extension)
+        {
+            switch (extension)
+            {
+                default:
+                    return new TxtWordParser();
+            }
+        }
+    }
+}
