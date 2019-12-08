0eb914e707cfe7ccd32f47ded828876a3d7dd820
Nicole <mrhomka.com@gmail.com>
2019-12-08 18:43:00 +0500

    Add test for EvenPainterWords

---
+++ TagsCloudContainer/PaintersWords/EvenPainterWordsTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System.Drawing;
+using TagsCloudContainer.TokensAndSettings;
+
+namespace TagsCloudContainer.PaintersWords
+{
+    [TestFixture]
+    class EvenPainterWordsTests
+    {
+        [Test]
+        public void GetNextBrush_OddNumber_Aqua()
+        {
+            var evenPainterWords = new EvenPainterWords();
+
+            var result = evenPainterWords.GetNextBrush(new WordToken("qqq", 1));
+
+            Assert.AreEqual(Brushes.Aqua, result);
+        }
+
+        [Test]
+        public void GetNextBrush_EvenNumber_Red()
+        {
+            var evenPainterWords = new EvenPainterWords();
+
+            var result = evenPainterWords.GetNextBrush(new WordToken("qqq", 6));
+
+            Assert.AreEqual(Brushes.Red, result);
+        }
+    }
+}
