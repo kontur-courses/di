9069b95c6bedad19456d3fb37c5395cebcbe924e
Nicole <mrhomka.com@gmail.com>
2019-12-08 14:32:54 +0500

    Added support for doc and docx formats

--- TagsCloudContainer/Readers/SimpleReader.cs
+++ TagsCloudContainer/Readers/SimpleReader.cs
@@ -1,4 +1,6 @@
 ﻿using System.IO;
+using System.Linq;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace TagsCloudContainer.Readers
 {
@@ -13,8 +15,34 @@ namespace TagsCloudContainer.Readers
 
         public string[] ReadAllLines()
         {
+            var splitPath = path.Split('.');
+            if (new[] {"doc", "docx" }.Contains(splitPath[splitPath.Length - 1]))
+            {
+                return ReadDoc(path);
+            }
+            return ReadOther(path);
+        }
+
+        private string[] ReadOther(string path)
+        {
             var stream = new StreamReader(path);
             return stream.ReadToEnd().Split('\n');
+        }
+
+        private string[] ReadDoc(string path)
+        {
+            Word.Application app = new Word.Application();
+            app.Documents.Open(path);
+            Word.Document doc = app.ActiveDocument;
+            var words = new string[doc.Paragraphs.Count - 1];
+            for (int i = 1; i < doc.Paragraphs.Count; i++)
+            {
+                var text = doc.Paragraphs[i].Range.Text;
+                words[i - 1] = text.Substring(0, text.Length - 1);
+            }
+            doc.Close();
+            app.Quit();
+            return words;
         }
     }
 }
---
+++ TagsCloudContainer/Readers/SimpleReaderTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainer.Readers
+{
+    [TestFixture]
+    class SimpleReaderTests
+    {
+        [Test]
+        public void ReadAllLines_Docx()
+        {
+            var simpleReader = new SimpleReader(@"E:\Projects\Shpora1\di\TagsCloudContainer\Words.docx");
+
+            var result = simpleReader.ReadAllLines();
+
+            var expect = new[] { "A", "A", "A", "A", "D", "D", "B", "B", "D", "A" };
+
+            Assert.AreEqual(expect.Length, result.Length);
+            Assert.AreEqual(expect, result);
+        }
+
+        [Test]
+        public void ReadAllLines_Doc()
+        {
+            var simpleReader = new SimpleReader(@"E:\Projects\Shpora1\di\TagsCloudContainer\Words.doc");
+
+            var result = simpleReader.ReadAllLines();
+
+            var expect = new[] { "A", "A", "A", "A", "D", "D", "B", "B", "D", "A", "D" };
+
+            Assert.AreEqual(expect.Length, result.Length);
+            Assert.AreEqual(expect, result);
+        }
+    }
+}
