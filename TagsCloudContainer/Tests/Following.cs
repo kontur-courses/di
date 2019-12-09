764d3832c93adf3f140d882f95b1919d5d3e8634
Данил Панков <mistertimechip@mail.ru>
2019-12-09 13:08:03 +0300

    Added class for cleaner testing

---
+++ TagsCloudContainer/Tests/Following.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.CodeDom;
+
+namespace TagsCloudContainer.Tests
+{
+    public static class Following
+    {
+        public static Action Code(Action action)
+        {
+            return action;
+        }
+    }
+}
