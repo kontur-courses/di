0e1549d2a8655bbdb26012b701716d019eb04cf9
Данил Панков <mistertimechip@mail.ru>
2019-12-09 12:35:07 +0300

    Added better argument check

--- TagsCloudContainer/ApplicationRunning/ConsoleApp/ConsoleCommands/GenerateCloudCommand.cs
+++ TagsCloudContainer/ApplicationRunning/ConsoleApp/ConsoleCommands/GenerateCloudCommand.cs
@@ -18,17 +18,22 @@ namespace TagsCloudContainer.ApplicationRunning.ConsoleApp.ConsoleCommands
         public void Act(string[] args)
         {
             if(args.Length < 4) throw new ArgumentException("Incorrect arguments count! Expected 4.");
-            if(!double.TryParse(args[2], out var step)) throw new ArgumentException($"Incorrect step value {args[2]}");
-            if(!int.TryParse(args[3], out var broadness)) throw new ArgumentException($"Incorrect broadness value {args[3]}");
-            if (!int.TryParse(args[1], out var size)) throw new ArgumentException($"Incorrect size value {args[1]}");
+            if(!double.TryParse(args[2], out var step) 
+               || step <= 0.1) throw new ArgumentException($"Incorrect step value {args[2]}");
+            if(!int.TryParse(args[3], out var broadness) 
+               || broadness <= 0
+               || broadness > 2) throw new ArgumentException($"Incorrect broadness value {args[3]}");
+            if (!int.TryParse(args[1], out var size) 
+                || size <= 0) throw new ArgumentException($"Incorrect size value {args[1]}");
             var algorithm = CloudLayoutingAlgorithms.TryGetLayoutingAlgorithm(args[0], step, broadness);
             if(algorithm == null) throw new ArgumentException($"Incorrect algorithm name {args[0]}");
             manager.ConfigureLayouterSettings(algorithm, size, step, broadness);
             cloud.GenerateTagCloud();
             Console.WriteLine("Successfully generated cloud.");
         }
+
         public string Name => "generate";
         public string Description => "generate tag cloud using settings";
-        public string Arguments => "algorithm size step broadness";
+        public string Arguments => "algorithm size[1...] step[0.1...] broadness[1, 2]";
     }
 }
