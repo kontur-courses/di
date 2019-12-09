d22923df6459e1c390c35db0fb03cbf6b454450a
Данил Панков <mistertimechip@mail.ru>
2019-12-09 13:13:13 +0300

    Added test for parse command

---
+++ TagsCloudContainer/Tests/Commands/ParseCommand_Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudContainer.ApplicationRunning;
+using TagsCloudContainer.ApplicationRunning.Commands;
+using TagsCloudContainer.CloudLayouters;
+using TagsCloudContainer.CloudVisualizers;
+using TagsCloudContainer.CloudVisualizers.ImageSaving;
+using TagsCloudContainer.TextParsing.CloudParsing;
+
+namespace TagsCloudContainer.Tests.Commands
+{
+    [TestFixture]
+    public class ParseCommand_Test
+    {
+        private ParseCommand command;
+        [SetUp]
+        public void SetUp()
+        {
+            var settings = new SettingsManager();
+            var parser = new CloudWordsParser(() => settings.GetWordsParserSettings());
+            var layouter = new CloudLayouter(() => settings.GetLayouterSettings());
+            var visualizer = new CloudVisualizer(() => settings.GetVisualizerSettings());
+            var saver = new ImageSaver(() => settings.GetImageSaverSettings());
+            var cloud = new TagsCloud(parser, layouter, visualizer, saver);
+            command = new ParseCommand(cloud, settings);
+        }
+
+        [Test]
+        public void Act_Should_ThrowArgumentException_When_FileDoesntExist()
+        {
+            Following.Code(() => command.Act(new[] {"idontexist"})).ShouldThrow<ArgumentException>();
+        }
+    }
+}
