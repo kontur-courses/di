c12346c914b5788c685a4d7c91dd596cf10cecb9
vaseQ-NotAllow <hayvasahay@gmail.com>
2019-12-10 19:00:13 +0500

    Add options of bg color

--- TagsCloudContainer/ImageSetting.cs
+++ TagsCloudContainer/ImageSetting.cs
@@ -1,14 +1,19 @@
+using System.Drawing;
+
 namespace TagsCloudContainer
 {
     public class ImageSetting
     {
         public int Height { get; }
         public int Width { get; }
+        
+        public Color BackGround { get; }
 
-        public ImageSetting(int height, int width)
+        public ImageSetting(int height, int width, string color)
         {
             Height = height;
             Width = width;
+            BackGround = Color.FromName(color);
         }
     }
 }
