78a5e62c36b8521e341aea07f8d699c00262955e
HenrickBolender <40788127+HenrickBolender@users.noreply.github.com>
2019-12-10 22:33:37 +0500

    removed cloud center customization

--- TagsCloudVisualization/TagsCloudVisualization/Logic/TagCloudVisualizer.cs
+++ TagsCloudVisualization/TagsCloudVisualization/Logic/TagCloudVisualizer.cs
@@ -26,6 +26,7 @@ namespace TagsCloudVisualization.Logic
 
         public Bitmap VisualizeTextFromFile(string fileName, ImageSettings imageSettings)
         {
+            var imageCenter = new Point(imageSettings.ImageSize.Width / 2, imageSettings.ImageSize.Height / 2);
             var text = TextRetriever.RetrieveTextFromFile(fileName);
             var wordTokens = textParser.ParseToTokens(text, boringWordsProvider.BoringWords);
             var bmp = new Bitmap(imageSettings.ImageSize.Width, imageSettings.ImageSize.Height);
@@ -35,24 +36,22 @@ namespace TagsCloudVisualization.Logic
             var cloudScale = 1f;
             foreach (var word in wordTokens)
             {
-                var tag = CreateTag(graphics, word, imageSettings);
-                var currentCloudScale = CalculateCloudScale(tag, imageSettings);
-
+                var tag = CreateTag(graphics, word, imageSettings, imageCenter);
+                var currentCloudScale = CalculateCloudScale(tag, imageSettings, imageCenter);
                 if (currentCloudScale < cloudScale)
                     cloudScale = currentCloudScale;
-
                 tags.Add(tag);
             }
             foreach (var tag in tags)
-                DrawTag(graphics, tag, cloudScale, imageSettings);
+                DrawTag(graphics, tag, cloudScale, imageSettings, imageCenter);
             layouter.Reset();
             return bmp;
         }
 
-        private float CalculateCloudScale(Tag tag, ImageSettings imageSettings)
+        private float CalculateCloudScale(Tag tag, ImageSettings imageSettings, Point imageCenter)
         {
-            var furthestRectanglePoint = GetFurthestRectanglePoint(imageSettings.CloudCenter, tag.TagBox);
-            var cloudBorderSize = GetCloudBorderSize(imageSettings);
+            var furthestRectanglePoint = GetFurthestRectanglePoint(imageCenter, tag.TagBox);
+            var cloudBorderSize = imageSettings.ImageSize;
             var distanceToImageBorder = Geometry
                 .GetLengthFromRectangleCenterToBorderOnVector(
                     new Rectangle(Point.Empty, cloudBorderSize),
@@ -81,50 +80,36 @@ namespace TagsCloudVisualization.Logic
             return posMinusCenterWithTagBorder;
         }
 
-        private Size GetCloudBorderSize(ImageSettings imageSettings)
-        {
-            return new Size(
-                Math.Min(
-                    imageSettings.CloudCenter.X,
-                    imageSettings.ImageSize.Width - imageSettings.CloudCenter.X
-                ) * 2,
-                Math.Min(
-                    imageSettings.CloudCenter.Y,
-                    imageSettings.ImageSize.Height - imageSettings.CloudCenter.Y
-                ) * 2
-            );
-        }
-
         private static Size CalculateWordSize(WordToken wordToken, Font font, Graphics graphics)
         {
             var size = graphics.MeasureString(wordToken.Word, font);
             return size.ToSize();
         }
 
-        private Tag CreateTag(Graphics graphics, WordToken wordToken, ImageSettings imageSettings)
+        private Tag CreateTag(Graphics graphics, WordToken wordToken, ImageSettings imageSettings, Point imageCenter)
         {
             var color = painter.GetTagColor();
             var fontSize = CalculateFontSize(wordToken, imageSettings);
             var wordFont = new Font(imageSettings.Font.FontFamily, fontSize, imageSettings.Font.Style);
             var wordRectangle = layouter.PutNextRectangle(CalculateWordSize(wordToken, wordFont, graphics));
             wordRectangle.Location = new Point(
-                wordRectangle.X + imageSettings.CloudCenter.X,
-                wordRectangle.Y + imageSettings.CloudCenter.Y
+                wordRectangle.X + imageCenter.X,
+                wordRectangle.Y + imageCenter.Y
             );
             return new Tag(wordToken, wordRectangle, fontSize, color);
         }
 
-        private void DrawTag(Graphics graphics, Tag tag, float cloudScale, ImageSettings imageSettings)
+        private void DrawTag(Graphics graphics, Tag tag, float cloudScale, ImageSettings imageSettings, Point imageCenter)
         {
             var scaledFontSize = tag.FontSize * cloudScale;
             var scaledFont = new Font(imageSettings.Font.FontFamily, scaledFontSize, imageSettings.Font.Style);
             var positionMinusCenter = new Point(
-                tag.TagBox.Location.X - imageSettings.CloudCenter.X,
-                tag.TagBox.Location.Y - imageSettings.CloudCenter.Y
+                tag.TagBox.Location.X - imageCenter.X,
+                tag.TagBox.Location.Y - imageCenter.Y
             );
             var newPointLocation = new Point(
-                imageSettings.CloudCenter.X + (int) (positionMinusCenter.X * cloudScale),
-                imageSettings.CloudCenter.Y + (int) (positionMinusCenter.Y * cloudScale)
+                imageCenter.X + (int) (positionMinusCenter.X * cloudScale),
+                imageCenter.Y + (int) (positionMinusCenter.Y * cloudScale)
             );
             graphics.DrawString(tag.WordToken.Word, scaledFont, new SolidBrush(tag.Color), newPointLocation);
         }
--- TagsCloudVisualization/TagsCloudVisualization/Services/ImageSettings.cs
+++ TagsCloudVisualization/TagsCloudVisualization/Services/ImageSettings.cs
@@ -7,21 +7,19 @@ namespace TagsCloudVisualization.Services
         private static readonly ImageSettings DefaultSettings = InitializeDefaultSettings();
         public  Font Font { get; set; }
         public  Size ImageSize { get; set; }
-        public  Point CloudCenter { get; set; }
         public  Color BackgroundColor { get; set; }
 
-        public ImageSettings(Font font, Size imageSize, Point cloudCenter, Color backgroundColor)
+        public ImageSettings(Font font, Size imageSize, Color backgroundColor)
         {
             BackgroundColor = backgroundColor;
             Font = font ?? DefaultSettings.Font;
             ImageSize = imageSize;
-            CloudCenter = cloudCenter;
         }
 
         public static ImageSettings InitializeDefaultSettings()
         {
             var font = new Font(FontFamily.GenericSansSerif, 30, FontStyle.Bold);
-            return new ImageSettings(font, new Size(1000, 1000), new Point(500,500), Color.AntiqueWhite);
+            return new ImageSettings(font, new Size(1000, 1000), Color.AntiqueWhite);
         }
     }
 }
