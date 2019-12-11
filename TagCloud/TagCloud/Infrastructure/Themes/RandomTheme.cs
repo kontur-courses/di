12ba47c2e411527c0c00ffe773518b1a52576982
neoezop <49761942+neoezop@users.noreply.github.com>
2019-12-11 05:52:01 +0500

    added themes

---
+++ TagCloud/TagCloud/Infrastructure/Themes/RandomTheme.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+
+namespace TagCloud
+{
+    public class RandomTheme : ITheme
+    {
+        private readonly Random random;
+
+        public RandomTheme()
+        {
+            random = new Random();
+        }
+
+        public Color BackgroundColor =>
+            Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+
+        public Color GetWordFontColor(WordToken wordToken) =>
+            Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+    }
+}
---
+++ TagCloud/TagCloud/Infrastructure/Themes/WhiteBgBlackWordsTheme.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace TagCloud
+{
+    public class WhiteBgBlackWordsTheme : ITheme
+    {
+        public Color BackgroundColor => Color.White;
+
+        public Color GetWordFontColor(WordToken wordToken) => Color.Black;
+    }
+}
