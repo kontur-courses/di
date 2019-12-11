cad9c51524a0b2c68763bc8530cadb3abecfddf4
neoezop <49761942+neoezop@users.noreply.github.com>
2019-12-12 03:05:22 +0500

    added upper case parser
    just for fun xD

---
+++ TagCloud/TagCloud/Infrastructure/Parsers/UpperCaseParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagCloud
+{
+    public class UpperCaseParser : IParser
+    {
+        public bool IsChecked { get; set; }
+
+        public string Name { get; private set; }
+
+        public UpperCaseParser()
+        {
+            IsChecked = true;
+            Name = "UpperCase parser";
+        }
+
+        public string[] ParseWords(string[] words) => words
+            .Select(word => word.ToUpper())
+            .ToArray();
+    }
+}
