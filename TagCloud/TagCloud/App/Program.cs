cca8d17e117847c0eedfb9da8688d576c70d8e84
neoezop <49761942+neoezop@users.noreply.github.com>
2019-12-12 00:13:47 +0500

    changed container builder

--- TagCloud/TagCloud/App/Program.cs
+++ TagCloud/TagCloud/App/Program.cs
@@ -23,68 +23,42 @@ namespace TagCloud
         private static IContainer BuildContainer()
         {
             var builder = new ContainerBuilder();
-            var filters = new HashSet<IFilter>(GetIFilterImplementations());
-            var filterList = new FilterList(filters, filters);
-            var parsers = new HashSet<IParser>(GetIParserImplementations());
-            var parserList = new ParserList(parsers, parsers);
-            var themes = new HashSet<ITheme>(GetIThemeImplementations());
-            var themeList = new ThemeList(themes, themes);
-            var boringWords = new HashSet<string>(File.ReadAllLines($"{HelperMethods.GetProjectDirectory()}\\BoringWords.txt"));
-            var boringWordsList = new BoringWordsList(boringWords, boringWords);
-            var speechParts = new HashSet<SpeechPart>(Enum.GetValues(typeof(SpeechPart)) as SpeechPart[]);
-            var speechPartList = new SpeechPartList(speechParts, speechParts);
+            var boringWords = File.ReadAllLines($"{HelperMethods.GetProjectDirectory()}\\BoringWords.txt");
+            foreach (var word in boringWords)
+                builder.RegisterInstance(new BoringWord(word)).As<BoringWord>();
+            var speechParts = Enum.GetValues(typeof(SpeechPartEnum)).Cast<SpeechPartEnum>();
+            foreach (var speechPart in speechParts)
+            {
+                var speechPartValue = Enum.GetName(typeof(SpeechPartEnum), speechPart);
+                builder.RegisterInstance(new SpeechPart(speechPartValue, speechPart)).As<SpeechPart>();
+            }
+            builder.RegisterType<Reader>().SingleInstance().As<Reader>();
+            builder.Register<IReader>((c, p) =>
+            {
+                var path = p.Named<string>("PathToFile");
+                if (path.Contains(".doc"))
+                    return new DocReader();
+                else
+                    return new TxtReader();
+            });
             builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
-                .Where(t => typeof(IUiAction).IsAssignableFrom(t)).As<IUiAction>();
+                .Where(t => typeof(IUiAction).IsAssignableFrom(t)).SingleInstance().As<IUiAction>();
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(t => typeof(IFilter).IsAssignableFrom(t)).SingleInstance().As<IFilter>();
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(t => typeof(IParser).IsAssignableFrom(t)).SingleInstance().As<IParser>();
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(t => typeof(ITheme).IsAssignableFrom(t)).SingleInstance().As<ITheme>();
             builder.RegisterType<DefaultExtractor>().SingleInstance().As<IExtractor>();
             builder.RegisterType<ArchimedeanSpiralLayouter>().SingleInstance().As<ILayouter>();
-            builder.RegisterType<LowerCaseParser>().SingleInstance().As<IParser>();
-            builder.RegisterType<DefaultTxtReader>().SingleInstance().As<IReader>();
+            builder.RegisterType<TxtReader>().SingleInstance().As<IReader>();
             builder.RegisterType<AppVisualizer>().SingleInstance().As<IVisualizer>();
             builder.RegisterType<ImageHolder>().SingleInstance().As<IImageHolder>().As<PictureBox>().As<ImageHolder>();
-            builder.Register(context => FileSettings.GetDefaultSettings()).SingleInstance().As<FileSettings>();
             builder.Register(context => ImageSettings.GetDefaultSettings()).SingleInstance().As<ImageSettings>();
             builder.Register(context => FontSettings.GetDefaultSettings()).SingleInstance().As<FontSettings>();
             builder.Register(context => LayouterSettings.GetDefaultSettings()).SingleInstance().As<LayouterSettings>();
-            builder.Register(context => parserList).SingleInstance().As<IItemList<IParser>>().As<ParserList>();
-            builder.Register(context => filterList).SingleInstance().As<IItemList<IFilter>>().As<FilterList>();
-            builder.Register(context => speechPartList).SingleInstance().As<IItemList<SpeechPart>>().As<SpeechPartList>();
-            builder.Register(context => themeList).SingleInstance().As<IItemList<ITheme>>().As<ThemeList>();
-            builder.Register(context => boringWordsList).SingleInstance().As<IItemList<string>>().As<BoringWordsList>();
             builder.RegisterType<MainForm>().SingleInstance().As<MainForm>();
             return builder.Build();
-        }
-
-        private static IEnumerable<IFilter> GetIFilterImplementations()
-        {
-            var builder = new ContainerBuilder();
-            var boringWords = new HashSet<string>(File.ReadAllLines($"{HelperMethods.GetProjectDirectory()}\\BoringWords.txt"));
-            var boringWordsList = new BoringWordsList(boringWords, boringWords);
-            var speechParts = new HashSet<SpeechPart>(Enum.GetValues(typeof(SpeechPart)) as SpeechPart[]);
-            var speechPartList = new SpeechPartList(speechParts, speechParts);
-            builder.Register(context => speechPartList).SingleInstance().As<IItemList<SpeechPart>>().As<SpeechPartList>();
-            builder.Register(context => boringWordsList).SingleInstance().As<IItemList<string>>().As<BoringWordsList>();
-            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
-                .Where(t => typeof(IFilter).IsAssignableFrom(t)).As<IFilter>();
-            var container = builder.Build();
-            return container.Resolve<IEnumerable<IFilter>>(); ;
-        }
-
-        private static IEnumerable<IParser> GetIParserImplementations()
-        {
-            var builder = new ContainerBuilder();
-            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
-                .Where(t => typeof(IParser).IsAssignableFrom(t)).As<IParser>();
-            var container = builder.Build();
-            return container.Resolve<IEnumerable<IParser>>();
-        }
-
-        private static IEnumerable<ITheme> GetIThemeImplementations()
-        {
-            var builder = new ContainerBuilder();
-            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
-                .Where(t => typeof(ITheme).IsAssignableFrom(t)).As<ITheme>();
-            var container = builder.Build();
-            return container.Resolve<IEnumerable<ITheme>>();
         }
     }
 }
