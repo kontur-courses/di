aa56103871be807af6b7e1ebbb6843d7d25c3164
Arthurique <aksel.artur@gmail.com>
2019-12-11 10:06:46 +0500

    added imageUtilities tests

---
+++ TagsCloudApplication/TagsCloudLayoutTests/ImageUtilities_Should.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using FluentAssertions;
+using TagsCloudVisualization;
+using System.Drawing.Imaging;
+using System;
+
+namespace TagsCloudApplicationTests
+{
+    [TestFixture]
+    public class ImageUtilities_Should
+    {
+        private Dictionary<string, ImageFormat> formatNameToFormat;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            formatNameToFormat = new Dictionary<string, ImageFormat>()
+            {
+                { "png" , ImageFormat.Png },
+                { "jpeg", ImageFormat.Jpeg },
+                { "gif", ImageFormat.Gif },
+                { "bmp", ImageFormat.Bmp },
+                { "tiff", ImageFormat.Tiff }
+            };
+        }
+
+        [TestCase("png")]
+        [TestCase("jpeg")]
+        [TestCase("gif")]
+        public void ParseFormatNameString_Properly(string formatNameString)
+        {
+            ImageUtilities.GetFormatFromString(formatNameString).Should().BeEquivalentTo(
+                formatNameToFormat[formatNameString]);
+        }
+
+        [TestCase("PNG")]
+        [TestCase("PnG")]
+        public void ParseFormatNameString_IndifferentToCase(string formatNameString)
+        {
+            ImageUtilities.GetFormatFromString(formatNameString).Should().BeEquivalentTo(
+                formatNameToFormat[formatNameString.ToLower()]);
+        }
+
+        [Test]
+        public void ThrowArgumentException_OnUnknownFormatName()
+        {
+            Action act = () => ImageUtilities.GetFormatFromString("???");
+
+            act.Should().Throw<ArgumentException>();
+        }
+    }
+}
