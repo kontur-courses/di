4a4701a79b3fc1f27eb33493ff925489af863af3
TVIGes <khdv66@gmail.com>
2019-12-11 19:59:25 +0500

    Apply SRP in the Layouts package

---
+++ TagsCloudContainer/Visualization/Measurers/IWordMeasurer.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+using TagsCloudContainer.Data;
+
+namespace TagsCloudContainer.Visualization.Layouts
+{
+    public interface IWordMeasurer
+    {
+        (Font font, Size size) Measure(Word word);
+    }
+}
---
+++ TagsCloudContainer/Visualization/Measurers/ProbabilityWordMeasurer.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using TagsCloudContainer.Data;
+
+namespace TagsCloudContainer.Visualization.Layouts
+{
+    internal class ProbabilityWordMeasurer : IWordMeasurer
+    {
+        private readonly FontFamily fontFamily;
+        private readonly float sizeFactor;
+
+        internal ProbabilityWordMeasurer(FontFamily fontFamily, float sizeFactor)
+        {
+            this.fontFamily = fontFamily;
+            this.sizeFactor = sizeFactor;
+        }
+
+        public (Font font, Size size) Measure(Word word)
+        {
+            var emSize = (int) (100 * word.Probability * sizeFactor);
+            var font = new Font(fontFamily, emSize);
+            var size = TextRenderer.MeasureText(word.Value, font);
+            return (font, size);
+        }
+    }
+}
