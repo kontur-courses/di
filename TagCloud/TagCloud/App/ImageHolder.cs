bb1087b46bad396ede23a04fc391bee46f0541da
neoezop <49761942+neoezop@users.noreply.github.com>
2019-12-11 06:03:15 +0500

    image holder class

---
+++ TagCloud/TagCloud/App/ImageHolder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TagCloud
+{
+    public class ImageHolder : PictureBox, IImageHolder
+    {
+        public Graphics StartDrawing()
+        {
+            FailIfNotInitialized();
+            return Graphics.FromImage(Image);
+        }
+
+        private void FailIfNotInitialized()
+        {
+            if (Image == null)
+                throw new InvalidOperationException("Call PictureBoxImageHolder.RecreateImage before other method call!");
+        }
+
+        public void UpdateUi()
+        {
+            Refresh();
+            Application.DoEvents();
+        }
+
+        public void RecreateImage(ImageSettings imageSettings)
+        {
+            Image = new Bitmap(imageSettings.Width, imageSettings.Height);
+        }
+
+        public void SaveImage(string fileName)
+        {
+            FailIfNotInitialized();
+            Image.Save(fileName);
+        }
+    }
+}
