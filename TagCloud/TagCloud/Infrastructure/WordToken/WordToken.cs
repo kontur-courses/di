1f0b7b87e1a9d14981aa1f5c28b2792ea20d47ee
neoezop <49761942+neoezop@users.noreply.github.com>
2019-12-11 05:50:52 +0500

    token and tokenizer changes
    now has a speech part (and default tokenizer tokenizes without it..)

--- TagCloud/TagCloud/Infrastructure/WordToken/WordToken.cs
+++ TagCloud/TagCloud/Infrastructure/WordToken/WordToken.cs
@@ -4,11 +4,13 @@
     {
         public readonly string Value;
         public readonly int Count;
+        public readonly SpeechPart SpeechPart;
 
-        public WordToken(string value, int count)
+        public WordToken(string value, int count, SpeechPart speechPart)
         {
             Value = value;
             Count = count;
+            SpeechPart = speechPart;
         }
     }
 }
--- TagCloud/TagCloud/Infrastructure/WordToken/WordTokenizer.cs
+++ TagCloud/TagCloud/Infrastructure/WordToken/WordTokenizer.cs
@@ -4,13 +4,13 @@ namespace TagCloud
 {
     public static class WordTokenizer
     {
-        public static WordToken[] Tokenize(string[] words)
+        public static WordToken[] TokenizeWithNoSpeechPart(string[] words)
         {
             var frequencyDictionary = words
                 .GroupBy(word => word)
                 .ToDictionary(group => group.Key, group => group.Count());
             var wordTokens = frequencyDictionary
-                .Select(pair => new WordToken(pair.Key, pair.Value))
+                .Select(pair => new WordToken(pair.Key, pair.Value, SpeechPart.None))
                 .ToArray();
             return wordTokens;
         }
