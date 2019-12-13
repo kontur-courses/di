a663f13499a31cfd82514d80c531a63905ae00aa
shemloft <shem.dar99@gmail.com>
2019-12-13 15:08:20 +0500

    Перенесены остальные тесты

---
+++ TagCloudTests/SegmentTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+using TagCloud.Algorithm.SpiralBasedLayouter;
+
+namespace TagCloudTests
+{
+    public class SegmentTests
+    {
+        [Test]
+        public void SegmentGetCenter_ShouldReturnCenter_WhenItIsInteger()
+        {
+            var segment = new Segment(new Point(0, 0), new Point(2, 0));
+            var expectedCenter = new PointF(1.0f, 0f);
+
+            var result = segment.GetCenter();
+
+            result.Should().Be(expectedCenter);
+
+        }
+
+        [Test]
+        public void SegmentGetCenter_ShouldReturnCenter_WhenItIsDouble()
+        {
+            var segment = new Segment(new Point(0, 0), new Point(1, 0));
+            var expectedCenter = new PointF(0.5f, 0f);
+
+            var result = segment.GetCenter();
+
+            result.Should().Be(expectedCenter);
+        }
+
+        [Test]
+        public void SegmentsAreIntersected_ShouldReturnTrue_WhenSegmentsAreIntersected()
+        {
+            var firstSegment = new Segment(1, 3);
+            var secondSegment = new Segment(2, 4);
+
+            Segment.SegmentsAreIntersected(firstSegment, secondSegment)
+                .Should()
+                .BeTrue();
+        }
+
+        [Test]
+        public void SegmentsAreIntersected_ShouldReturnFalse_WhenSegmentsAreNotIntersected()
+        {
+            var firstSegment = new Segment(1, 3);
+            var secondSegment = new Segment(4, 5);
+
+            Segment.SegmentsAreIntersected(firstSegment, secondSegment)
+                .Should()
+                .BeFalse();
+        }
+
+        [Test]
+        public void SegmentsAreIntersected_ShouldReturnFalse_WhenSegmentsAreTouching()
+        {
+            var firstSegment = new Segment(1, 3);
+            var secondSegment = new Segment(3, 5);
+
+            Segment.SegmentsAreIntersected(firstSegment, secondSegment)
+                .Should()
+                .BeFalse();
+        }
+    }
+}
---
+++ TagCloudTests/SpiralTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FakeItEasy;
+using FluentAssertions;
+using NUnit.Framework;
+using TagCloud.Algorithm.SpiralBasedLayouter;
+using TagCloud.Infrastructure;
+
+namespace TagCloudTests
+{
+    [TestFixture]
+    public class SpiralTests
+    {
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        public void GetNextPoint_ShouldContainPointsFromSpiral_OnCorrespondingInput(int expectedPointShift)
+        {
+            const int parameter = 2;
+            const float stepInRadians = (float)(Math.PI / 6);
+            var expectedPoint = GeometryUtils.ConvertPolarToIntegerCartesian(
+                parameter * stepInRadians * expectedPointShift, 
+                stepInRadians * expectedPointShift);
+            var config = A.Fake<PictureConfig>();
+            config.Parameters = A.Fake<LayouterParameters>();
+            config.Parameters.Parameter = parameter;
+            config.Parameters.StepInDegrees = 30;
+            var spiral = new Spiral(config);
+
+            for (var i = 0; i < expectedPointShift; i++)
+                spiral.GetNextPoint();
+            var pointFromSpiral = spiral.GetNextPoint();
+
+            pointFromSpiral.Should().Be(expectedPoint);
+        }
+    }
+}
