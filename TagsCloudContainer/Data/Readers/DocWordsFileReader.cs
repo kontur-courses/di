3c71101121b856728640d16f33e64790ad868467
TVIGes <khdv66@gmail.com>
2019-12-18 23:29:37 +0500

    Fix reading .docx file

--- TagsCloudContainer/Data/Readers/DocWordsFileReader.cs
+++ TagsCloudContainer/Data/Readers/DocWordsFileReader.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Xceed.Words.NET;
 
 namespace TagsCloudContainer.Data.Readers
@@ -6,6 +7,10 @@ namespace TagsCloudContainer.Data.Readers
     public class DocWordsFileReader : IFileFormatReader
     {
         public IEnumerable<string> Extensions { get; } = new[] {".doc", ".docx"};
-        public IEnumerable<string> ReadAllWords(string path) => DocX.Load(path).Text.Split();
+
+        public IEnumerable<string> ReadAllWords(string path)
+        {
+            return DocX.Load(path).Paragraphs.Select(p => p.Text);
+        }
     }
 }
