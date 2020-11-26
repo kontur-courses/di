2044a41c949cab84340a3cb54d1e78eabb48a32a
NataashaBanan <49069370+NataashaBanan@users.noreply.github.com>
2020-11-26 23:47:37 +0500

    Add frequency analyzer

---
+++ TagCloud/FrequencyAnalyzer/FrequencyAnalyzer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TagCloud
+{
+    public class FrequencyAnalyzer: IFrequencyAnalyzer
+    {
+        private Dictionary<string, int> Frequencies;
+        private string[] words;
+        
+        public FrequencyAnalyzer(IWordParser wordParser)
+        {
+            words = wordParser.GetWords();
+        }
+
+
+        public Dictionary<string, double> GetFrequencyDictionary()
+        {
+            FillFrequencyDictionary();
+            var result = new Dictionary<string, double>();
+            foreach (var key in Frequencies.Keys)
+            {
+                result[key] = (double)Frequencies[key] / words.Length;
+            }
+
+            return result;
+        }
+
+        private void FillFrequencyDictionary()
+        {
+            foreach (var word in words)
+            {
+                if (Frequencies.ContainsKey(word))
+                {
+                    Frequencies[word]++;
+                }
+                else
+                {
+                    Frequencies[word] = 1;
+                }
+            }
+        }
+    }
+}
