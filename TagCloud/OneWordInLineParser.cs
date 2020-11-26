4d6d50844eef21a0947ef1b758d08c47a3b420f8
NataashaBanan <49069370+NataashaBanan@users.noreply.github.com>
2020-11-27 01:18:27 +0500

    Fix path in wordParser

--- TagCloud/OneWordInLineParser.cs
+++ TagCloud/OneWordInLineParser.cs
@@ -8,7 +8,11 @@ namespace TagCloud
         private readonly string[] words;
         public OneWordInLineParser(string inputFileName)
         {
-            words = File.ReadLines(inputFileName).ToArray();
+            var workingDirectory = Directory.GetCurrentDirectory();
+            var index = workingDirectory.IndexOf("TagCloud");
+            var tagCloudPath = workingDirectory.Substring(0, index);
+            var path = tagCloudPath + inputFileName;
+            words = File.ReadLines(path).ToArray();
         }
         public string[] GetWords()
         {
