b39fc2418a07a8bbf680231e22238d69be59a74c
NataashaBanan <49069370+NataashaBanan@users.noreply.github.com>
2020-11-26 23:47:09 +0500

    Add word parser

---
+++ TagCloud/OneWordInLineParser.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace TagCloud
+{
+    public class OneWordInLineParser : IWordParser
+    {
+        private readonly string[] words;
+        public OneWordInLineParser(string inputFileName)
+        {
+            words = File.ReadLines(inputFileName).ToArray();
+        }
+        public string[] GetWords()
+        {
+            return words;
+        }
+    }
+}
