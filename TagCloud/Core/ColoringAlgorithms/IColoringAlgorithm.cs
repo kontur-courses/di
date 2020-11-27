c7198037b4258c8362a27bedc42ecd8aec4e674e
Anton Nechugovskikh <nechugovskihanton@gmail.com>
2020-11-27 19:54:19 +0500

    feature     add ColoringAlgorithm entity     add RainbowColoring stub

---
+++ TagCloud/Core/ColoringAlgorithms/IColoringAlgorithm.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace TagCloud.Core.ColoringAlgorithms
+{
+    public interface IColoringAlgorithm
+    {
+        public Color GetNextColor(Tag tag);
+    }
+}
---
+++ TagCloud/Core/ColoringAlgorithms/RainbowColoring.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace TagCloud.Core.ColoringAlgorithms
+{
+    public class RainbowColoring : IColoringAlgorithm
+    {
+        public Color GetNextColor(Tag tag)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
