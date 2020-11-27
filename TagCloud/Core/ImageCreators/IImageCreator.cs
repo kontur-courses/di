de721bf96b63dc09c39cf55e9c2327d4476dc332
Anton Nechugovskikh <nechugovskihanton@gmail.com>
2020-11-27 19:53:30 +0500

    feature     add ImageCreator entity     add TagCloudImageCreator stub

---
+++ TagCloud/Core/ImageCreators/IImageCreator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagCloud.Core.ImageCreators
+{
+    public interface IImageCreator    
+    {
+        public Bitmap Create(IEnumerable<Tag> tags,Font font, Size size);
+    }
+}
---
+++ TagCloud/Core/ImageCreators/TagCloudImageCreator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagCloud.Core.ImageCreators
+{
+    public class TagCloudImageCreator : IImageCreator
+    {
+        public Bitmap Create(IEnumerable<Tag> tags, Font font, Size size)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
