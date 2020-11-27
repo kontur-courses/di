cf4dafffa82156762df1ac81b08d3a1125fcc1fc
Anton Nechugovskikh <nechugovskihanton@gmail.com>
2020-11-27 19:39:56 +0500

    feature     add FileReader entity     add TxtReader stub

---
+++ TagCloud/Core/FileReaders/IFileReader.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TagCloud.Core.FileReaders
+{
+    public interface IFileReader
+    {
+        public IEnumerable<string> ReadAllWords(string filePath);
+        public bool IsAbleToRead(string filePath);
+    }
+}
---
+++ TagCloud/Core/FileReaders/TxtReader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace TagCloud.Core.FileReaders
+{
+    public class TxtReader : IFileReader
+    {
+        public IEnumerable<string> ReadAllWords(string filePath)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsAbleToRead(string filePath)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
