1dd4bf4bbca848d966d173b5dfab61811fe4da65
Mikhail Medvedev <m.medvedev99@mail.ru>
2020-11-27 12:05:08 +0500

    add project TagCloud add project TagCloudTest

---
+++ TagCloud/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TagCloud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
