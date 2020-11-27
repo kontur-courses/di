2f4e292f4b66f97062366be45518ac38e53b341d
klim0o0 <kirill.klimmmm@gmail.com>
2020-11-27 23:55:29 +0500

    Первая реализация

---
+++ TagCloud/WordForCloud.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace TagCloud
+{
+    public class WordForCloud
+    {
+        public Font Font { get; }
+        public int FontSize { get; }
+        public Size WordSize { get; }
+
+        public string Word { get; }
+        public Color WordColor { get; }
+
+        public WordForCloud(string font, int fontSize, string word, Size wordSize, Color wordColor)
+        {
+            Font = new Font(font, fontSize);
+            FontSize = fontSize;
+            WordSize = wordSize;
+            WordColor = wordColor;
+            Word = word;
+        }
+    }
+}
