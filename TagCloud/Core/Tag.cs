e9ca4f95ba3ce96a429ac683c6b85f3be6ec156b
Anton Nechugovskikh <nechugovskihanton@gmail.com>
2020-11-27 19:52:57 +0500

    feature     add Tag entity

---
+++ TagCloud/Core/Tag.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace TagCloud.Core
+{
+    public class Tag
+    {
+        public string Word { get; }
+        public Rectangle Rectangle { get; }
+        public int FontSize { get; }
+
+        public Tag(string word, Rectangle rectangle, int fontSize)
+        {
+            Word = word;
+            Rectangle = rectangle;
+            FontSize = fontSize;
+        }
+    }
+}
