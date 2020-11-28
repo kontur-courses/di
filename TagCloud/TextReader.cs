550086e307e2aad6edce5e23a59fd1c60a96551d
Mikhail Medvedev <m.medvedev99@mail.ru>
2020-11-28 15:32:02 +0500

    add TextReader

---
+++ TagCloud/TextReader.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace TagCloud
+{
+    internal class TextReader
+    {
+        internal string ReadText(string path)
+        {
+            if (TryReadFile(path, out var result))
+                return result;
+            return result;
+        }
+        private bool TryReadFile(string path, out string result)
+        {
+            using (var sr = File.OpenText(path))
+            {
+                try
+                {
+                    result = sr.ReadToEnd();
+                }
+                catch
+                {
+                    result = null;
+                }
+            }
+            return result != null;
+        }
+    }
+}
