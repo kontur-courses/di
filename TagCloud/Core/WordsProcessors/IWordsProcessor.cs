d5826f2eb3fd97feac02c91c388aa3fcd648aee3
Anton Nechugovskikh <nechugovskihanton@gmail.com>
2020-11-28 16:53:34 +0500

    feature     add WordsProcessor entity     implement WordsProcessor

---
+++ TagCloud/Core/WordsProcessors/IWordsProcessor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace TagCloud.Core.WordsProcessors
+{
+    public interface IWordsProcessor
+    {
+        public IEnumerable<string> Process(IEnumerable<string> words);
+    }
+}
---
+++ TagCloud/Core/WordsProcessors/WordsProcessor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TagCloud.Core.WordConverters;
+using TagCloud.Core.WordsFilters;
+
+namespace TagCloud.Core.WordsProcessors
+{
+    public class WordsProcessor : IWordsProcessor
+    {
+        private readonly List<IWordFilter> filters;
+        private readonly List<IWordConverter> converters;
+
+        public WordsProcessor(IEnumerable<IWordFilter> filters,
+            IEnumerable<IWordConverter> converters)
+        {
+            this.converters = converters.ToList();
+            this.filters = filters.ToList();
+        }
+
+        public IEnumerable<string> Process(IEnumerable<string> words)
+        {
+            return words.Where(IsValid).Select(Convert);
+        }
+
+        private string Convert(string word)
+        {
+            return converters.Aggregate(word,
+                (current, wordHandler) => wordHandler.Convert(current));
+        }
+
+        private bool IsValid(string word)
+        {
+            return filters.All(filter => filter.IsValid(word));
+        }
+    }
+}
