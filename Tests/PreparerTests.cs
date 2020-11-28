16705e26a5a693750221699c7fcc629c52dfb6f2
TerribleVoice <terriblevoice@ya.ru>
2020-11-28 14:38:26 +0500

    добавил проект с тестами

---
+++ Tests/PreparerTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using WordCloudGenerator;
+
+namespace Tests
+{
+    public class PreparerTests
+    {
+        [Test]
+        public void GetCountedWords_ShouldReturnCountedWord_OneWordInEachLine()
+        {
+            var text = "abc\nabc\nefg";
+            var preparer = new Preparer(null);
+
+            preparer.GetCountedWords(text).Should().BeEquivalentTo(
+                new Dictionary<string, int> {["abc"] = 2, ["efg"] = 1});
+        }
+
+        [Test]
+        public void GetCountedWords_ShouldSkipBoringWords_OneBoringWord()
+        {
+            var boringWords = new[] {"abc"};
+            var text = "abc\nabc\nefg\nefg\nxyz";
+            var preparer = new Preparer(boringWords);
+            
+            preparer.GetCountedWords(text).Should().BeEquivalentTo(
+                new Dictionary<string, int> {["efg"] = 2, ["xyz"] = 1});
+        }
+    }
+}
---
+++ Tests/ReaderTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+using WordCloudGenerator;
+
+namespace Tests
+{
+    public class ReaderTests
+    {
+        private Reader reader;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            reader = new Reader();
+        }
+        
+        [Test]
+        public void ReadFile_ShouldReturnPlainText_ExistingFile()
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "../../..", "testFile.txt");
+            reader.ReadFile(path).Should().Be("abc\r\nabc\r\nefg");
+        }
+
+        [Test]
+        public void ReadFile_ShouldThrow_NotExistingFile()
+        {
+            var func = new Action(() => reader.ReadFile("absolutely not a file path"));
+
+            func.Should().Throw<FileNotFoundException>();
+        }
+    }
+}
