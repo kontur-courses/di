5b52fde356fa77c6a983bd0dffbbe7704b49424a
TerribleVoice <terriblevoice@ya.ru>
2020-11-29 03:27:20 +0500

    добавил дефолтный класс палитры

---
+++ WordCloudGenerator/Palette.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace WordCloudGenerator
+{
+    public class Palette : IPalette
+    {
+        private readonly Color[] colors = {Color.Navy, Color.Red, Color.Gold, Color.Green};
+        private int colorIndex = -1;
+
+        public Color GetNextColor()
+        {
+            colorIndex = (colorIndex + 1) % colors.Length;
+            return colors[colorIndex];
+        }
+    }
+}
