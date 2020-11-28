a40ef80660425b19e7f0dd78a6ad4b87e7984bbe
TerribleVoice <terriblevoice@ya.ru>
2020-11-28 14:38:57 +0500

    добавил проект из задания TDD

---
+++ CircularCloudLayouter/README.md
@@ -0,0 +1,5 @@
+# Примеры работы 
+
+![pic1](https://github.com/TerribleVoice/tdd/blob/master/cs/CloudTag/Example%20Images/pic1.png "Пример 1")
+![pic2](https://github.com/TerribleVoice/tdd/blob/master/cs/CloudTag/Example%20Images/pic2.png "Пример 2")
+![pic3](https://github.com/TerribleVoice/tdd/blob/master/cs/CloudTag/Example%20Images/pic3.png "Пример 3")
---
+++ CircularCloudLayouter/Spiral.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+
+namespace CircularCloudLayouter
+{
+    public class Spiral
+    {
+        private readonly Point center;
+        private double angle;
+
+        public Spiral(Point center)
+        {
+            this.center = center;
+        }
+
+        public Point GetNextPoint()
+        {
+            var x = (int) (angle * Math.Cos(angle) + center.X);
+            var y = (int) (angle * Math.Sin(angle) + center.Y);
+            angle += Math.PI / 180;
+            return new Point(x, y);
+        }
+    }
+}
