684af0898b0eabe21c79480cbef4f516f6601cb5
TerribleVoice <terriblevoice@ya.ru>
2020-11-29 03:27:43 +0500

    RectangleExtensions вынес из CircularLayouter

---
+++ WordCloudGenerator/RectangleExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace WordCloudGenerator
+{
+    public static class RectangleExtensions
+    {
+        public static RectangleF SetCenter(this RectangleF rectangle, Point centerPoint)
+        {
+            rectangle.X = centerPoint.X - rectangle.Width / 2;
+            rectangle.Y = centerPoint.Y - rectangle.Height / 2;
+            return rectangle;
+        }
+
+        public static RectangleF Shift(this RectangleF rectangle, PointF shift)
+        {
+            rectangle.Offset(shift);
+            return rectangle;
+        }
+    }
+}
