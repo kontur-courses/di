4140bfa4667827ab172a1ff6556624573b9d0502
Alisher Kazantsev <alisher021299@gmail.com>
2020-11-29 15:35:47 +0500

    Added argument parser

---
+++ TagsCloudContainer/CommandLineOptions.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+
+namespace TagsCloudContainer
+{
+    public class CommandLineOptions
+    {
+        [Option("-width", Required = false, HelpText = "Set output image width.", Default = 500)]
+        public int Width { get; set; }
+        
+        [Option("-height", Required = false, HelpText = "Set output image height.", Default = 500)]
+        public int Height { get; set; }
+        
+        [Option("-font", Required = false, HelpText = "Set output text font.", Default = "Times New Roman")]
+        public string Font { get; set; }
+    }
+}
---
+++ TagsCloudContainer/Program.cs
@@ -0,0 +1,12 @@
+﻿using CommandLine;
+
+namespace TagsCloudContainer
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var parsedArguments = Parser.Default.ParseArguments<CommandLineOptions>(args);
+        }
+    }
+}
