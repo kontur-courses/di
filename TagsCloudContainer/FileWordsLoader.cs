460cd252a64ca19206c3cc630ad96d702bbd1cfd
Denis Bautin <denis020301@mail.ru>
2020-11-30 21:26:20 +0500

    Create IWordsLoader interface, FileWordsLoader class and tests for it.

---
+++ TagsCloudContainer/FileWordsLoader.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace TagsCloudContainer
+{
+    public class FileWordsLoader : IWordsLoader
+    {
+        private readonly string pathToFile;
+        private readonly string[] supportedExt = {".docx", ".txt"};
+
+        public FileWordsLoader(string pathToFile)
+        {
+            if (!File.Exists(pathToFile))
+                throw new ArgumentException($"The specified file does not exist: {pathToFile}");
+            
+            if (!supportedExt.Contains(Path.GetExtension(pathToFile)))
+                throw new ArgumentException($"Format of this file isn't supported: {pathToFile}");
+
+            this.pathToFile = pathToFile;
+        }
+        
+        public string[] GetWords()
+        {
+            return Path.GetExtension(pathToFile) switch
+            {
+                ".docx" => FromDocx(),
+                ".txt" => FromTxt()
+            };
+        }
+
+        private string[] FromDocx()
+        {
+            var words = new List<string>();
+            using (WordprocessingDocument wordDocument = WordprocessingDocument.Open(pathToFile, false))
+            {
+                var paragraphs = wordDocument.MainDocumentPart.Document.Body.OfType<Paragraph>();
+                words.AddRange(paragraphs.Select(paragraph => paragraph.InnerText));
+            }
+            
+            return words.ToArray();
+        }
+
+        private string[] FromTxt()
+        {
+            return File.ReadAllLines(pathToFile);
+        }
+    }
+}
---
+++ TagsCloudContainerTests/FileWordsLoaderTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudContainer;
+
+namespace TagsCloudContainerTests
+{
+    [TestFixture]
+    public class FileWordsLoaderTests
+    {
+        [Test]
+        public void Ctor_IfFileFromPathDoesNotExist_ThrowArgumentException()
+        {
+            Action act = () => new FileWordsLoader("doesn't exist");
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void Ctor_IfFileExtensionIsNotSupported_ThrowArgumentException()
+        {
+            Action act = () => new FileWordsLoader("../../../TestFiles/test.rtf");
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [TestCase("../../../TestFiles/test.txt", TestName = "WithTxtFile")]
+        [TestCase("../../../TestFiles/test.docx", TestName = "WithDocxFile")]
+        public void GetWords_WorkWithDifferentFormatOfFiles(string path)
+        {
+            var loader = new FileWordsLoader(path);
+
+            loader.GetWords().Should().BeEquivalentTo(
+                new[] {"1", "2", "3"},
+                options => options.WithStrictOrdering());
+        }
+    }
+}
