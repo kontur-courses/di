4f7bdbc2eb5ed47b47d70f503d437588712232ba
Denis Bautin <denis020301@mail.ru>
2020-11-30 21:30:20 +0500

    Create IImageSaver interface, ImageSaver class, and tests for it

---
+++ TagsCloudContainer/ImageSaver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace TagsCloudContainer
+{
+    public class ImageSaver : IImageSaver
+    {
+        private readonly string imagePath;
+        private ImageFormat format;
+
+        public ImageSaver(string imagePath)
+        {
+            this.imagePath = imagePath;
+            format = GetImageFormat(Path.GetExtension(imagePath));
+        }
+        
+        public void Save(Bitmap image)
+        {
+            image.Save(imagePath, format);
+        }
+
+        private static ImageFormat GetImageFormat(string fileExt)
+        {
+            return fileExt switch
+            {
+                var ext when ext == ".jpeg" || ext == ".jpg" => ImageFormat.Jpeg,
+                var ext when ext == ".tiff" || ext == ".tif" => ImageFormat.Tiff,
+                ".png" => ImageFormat.Png,
+                ".gif" => ImageFormat.Gif,
+                ".bmp" => ImageFormat.Bmp,
+                _ => throw new ArgumentException($"This format of image is not supported {fileExt}")
+            };
+        }
+    }
+}
---
+++ TagsCloudContainerTests/ImageSaverTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudContainer;
+
+namespace TagsCloudContainerTests
+{
+    [TestFixture]
+    public class ImageSaverTests
+    {
+        [Test]
+        public void Ctor_IfImageFormatIsNotSupported_ThrowArgumentException()
+        {
+            Action act = () => new ImageSaver("image.blabla");
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [TestCase("image.png", TestName = "WithPng")]
+        [TestCase("image.gif", TestName = "WithGif")]
+        [TestCase("image.bmp", TestName = "WithBmp")]
+        [TestCase("image.jpg", TestName = "WithJpg")]
+        [TestCase("image.tif", TestName = "WithTiff")]
+        public void Save_CorrectWorkWithDifferentFormat(string imagePath)
+        {
+            var image = new Bitmap(100, 100);
+            var saver = new ImageSaver(imagePath);
+            
+            saver.Save(image);
+
+            File.Exists(imagePath).Should().BeTrue();
+            
+            File.Delete(imagePath);
+        }
+    }
+}
