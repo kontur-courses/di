89db26d2a3edbe1e1252e2e66d0a1b3d7953a6e0
NataashaBanan <49069370+NataashaBanan@users.noreply.github.com>
2020-11-30 19:31:35 +0500

    fix for files one string in line

--- TagCloud/LiteratureTextParser.cs
+++ TagCloud/LiteratureTextParser.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using NUnit.Framework;
 using WeCantSpell.Hunspell;
 
 namespace TagCloud
@@ -19,13 +21,21 @@ namespace TagCloud
             var pathAff = creater.GetCurrentPath() + "ru_RU.aff";
             var pathDic = creater.GetCurrentPath() + "ru_RU.dic";
             var dictionary = WordList.CreateFromFiles(pathDic, pathAff);
-            var words = File.ReadAllText(creater.GetCurrentPath() + inputFileName)
-                .Split(' ', '.', ',', ':', '!')
+            var lines = File.ReadLines(creater.GetCurrentPath() + inputFileName);
+            var words = new List<string>();
+            foreach (var line in lines)
+            {
+                words.AddRange(line.Split(' ', '.', ',', ':', '!'));
+            }
+            
+            return words
                 .Where(str => str.Length > 2)
-                .Select(word => dictionary.CheckDetails(word).Root)
+                .Select(word => dictionary.ContainsEntriesForRootWord(word)
+                    ? word
+                    : dictionary.CheckDetails(word).Root)
                 .Where(word => dictionary.ContainsEntriesForRootWord(word))
-                .Select(word => word.ToLower());
-            return words.ToArray();
+                .Select(word => word.ToLower())
+                .ToArray();
         }
     }
 }
