20a7b18567c48e0ada888429cf83235498ba5e40
Anton Nechugovskikh <nechugovskihanton@gmail.com>
2020-12-01 01:48:09 +0500

    feature     add module into TagCloud assembly

---
+++ TagCloud/TagCloudModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using TagCloud.Core.FileReaders;
+
+namespace TagCloud
+{
+    public class TagCloudModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(typeof(TxtReader).Assembly)
+                .AsImplementedInterfaces();
+        }
+    }
+}
