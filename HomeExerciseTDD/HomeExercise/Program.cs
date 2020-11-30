8a87b6ffaae63ef29f33ac21d4c117dac7eeb874
FoolLikeYou <Lambda2712@gmail.com>
2020-11-30 20:19:08 +0500

    added Program.cs

---
+++ HomeExerciseTDD/HomeExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace HomeExerciseTDD
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
