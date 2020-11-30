c821e7940d4b38e3a8560ee8cf7923828129f9b8
BandLComplete <boroviki2000@mail.ru>
2020-11-30 13:18:30 +0500

    Добавил SquarePoints генератор.

---
+++ TagsCloud/PointsLayouts/SquarePoints.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloud
+{
+    public class SquarePoints : IPointsLayout
+    {
+        private readonly HashSet<Point> generatedPoints = new HashSet<Point>{new Point()};
+        private int radius = 1;
+        
+        public IEnumerable<Point> GetPoints()
+        {
+            foreach (var point in generatedPoints)
+                yield return point;
+            
+            while (true)
+            {
+                var squarePoints = GetSquarePoints();
+                foreach (var point in squarePoints)
+                    generatedPoints.Add(point);
+                radius+=10;
+
+                foreach (var point in squarePoints)
+                    yield return point;
+            }
+        }
+
+        private HashSet<Point> GetSquarePoints()
+        {
+            var squarePoints = new HashSet<Point>();
+
+            for (var x = -radius; x <= radius; x++)
+            {
+                if (Math.Abs(x) != radius)
+                {
+                    squarePoints.Add(new Point(x, radius));
+                    squarePoints.Add(new Point(x, -radius));
+                    continue;
+                }
+                for (var y = -radius; y <= radius; y++)
+                {
+                    squarePoints.Add(new Point(x, y));
+                }
+            }
+
+            return squarePoints;
+        }
+    }
+}
