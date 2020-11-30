8ea9957c7da760063996df42a16f545637d16cfa
Denis Bautin <denis020301@mail.ru>
2020-11-30 21:35:23 +0500

    Create IFontColorCreator interface, FontColorCreator class

---
+++ TagsCloudContainer/FontColorCreator.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace TagsCloudContainer
+{
+    public class FontColorCreator : IFontColorCreator
+    {
+        private readonly Color fontColor;
+
+        public FontColorCreator(Color fontColor)
+        {
+            this.fontColor = fontColor;
+        }
+
+        public Color GetFontColor(int wordsCount, int maxWordsCount)
+        {
+            return fontColor;
+        }
+    }
+}
---
+++ TagsCloudContainer/Interfaces/IFontColorCreator.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace TagsCloudContainer
+{
+    public interface IFontColorCreator
+    {
+        Color GetFontColor(int wordsCount, int maxWordsCount);
+    }
+}
