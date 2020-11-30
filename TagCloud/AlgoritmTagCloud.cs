731c4c987eea3d0e5884df6f23791d95f71bd8b2
Mikhail Medvedev <m.medvedev99@mail.ru>
2020-11-30 16:34:41 +0500

    add AlgoritmTagCloud

---
+++ TagCloud/AlgoritmTagCloud.cs
@@ -0,0 +1,16 @@
+﻿using TagCloud.PointGetters;
+using TagCloud.WordsMetrics;
+using TagCloud.TextConverters.TextProcessors;
+
+namespace TagCloud
+{
+    public static class AlgoritmTagCloud
+    {
+        internal static TagCloud GetTagCloud(string text, IPointGetter pointGetter, 
+            ITextProcessor textProcessor, IWordsMetric wordsMetric)
+        {
+            var metric = wordsMetric.Process(textProcessor.Process(text));
+            return new TagCloud(metric, pointGetter);
+        }
+    }
+}
