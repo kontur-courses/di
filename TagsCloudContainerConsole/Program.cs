8e5b2c324c14ecf66a996f94854e93ec5a307afe
MaxCross <dan25001@gmail.com>
2020-12-01 04:27:17 +0500

    Возможность исключать слова из файла из облака

--- TagsCloudContainerConsole/Program.cs
+++ TagsCloudContainerConsole/Program.cs
@@ -2,6 +2,8 @@
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using CommandLine;
 using TagsCloudContainer;
 
@@ -14,16 +16,22 @@ namespace TagsCloudContainerConsole
             [Option('i', "input", Required = true, HelpText = "Input files or strings")]
             public IEnumerable<string> Inputs { get; set; }
             
+            [Option('e', "excluding", Default = new [] {"excluding.txt", "preps.txt", "pronouns.txt"},
+                HelpText = "Files of words that will be excluded from cloud")]
+            public IEnumerable<string> Excluding { get; set; }
+            
             [Option('o', "output", Default = "out.png", HelpText = "Output image file")]
             public string OutputFile { get; set; }
 
             [Option('w', "width", Default = 800, HelpText = "Output image width")]
             public int Width { get; set; }
+            
             [Option('h', "height", Default = 600, HelpText = "Output image height")]
             public int Height { get; set; }
             
             [Option(Default = "Times New Roman")]
             public string Font { get; set; }
+            
             [Option(Default = 32)]
             public int FontSize { get; set; }
         }
@@ -41,12 +49,24 @@ namespace TagsCloudContainerConsole
                     else container.AddFromText(inputFile);
                 }
                 
+                foreach (var excluded in options.Excluding)
+                {
+                    if (File.Exists(excluded))
+                        container.Excluding(ReadWordsFromFile(excluded));
+                }
+                
                 var output = new Bitmap(options.Width, options.Height);
                 container.Rendering(new WordRendererToImage(output)
                     .WithFont(new Font(options.Font, options.FontSize, GraphicsUnit.Pixel)));
                 container.Render();
                 output.Save(options.OutputFile, ImageFormat.Png);
             });
+        }
+
+        public static HashSet<string> ReadWordsFromFile(string fileName)
+        {
+            var text = File.ReadAllText(fileName);
+            return Regex.Matches(text, @"\b\w+\b").Cast<Match>().Select(m => m.Value).ToHashSet();
         }
     }
 }
