a52401884dfc1f96d4d90f7ef499461655ec0fae
CakeOrRiot <pozdnyakovmaks99@mail.ru>
2020-11-30 14:17:32 +0500

    Reworked CircularWordsProvider

--- TagCloud/TagCloud/WordsProvider/CircularWordsProvider.cs
+++ TagCloud/TagCloud/WordsProvider/CircularWordsProvider.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using TagCloud;
 
 namespace TagCloudTest
 {
     public class CircularWordsProvider : IWordsProvider
     {
+        public const int WordsCount = 100;
+        public List<string> words = new List<string> {"word", "another", "123", "VeryVeryLongWord", "word"};
+
         public IEnumerable<string> GetWords()
         {
-            while (true)
+            var i = 0;
+            while (i < WordsCount)
             {
-                yield return "word";
-                yield return "another word";
+                yield return words[i % words.Count];
+                i++;
             }
         }
     }
