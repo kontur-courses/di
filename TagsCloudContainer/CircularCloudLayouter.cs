8de7e8015d6429c44b08214d48923aacce63bcde
MaxCross <dan25001@gmail.com>
2020-12-01 02:43:31 +0500

    Реализация CircularCloudLayouter

---
+++ TagsCloudContainer/CircularCloudLayouter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace TagsCloudContainer
+{
+    public class CircularCloudLayouter : IWordsLayouter
+    {
+        public IEnumerable<LayoutedWord> LayoutWords(IEnumerable<LayoutedWord> words)
+        {
+            var layouter = new TagsCloudVisualization.CircularCloudLayouter(Point.Empty);
+            return words
+                .OrderByDescending(word => word.Count)
+                .Select(w => new LayoutedWord(w.Word, w.Count,
+                    layouter.PutNextRectangle(ToSize(w.Rectangle.Size))));
+        }
+
+        private Size ToSize(SizeF size) => new Size((int) Math.Ceiling(size.Width), (int) Math.Ceiling(size.Height));
+    }
+}
