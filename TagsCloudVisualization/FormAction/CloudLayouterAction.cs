efaf08ee538275fb47117aec04f07e435c895240
VladSavitskiy <cyberl1fe@yandex.ru>
2020-11-30 20:37:45 +0500

    Added CloudLayouterAction.cs into form menu

---
+++ TagsCloudVisualization/FormAction/CloudLayouterAction.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows.Forms;
+using TagsCloudVisualization.AppSettings;
+using TagsCloudVisualization.PointsGenerators;
+using TagsCloudVisualization.TagCloudBuilders;
+using TagsCloudVisualization.TagCloudVisualizers;
+
+namespace TagsCloudVisualization.FormAction
+{
+    public class CloudLayouterAction : IFormAction
+    {
+        public string Category => "Tag cloud";
+        public string Name => "Build tag cloud";
+        public string Description => "Build your tag cloud from text";
+
+        private readonly ITagCloudBuilder tagCloudBuilder;
+        private readonly ICloudVisualizer cloudVisualizer;
+        private readonly SpiralParams spiralParams;
+
+        public CloudLayouterAction(
+            ITagCloudBuilder tagCloudBuilder, ICloudVisualizer cloudVisualizer, SpiralParams spiralParams)
+        {
+            this.tagCloudBuilder = tagCloudBuilder;
+            this.cloudVisualizer = cloudVisualizer;
+            this.spiralParams = spiralParams;
+        }
+
+        public void Perform()
+        {
+            var dialog = new OpenFileDialog
+            {
+                Title = "Select file to build a tag cloud",
+                CheckFileExists = false,
+                Multiselect = false,
+                DefaultExt = "txt",
+                InitialDirectory = @"C:\Users\Public\Documents",
+                Filter = "Текстовый документ (*.txt)|*.txt;" 
+            };
+            var res = dialog.ShowDialog();
+            if (res != DialogResult.OK)
+                return;
+            SettingsForm.For(spiralParams).ShowDialog();
+            var wordsFrequency = TextHandler.TextHandler.GetWordsFrequencyFromFile(dialog.FileName);
+            var tagCloud = tagCloudBuilder.Build(wordsFrequency);
+            cloudVisualizer.PrintTagCloud(tagCloud);
+        }
+    }
+}
