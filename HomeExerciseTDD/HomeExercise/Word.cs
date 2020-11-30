db65f58ecfd8d1d59a73b4f01e69b7d05bbf4a6c
FoolLikeYou <Lambda2712@gmail.com>
2020-11-30 20:19:58 +0500

    added word classes

---
+++ HomeExerciseTDD/HomeExercise/Word.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace HomeExerciseTDD
+{
+    public class Word : IWord
+    {
+        public string Text { get; }
+        public int Frequency { get; }
+        public FontFamily Font { get; }
+        
+        public Word(string text, int frequency, FontFamily font)
+        {
+            Text = text;
+            Frequency = frequency;
+            Font = font;
+        }
+    }
+}
