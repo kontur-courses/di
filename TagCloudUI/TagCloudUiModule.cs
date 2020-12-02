3a533fd61a82824002d7fdbfc54b1b0d807dc86c
Anton Nechugovskikh <nechugovskihanton@gmail.com>
2020-12-02 21:53:54 +0500

    feature     add module into TagCloudUI assembly

---
+++ TagCloudUI/TagCloudUiModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using TagCloudUI.Infrastructure;
+
+namespace TagCloudUI
+{
+    public class TagCloudUiModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                .AsImplementedInterfaces();
+
+            builder.RegisterType<SpiralFactory>().AsImplementedInterfaces();
+            builder.Register(context => context.Resolve<ISpiralFactory>().Create())
+                .AsImplementedInterfaces().SingleInstance();
+        }
+    }
+}
