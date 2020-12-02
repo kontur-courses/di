ac6b4c1308aa1ca22e98887097ea8bab34cbfbf2
Anton Nechugovskikh <nechugovskihanton@gmail.com>
2020-12-02 23:49:10 +0500

    feature     add reader for .doc files

---
+++ TagCloud/Core/FileReaders/DocReader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Xceed.Words.NET;
+
+namespace TagCloud.Core.FileReaders
+{
+    public class DocReader : IFileReader
+    {
+        public FileExtension Extension => FileExtension.Doc;
+
+        public IEnumerable<string> ReadAllWords(string path)
+        {
+            return DocX.Load(path).Text.Split();
+        }
+    }
+}
--- TagCloud/Core/FileReaders/FileExtension.cs
+++ TagCloud/Core/FileReaders/FileExtension.cs
@@ -2,6 +2,7 @@
 {
     public enum FileExtension
     {
-        Txt
+        Txt,
+        Doc
     }
 }
