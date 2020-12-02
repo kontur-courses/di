7972c678d37fbfe74732385635f25ab9fcccc406
Artemy Gordon <artemy.gordon@gmail.com>
2020-12-02 22:59:27 +0500

    Added class 'ImageSaverProvider'

---
+++ TagsCloud/App/ImageSaverProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TagsCloud.Infrastructure;
+
+namespace TagsCloud.App
+{
+    public class ImageSaverProvider
+    {
+        private readonly IEnumerable<IImageSaver> imageSavers;
+
+        public ImageSaverProvider(IEnumerable<IImageSaver> imageSavers)
+        {
+            this.imageSavers = imageSavers;
+        }
+
+        public IImageSaver GetImageSaver(string extension)
+        {
+            return imageSavers.FirstOrDefault(x => x.Extensions.Contains(extension))
+                   ?? imageSavers.FirstOrDefault(x => x.Extensions.Contains(".png"));
+        }
+    }
+}
