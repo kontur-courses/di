9fafa55274e56c112126147060467a63d915709f
Ilya_Shumilin <i.sh-vk@yandex.ru>
2020-12-02 18:59:31 +0500

    small fix Test Project

--- TagsCloudTest/Program.cs
+++ TagsCloudTest/Program.cs
@@ -5,15 +5,11 @@ namespace TagsCloudTest
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
         }
     }
 }
