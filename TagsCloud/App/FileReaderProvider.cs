10d46d66786f97fe06435beaf84353941575c033
Artemy Gordon <artemy.gordon@gmail.com>
2020-12-02 21:21:37 +0500

    Added class 'FileReaderProvider'

---
+++ TagsCloud/App/FileReaderProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TagsCloud.Infrastructure;
+
+namespace TagsCloud.App
+{
+    public class FileReaderProvider
+    {
+        private readonly IEnumerable<IFileAllLinesReader> fileReaders;
+
+        public FileReaderProvider(IEnumerable<IFileAllLinesReader> fileReaders)
+        {
+            this.fileReaders = fileReaders;
+        }
+
+        public IFileAllLinesReader GetFileReader(string extension)
+        {
+            return fileReaders.FirstOrDefault(x => x.Extensions.Contains(extension));
+        }
+    }
+}
