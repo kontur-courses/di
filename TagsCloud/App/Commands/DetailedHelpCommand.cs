b694feeb62698dfe03002a3aca865333965419e0
Artemy Gordon <artemy.gordon@gmail.com>
2020-12-04 00:00:48 +0500

    Added help commands

---
+++ TagsCloud/App/Commands/DetailedHelpCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using TagsCloud.Infrastructure;
+
+namespace TagsCloud.App.Commands
+{
+    public class DetailedHelpCommand : ICommand
+    {
+        private readonly Lazy<IClient> client;
+        private readonly TextWriter writer;
+        public string Name { get; } = "help";
+        public DetailedHelpCommand(Lazy<IClient> client, TextWriter writer)
+        {
+            this.client = client;
+            this.writer = writer;
+        }
+
+        public string Description { get; } = "help <command>      # prints help for <command>";
+        public void Execute(string[] args)
+        {
+            var commandName = args[0];
+            var cmd = client.Value.FindCommandByName(commandName);
+            if (cmd != null)
+                writer.WriteLine(cmd.Description);
+            else
+                writer.WriteLine("Not a command " + commandName);
+        }
+    }
+}
---
+++ TagsCloud/App/Commands/HelpCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using TagsCloud.Infrastructure;
+
+namespace TagsCloud.App.Commands
+{
+    public class HelpCommand : ICommand
+    {
+        private readonly Lazy<IClient> client;
+        private readonly TextWriter writer;
+
+        public HelpCommand(Lazy<IClient> client, TextWriter writer)
+        {
+            this.client = client;
+            this.writer = writer;
+        }
+
+        public string Name { get; } = "h";
+        public string Description { get; } = "h      # prints available commands list";
+
+        public void Execute(string[] args)
+        {
+            writer.WriteLine("Available commands: " + string.Join(", ", client.Value.GetAvailableCommandName()));
+        }
+    }
+}
