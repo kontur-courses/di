e272f11d6ec1887769507f24bfd9279486342a83
NataashaBanan <49069370+NataashaBanan@users.noreply.github.com>
2020-12-04 04:16:06 +0500

    Add text reader test

---
+++ TagCloud/Tests/TextReaderTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using TagCloud.TextProcessing;
+
+namespace TagCloud.Tests
+{
+    public class TextReaderTests
+    {
+        private PathCreater creator = new PathCreater();
+        [Test]
+        public void TxtReaderTest()
+        {
+            var txtReader = new TxtTextReader();
+            txtReader.ReadStrings(creator.GetPathToFile("input.txt"))
+                .Should().HaveCount(24)
+                .And.Contain("cat")
+                .And.Contain("кошка")
+                .And.Contain("крокодил");
+        }
+
+        [Test]
+        public void DocxReaderTest()
+        {
+            var docxReader = new DocxTextReader();
+            docxReader.ReadStrings(creator.GetPathToFile("input.docx"))
+                .Where(word => word != "")
+                .Should().HaveCount(24)
+                .And.Contain("cat")
+                .And.Contain("кошка")
+                .And.Contain("крокодил");
+        }
+    }
+}
