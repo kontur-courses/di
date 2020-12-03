05965758396d7837b278065097fd9fa4d61d8a49
Ilya_Shumilin <i.sh-vk@yandex.ru>
2020-12-03 23:31:05 +0500

    feature:     - add ServiceFactory

---
+++ TagsCloud/Factory/IServiceFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TagsCloud.Factory
+{
+    public interface IServiceFactory<TService>
+    {
+        TService Create();
+        IEnumerable<string> GetServiceNames();
+        IServiceFactory<TService> Register(string serviceName, Func<TService> wordConverter);
+    }
+}
---
+++ TagsCloud/Factory/ServiceFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TagsCloud.Factory
+{
+    public abstract class ServiceFactory<TService> : IServiceFactory<TService>
+    {
+        protected readonly Dictionary<string, Func<TService>> services = new Dictionary<string, Func<TService>>();
+
+        public abstract TService Create();
+
+        public IEnumerable<string> GetServiceNames() => services.Select(pair => pair.Key);
+
+        public IServiceFactory<TService> Register(string serviceName, Func<TService> creationFunc)
+        {
+            services[serviceName] = creationFunc;
+            return this;
+        }
+    }
+}
