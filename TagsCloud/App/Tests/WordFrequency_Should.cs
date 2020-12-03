e2934ebeb858d31c8612ff4afa10123b8d4c8ddf
Artemy Gordon <artemy.gordon@gmail.com>
2020-12-04 01:33:53 +0500

    Added tests for 'WordFrequency'

---
+++ TagsCloud/App/Tests/WordFrequency_Should.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using FakeItEasy;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloud.Infrastructure;
+
+namespace TagsCloud.App.Tests
+{
+    public class WordFrequency_Should
+    {
+        private IWordChecker wordChecker;
+        private WordFrequency wordFrequency;
+        private string[] testData;
+
+        [SetUp]
+        public void SetUp()
+        {
+            testData = new[] {"ASD", "aSd", "asd", "zxc", "zxc", "qwe"};
+            wordChecker = A.Fake<IWordChecker>();
+            wordFrequency = new WordFrequency(wordChecker);
+        }
+
+        [Test]
+        public void OnGet_CallWordCheckerOnEveryElement()
+        {
+            wordFrequency.Get(testData);
+            A.CallTo(() => wordChecker.IsWordNotBoring(A<string>.Ignored))
+                .MustHaveHappenedANumberOfTimesMatching(x => x == testData.Length);
+        }
+
+        [Test]
+        public void Get_ReturnsCorrectFrequency()
+        {
+            A.CallTo(() => wordChecker.IsWordNotBoring(A<string>.Ignored)).Returns(true);
+            var expected = new Dictionary<string, double>
+            {
+                ["asd"] = Math.Round(3.0 / 6, 2),
+                ["zxc"] = Math.Round(2.0 / 6, 2),
+                ["qwe"] = Math.Round(1.0 / 6, 2)
+            };
+            wordFrequency.Get(testData).Should().BeEquivalentTo(expected);
+
+        }
+    }
+}
