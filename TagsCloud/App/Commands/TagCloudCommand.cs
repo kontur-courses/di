3cdf36e549d9fbf0f8c286333b883d977bfea133
Artemy Gordon <artemy.gordon@gmail.com>
2020-12-03 13:57:58 +0500

    Refactored

--- TagsCloud/App/Commands/TagCloudCommand.cs
+++ TagsCloud/App/Commands/TagCloudCommand.cs
@@ -8,14 +8,15 @@ namespace TagsCloud.App.Commands
 {
     public class TagCloudCommand : ICommand
     {
+        private const int MaxFontSize = 80;
+        private const int MinFontSize = 10;
         private readonly Func<Point, ILayoutAlgorithm> algorithmGenerator;
+        private readonly FileReader fileReader;
         private readonly IFontFamilyProvider fontFamilyProvider;
         private readonly IImageSizeProvider imageSizeProvider;
         private readonly Func<ILayoutAlgorithm, TagCloudLayouter> layouterGenerator;
         private readonly TagCloudPainter tagCloudPainter;
         private readonly WordFrequency wordFrequency;
-        private const int MaxFontSize = 80;
-        private const int MinFontSize = 10;
 
         public TagCloudCommand(
             TagCloudPainter tagCloudPainter,
@@ -23,7 +24,8 @@ namespace TagsCloud.App.Commands
             IImageSizeProvider imageSizeProvider,
             IFontFamilyProvider fontFamilyProvider,
             Func<Point, ILayoutAlgorithm> algorithmGenerator,
-            Func<ILayoutAlgorithm, TagCloudLayouter> layouterGenerator)
+            Func<ILayoutAlgorithm, TagCloudLayouter> layouterGenerator,
+            FileReader fileReader)
         {
             this.tagCloudPainter = tagCloudPainter;
             this.wordFrequency = wordFrequency;
@@ -31,6 +33,7 @@ namespace TagsCloud.App.Commands
             this.fontFamilyProvider = fontFamilyProvider;
             this.algorithmGenerator = algorithmGenerator;
             this.layouterGenerator = layouterGenerator;
+            this.fileReader = fileReader;
         }
 
         public string Category { get; }
@@ -41,23 +44,23 @@ namespace TagsCloud.App.Commands
         {
             var placedWords = new List<Word>();
             var graphics = Graphics.FromImage(new Bitmap(1, 1));
-            var wordFrequencies = wordFrequency.GetFromFile(args[0]);
+            var wordFrequencies = wordFrequency.Get(fileReader.ReadLines(args[0]));
             var tagCloudLayouter = GetTagCloudLayouter();
             foreach (var (word, frequency) in wordFrequencies
                 .OrderByDescending(x => x.Value))
             {
                 var font = new Font(fontFamilyProvider.FontFamily, GetFontSize(frequency));
                 var wordSize = graphics.MeasureString(word, font).ToSize();
-                var wordPoint = tagCloudLayouter.PutNextRectangle(wordSize);
-                placedWords.Add(new Word(word, font, wordPoint));
+                var wordRectangle = tagCloudLayouter.PutNextRectangle(wordSize);
+                placedWords.Add(new Word(word, font, wordRectangle));
             }
 
             tagCloudPainter.Paint(placedWords);
         }
 
-        private int GetFontSize(double wordFrequency)
+        private int GetFontSize(double frequency)
         {
-            return Math.Max((int) Math.Round(MaxFontSize * wordFrequency), MinFontSize);
+            return Math.Max((int) Math.Round(MaxFontSize * frequency), MinFontSize);
         }
 
         private TagCloudLayouter GetTagCloudLayouter()
