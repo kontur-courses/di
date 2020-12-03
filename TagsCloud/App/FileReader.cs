d9835a0b84d13dbe6924bb5d8670ee5a86b4fa34
Artemy Gordon <artemy.gordon@gmail.com>
2020-12-03 13:43:54 +0500

    Added class 'FileReader'

---
+++ TagsCloud/App/FileReader.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace TagsCloud.App
+{
+    public class FileReader
+    {
+        private readonly FileReaderProvider fileReaderProvider;
+
+        public FileReader(FileReaderProvider fileReaderProvider)
+        {
+            this.fileReaderProvider = fileReaderProvider;
+        }
+
+        public string[] ReadLines(string filePath)
+        {
+            var extension = Path.GetExtension(filePath);
+            return fileReaderProvider.GetFileReader(extension).ReadAllLines(filePath);
+        }
+    }
+}
