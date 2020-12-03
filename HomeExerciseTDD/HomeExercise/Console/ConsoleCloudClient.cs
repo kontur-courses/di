3101715aebff8034f254ebd3b352ec6b7fab61ed
FoolLikeYou <Lambda2712@gmail.com>
2020-12-03 22:24:15 +0500

    updated Console

--- HomeExerciseTDD/HomeExercise/Console/ConsoleCloudClient.cs
+++ HomeExerciseTDD/HomeExercise/Console/ConsoleCloudClient.cs
@@ -1,7 +1,82 @@
-﻿namespace HomeExerciseTDD
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Reflection;
+using Autofac;
+using Autofac.Core;
+using CommandLine;
+using HomeExerciseTDD.Options;
+using HomeExerciseTDD.settings;
+
+namespace HomeExerciseTDD
 {
     public class ConsoleCloudClient
     {
+        public void ConsoleHandler(string[] args, ContainerBuilder builder)
+        {
+            Parser.Default
+                .ParseArguments<Options.Options>(args)
+                .WithParsed(options => 
+            {
+                FilesOptionHandler(options.WordsPath, options.BoringPath, builder);
+                WordOptionHandler(options.Font, options.Coefficient, builder);
+                SpiralOptionHandler(options.CenterX, options.CenterY, builder);
+                PainterOptionHandler(options.Wight, options.Height, options.ImageName, options.Format, options.Color, builder);
+            });
+        }
+
+        private void FilesOptionHandler(string wordPath, string boringWordPath, ContainerBuilder builder)
+        {
+            builder.RegisterType<FileProcessor>().As<IFileProcessor>()
+                .WithParameters(new Parameter[] 
+                {new NamedParameter("pathWords", wordPath), 
+                    new NamedParameter("pathBoringWords", boringWordPath) });
+        }
+
+        private void WordOptionHandler(string fontText, int coefficient, ContainerBuilder builder)
+        {
+            FontFamily font;
+            try
+            { 
+                font = new FontFamily(fontText);
+            }
+            catch (ArgumentException e)
+            {
+                font = new FontFamily("Microsoft Sans Serif");
+            }
+            var wordSettings = new WordSettings(font, coefficient);
+            builder.RegisterInstance(wordSettings).As<WordSettings>();
+        }
+        
+        private void SpiralOptionHandler(int x, int y, ContainerBuilder builder)
+        {
+            var center = new Point(x,y);
+            var spiralSettings = new SpiralSettings(center);
+            builder.RegisterInstance(spiralSettings).As<SpiralSettings>();
+        }
+
+        private void PainterOptionHandler(int width, int height, string fileName, string format, KnownColor knownColor, ContainerBuilder builder)
+        {
+            var resultFormat = GetImageFormat(format);
+            var color = Color.FromKnownColor(knownColor);
+            var painterSettings = new PainterSettings(width,height,fileName,resultFormat, color);
+            builder.RegisterInstance(painterSettings).As<PainterSettings>();
+        }
+        
+        private static ImageFormat GetImageFormat(string extension)  
+        {
+            var prop = typeof(ImageFormat)
+                .GetProperties()
+                .FirstOrDefault(p => p.Name.Equals(extension, StringComparison.InvariantCultureIgnoreCase));
+
+            var result = prop != null
+                ? prop.GetValue(prop) as ImageFormat
+                : ImageFormat.Png;
+    
+            return result;  
+        }  
         
     }
 }
---
+++ HomeExerciseTDD/HomeExercise/Console/Options.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using CommandLine;
+
+namespace HomeExerciseTDD.Options
+{
+    [Verb("options", HelpText = "Input options.")]
+    public class Options
+    {
+        [Option("words", Required = true, HelpText = "Input files with words.")]
+        public string WordsPath { get; set; }
+    
+        [Option( "boring", Required = false, HelpText = "Input files with boring words.")]
+        public string BoringPath { get; set; }
+        
+        [Option("format", Default = "png",Required = false, HelpText = "Input image format.")]
+        public string Format { get; set; }
+        
+        [Option('w', "wight", Default = 3000,Required = false, HelpText = "Input image wight.")]
+        public int Wight { get; set; }
+        
+        [Option('h', "height", Default = 3000,Required = false, HelpText = "Input image height.")]
+        public int Height { get; set; }
+        
+        [Option("font",Default = "Microsoft Sans Serif",Required = false, HelpText = "Input font family.")]
+        public string Font { get; set; }
+        
+        [Option("imageName", Default = "name", Required = false, HelpText = "Input image file name.")]
+        public string ImageName { get; set; }
+        
+        [Option("color", Default = 22,Required = false, HelpText = "Input color enum.")]
+        public KnownColor Color { get; set; }
+        
+        [Option('c',"coefficient",Default = 40, Required = false, HelpText = "Input coefficient.")]
+        public int Coefficient { get; set; }
+        
+        [Option("centerX",Default = 0,Required = false, HelpText = "Input center point.")]
+        public int CenterX { get; set; }
+        [Option("centerX",Required = false, HelpText = "Input center point.")]
+        public int CenterY { get; set; }
+    }
+}
