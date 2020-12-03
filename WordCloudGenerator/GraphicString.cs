fb946f21b7bc903cc4c13eb52dfa188d9c0cbece
TerribleVoice <terriblevoice@ya.ru>
2020-12-03 17:34:04 +0500

    добавил структуры WordFrequency и GraphicString

---
+++ WordCloudGenerator/GraphicString.cs
@@ -0,0 +1,14 @@
+﻿namespace WordCloudGenerator
+{
+    public readonly struct GraphicString
+    {
+        public GraphicString(string value, float fontSize)
+        {
+            FontSize = fontSize;
+            Value = value;
+        }
+
+        public string Value { get; }
+        public float FontSize { get; }
+    }
+}
---
+++ WordCloudGenerator/WordFrequency.cs
@@ -0,0 +1,14 @@
+﻿namespace WordCloudGenerator
+{
+    public readonly struct WordFrequency
+    {
+        public WordFrequency(string word, double frequency)
+        {
+            Word = word;
+            Frequency = frequency;
+        }
+
+        public string Word { get; }
+        public double Frequency { get; }
+    }
+}
