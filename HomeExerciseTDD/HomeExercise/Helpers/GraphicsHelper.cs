cd0de3ba4684758c4c840916523ff8c0c3a6a9fd
FoolLikeYou <Lambda2712@gmail.com>
2020-12-04 13:34:29 +0500

    refactored Helpers

---
+++ HomeExerciseTDD/HomeExercise/Helpers/GraphicsHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace HomeExerciseTDD.Helpers
+{
+    public static class GraphicsHelper
+    {
+        public static Size MeasureString(string text, Font font)
+        {
+            using var image = new Bitmap(1, 1);
+            using var g = Graphics.FromImage(image);
+            var result = g.MeasureString(text, font).ToSize();
+
+            return result;
+        }
+    }
+}
