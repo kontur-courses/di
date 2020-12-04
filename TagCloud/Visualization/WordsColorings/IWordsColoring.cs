cf97180f697103cd0e6e2ad97c69a165a1740d70
Mikhail Medvedev <m.medvedev99@mail.ru>
2020-12-05 00:50:56 +0500

    add IWordsColoring

---
+++ TagCloud/Visualization/WordsColorings/IWordsColoring.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace TagCloud.Visualization.WordsColorings
+{
+    public interface IWordsColoring
+    {
+        public Color GetColor(string word, Rectangle location, TagCloud cloud);
+    }
+}
