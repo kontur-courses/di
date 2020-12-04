721417a7c8aca1e443bc15dcd69efd934dd7a1d5
Ilya_Shumilin <i.sh-vk@yandex.ru>
2020-12-04 22:59:54 +0500

    refactor:     - add CircularCloudLayouterCreator

---
+++ TagsCloud/Layouter/CircularCloudLayouterCreator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagsCloud.ImageProcessing.Config;
+
+namespace TagsCloud.Layouter
+{
+    public class CircularCloudLayouterCreator
+    {
+        private readonly ImageConfig imageConfig;
+
+        public CircularCloudLayouterCreator(ImageConfig imageConfig)
+        {
+            this.imageConfig = imageConfig;
+        }
+
+        public CircularCloudLayouter Create()
+        {
+            var center = new Point(imageConfig.ImageSize.Width / 2, imageConfig.ImageSize.Height / 2);
+            return new CircularCloudLayouter(center);
+        }
+    }
+}
