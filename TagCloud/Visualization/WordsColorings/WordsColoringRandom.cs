ad594addb8345eda2a2a51d73c105463d4f3a261
Mikhail Medvedev <m.medvedev99@mail.ru>
2020-12-05 01:01:20 +0500

    add WordsColoringRandom

---
+++ TagCloud/Visualization/WordsColorings/WordsColoringRandom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace TagCloud.Visualization.WordsColorings
+{
+    class WordsColoringRandom : IWordsColoring
+    {
+        private readonly Random random = new Random();
+        public Color GetColor(string word, Rectangle location, TagCloud cloud) =>
+            Color.FromArgb(255, random.Next(255), random.Next(255), random.Next(255));
+    }
+}
