1320e1faa467eb080f79db368382b8aee732f3b7
Sergey Uvarov <uvsean@gmail.com>
2020-12-05 17:22:26 +0500

    Modules introduced

---
+++ TagCloud/WinUI/GuiModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+
+namespace WinUI
+{
+    public class GuiModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                .AsSelf()
+                .AsImplementedInterfaces()
+                .SingleInstance()
+                .OwnedByLifetimeScope();
+        }
+    }
+}
