b9dfb7e9cee62203fd8997cc4e0026a753bff54a
Danil <danil7953@mail.ru>
2020-12-06 03:23:21 +0500

    Create  EnumerableExtensions

---
+++ TagsCloud/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TagsCloud.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IReadOnlyDictionary<T, double> GetFrequency<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable
+                .GroupBy(x => x)
+                .ToDictionary(pair => pair.Key,
+                    pair => pair.Count() / (double) enumerable.Count());
+        }
+    }
+}
