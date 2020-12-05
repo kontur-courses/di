971298a0a40097f98db96468c396b2c66122ef01
Danil <danil7953@mail.ru>
2020-12-06 03:35:40 +0500

    Refactoring BitmapSaver.cs and Delete IBitmapSaver

--- TagsCloud/BitmapSaver/BitmapSaver.cs
+++ TagsCloud/BitmapSaver/BitmapSaver.cs
@@ -1,27 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 
 namespace TagsCloud.BitmapSaver
 {
-    class BitmapSaver : IBitmapSaver
+    public static class BitmapSaver
     {
-        public void Save(Bitmap bitmap, ImageFormat format, string directoryPath = ".")
+        private static readonly Dictionary<string, ImageFormat> _supportedFormats =
+            new Dictionary<string, ImageFormat>(StringComparer.CurrentCultureIgnoreCase)
+            {
+                {"png", ImageFormat.Png},
+                {"jpeg", ImageFormat.Jpeg},
+                {"bmp", ImageFormat.Bmp},
+                {"jpg", ImageFormat.Jpeg}
+            };
+
+        public static void Save(Bitmap bitmap, string format, string directoryPath = ".")
         {
+            if (!_supportedFormats.ContainsKey(format))
+                throw new ArgumentException("This file format is not supported.");
+
             if (!Directory.Exists(directoryPath))
                 throw new ArgumentException("This directory does not exist.");
 
+            var imageFormat = _supportedFormats[format];
             var filePath = Path.GetFullPath(CreateFilePath(directoryPath, format));
 
-            bitmap.Save(filePath, ImageFormat.Png);
+            bitmap.Save(filePath, imageFormat);
+            Console.WriteLine($"The file has been saved {filePath}");
         }
 
-        private static string CreateFilePath(string directoryPath, ImageFormat format)
+        private static string CreateFilePath(string directoryPath, string format)
         {
-            var fileName = DateTime.Now.ToString("yyyyMMddhhmmss"); 
-
-            return $@"{directoryPath}\{fileName}.{format.ToString().ToLower()}";
+            var fileName = DateTime.Now.ToString("yyyyMMddhhmmss");
+            
+            return $@"{directoryPath}\{fileName}.{format.ToLower()}";
         }
     }
 }
