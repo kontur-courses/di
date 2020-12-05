7f26e68bc257b7802502472c00e48aa089a3d9c1
TerribleVoice <terriblevoice@ya.ru>
2020-12-06 02:43:21 +0500

    добавил ввод цветов и переделал UI под фабрики

---
+++ WordCloudGenerator/ColorConverterExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+
+namespace WordCloudGenerator
+{
+    public static class ColorConverterExtensions
+    {
+        public static bool CanConvertFrom(this ColorConverter converter, object value)
+        {
+            if (value == null)
+                return false;
+            try
+            {
+                converter.ConvertFrom(value);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- WordCloudGenerator/ConsoleUI.cs
+++ WordCloudGenerator/ConsoleUI.cs
@@ -1,12 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
-using Autofac;
+using System.Drawing;
+using System.Linq;
 
 namespace WordCloudGenerator
 {
     public class ConsoleUI : IUserInterface
     {
-        public void Run(IContainer container)
+        private readonly IPreparer.Factory preparerFactory;
+        private readonly IPalette.Factory paletteFactory;
+        private readonly IPainter.Factory painterFactory;
+
+        public ConsoleUI(IPreparer.Factory preparerFactory, IPalette.Factory paletteFactory, IPainter.Factory painterFactory)
+        {
+            this.preparerFactory = preparerFactory;
+            this.paletteFactory = paletteFactory;
+            this.painterFactory = painterFactory;
+        }
+
+        public void Run()
+        {
+            var prepared = PrepareWords();
+
+            var graphicStrings = GenerateCloud(prepared);
+
+            var palette = EnterPalette();
+            var painter = painterFactory(palette);
+            var img = PerformAndReport("Рисование облака", () => painter.Paint(graphicStrings));
+
+            var pathToSave = AskUntilCorrect("путь по которому сохранить изображение", Saver.IsPathCorrect,
+                "путь неверный");
+            Saver.SaveImage(img, pathToSave);
+
+            Console.WriteLine($"Изображение сохранено в {pathToSave}");
+        }
+
+        private IPalette EnterPalette()
+        {
+            var colorConverter = new ColorConverter();
+            
+            // ReSharper disable once PossibleNullReferenceException
+            // cant be null because of CanConvertFrom
+            var bgColor = (Color) colorConverter.ConvertFromString(AskUntilCorrect("цвет фона",
+                colorConverter.CanConvertFrom, "такого цвета не существует").Trim());
+
+            var colorsStr =
+                RepeatUntil(() => AskUntilCorrect("цвета палитры (пустая строка чтобы прекратить)",
+                    colorConverter.CanConvertFrom,
+                    "такого цвета не существует"), string.IsNullOrEmpty);
+            var mainColors = colorsStr.Select(colorConverter.ConvertFromString).Cast<Color>();
+
+            var palette = paletteFactory(mainColors, bgColor);
+            return palette;
+        }
+
+        private IEnumerable<GraphicString> GenerateCloud(IEnumerable<WordFrequency> prepared)
+        {
+            var algoChoice = AskUntilCorrect(
+                "какой алгоритм использовать - экспоненциальный или пропорциональный? (1/2)",
+                str => str == "1" || str == "2", "нужно выбрать 1 или 2");
+            var algorithm = AlgorithmFabric.Create((AlgorithmType) int.Parse(algoChoice));
+            return algorithm(prepared);
+        }
+
+        private IEnumerable<WordFrequency> PrepareWords()
         {
             var pathToText = Ask("полный путь к файлу с текстом");
             var text = Reader.ReadFile(pathToText);
@@ -14,31 +71,11 @@ namespace WordCloudGenerator
                 () => Ask("Введите слово которое нужно пропускать (пустая строка, чтобы закончить)"),
                 string.IsNullOrEmpty);
 
-            using var scope = container.BeginLifetimeScope();
-            var preparer = scope.Resolve<Preparer>(new NamedParameter("wordsToSkip", wordsToSkip));
-            var wordCount = int.Parse(
-                AskUntilCorrect("сколько слов должно быть в облаке", str => int.TryParse(str, out _)));
-
-            var prepared = PerformAndReport("Обработка текста", () => preparer.CreateWordFreqList(text, wordCount));
-
-            var algoChoice = AskUntilCorrect(
-                "какой алгоритм использовать - экспоненциальный или пропорциональный? (1/2)",
-                str => str == "1" || str == "2");
-
-            var algorithm = algoChoice == "1" ? GeneratorAlgorithms.Exponential : GeneratorAlgorithms.Proportional;
-
-            var generator = scope.Resolve<Generator>(new TypedParameter(algorithm.GetType(), algorithm));
-            var graphicStrings = PerformAndReport("Генерация облака слов",
-                () => generator.CalculateFontSizeForWords(prepared));
-
-            var painter = scope.Resolve<Painter>();
-
-            var img = PerformAndReport("Рисование облака", () => painter.Paint(graphicStrings));
-
-            var pathToSave = AskUntilCorrect("путь по которому сохранить изображение", Saver.IsPathCorrect);
-            Saver.SaveImage(img, pathToSave);
-
-            Print($"Изображение сохранено в {pathToSave}");
+            var preparer = preparerFactory(wordsToSkip);
+            var wordCount = int.Parse(AskUntilCorrect("сколько слов должно быть в облаке",
+                str => int.TryParse(str, out _), "это не является числом"));
+            
+            return PerformAndReport("Обработка текста", () => preparer.CreateWordFreqList(text, wordCount));
         }
 
         private string Ask(string what)
@@ -48,18 +85,12 @@ namespace WordCloudGenerator
             return Console.ReadLine();
         }
 
-        private void Print(string str)
+        private string AskUntilCorrect(string what, Func<string, bool> isCorrect, string ifErr)
         {
-            Console.WriteLine(str);
-        }
-
-        private string AskUntilCorrect(string what, Func<string, bool> isCorrect)
-        {
-            Console.WriteLine($"Введите {what}");
-            var read = Console.ReadLine();
+            var read = Ask(what);
             while (!isCorrect(read))
             {
-                Console.WriteLine("Вы ввели неправильное значение, введите снова");
+                Console.WriteLine($"{ifErr}, введите снова");
                 read = Console.ReadLine();
             }
 
@@ -83,14 +114,13 @@ namespace WordCloudGenerator
             Console.Write($"{actionName} выполняется");
             var result = func();
             ClearLine();
-            Console.Write($"{actionName} закончена");
+            Console.WriteLine($"{actionName} закончена");
             return result;
         }
 
         private void ClearLine()
         {
-            Console.SetCursorPosition(0, Console.CursorTop);
-            Console.Write(new string(' ', Console.BufferWidth));
+            Console.Write('\r' + new string(' ', Console.BufferWidth));
         }
     }
 }
