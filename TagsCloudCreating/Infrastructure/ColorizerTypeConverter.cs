0d6a7347739949d06f831ccd4d15c4982bfa91e7
AppleePie <64743297+AppleePie@users.noreply.github.com>
2020-12-07 00:35:08 +0500

    Added Added additional classes and programs for core of the algorithm

---
+++ TagsCloudCreating/Infrastructure/ColorizerTypeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using TagsCloudLayouters.Contracts;
+
+namespace TagsCloudLayouters.Infrastructure
+{
+    public class ColorizerTypeConverter : ExpandableObjectConverter
+    {
+        public override object ConvertTo(
+            ITypeDescriptorContext context,
+            CultureInfo culture,
+            object value,
+            Type destType)
+        {
+            if (destType != typeof(string) || value is not IColorizer)
+                return base.ConvertTo(context, culture, value, destType);
+            return ((IColorizer) value).Name;
+        }
+    }
+}
---
+++ TagsCloudCreating/Infrastructure/PointExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Drawing;
+
+namespace TagsCloudLayouters.Infrastructure
+{
+    internal static class PointExtension
+    {
+        public static double DistanceTo(this Point first, Point second) => Math.Sqrt(
+            (first.X - second.X) * (first.X - second.X) + (first.Y - second.Y) * (first.Y - second.Y)
+        );
+    }
+}
