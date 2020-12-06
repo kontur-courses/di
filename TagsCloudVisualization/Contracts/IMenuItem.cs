19200737fe742dc4d70d6cf939a2e4d301b182f6
AppleePie <64743297+AppleePie@users.noreply.github.com>
2020-12-07 00:37:01 +0500

    Added required contracts for menu and getting data

---
+++ TagsCloudVisualization/Contracts/IMenuItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Forms;
+
+namespace TagsCloudVisualization.Contracts
+{
+    public interface IMenuItem
+    {
+        public string MenuAffiliation { get; }
+        public string ItemName { get; }
+        public DialogResult Execute();
+    }
+}
---
+++ TagsCloudVisualization/Contracts/IWordsReader.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace TagsCloudVisualization.Infrastructure
+{
+    public interface IWordsReader
+    {
+        public IEnumerable<string> GetAllData(string source);
+    }
+}
