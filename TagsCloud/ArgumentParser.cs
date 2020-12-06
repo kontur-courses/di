30acfd4ff668f891e64ff8d315726b1412cd603f
Danil <danil7953@mail.ru>
2020-12-06 14:22:18 +0500

    Add Program.cs and ArgumentParser.cs

---
+++ TagsCloud/ArgumentParser.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+
+namespace TagsCloud
+{
+    public static class ArgumentParser
+    {
+        public static string CheckFilePath(string filePath)
+        {
+            if (!File.Exists(filePath))
+                throw new ArgumentException("File does not exist");
+
+            if (!filePath.EndsWith(".txt") && !filePath.EndsWith("docx"))
+                throw new ArgumentException("Unsupported file format");
+
+            return filePath;
+        }
+
+        public static Color GetColor(string colorName)
+        {
+            try
+            {
+                return Color.FromName(colorName);
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("Unknown color");
+            }
+        }
+
+        public static FontFamily GetFontFamily(string fontFamilyName)
+        {
+            try
+            {
+                return new FontFamily(fontFamilyName);
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("Unknown font family");
+            }
+        }
+
+        public static bool IsCorrectFormat(string format)
+        {
+            if (format == "png" || format == "jpg" || format == "jpeg" || format == "bmp")
+                return true;
+
+            throw new ArgumentException("Unsupported file format");
+        }
+
+        public static Size GetSize(string size)
+        {
+            var widthAndHeight = size.Split('x');
+
+            var result =
+                int.TryParse(widthAndHeight[0], out var width) && int.TryParse(widthAndHeight[1], out var height)
+                    ? new Size(width, height)
+                    : throw new ArgumentException("The size is set incorrectly");
+
+            return result;
+        }
+    }
+}
--- TagsCloud/Program.cs
+++ TagsCloud/Program.cs
@@ -1,24 +1,109 @@
 using System;
-using System.Collections;
-using WeCantSpell.Hunspell;
-using System.Windows.Forms;
-using Autofac;
+using System.Drawing;
+using System.Linq;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+using TagsCloud.BitmapCreator;
+using TagsCloud.ColoringAlgorithms;
+using TagsCloud.FileReaders;
+using TagsCloud.ImageConfig;
+using TagsCloud.LayoutAlgorithms;
+using TagsCloud.PointGenerator;
+using TagsCloud.WordFilters;
 
 namespace TagsCloud
 {
-    static class Program
+    public static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        private static string _pathToInputFile;
+        private static Color _backgroundColor;
+        private static Color _textColor;
+        private static FontFamily _fontFamily;
+        private static string _format;
+        private static Size _size;
+        private static IServiceProvider serviceProvider;
+
+        public static int Main(string[] args)
         {
-            // TODO здесь по идее должна быть выборка Ui.
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            var app = new CommandLineApplication();
+
+            app.HelpOption();
+
+            var input = app.Option("-i|--input <INPUT>", "Input file path", CommandOptionType.SingleValue);
+            var backgroundColor = app.Option<Color>("-b|--bgcolor <BGCOLOR>", "Background color",
+                CommandOptionType.SingleValue);
+            var textColor = app.Option<Color>("-t|--tagcolor <TAGCOLOR>", "Tags color", CommandOptionType.SingleValue);
+            var font = app.Option("-f|--font <FONT>", "Font family", CommandOptionType.SingleValue);
+            var format = app.Option("-r|--format <FORMAT>", "Result image format", CommandOptionType.SingleValue);
+            var size = app.Option("-s|--size <SIZE>", "Result image size", CommandOptionType.SingleValue);
+
+            app.OnExecute(() =>
+                {
+                    _pathToInputFile = ArgumentParser.CheckFilePath(input.Value());
+
+                    _backgroundColor = backgroundColor.HasValue()
+                        ? ArgumentParser.GetColor(backgroundColor.Value())
+                        : Color.Bisque;
+
+                    _textColor = textColor.HasValue()
+                        ? ArgumentParser.GetColor(textColor.Value())
+                        : Color.Aquamarine;
+
+                    _fontFamily = font.HasValue()
+                        ? ArgumentParser.GetFontFamily(font.Value())
+                        : new FontFamily("Arial");
+
+                    _format = format.HasValue() && ArgumentParser.IsCorrectFormat(format.Value())
+                        ? format.Value()
+                        : "png";
+
+                    _size = size.HasValue()
+                        ? ArgumentParser.GetSize(size.Value())
+                        : new Size(1200, 1200);
+
+                    return 0;
+                }
+            );
+
+            app.Execute(args);
+
+            ConfigureServices();
+
+            var text = serviceProvider.GetService<IFileReader>()?.GetWordsFromFile(_pathToInputFile);
+            var words = serviceProvider.GetService<IWordFilter>()?.FilterWords(text).Select(x => x.ToLower());
+            var image = serviceProvider.GetService<IBitmapCreator>()?.Create(words);
+            BitmapSaver.BitmapSaver.Save(image, _format);
+
+            return 0;
+        }
+
+        private static void ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<IWordFilter>(new PartsOfSpeechFilter(PartsOfSpeech.ADVPRO, PartsOfSpeech.APRO,
+                PartsOfSpeech.CONJ));
+            services.AddSingleton<ILayoutAlgorithm>(
+                new CircularCloudLayouter(new Point(_size.Width / 2, _size.Height / 2)));
+            services.AddSingleton<IColoringAlgorithm>(new DefaultColoringAlgorithm(_textColor));
+            services.AddSingleton<IImageConfig>(new ImageConfig.ImageConfig(_size, _fontFamily, _backgroundColor,
+                new DefaultColoringAlgorithm(_textColor)));
+            services.AddSingleton<IPointGenerator, ArchimedeanSpiral>();
+            services.AddSingleton(ChooseReader(services));
+            services.AddSingleton<IBitmapCreator, BitmapCreator.BitmapCreator>();
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        private static IFileReader ChooseReader(ServiceCollection serviceCollection)
+        {
+            if (_pathToInputFile.EndsWith(".txt"))
+                return new TxtFileReader();
+
+            if (_pathToInputFile.EndsWith(".docx"))
+                return new DocxFileReader();
+
+            throw new ArgumentException("Unsupported file format");
         }
     }
 }
+
