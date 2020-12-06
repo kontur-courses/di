ee43f31a3b06c8b749ac520899beef1483645aab
Danil <danil7953@mail.ru>
2020-12-06 14:15:16 +0500

    Add coloring algorithms

---
+++ TagsCloud/ColoringAlgorithms/DefaultColoringAlgorithm.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace TagsCloud.ColoringAlgorithms
+{
+    class DefaultColoringAlgorithm : IColoringAlgorithm
+    {
+        private Color _color;
+        public DefaultColoringAlgorithm(Color color)
+        {
+            _color = color;
+        }
+        public Color GetNextColor()
+        {
+            return _color;
+        }
+    }
+}
---
+++ TagsCloud/ColoringAlgorithms/RandomColoringAlgorithm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+
+namespace TagsCloud.ColoringAlgorithms
+{
+    public class RandomColoringAlgorithm : IColoringAlgorithm
+    {
+        private Random random = new Random();
+        public Color GetNextColor()
+        {
+            return Color.FromArgb(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256));
+        }
+    }
+}
