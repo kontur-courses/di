c6340dbda33864d3a6cec4f0da72a874550197ef
Danil <danil7953@mail.ru>
2020-12-06 23:22:47 +0500

    Refactoring EnumerableExtensions

--- TagsCloud/Extensions/EnumerableExtensions.cs
+++ TagsCloud/Extensions/EnumerableExtensions.cs
@@ -7,10 +7,11 @@ namespace TagsCloud.Extensions
     {
         public static IReadOnlyDictionary<T, double> GetFrequency<T>(this IEnumerable<T> enumerable)
         {
-            return enumerable
+            var list = enumerable.ToList();
+            return list
                 .GroupBy(x => x)
                 .ToDictionary(pair => pair.Key,
-                    pair => pair.Count() / (double) enumerable.Count());
+                    pair => pair.Count() / (double) list.Count);
         }
     }
 }
