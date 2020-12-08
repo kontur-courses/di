0d81c37562417eaa9d368c87c8994b970c8d5e4c
catsAndSuicide <anisa.hamidullina00@gmail.com>
2020-12-09 00:17:31 +0500

    added tests

---
+++ TagsCloudContainerTests/DataReaderTests.cs
@@ -0,0 +1,74 @@
+﻿using TagsCloudContainer.Infrastructure.DataReader;
+using TagsCloudContainer.App.Settings;
+using System;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudContainer.App.DataReader;
+
+namespace TagsCloudContainerTests
+{
+    internal class DataReaderTests
+    {
+        private readonly string txtFilePath = Path.Combine(Directory.GetCurrentDirectory(),
+            "..", "..", "..", "files", "input.txt");
+        private readonly string docxFilePath = Path.Combine(Directory.GetCurrentDirectory(),
+            "..", "..", "..", "files", "input.docx");
+        private readonly IDataReaderFactory dataReaderFactory;
+        private readonly InputSettings inputSettings;
+
+        public DataReaderTests()
+        {
+            dataReaderFactory = new DataReaderFactory(InputSettings.Instance);
+            inputSettings = InputSettings.Instance;
+        }
+
+        [Test]
+        public void DataReaderFactory_ShouldCreateTxtReader()
+        {
+            DataReaderFactory_ShouldCreateReaderForFile(txtFilePath);
+        }
+
+        [Test]
+        public void DataReaderFactory_ShouldCreateDocxReader()
+        {
+            DataReaderFactory_ShouldCreateReaderForFile(docxFilePath);
+        }
+
+        [Test]
+        public void DataReaderFactory_ShouldThrowNotImplementedException_IfInputFileIsWithInvalidExtension()
+        {
+            inputSettings.InputFileName = "file.png";
+            Func<IDataReader> func = () => dataReaderFactory.CreateDataReader();
+            func.Should().Throw<NotImplementedException>();
+        }
+
+        [Test]
+        public void TxtReader_ShouldReadLines()
+        {
+            Reader_ShouldReadLinesFromFile(txtFilePath, new[] { "Это", "Txt", "Файл" });
+        }
+
+        [Test]
+        public void DocxReader_ShouldReadLines()
+        {
+            Reader_ShouldReadLinesFromFile(docxFilePath, new[] {"Это", "Docx", "Файл", ""});
+        }
+
+        private void Reader_ShouldReadLinesFromFile(string filePath, string[] lines)
+        {
+            inputSettings.InputFileName = filePath;
+            var docxReader = dataReaderFactory.CreateDataReader();
+            docxReader.ReadLines().ToArray().Should().BeEquivalentTo(lines);
+        }
+
+        private void DataReaderFactory_ShouldCreateReaderForFile(string filePath)
+        {
+            inputSettings.InputFileName = filePath;
+            Func<IDataReader> func = () => dataReaderFactory.CreateDataReader();
+            func.Should().NotThrow();
+            func.Invoke().Should().NotBeNull();
+        }
+    }
+}
---
+++ TagsCloudContainerTests/TextAnalyzerTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using TagsCloudContainer.App;
+using TagsCloudContainer.App.Settings;
+using TagsCloudContainer.App.TextAnalyzer;
+using TagsCloudContainer.Infrastructure.TextAnalyzer;
+using YandexMystem.Wrapper;
+using YandexMystem.Wrapper.Enums;
+
+namespace TagsCloudContainerTests
+{
+    class TextAnalyzerTests
+    {
+        private readonly ITextAnalyzer textAnalyzer;
+        private readonly Mysteam mysteam;
+        private readonly FilteringWordsSettings filteringSettings;
+
+        public TextAnalyzerTests()
+        {
+            var serviceProvider = Program.GetAppServiceProvider();
+            textAnalyzer = serviceProvider.GetRequiredService<ITextAnalyzer>();
+            mysteam = serviceProvider.GetRequiredService<Mysteam>();
+            filteringSettings = FilteringWordsSettings.Instance;
+        }
+
+        [Test]
+        public void LiteratureTextParser_ShouldParseTextToWords()
+        {
+            new LiteratureTextParser()
+                .GetWords(new[] {"строка с символами, словами и не 6675 словами"})
+                .ToArray()
+                .Should()
+                .BeEquivalentTo("строка", "с", "символами", "словами", "и", "не", "словами");
+        }
+
+        [TestCase(GramPartsEnum.Conjunction, "и")]
+        [TestCase(GramPartsEnum.Interjection, "ах")]
+        [TestCase(GramPartsEnum.NounPronoun, "он")]
+        public void PartOfSpeechFilter_ShouldFilterPOS_IfPOSAreBoring(GramPartsEnum gramPart, string word)
+        {
+            filteringSettings.BoringGramParts = new[] {gramPart}.ToImmutableHashSet();
+            new PartOfSpeechFilter(filteringSettings, mysteam).IsBoring(word).Should().BeTrue();
+        }
+
+        [TestCase(GramPartsEnum.Conjunction, "и")]
+        [TestCase(GramPartsEnum.Interjection, "ах")]
+        [TestCase(GramPartsEnum.NounPronoun, "он")]
+        public void PartOfSpeechFilter_ShouldNotFilterPOS_IfPOSAreNotBoring(GramPartsEnum gramPart, string word)
+        {
+            filteringSettings.BoringGramParts = filteringSettings.BoringGramParts.Except(new[] {gramPart});
+            new PartOfSpeechFilter(filteringSettings, mysteam).IsBoring(word).Should().BeFalse();
+        }
+
+        [TestCase("слова", "слово")]
+        [TestCase("слов", "слово")]
+        [TestCase("слову", "слово")]
+        [TestCase("словам", "слово")]
+        public void ToInitialFormNormalizer_ShouldReturnInitialFormOfWord(string word, string initialForm)
+        {
+            new ToInitialFormNormalizer(mysteam).NormalizeWord(word).Should().BeEquivalentTo(initialForm);
+        }
+
+        [Test]
+        public void TextAnalyzer_ShouldReturnCorrectFrequencyDictionary()
+        {
+            var text = new[] {"слова", "слово", "количество"};
+            textAnalyzer
+                .GenerateFrequencyDictionary(text)
+                .Should()
+                .HaveCount(2)
+                .And.ContainKeys("слово", "количество").And.ContainValues(2.0 / 3, 1.0 / 3);
+        }
+    }
+}
