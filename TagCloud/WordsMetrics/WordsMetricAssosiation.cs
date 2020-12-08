dc554b0223f6aa4cd06731924930a2dc6bbd94dc
Mikhail Medvedev <m.medvedev99@mail.ru>
2020-12-08 14:50:57 +0500

    add WordsMetricAssosiation

---
+++ TagCloud/WordsMetrics/WordsMetricAssosiation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace TagCloud.WordsMetrics
+{
+    public static class WordsMetricAssosiation
+    {
+        private static readonly Dictionary<string, IWordsMetric> metrics =
+            new Dictionary<string, IWordsMetric>
+            {
+                ["count"] = new CountWordMetric()
+            };
+
+        public static IWordsMetric GetMetric(string name) => 
+            metrics.TryGetValue(name, out var metric) ? metric : null;
+    }
+}
