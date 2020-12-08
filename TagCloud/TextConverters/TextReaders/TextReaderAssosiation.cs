620b481b02127fae3fedea2edd6d9f188dd91488
Mikhail Medvedev <m.medvedev99@mail.ru>
2020-12-08 13:47:00 +0500

    add TextReaderAssosiation

---
+++ TagCloud/TextConverters/TextReaders/TextReaderAssosiation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace TagCloud.TextConverters.TextReaders
+{
+    public static class TextReaderAssosiation
+    {
+        private static readonly Dictionary<string, ITextReader> textReaders =
+            new Dictionary<string, ITextReader>
+            {
+                [".txt"] = new TextReaderTxt()
+            };
+
+        public static ITextReader GetTextReader(string path) =>
+            textReaders.TryGetValue(path[path.LastIndexOf('.')..path.Length], out var reader) ? reader : null;
+    }
+}
