a6fc248ca46d1a9fdb7b2f80d406be7eef6534f6
Artemy Gordon <artemy.gordon@gmail.com>
2020-12-11 18:32:37 +0500

    Added setters for properties

--- TagsCloud/App/ImageSize.cs
+++ TagsCloud/App/ImageSize.cs
@@ -1,8 +1,32 @@
-﻿namespace TagsCloud.App
+﻿using System;
+
+namespace TagsCloud.App
 {
     public class ImageSize
     {
-        public int Width { get; set; } = 600;
-        public int Height { get; set; } = 600;
+        private int height = 600;
+        private int width = 600;
+
+        public int Width
+        {
+            get => width;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("", $"{nameof(Width)} must be positive");
+                width = value;
+            }
+        }
+
+        public int Height
+        {
+            get => height;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("", $"{nameof(Height)} must be positive");
+                height = value;
+            }
+        }
     }
 }
