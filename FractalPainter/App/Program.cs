c243a6156cbf3597f9b91f1f8e43fe49c2b4b1fe
Kirill Boldyrev <bke@skbkontur.ru>
2021-04-28 06:31:40 +0500

    1. Get MainFrom via DI. Get IUiAction[] via DI. Remove MainForm constructor with no parametres

--- FractalPainter/App/Program.cs
+++ FractalPainter/App/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
+using FractalPainting.App.Actions;
+using FractalPainting.Infrastructure.UiActions;
 using Ninject;
 
 namespace FractalPainting.App
@@ -19,9 +21,16 @@ namespace FractalPainting.App
                 // start here
                 // container.Bind<TService>().To<TImplementation>();
 
+                container.Bind<Form>().To<MainForm>();
+                container.Bind<IUiAction>().To<SaveImageAction>(); // is it possible to use IUiAction[] here?
+                container.Bind<IUiAction>().To<DragonFractalAction>();
+                container.Bind<IUiAction>().To<KochFractalAction>();
+                container.Bind<IUiAction>().To<ImageSettingsAction>();
+                container.Bind<IUiAction>().To<PaletteSettingsAction>();
+
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new MainForm());
+                Application.Run(container.Get<Form>());
             }
             catch (Exception e)
             {
