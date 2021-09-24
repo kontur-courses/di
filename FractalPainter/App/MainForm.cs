768a67c08b20591a24e01b95c141abfbc55e8bdd
sit@332lab.ru <sit@332lab.ru>
2021-09-24 11:57:47 +0700

    Изменил зависимость от SettingsManager на зависимость сразу от ImageSettings

--- FractalPainter/App/MainForm.cs
+++ FractalPainter/App/MainForm.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using FractalPainting.Infrastructure.Common;
 using FractalPainting.Infrastructure.UiActions;
 
 namespace FractalPainting.App
 {
     public class MainForm : Form
     {
-        public MainForm(IUiAction[] actions, SettingsManager settingsManager, PictureBoxImageHolder pictureBox)
+        public MainForm(IUiAction[] actions, ImageSettings imageSettings, PictureBoxImageHolder pictureBox)
         {
-            var imageSettings = settingsManager.Load().ImageSettings;
             ClientSize = new Size(imageSettings.Width, imageSettings.Height);
 
             var mainMenu = new MenuStrip();
