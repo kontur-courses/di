b3d77c762b81782ef4548fb156667c5436e1a37b
FreeTrally <serega2012111@gmail.com>
2021-12-03 19:36:20 +0500

    FIrst interfaces and infrastructure

---
+++ TagsCloudContainer/Infrastructure/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace TagsCloudContainer.Infrastructure
+{
+    public class Settings
+    {
+        public Color PrimaryColor { get; set; }
+        public Color BackgroundColor { get; set; }
+        public Font TextFont { get; set; }
+        public Size ImageSize { get; set; }
+    }
+}
---
+++ TagsCloudContainer/Interfaces/IParser.cs
@@ -0,0 +1,6 @@
+﻿namespace TagsCloudContainer.Interfaces;
+
+public interface IParser
+{
+    IEnumerable<string> Parse(string path);
+}
