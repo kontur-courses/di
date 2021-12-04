29b2218e7be52356befc74413aeff8b9031b0835
Georgy Grantsev <mrxenonmr@gmail.com>
2021-12-04 21:54:10 +0500

    Очепятки 2

--- TagCloudContainer/Infrastructure/Common/AppSettings.cs
+++ TagCloudContainer/Infrastructure/Common/AppSettings.cs
@@ -10,15 +10,15 @@ public class AppSettings : IAppSettings
     [Option('h', "imageHeight", Default = 1000, HelpText = "Height of output image")]
     public int ImageHeight { get; set; } = 1000;
 
-    [Option('i', "inputFile", Required = true, HelpText = "Path of input text")]
+    [Option('i', "inputPath", Required = true, HelpText = "Path of input text")]
     public string InputPath { get; set; } = "input.txt";
 
-    [Option('o', "inputPath", Default = "output", HelpText = "Path of output image")]
+    [Option('o', "outputPath", Default = "output", HelpText = "Path of output image")]
     public string OutputPath { get; set; } = "output.png";
 
-    [Option('e', "outputExtension", Default = "png", HelpText = "Extension of output image")]
+    [Option('e', "outputFormat", Default = "png", HelpText = "Extension of output image")]
     public string OutputFormat { get; set; } = "png";
 
-    [Option('f', "textFont", Default = "Roboto", HelpText = "Font used to display words")]
+    [Option('f', "fontName", Default = "Roboto", HelpText = "Font used to display words")]
     public string FontName { get; set; } = "Roboto";
 }
