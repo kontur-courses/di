585e6830be5f8796b19146b850117cb2ccdb94d9
gODealOAple <maksim.math@gmail.com>
2021-12-04 18:58:50 +0500

    Add square calculator method

--- TagsCloud/TagsCloudVisualization.Tests/Utils/SquareCalculator.cs
+++ TagsCloud/TagsCloudVisualization.Tests/Utils/SquareCalculator.cs
@@ -1,17 +1,14 @@
 ﻿using System;
+using System.Drawing;
 
 namespace TagsCloudVisualization.Tests
 {
     public static class SquareCalculator
     {
-        public static double CalculateCircleSquare(double radius)
-        {
-            return Math.PI * radius * radius;
-        }
+        public static double CalculateCircleSquare(double radius) => Math.PI * radius * radius;
 
-        public static double CalculateRectangleSquare(double width, double height)
-        {
-            return width * height;
-        }
+        public static double CalculateRectangleSquare(double width, double height) => width * height;
+
+        public static double CalculateRectangleSquare(Size size) => CalculateRectangleSquare(size.Width, size.Height);
     }
 }
