8260fb2991845bc04d0dc141b90a246ba12015da
gODealOAple <maksim.math@gmail.com>
2021-12-05 18:42:12 +0500

    Add integration test

---
+++ TagsCloud/TagsCloudVisualization.Tests/IntegrationTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Autofac;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization.Tests
+{
+    [TestFixture]
+    public class IntegrationTests
+    {
+        private const string ImageFile = "TagsCloud";
+
+        private readonly TagsCloudDrawerModuleSettings _settings = new()
+        {
+            WordsFile = Path.Combine(Directory.GetCurrentDirectory(), "Words.txt"),
+            BoredWords = new[] { "a", "b", "c" }
+        };
+
+        [OneTimeSetUp]
+        public void BeforeAll()
+        {
+            File.Create(_settings.WordsFile).Close();
+        }
+
+        [OneTimeTearDown]
+        public void AfterAll()
+        {
+            File.Delete(_settings.WordsFile);
+        }
+
+        [Test]
+        public void TagsCloudVisualisation_ShouldCreateImageWithTagsCloud()
+        {
+            var uniqueWords = new[]
+                {
+                    "A",
+                    "B",
+                    "C"
+                }.Concat(Enumerable.Range(0, 100).Select(i => $"Word{i}"))
+                 .ToArray();
+            var generated = GenerateWordsList(uniqueWords, 1000);
+            File.WriteAllLines(_settings.WordsFile, generated);
+
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new TagsCloudDrawerModule(_settings));
+            var container = builder.Build();
+
+            var visualizer = container.Resolve<TagsCloudVisualizer>();
+            visualizer.Visualize(ImageFile);
+        }
+
+        private static IEnumerable<string> GenerateWordsList(IList<string> uniqueWords, int count)
+        {
+            var rnd = new Random();
+            return Enumerable.Range(0, count).Select(_ => uniqueWords[rnd.Next(uniqueWords.Count)]);
+        }
+    }
+}
