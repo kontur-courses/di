421b89f5796b2ca4b8e51effed35fbe4c39db635
Dmitry Morozov <libertariansessions@gmail.com>
2021-12-05 14:13:57 +0500

    Удалено лишнее, создан новый solution

--- FractalPainter/Solved/Step11/App/Fractals/IDragonPainterFactory.cs
+++
@@ -1,7 +0,0 @@
-namespace FractalPainting.Solved.Step11.App.Fractals
-{
-    public interface IDragonPainterFactory
-    {
-        DragonPainter Create(DragonSettings settings);
-    }
-}
---
+++ TagCloud2/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TagCloud2
+{
+    public class Class1
+    {
+    }
+}
