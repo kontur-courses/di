0d540ea33308377f306731957b89a9302f14cd50
DaGnYMilleR <danil.lunyow@mail.ru>
2021-12-05 17:04:19 +0500

    add image saver and save settings

---
+++ Homework/TagsCloud.Visualization/ImagesSaver/ImageSaver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace TagsCloud.Visualization.ImagesSaver
+{
+    public class ImageSaver : IImageSaver
+    {
+        private readonly SaveSettings settings;
+
+        public ImageSaver(SaveSettings settings)
+        {
+            this.settings = settings;
+        }
+
+        public void Save(Image image)
+        {
+            if (!Directory.Exists(settings.OutputDirectory))
+                throw new Exception($"directory {settings.OutputDirectory} not found");
+            
+            var path = Path.Combine(settings.OutputDirectory, $"{settings.OutputFileName}.{settings.Extension}");
+            image.Save(path);
+        } 
+    }
+}
---
+++ Homework/TagsCloud.Visualization/ImagesSaver/SaveSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace TagsCloud.Visualization.ImagesSaver
+{
+    public class SaveSettings
+    {
+        public string OutputDirectory { get; set; }
+        public string OutputFileName { get; set; }
+        public string Extension { get; set; }
+    }
+}
