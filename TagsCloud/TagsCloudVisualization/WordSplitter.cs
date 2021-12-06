e9d46c65a11c9c7efe3bf22b1805e3cfd867d9ab
gODealOAple <maksim.math@gmail.com>
2021-12-07 02:37:42 +0500

    Add word splitter

---
+++ TagsCloud/TagsCloudVisualization/WordSplitter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TagsCloudVisualization
+{
+    public static class WordSplitter
+    {
+        public static IEnumerable<string> Split(string input)
+        {
+            var matches = Regex.Matches(input, @"\b[\w']*\b");
+            return matches.Where(m => !string.IsNullOrEmpty(m.Value)).Select(m => TrimSuffix(m.Value));
+        }
+
+        private static string TrimSuffix(string word)
+        {
+            var apostropheLocation = word.IndexOf('\'');
+            if (apostropheLocation != -1) word = word[..apostropheLocation];
+            return word;
+        }
+    }
+}
