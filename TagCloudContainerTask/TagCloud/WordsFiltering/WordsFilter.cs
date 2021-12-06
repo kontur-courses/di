519df39c3609c089beae8c92d86288827ed102eb
SquirrelLeonid <rm200002@yandex.ru>
2021-12-06 18:54:23 +0500

    Added simplest words filter

---
+++ TagCloudContainerTask/TagCloud/WordsFiltering/WordsFilter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TagCloud.WordsFiltering
+{
+    public class WordsFilter
+    {
+        public List<string> FilterWords(List<string> words)
+        {
+            return words.Where(word => word.Length > 3).ToList();
+        }
+    }
+}
---
+++ TagCloudContainerTask/TagCloud/WordsPreprocessing/WordPreprocessor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TagCloud.WordsPreprocessing
+{
+    public class WordPreprocessor
+    {
+        public List<string> LeadingWordsToLower(List<string> words)
+        {
+            return words.Select(word => word.ToLower()).ToList();
+        }
+    }
+}
