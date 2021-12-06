bc2f882127bbe7e6282ce79a40b0e644f29bc865
Pavel_Lediankin <a.lediankin@yandex.ru>
2021-12-06 21:26:19 +0500

    Helper
    (для внутреннего использования)

---
+++ TagWriterHelper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TagWriterHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Helper - чтобы не заполнять файлы с тэгами вручную
+            var path = "...";
+            Writer.Write(path, GetTags());
+        }
+
+        static IEnumerable<Tag> GetTags()
+        {
+            //....
+            yield break;
+        }
+    }
+}
---
+++ TagWriterHelper/Writer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TagsCloudContainer.Common;
+
+namespace TagWriterHelper
+{
+    public static class Writer
+    {
+        public static void Write(string path, IEnumerable<Tag> tags)
+        {
+            var serializer = new Serializer();
+            var storage = new FileBlobStorage();
+            var str = GetTagsToString(tags);
+            var data = serializer.Serialize(str);
+            storage.Set(path, data);
+        }
+
+        private static string GetTagsToString(IEnumerable<Tag> tags) 
+            => string.Join("\n", GetWords(tags));
+
+        private static IEnumerable<string> GetWords(IEnumerable<Tag> tags)
+            => tags.SelectMany(tag => tag.GetWords());
+    }
+}
