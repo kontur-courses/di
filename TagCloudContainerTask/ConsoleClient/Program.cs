08f87786424d217bc9ad6227ac48178cb071b45a
SquirrelLeonid <rm200002@yandex.ru>
2021-12-06 15:20:44 +0500

    Added project for console client

---
+++ TagCloudContainerTask/ConsoleClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleClient
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
