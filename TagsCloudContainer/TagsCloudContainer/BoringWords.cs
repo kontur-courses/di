db5b35e76f6465a76b202e3e8354ed2525ca2ff9
KirillKalutsky <kefirchik.2000@mail.ru>
2021-12-08 20:44:44 +0500

    вынес скучные слова в отдельный класс

---
+++ TagsCloudContainer/TagsCloudContainer/BoringWords.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainer
+{
+    public static class BoringWords
+    {
+        public static readonly List<string> Prepositions = new List<string>()
+        {
+            "без", "безо", "близ", "в", "во", "вместо", "вне","для", "до", "за", "из", "изо",
+            "из-за", "из-под", "к", "ко", "кроме", "между", "меж", "на", "над", "надо", "о", "об", "обо",
+            "от", "ото", "перед", "передо", "пред", "предо", "пo", "под", "подо", "при", "про", "ради", "с", "со",
+            "сквозь", "среди", "у", "через", "чрез"
+        };
+
+        public static readonly List<string> Pronouns = new List<string>()
+        {
+            "я", "ты", "вы", "мы", "он", "оно", "она", "они", "себя", "кто", "что", "кто-то", "что-то", "кто-нибудь",
+            "что-нибудь", "кто-либо", "что-либо", "кое-что", "кое-кто", "некто", "нечто", "никто", "ничто", "некого", "нечего"
+        };
+    }
+}
