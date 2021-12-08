dc05e35cf8b0b9e5a20f55fc03caa17d38fdabed
MaxxLorge <MaxxLorge@gmail.com>
2021-12-08 12:08:55 +0500

    Добавил класс создание частотного словаря

---
+++ Homework/TagsCloudContainer/WordsFrequencyAnalyzers/IWordsFrequencyAnalyzer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace TagsCloudContainer.WordsFrequencyAnalyzer
+{
+    public interface IWordsFrequencyAnalyzer
+    {
+        public Dictionary<string, int> GetWordsFrequency(IEnumerable<string> words);
+    }
+}
---
+++ Homework/TagsCloudContainer/WordsFrequencyAnalyzers/WordsFrequencyAnalyzer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TagsCloudContainer.WordsFrequencyAnalyzer;
+
+namespace TagsCloudContainer.WordsFrequencyAnalyzers
+{
+    public class WordsFrequencyAnalyzer : IWordsFrequencyAnalyzer
+    {
+        public Dictionary<string, int> GetWordsFrequency(IEnumerable<string> words)
+        {
+            var freqDict = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                if (!freqDict.ContainsKey(word))
+                    freqDict[word] = 1;
+                else
+                    freqDict[word] += 1;
+            }
+
+            return freqDict;
+        }
+    }
+}
