b65529bc9f4858d7f7c75e464b1084c7363852bc
KirillKalutsky <kefirchik.2000@mail.ru>
2021-12-08 20:43:43 +0500

    решение

---
+++ TagsCloudContainer/TagsCloudContainerTest/LineByLineReaderTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainerTest
+{
+    public class LineByLineReaderTest
+    {
+        [Test]
+        public void LengthShouldBe()
+        {
+
+        }
+    }
+}
