479d002d502f2e2910ae6d6c69a09fe288e8dd3d
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2021-12-08 21:18:43 +0500

    add image saver

---
+++ TagsCloudVisualization/Saver/IImageSaver.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization.Saver
+{
+    public interface IImageSaver
+    {
+        void Save(Image image);
+    }
+}
---
+++ TagsCloudVisualization/Saver/ImageSaver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace TagsCloudVisualization.Saver
+{
+    public class ImageSaver:  IImageSaver
+    {
+        private readonly string directory;
+        private readonly string imageName;
+
+        public ImageSaver(string directory, string imageName)
+        {
+            this.directory = directory;
+            if (!Directory.Exists(directory))
+                throw new ArgumentException($"No such directory {directory}");
+            this.imageName = imageName;
+        }
+
+        public void Save(Image image)
+        {
+            var pathToDirectory = Path.Combine(Directory.GetCurrentDirectory(), directory);
+            var path = Path.Combine(pathToDirectory, imageName);
+            image.Save(path, ImageFormat.Bmp);
+        }
+    }
+}
