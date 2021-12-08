ff8f3c2ad5bf04af9d18a9e95a56ff935734fb38
KirillKalutsky <kefirchik.2000@mail.ru>
2021-12-08 21:09:29 +0500

    классы для чтения слов из файла

---
+++ TagsCloudContainer/TagsCloudContainer/TextReaders/LineByLineWordReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainer
+{
+    public class LineByLineWordReader : IWordReader
+    {
+        public LineByLineWordReader()
+        {
+
+        }
+
+        public IEnumerable<string> Read(string pathToFile)
+        {
+            return System.IO.File.ReadLines(pathToFile);
+        }
+    }
+}
