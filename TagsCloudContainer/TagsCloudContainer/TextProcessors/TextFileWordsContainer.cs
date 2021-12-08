86f269fa15061b6f23401801c35ab512199f401f
KirillKalutsky <kefirchik.2000@mail.ru>
2021-12-08 21:07:13 +0500

    предобработка слов

---
+++ TagsCloudContainer/TagsCloudContainer/TextProcessors/TextFileWordsContainer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainer
+{
+    public class TextFileWordsContainer : IWordsContainer
+    {
+        private IWordReader wordReader;
+        private IWordProcessor wordProcessor;
+        private string path;
+        public TextFileWordsContainer(IWordReader wordReader, IWordProcessor wordProcessor, string path)
+        {
+            this.wordReader = wordReader;
+            this.wordProcessor = wordProcessor;
+            this.path = path;
+        }
+
+        public Dictionary<string, int> GetWords()
+        {
+            var words = wordReader.Read(path);
+            words = wordProcessor.Process(words);
+
+            return words.GroupBy(word => word).ToDictionary(w => w.Key, w => w.Count());
+        }
+    }
+}
