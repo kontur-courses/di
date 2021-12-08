b39535772c1e2aa20b3bcdd0d5fe4cfbc676c77b
MaxxLorge <MaxxLorge@gmail.com>
2021-12-08 11:51:48 +0500

    добавил фильтр

---
+++ Homework/TagsCloudContainer/WordsFilter/IWordsFilter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using TagsCloudContainer.WordsPreparator;
+
+namespace TagsCloudContainer.WordsFilter
+{
+    public interface IWordsFilter
+    {
+        public IEnumerable<string> Filter(IEnumerable<WordInfo> words);
+    }
+}
---
+++ Homework/TagsCloudContainer/WordsFilter/WordsFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using TagsCloudContainer.WordsPreparator;
+
+namespace TagsCloudContainer.WordsFilter
+{
+    public record WordsFilter : IWordsFilter
+    {
+        private ImmutableHashSet<SpeechPart> excludedSpeechParts = ImmutableHashSet<SpeechPart>.Empty;
+        private Predicate<WordInfo> isBoring = wi => wi.Lemma.Length <= 2;
+
+
+        public IEnumerable<string> Filter(IEnumerable<WordInfo> words)
+        {
+            return words
+                .Where(word => !excludedSpeechParts.Contains(word.SpeechPart) && !isBoring(word))
+                .Select(word => word.Lemma);
+        }
+
+        public WordsFilter Excluding(SpeechPart speechPart)
+        {
+            return new WordsFilter() with {excludedSpeechParts = excludedSpeechParts.Add(speechPart)};
+        }
+
+        public WordsFilter Excluding(Predicate<WordInfo> boringWords)
+        {
+            return new WordsFilter() with {isBoring = boringWords};
+        }
+    }
+}
