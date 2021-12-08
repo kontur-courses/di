da08f8dae390b21ad99f1789695d42e88b1ff7b3
KirillKalutsky <kefirchik.2000@mail.ru>
2021-12-08 21:02:27 +0500

    отрисовка облака

---
+++ TagsCloudContainer/TagsCloudContainer/CloudPainters/IWordCloudPainter.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace TagsCloudContainer
+{
+    public interface IWordCloudPainter
+    {
+        public Bitmap PaintWords(ImageSettings imageSettings);
+    }
+}
---
+++ TagsCloudContainer/TagsCloudContainer/CloudPainters/WordCloudPainter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainer
+{
+    public class WordCloudPainter : IWordCloudPainter
+    {
+        private IWordCloudCreator cloudCreater;
+
+        public WordCloudPainter(IWordCloudCreator cloudCreater)
+        {
+            this.cloudCreater = cloudCreater;
+        }
+
+        public Bitmap PaintWords(ImageSettings imageSettings)
+        {
+            var imageSize = imageSettings.ImageSize;
+            var bitmap = new Bitmap(imageSize.Width, imageSize.Height);
+
+            var graphic = Graphics.FromImage(bitmap);
+            graphic.Clear(imageSettings.BackgroundColor);
+            var words = cloudCreater.GetWordCloud(graphic, imageSettings);
+
+            foreach (var word in words)
+            {
+                graphic.DrawString(word.Text, word.Font, new SolidBrush(imageSettings.TextColor), word.Border);
+                //graphic.DrawRectangle(Pens.Black, Rectangle.Round(word.Border));
+            }
+
+            return bitmap;
+        }
+    }
+}
