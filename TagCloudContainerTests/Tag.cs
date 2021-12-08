4ebc714e9b4480b4e5e49c3635e6f44c832e50d6
Pavel_Lediankin <a.lediankin@yandex.ru>
2021-12-08 19:06:27 +0500

    перенес TagsWriterHelper в тесты

---
+++ TagCloudContainerTests/Tag.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace TagCloudContainerTests
+{
+    public class Tag
+    {
+        public readonly string Word;
+        public readonly int Count;
+
+        public Tag(string word, int count)
+        {
+            Word = word;
+            Count = count;
+        }
+
+        public IEnumerable<string> GetWords()
+        {
+            for (var i = 0; i < Count; i++)
+                yield return Word;
+        }
+    }
+}
