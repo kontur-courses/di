9226b4c1d575e12533aa6b3f03e8ce64dd6a09ba
DaGnYMilleR <danil.lunyow@mail.ru>
2021-12-08 17:40:56 +0500

    add pdf file reader

--- Homework/TagsCloud.Visualization/WordsReaders/FileReaders/PdfFileReader.cs
+++ Homework/TagsCloud.Visualization/WordsReaders/FileReaders/PdfFileReader.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using System.Text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
 
 namespace TagsCloud.Visualization.WordsReaders.FileReaders
 {
     public class PdfFileReader : IFileReader
     {
-        public string Read(string filename) => throw new NotImplementedException();
-        public bool CanRead(string extension) => throw new NotImplementedException();
+        public string Read(string filename)
+        {
+            var text = new StringBuilder();
+
+            using var reader = new PdfReader(filename);
+
+            for (var i = 1; i <= reader.NumberOfPages; i++) text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+
+            return text.ToString();
+        }
+
+        public bool CanRead(string extension) => extension == "pdf";
     }
 }
