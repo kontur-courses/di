1fdb69f299532aafd37cc6b289891bceffc8b7bf
DaGnYMilleR <danil.lunyow@mail.ru>
2021-12-08 19:08:17 +0500

    add functional tests

---
+++ Homework/TagsCloud.FunctionalTests/VisualizerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Autofac;
+using NUnit.Framework;
+using TagsCloud.Visualization;
+using TagsCloud.Visualization.ContainerVisitor;
+using TagsCloud.Visualization.LayouterCores;
+
+namespace TagsCloud.FunctionalTests
+{
+    public class VisualizerTests
+    {
+        [TestCase("txt", TestName = "txt")]
+        [TestCase("doc", TestName = "doc")]
+        [TestCase("docx", TestName = "docx")]
+        [TestCase("pdf", TestName = "pdf")]
+        public void Should_ReadWords_From(string extension)
+        {
+            var settings = GenerateDefaultSettings(extension);
+
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new TagsCloudModule(settings));
+            var container = builder.Build();
+
+            var visualizer = container.Resolve<ILayouterCore>();
+            visualizer.GenerateImage().Dispose();
+        }
+
+        private TagsCloudModuleSettings GenerateDefaultSettings(string extension) =>
+            new()
+            {
+                InputWordsFile = Path.Combine(GetTestDataFolderPath(), $"test.{extension}"),
+                LayouterType = typeof(CircularCloudLayouter),
+                LayoutVisitor = new RandomColorDrawerVisitor()
+            };
+
+        private string GetTestDataFolderPath()
+            => Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\TestData\\"));
+    }
+}
--- Homework/TagsCloud.Visualization/FontFactory/FontSettings.cs
+++ Homework/TagsCloud.Visualization/FontFactory/FontSettings.cs
@@ -4,8 +4,8 @@ namespace TagsCloud.Visualization.FontFactory
 {
     public class FontSettings
     {
-        public string FamilyName { get; init; }
-        public int MaxSize { get; init; }
-        public FontStyle FontStyle { get; init; }
+        public string FamilyName { get; init; } = "Times new roman";
+        public int MaxSize { get; init; } = 2000;
+        public FontStyle FontStyle { get; init; } = FontStyle.Regular;
     }
 }
