ebf2e7639f4fd1290d69a5d33db3c3ce43ac2e52
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2021-12-08 21:18:31 +0500

    add image creator

---
+++ TagsCloudVisualization/ImageCreator/IImageCreator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using TagsCloudVisualization.Drawable;
+
+namespace TagsCloudVisualization.ImageCreator
+{
+    public interface IImageCreator
+    {
+        Image Draw(IEnumerable<IDrawable> layout);
+    }
+}
---
+++ TagsCloudVisualization/ImageCreator/ImageCreator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using TagsCloudVisualization.Drawable;
+using TagsCloudVisualization.Extensions;
+
+namespace TagsCloudVisualization.ImageCreator
+{
+    public class ImageCreator : IImageCreator
+    {
+        public Image Draw(IEnumerable<IDrawable> layout)
+        {
+            var size = layout.GetMinCanvasSize();
+            var bitmap = new Bitmap(size.Width, size.Height);
+            using var graphics = Graphics.FromImage(bitmap);
+            
+            graphics.TranslateTransform(bitmap.Width / 2f, bitmap.Height / 2f);
+
+            foreach (var drawableTag in layout)
+                drawableTag.Draw(graphics);
+            
+            return bitmap;
+        }
+    }
+}
