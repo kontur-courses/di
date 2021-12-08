f12154cfe66ef4fcc9767b9708376b40086418a4
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2021-12-08 21:19:18 +0500

    add di container

--- TagsCloudVisualization/Program.cs
+++ TagsCloudVisualization/Program.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Linq;
+﻿using System.Drawing;
+using Autofac;
 
 namespace TagsCloudVisualization
 {
@@ -11,33 +7,21 @@ namespace TagsCloudVisualization
     {
         static void Main(string[] args)
         {
-            var directory = Path.Combine(Directory.GetCurrentDirectory(), "ImageExamples");
-            var drawer = new RectanglesDrawer();
-            var i = 1;
-            foreach (var parameter in GetTagCloudArguments())
+            var settings = new Settings()
             {
-                var layout = new CircularCloudLayouter(new Point(0, 0));
-                var count = parameter.Count;
-                var factory = parameter.Factory;
-                var color = parameter.Color;
-                var rectangles = Enumerable.Range(1, count).Select(rec => layout.PutNextRectangle(factory.Invoke()));
-                var image = drawer.Draw(rectangles.ToArray(), color);
-                var filename = $"{i++}.bmp";
-                var path = Path.Combine(directory,  filename);
-                image.Save(path, ImageFormat.Bmp);
-            }
-        }
-
-        private static IEnumerable<TagCloudArguments> GetTagCloudArguments()
-        {
-            var rnd = new Random();
-            
-            yield return new TagCloudArguments(
-                100, () => new Size(rnd.Next(50, 80), rnd.Next(50, 80)), Color.Azure);
-            yield return new TagCloudArguments(
-                500, () => new Size(rnd.Next(40, 80), rnd.Next(10, 20)), Color.LawnGreen);
-            yield return new TagCloudArguments(
-                1000, () => new Size(rnd.Next(10, 20), rnd.Next(10, 30)), Color.Gold);
+                Directory = "ImageExamples",
+                ImageName = "example.bmp",
+                FontFamilyName = "Arial",
+                TagColor = Color.Firebrick,
+                StartPoint = new Point(0, 0),
+                FileWithWords = "text.txt",
+                MaxFontSize = 100,
+                BoringWords = new [] { "в", "что", "не", "и", "с", "на", "то", "а", "он", "его", "для", "из" }
+            };
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new TagsCloudDrawerModule(settings));
+            var container = builder.Build();
+            container.Resolve<Visualizer>().Visualize();
         }
     }
 }
---
+++ TagsCloudVisualization/TagsCloudModule.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
+using TagsCloudVisualization.ColorService;
+using TagsCloudVisualization.FontService;
+using TagsCloudVisualization.ImageCreator;
+using TagsCloudVisualization.Layouter;
+using TagsCloudVisualization.PointGenerators;
+using TagsCloudVisualization.Saver;
+using TagsCloudVisualization.SizeService;
+using TagsCloudVisualization.WordsPrepare;
+using TagsCloudVisualization.WordsPrepare.Preparers;
+using TagsCloudVisualization.WordsProvider;
+using TagsCloudVisualization.WordsProvider.FileReader;
+using TagsCloudVisualization.WordsToTagTransformer;
+
+namespace TagsCloudVisualization
+{
+    public class TagsCloudDrawerModule : Module
+    {
+        private readonly Settings settings;
+
+        public TagsCloudDrawerModule(Settings settings)
+        {
+            this.settings = settings ?? throw new ArgumentNullException(nameof(settings));
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+            builder.RegisterType<TxtFileReader>().As<IWordsReader>();
+            builder.Register(ctx => new FileReadService(settings.FileWithWords,
+                    ctx.Resolve<IEnumerable<IWordsReader>>()))
+                .As<IFileReadService>();
+            builder.RegisterType<WordsToLowerPrepare>().As<IWordsPreparer>();
+            builder.RegisterInstance(new BoringWordsFilter(settings.BoringWords)).As<IWordsPreparer>();
+            builder.RegisterComposite<IWordsPreparer>((_, processors) => new WordsPreparer(processors));
+            builder.RegisterInstance(new TagColorService(settings.TagColor)).As<ITagColorService>();
+            builder.RegisterType<WordsToTagTransformer.WordsToTagTransformer>().As<IWordsToTagTransformer>();
+            builder.Register(_ => new ArchimedeanSpiralPointGenerator(settings.StartPoint)).As<IPointGenerator>();
+            builder.RegisterType<CircularCloudLayouter>().As<ILayouter>();
+            builder.Register(_ => new TagFontService(settings.MaxFontSize, settings.FontFamilyName)).As<ITagFontService>();
+            builder.RegisterType<TagSizeService>().As<ITagSizeService>();
+            builder.RegisterType<TagToDrawableTagTransformer>().AsSelf();
+            builder.RegisterInstance(new ImageSaver(settings.Directory, settings.ImageName)).As<IImageSaver>();
+            builder.RegisterType<ImageCreator.ImageCreator>().As<IImageCreator>();
+            builder.RegisterType<Visualizer>().AsSelf();
+        }
+    }
+}
