9371e5a14e7a4b05e31bbc8174c14a632b4ee183
MaxxLorge <MaxxLorge@gmail.com>
2021-12-08 18:59:14 +0500

    Добавил saver

---
+++ Homework/TagsCloudContainer/BitmapSaver/BitmapSaver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace TagsCloudContainer.BitmapSaver
+{
+    public class BitmapSaver : IBitmapSaver
+    {
+        public void Save(Bitmap bmp, DirectoryInfo directory, string fileName,ImageFormat format)
+        {
+            if(!directory.Exists)
+                directory.Create();
+            var fullPath = Path.Combine(directory.FullName, fileName);
+            try
+            {
+                bmp.Save(fullPath, format);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Не удалось сохранить файл", e);
+            }
+            finally
+            {
+                bmp.Dispose();
+            }
+        }
+    }
+}
---
+++ Homework/TagsCloudContainer/BitmapSaver/IBitmapSaver.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace TagsCloudContainer.BitmapSaver
+{
+    public interface IBitmapSaver
+    {
+        public void Save(Bitmap bmp, DirectoryInfo directory, string fileName, ImageFormat format);
+    }
+}
