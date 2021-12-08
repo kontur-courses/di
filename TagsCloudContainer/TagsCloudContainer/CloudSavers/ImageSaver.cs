2b8121f457a4b1345166bd931ca2be417a22237c
KirillKalutsky <kefirchik.2000@mail.ru>
2021-12-08 21:04:57 +0500

    сохранение облака

---
+++ TagsCloudContainer/TagsCloudContainer/CloudSavers/ImageSaver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainer
+{
+    public class ImageSaver
+    {
+        private string pathToSaveDirectory;
+        string fileFormat;
+        public ImageSaver(string pathToSaveDirectory, string fileFormat="png")
+        {
+            this.pathToSaveDirectory = pathToSaveDirectory;
+            this.fileFormat = fileFormat;
+        }
+
+        public string Save(Bitmap image, string name)
+        {
+            var path = @$"{pathToSaveDirectory}\{name}.{fileFormat}";
+            image.Save(path);
+            return Path.GetFullPath(path);
+        }
+    }
+}
---
+++ TagsCloudContainer/TagsCloudContainer/CloudSavers/WordCloudSaver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainer
+{
+    public class WordCloudSaver 
+    {
+        private IWordCloudPainter cloudPainter;
+        private ImageSaver saver;
+
+        public WordCloudSaver(IWordCloudPainter wordCloudPainter, ImageSaver saver)
+        {
+            cloudPainter = wordCloudPainter;
+            this.saver = saver;
+        }
+
+        public string SaveCloud(string name, ImageSettings imageSettings)
+        {
+            var image = cloudPainter.PaintWords(imageSettings);
+            var path = saver.Save(image, name);
+            return path;
+        }
+    }
+}
