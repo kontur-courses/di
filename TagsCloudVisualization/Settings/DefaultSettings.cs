1c0af953363509aa0acd8fc6adc3fda60fb00b42
SlivnyiArtem <slivnay1965@gmail.com>
2021-12-08 23:08:47 +0500

    step 1

---
+++ TagsCloudVisualization/Settings/DefaultSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagsCloudVisualization.FileReader;
+
+namespace TagsCloudVisualization.Settings
+{
+    public class DefaultSettings : SettingsConfiguration
+    {
+        public override TextFileReader FileReader { get; }
+    }
+}
--- TagsCloudVisualization/Settings/SettingsConfiguration.cs
+++ TagsCloudVisualization/Settings/SettingsConfiguration.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TagsCloudVisualization.FileReader;
 
 namespace TagsCloudVisualization.Settings
 {
-    interface ISettingsConfiguration
+    public abstract class SettingsConfiguration
     {
+        public abstract TextFileReader FileReader { get; }
+
     }
 }
