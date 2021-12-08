f617ab60189fc87e4966e7d70b8226d6a12f3dde
MaxxLorge <MaxxLorge@gmail.com>
2021-12-09 00:26:33 +0500

    немного тестов на WordsFrequencyAnalyzer

---
+++ Homework/TagsCloudContainer_Tests/WordsFrequencyAnalyzer_Should.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudContainer.WordsFrequencyAnalyzers;
+
+namespace TagsCloudContainer_Tests
+{
+    [TestFixture]
+    public class WordsFrequencyAnalyzer_Should
+    {
+        private IWordsFrequencyAnalyzer sut = new WordsFrequencyAnalyzer();
+
+        [Test]
+        public void ReturnEmptyDict_WhenEmptyInput()
+        {
+            var input = System.Array.Empty<string>();
+            sut.GetWordsFrequency(input).Should().BeEmpty();
+        }
+
+        [Test]
+        public void ReturnCorrectDict_WhenNoRepeats()
+        {
+            var input = new[]
+            {
+                "привет",
+                "мир",
+                "война"
+            };
+            var result = sut.GetWordsFrequency(input);
+            result.Should()
+                .HaveCount(3)
+                .And
+                .OnlyContain(kv => kv.Value == 1);
+        }
+
+        [Test]
+        public void ReturnCorrectDict_WhenWordsRepeat()
+        {
+            var input = new[]
+            {
+                "привет",
+                "привет",
+                "мир",
+            };
+            var result = sut.GetWordsFrequency(input);
+            result.Should()
+                .HaveCount(2)
+                .And
+                .Contain(kv => kv.Key == "привет" && kv.Value == 2);
+        }
+    }
+}
