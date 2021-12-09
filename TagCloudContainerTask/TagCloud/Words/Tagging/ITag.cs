88faf2d896cd591abecec920cb20d3a175030b22
SquirrelLeonid <rm200002@yandex.ru>
2021-12-09 22:01:48 +0500

    Implemented dirt version of words tagging
    Clear Program.Main() body to exclude compile errors

---
+++ TagCloudContainerTask/TagCloud/Words/Tagging/ITag.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace TagCloud.Words.Tagging
+{
+    public interface ITag
+    {
+        string Word { get; }
+        float KegelSize { get; }
+        Size WordRectangleSize { get; }
+    }
+}
---
+++ TagCloudContainerTask/TagCloud/Words/Tagging/Tag.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace TagCloud.Words.Tagging
+{
+    public class Tag : ITag
+    {
+        public Tag(string word, float wordKegelSize, Size wordRectangleSize)
+        {
+            Word = word;
+            KegelSize = wordKegelSize;
+            WordRectangleSize = wordRectangleSize;
+        }
+
+        public string Word { get; }
+
+        public float KegelSize { get; }
+
+        public Size WordRectangleSize { get; }
+    }
+}
