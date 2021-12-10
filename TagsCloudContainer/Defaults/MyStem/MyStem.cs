c58e8d7886f52c7cd0bf5acb12b60788eb8f0c7a
Aripov Sergey <aripovsergey3@gmail.com>
2021-12-10 11:51:27 +0500

    Singleton mystem + обработка 'странных' слов

--- TagsCloudContainer/Defaults/MyStem/MyStem.cs
+++ TagsCloudContainer/Defaults/MyStem/MyStem.cs
@@ -1,31 +1,39 @@
 ﻿using Autofac;
 using System.Diagnostics;
-using TagsCloudContainer.Registrations;
 
 namespace TagsCloudContainer.Defaults.MyStem;
 
 public class MyStem : IDisposable
 {
-    private bool disposedValue;
-    private readonly Process myStemProccess;
-    bool isStarted = false;
-    private readonly Dictionary<string, WordStat> cache = new();
+    private static bool disposedValue;
+    private static readonly ProcessStartInfo startInfo = new()
+    {
+        UseShellExecute = false,
+        RedirectStandardInput = true,
+        RedirectStandardOutput = true,
+        RedirectStandardError = true,
+        FileName = "mystem.exe",
+        Arguments = "-nil -e cp866 - -"
+    };
+    private static readonly Process myStemProccess = new()
+    {
+        StartInfo = startInfo
+    };
+    private static bool isStarted = false;
+    private static readonly Dictionary<string, WordStat> cache = new();
+    private static readonly Dictionary<string, WordStat[]> complexWordCache = new();
 
     public MyStem()
     {
-        myStemProccess = new Process();
-        myStemProccess.StartInfo.UseShellExecute = false;
-        myStemProccess.StartInfo.RedirectStandardInput = true;
-        myStemProccess.StartInfo.RedirectStandardOutput = true;
-        myStemProccess.StartInfo.RedirectStandardError = true;
-        myStemProccess.StartInfo.FileName = "mystem.exe";
-        myStemProccess.StartInfo.Arguments = "-nil -e cp866 - -";
     }
 
-    public WordStat AnalyzeWord(string word)
+    public WordStat? AnalyzeWord(string word)
     {
         if (disposedValue)
             throw new ObjectDisposedException(nameof(MyStem));
+
+        if (word == null)
+            return null;
 
         if (cache.TryGetValue(word, out var stat))
             return stat;
@@ -39,19 +47,44 @@ public class MyStem : IDisposable
         }
 
         myStemProccess.StandardInput.WriteLine(word);
-        var result = myStemProccess.StandardOutput.ReadLine();
-        var stemGrSeparation = result.IndexOf('=');
-        var grEnd = result.IndexOf('=', stemGrSeparation + 1);
-        var part = result[(stemGrSeparation + 1)..grEnd].Split(',')[0];
-        stat = new(result[..stemGrSeparation], Enum.Parse<SpeechPart>(part));
-        cache[word] = stat;
+
+        var stats = ParseWordStats(ReadAllLines(myStemProccess.StandardOutput));
+        stat = stats.Length > 0 ? stats[0] : null;
+        if (stat != null)
+        {
+            cache[word] = stat;
+            cache[stat.Stem] = stat;
+        }
+
         return stat;
     }
 
-    [Register]
-    public static void Register(ContainerBuilder builder)
+    private static IEnumerable<string> ReadAllLines(StreamReader reader)
     {
-        builder.RegisterType<MyStem>().AsSelf().SingleInstance();
+        yield return reader.ReadLine()!;
+        while (reader.Peek() != -1)
+        {
+            yield return reader.ReadLine()!;
+        }
+    }
+
+    private static WordStat[] ParseWordStats(IEnumerable<string> lines)
+    {
+        return lines.Select(ParseWordStat)
+            .Where(x => x != null)
+            .OrderByDescending(x => x!.Stem.Length)
+            .ToArray()!;
+    }
+
+    private static WordStat? ParseWordStat(string wordStat)
+    {
+        if (wordStat.EndsWith("??"))
+            return null;
+        var stemGrSeparation = wordStat.IndexOf('=');
+        var grEnd = wordStat.IndexOf('=', stemGrSeparation + 1);
+        var part = wordStat[(stemGrSeparation + 1)..grEnd].Split(',')[0];
+        var stem = wordStat[..stemGrSeparation].TrimEnd('?');
+        return new(stem, Enum.Parse<SpeechPart>(part));
     }
 
     private void HandleErrorData(object sender, DataReceivedEventArgs e)
