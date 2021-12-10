33c4bffa6349ad5756ebb549ba7eb9e095ab788b
MaxxLorge <MaxxLorge@gmail.com>
2021-12-10 13:24:08 +0500

    добавил генератор расцветки слов

---
+++ Homework/TagsCloudContainer/Visualizer/IColorGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudContainer.Visualizer
+{
+    public interface IColorGenerator
+    {
+        public IEnumerable<Color> GetColor();
+
+    }
+}
---
+++ Homework/TagsCloudContainer/Visualizer/RandomColorGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudContainer.Visualizer
+{
+    public class RandomColorGenerator : IColorGenerator
+    {
+        public IEnumerable<Color> GetColor()
+        {
+            var rnd = new Random();
+            while (true)
+                yield return Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255));
+        }
+    }
+}
