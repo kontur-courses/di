98d1ae4ede2cc1ecfb0e2ec9f4441b7d091d1fa7
Aripov Sergey <aripovsergey3@gmail.com>
2021-12-10 12:58:00 +0500

    Изменения размеров в styleprovider

--- TagsCloudContainer/Defaults/SettingsProviders/StyleProvider.cs
+++ TagsCloudContainer/Defaults/SettingsProviders/StyleProvider.cs
@@ -11,11 +11,11 @@ public class StyleProvider : ICliSettingsProvider
     private static Color brushColor = Color.White;
 
     public string FontFamilyName { get => fontFamilyName; private set => fontFamilyName = value; }
-    public double MaxSize { get => maxSize; private set => maxSize = value; }
+    public double MinSize { get => maxSize; private set => maxSize = value; }
     public Color BrushColor { get => brushColor; private set => brushColor = value; }
     public Style GetStyle(ITag tag)
     {
-        var font = new Font(new FontFamily(FontFamilyName), (float)(tag.RelativeSize * MaxSize));
+        var font = new Font(new FontFamily(FontFamilyName), (float)((1+tag.RelativeSize) * MinSize));
         return new(font, new SolidBrush(BrushColor));
     }
 
@@ -24,7 +24,7 @@ public class StyleProvider : ICliSettingsProvider
         var options = new OptionSet()
         {
             {"font-family=", $"Sets the font family name for tags. Defaults to {FontFamilyName}", v => FontFamilyName = v },
-            {"max-size=", $"Sets the max size for tags. Defaults to {MaxSize}", (double v) => MaxSize = v },
+            {"min-size=", $"Sets the min size for tags. Defaults to {MinSize}", (double v) => MinSize = v },
             {"color=", $"Sets the color for tags. Defaults to {BrushColor.Name}", (Color v) => BrushColor = v },
         };
 
