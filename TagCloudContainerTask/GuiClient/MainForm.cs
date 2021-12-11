5100c0e120794bf94331c3e3f2c4757fe6886881
SquirrelLeonid <rm200002@yandex.ru>
2021-12-11 20:22:51 +0500

    Added MainForm to GuiClient
    Added Microsoft.Extensions.DependencyInjection
    Injected DI container

---
+++ TagCloudContainerTask/GuiClient/MainForm.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using App.Infrastructure.SettingsHolders;
+using GuiClient.UiActions;
+
+namespace GuiClient
+{
+    public class MainForm : Form
+    {
+        private readonly MenuStrip mainMenu;
+
+        public MainForm(
+            IEnumerable<IUiAction> actions,
+            PictureBoxImageHolder pictureBox,
+            IImageSizeSettingsHolder imageSizeSettings)
+        {
+            ClientSize = imageSizeSettings.Size;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            mainMenu = new MenuStrip();
+            mainMenu.Items.AddRange(actions.ToArray().ToMenuItems());
+            Controls.Add(mainMenu);
+            pictureBox.RecreateImage();
+            pictureBox.Dock = DockStyle.Fill;
+            Controls.Add(pictureBox);
+        }
+    }
+}
---
+++ TagCloudContainerTask/GuiClient/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using App.Implementation;
+using App.Implementation.FileInteractions.Readers;
+using App.Implementation.FileInteractions.Writers;
+using App.Implementation.LayoutingAlgorithms;
+using App.Implementation.SettingsHolders;
+using App.Implementation.Visualization;
+using App.Implementation.Words.Filters;
+using App.Implementation.Words.FrequencyAnalyzers;
+using App.Implementation.Words.Preprocessors;
+using App.Implementation.Words.Tags;
+using App.Infrastructure;
+using App.Infrastructure.FileInteractions.Readers;
+using App.Infrastructure.FileInteractions.Writers;
+using App.Infrastructure.LayoutingAlgorithms;
+using App.Infrastructure.SettingsHolders;
+using App.Infrastructure.Visualization;
+using App.Infrastructure.Words.Filters;
+using App.Infrastructure.Words.FrequencyAnalyzers;
+using App.Infrastructure.Words.Preprocessors;
+using App.Infrastructure.Words.Tags;
+using GuiClient.UiActions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GuiClient
+{
+    public static class Startup
+    {
+        public static ServiceProvider GetAppServiceProvider()
+        {
+            var imageSettings = new ImageSizeSettings();
+            var fontSettings = new FontSettings();
+            var inputFileSettings = new InputFileSettings();
+            var outputResultSettings = new OutputResultSettings();
+            var paletteSettings = new PaletteSettings();
+
+            return new ServiceCollection()
+                .AddSingleton(imageSettings)
+                .AddSingleton<IImageSizeSettingsHolder>(imageSettings)
+                .AddSingleton(fontSettings)
+                .AddSingleton<IFontSettingsHolder>(fontSettings)
+                .AddSingleton(inputFileSettings)
+                .AddSingleton<IInputFileSettingsHolder>(inputFileSettings)
+                .AddSingleton(outputResultSettings)
+                .AddSingleton<IOutputResultSettingsHolder>(outputResultSettings)
+                .AddSingleton(paletteSettings)
+                .AddSingleton<IPaletteSettingsHolder>(paletteSettings)
+                .AddSingleton<ITagger, Tagger>()
+                .AddSingleton<IReaderFactory, ReaderFactory>()
+                .AddSingleton<ILineWriter, LineWriter>()
+                .AddSingleton<IPreprocessor, ToLowerCasePreprocessor>()
+                .AddSingleton<IPreprocessor, ToInitFormPreprocessor>()
+                .AddSingleton<IFilter, WordsLengthFilter>()
+                .AddSingleton<IFrequencyAnalyzer, WordsFrequencyAnalyzer>()
+                .AddSingleton<ICloudGenerator, CloudGenerator>()
+                .AddSingleton<IDrawer, Drawer>()
+                .AddSingleton<ILayouterFactory, LayouterFactory>()
+                .AddSingleton<IVisualizer, Visualizer>()
+                .AddSingleton(provider => new Lazy<MainForm>(() => provider.GetRequiredService<MainForm>()))
+                .AddSingleton<PictureBoxImageHolder, PictureBoxImageHolder>()
+                .AddSingleton<IImageHolder>(x => x.GetRequiredService<PictureBoxImageHolder>())
+                .AddSingleton(provider => new Lazy<IImageHolder>(() => provider.GetRequiredService<IImageHolder>()))
+                .AddSingleton<IUiAction, ImageSettingsAction>()
+                .AddSingleton<IUiAction, PaletteSettingsAction>()
+                .AddSingleton<IUiAction, FontSettingsAction>()
+                .AddSingleton<IUiAction, SaveImageAction>()
+                .AddSingleton<IUiAction, OpenFileAction>()
+                .AddSingleton<MainForm, MainForm>()
+                .BuildServiceProvider();
+        }
+    }
+}
