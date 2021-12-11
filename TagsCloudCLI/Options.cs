cf114378ca6de5c0037f4ef97183e16138009a9b
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2021-12-11 23:57:00 +0500

    fix settings: 1. add short names 2. chang help text 3. make Directory and File with words as required arguments

--- TagsCloudCLI/Options.cs
+++ TagsCloudCLI/Options.cs
@@ -4,22 +4,22 @@ namespace TagsCloudCLI
 {
     internal class Options
     {
-        [Option("directory", HelpText = "Path to file with words", Default = "ImageExamples")]
+        [Option('d', "directory", HelpText = "Path to save image", Required = true )]
         public string Directory { get; set; }
 
-        [Option("image_name", HelpText = "Image name", Default = "example.png")]
+        [Option('i', "image_name", HelpText = "Image name", Default = "example.png")]
         public string ImageName { get; set; }
 
-        [Option("font_family", HelpText = "Family name of the words", Default = "Arial")]
+        [Option('f',"font_family", HelpText = "Family name of the words", Default = "Arial")]
         public string FontFamilyName { get; set; }
 
-        [Option("color", HelpText = "Words color", Default = "Firebrick")]
+        [Option('c',"color", HelpText = "Words color", Default = "Blue")]
         public string TagColor { get; set; }
 
-        [Option("file_with_words", HelpText = "Words color", Default = "text.txt")]
+        [Option('w', "file_with_words", HelpText = "File with words", Required = true)]
         public string FileWithWords { get; set; }
 
-        [Option("font_size", HelpText = "Max font size", Default = 100)]
+        [Option('s',"font_size", HelpText = "Max font size", Default = 100)]
         public int MaxFontSize { get; set; }
     }
 }
