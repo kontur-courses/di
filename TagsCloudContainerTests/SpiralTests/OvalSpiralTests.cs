8cfee86fa897e024277d9c87eeafab033aff81b9
FreeTrally <serega2012111@gmail.com>
2021-12-11 08:23:49 +0500

    SpiralTests

---
+++ TagsCloudContainerTests/SpiralTests/OvalSpiralTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Drawing;
+using TagsCloudContainer;
+
+namespace TagsCloudContainerTests
+{
+    internal class OvalSpiralTests
+    {
+        private Point center;
+        private OvalSpiral spiral;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var settings = SettingsProvider.GetSettings();
+            center = settings.Center;
+            spiral = new OvalSpiral(settings);
+        }
+
+        [Test]
+        public void Should_StartAtCenter()
+        {
+            center.Should().BeEquivalentTo(spiral.GetNext());
+        }
+
+        [Test]
+        public void Should_IncreaseDistanceFromCenter()
+        {
+            var spiralPoint = spiral.GetNext();
+            var distance = center.GetDistance(spiralPoint);
+
+            for (var i = 1; i <= 1000; i++)
+            {
+                spiralPoint = spiral.GetNext();
+                if (i % 250 == 0)
+                {
+                    var newDistance = center.GetDistance(spiralPoint);
+                    newDistance.Should().BeGreaterThan(distance);
+                    distance = newDistance;
+                }
+            }
+        }
+    }
+}
