87e0361b4fa893372d8091747cc14d41b44d5f34
Dmitry_Murzin <dmitrymurzin91@gmail.com>
2021-12-11 13:47:49 +0500

    Add tests project, add WordsGetter class

---
+++ Homework/TagCloudContainerTests/WordsGetterShould.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using TagsCloudContainer;
+
+namespace TagCloudContainerTests
+{
+    [TestFixture]
+    public class WordsGetterShould
+    {
+        private HashSet<string> boringWords;
+        [SetUp]
+        public void CreateEmptyBoringWords()
+        {
+            boringWords = new HashSet<string>();
+        }
+
+        [Test]
+        public void Throw_FileNotFoundException_WhenPathIsIncorrect()
+        {
+            string incorrectPath = "words";
+            Action wordsGetterCreationWithIncorrectPath =
+                () => new WordsGetter(incorrectPath, boringWords);
+
+            wordsGetterCreationWithIncorrectPath.Should().Throw<FileNotFoundException>();
+        }
+
+        [Test]
+        public void Throw_ArgumentNullException_WhenPathIsNull()
+        {
+            Action wordsGetterCreationWithIncorrectPath =
+                () => new WordsGetter(null, boringWords);
+
+            wordsGetterCreationWithIncorrectPath.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void ShowDistinctWordsCountRight()
+        {
+            var path = @"D:\di\Homework\TagsCloudContainer\words.txt";
+            boringWords = new HashSet<string>() { "I", "" };
+
+            var wordsGetter = new WordsGetter(path, boringWords);
+            var distinctWordsCount = wordsGetter.GetDistinctWordsCount();
+
+            distinctWordsCount.Should().Be(19);
+        }
+    }
+}
---
+++ Homework/TagsCloudContainer/WordsGetter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TagsCloudContainer
+{
+    public class WordsGetter
+    {
+        private readonly string path;
+        private readonly HashSet<string> boringWords;
+        private readonly Dictionary<string, int> wordsQuantities;
+
+        public WordsGetter(string path, IEnumerable<string> boringWords)
+        {
+            this.path = path;
+            this.boringWords = boringWords.ToHashSet();
+            wordsQuantities = GetWordsFromFile(path);
+        }
+
+        public IReadOnlyDictionary<string, int> GetWordsQuantities()
+            => wordsQuantities;
+
+        public int GetDistinctWordsCount()
+            => wordsQuantities.Count;
+
+        private Dictionary<string, int> GetWordsFromFile(string path)
+        {
+            var words = new Dictionary<string, int>();
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    line = line.ToLower();
+                    if (boringWords.Contains(line)) continue;
+                    if (!words.TryGetValue(line,  out _))
+                        words.Add(line, 0);
+                    words[line]++;
+                }
+            }
+
+            return words;
+        }
+    }
+}
