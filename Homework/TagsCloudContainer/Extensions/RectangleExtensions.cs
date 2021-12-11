2d68db804742f35182a02009274f8808084f26df
MaxxLorge <MaxxLorge@gmail.com>
2021-12-12 04:49:26 +0500

    удалил лишние extension-методы

--- Homework/TagsCloudContainer/Extensions/RectangleExtensions.cs
+++ Homework/TagsCloudContainer/Extensions/RectangleExtensions.cs
@@ -4,8 +4,6 @@ using System.Drawing;
 using System.Linq;
 using System.Runtime.CompilerServices;
 
-[assembly:InternalsVisibleToAttribute("TagsCloudContainer_Tests")]
-
 namespace TagsCloudContainer.Extensions
 {
     internal static class RectangleExtensions
@@ -13,25 +11,6 @@ namespace TagsCloudContainer.Extensions
         internal static bool IntersectsWith(this Rectangle rectangle, IEnumerable<Rectangle> rectangles)
         {
             return rectangles.Any(r => r.IntersectsWith(rectangle));
-        }
-
-        internal static Rectangle GetRectanglesContainer(this ICollection<Rectangle> rectangles)
-        {
-            if (rectangles == null)
-                throw new ArgumentNullException(nameof(rectangles));
-            if (!rectangles.Any())
-                throw new ArgumentException($"{nameof(rectangles)} is empty", nameof(rectangles));
-            var leftXCoordinate = rectangles.Min(rect => rect.Left);
-            var rightXCoordinate = rectangles.Max(rect => rect.Right);
-            var topYCoordinate = rectangles.Min(rect => rect.Top);
-            var bottomYCoordinate = rectangles.Max(rect => rect.Bottom);
-            return new Rectangle(new Point(leftXCoordinate, topYCoordinate),
-                new Size(rightXCoordinate - leftXCoordinate, bottomYCoordinate - topYCoordinate));
-        }
-
-        internal static int GetSummaryArea(this IEnumerable<Rectangle> rectangles)
-        {
-            return rectangles.Select(rect => rect.Width * rect.Height).Sum();
         }
     }
 }
