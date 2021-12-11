3320016e3b3f89847da7ff405287544c1a189890
FreeTrally <serega2012111@gmail.com>
2021-12-12 02:57:30 +0500

    Autoscaling

--- TagsCloudContainer/Infrastructure/TagCloudPainter.cs
+++ TagsCloudContainer/Infrastructure/TagCloudPainter.cs
@@ -5,6 +5,7 @@ namespace TagsCloudContainer;
 
 public class TagCloudPainter
 {
+    private const int AddedImageSize = 300;
     private ICloudLayouter layouter;
     private Settings settings;
 
@@ -17,12 +18,26 @@ public class TagCloudPainter
 
     public string Paint(IEnumerable<PaintedTag> tags)
     {
+        var cloudTags = PutCloudTags(tags).ToList();
+        var neededSize = CalculateCoverageSize(cloudTags.Select(tag => tag.Rectangle)) 
+            + new Size(AddedImageSize, AddedImageSize);
+        var imageOffset = neededSize / 2 - new Size(settings.Center);
+        var scaleX = settings.ImageSize.Width / (float)neededSize.Width;
+        var scaleY = settings.ImageSize.Height / (float)neededSize.Height;
+        return DrawTags(scaleX, scaleY, cloudTags, imageOffset);
+    }
+
+    private string DrawTags(float scaleX, float scaleY, 
+        IEnumerable<CloudTag> cloudTags, Size imageOffset)
+    {
         var bm = new Bitmap(settings.ImageSize.Height, settings.ImageSize.Width);
         var graphics = Graphics.FromImage(bm);
+        graphics.ScaleTransform(scaleX, scaleY);
         graphics.Clear(settings.Palette.Background);
 
-        foreach (var tag in PutCloudTags(tags))
+        foreach (var tag in cloudTags)
         {
+            tag.Rectangle.Offset(new Point(imageOffset));
             graphics.DrawString(tag.Text, tag.Label.Font,
                 new SolidBrush(tag.Color), tag.Rectangle);
         }
@@ -33,7 +48,7 @@ public class TagCloudPainter
         return savePath;
     }
 
-    public IEnumerable<CloudTag> PutCloudTags(IEnumerable<PaintedTag> tags)
+    private IEnumerable<CloudTag> PutCloudTags(IEnumerable<PaintedTag> tags)
     {
         var averageFrequency = tags.Select(tag => tag.Frequency).Sum()
             / tags.Count();
@@ -48,5 +63,18 @@ public class TagCloudPainter
             var rectangle = layouter.PutNextRectangle(size);
             yield return new CloudTag(tag, label, rectangle);
         }
+    }
+
+    private static Size CalculateCoverageSize(IEnumerable<Rectangle> rectangles)
+    {
+        var maxX = rectangles.Max(x => x.X + x.Size.Width);
+        var minX = rectangles.Min(x => x.X);
+        var width = maxX - minX;
+
+        var maxY = rectangles.Max(x => x.Y);
+        var minY = rectangles.Min(x => x.Y - x.Size.Height);
+        var height = maxY - minY;
+
+        return new Size(width, height);
     }
 }
