6ef86b0d5ecec9851f71e298e62bb63025d489b2
FreeTrally <serega2012111@gmail.com>
2021-12-11 08:23:12 +0500

    ParserTests

---
+++ TagsCloudContainerTests/ParserTests/DocParserTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.Linq;
+using TagsCloudContainer;
+using TagsCloudContainer.Interfaces;
+
+namespace TagsCloudContainerTests
+{
+    internal class DocParserTests
+    {
+        private string textsFolder;
+        private IParser parser;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            textsFolder = Path.GetFullPath(@"..\..\..\texts");
+            parser = new DocParser();
+        }
+
+        [Test]
+        public void Should_Throw_OnNonExistingFile()
+        {
+            var path = Path.Combine(textsFolder, "amogus.docx");
+
+            Assert.Throws<ArgumentException>(() => parser.Parse(path).ToArray());
+        }
+
+        [Test]
+        public void Should_ParseCorrectly()
+        {
+            var path = Path.Combine(textsFolder, "parserTest.docx");
+
+            var result = parser.Parse(path);
+            var expected = new [] { "this", "Is", "parser", "test" };
+
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
