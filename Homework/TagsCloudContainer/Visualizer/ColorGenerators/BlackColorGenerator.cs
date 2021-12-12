402f24dbe4c58c6689089018a447e1adaff1c794
MaxxLorge <MaxxLorge@gmail.com>
2021-12-13 04:04:13 +0500

    добавил черную расцветку

---
+++ Homework/TagsCloudContainer/Visualizer/ColorGenerators/BlackColorGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudContainer.Visualizer.ColorGenerators
+{
+    public class BlackColorGenerator : IColorGenerator
+    {
+        public PalleteType PalleteType => PalleteType.Black;
+        public Stack<Color> GetColors(int count)
+        {
+            var colors = new Stack<Color>();
+            for (var i = 0; i < count; i++)
+            {
+                colors.Push(Color.Black);
+            }
+
+            return colors;
+        }
+    }
+}
--- Homework/TagsCloudContainer/Visualizer/ColorGenerators/PalleteType.cs
+++ Homework/TagsCloudContainer/Visualizer/ColorGenerators/PalleteType.cs
@@ -5,6 +5,7 @@ namespace TagsCloudContainer.Visualizer.ColorGenerators
     [Flags]
     public enum PalleteType
     {
-        Random = 0x0
+        Random = 0x0,
+        Black = 0x1,
     }
 }
