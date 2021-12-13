82c184f594a6ac0daa6d39737855c3d2a2f1bfbf
KirillKalutsky <kefirchik.2000@mail.ru>
2021-12-13 23:50:41 +0500

    добавил предобработчик слов приводящий их в начальную форму

---
+++ TagsCloudContainer/TagsCloudContainer/TextProcessors/LemmatizationProcessor.cs
@@ -0,0 +1,23 @@
+﻿using DeepMorphy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainer.TextProcessors
+{
+    public class LemmatizationProcessor : IWordProcessor
+    {
+        private readonly MorphAnalyzer morph;
+        public LemmatizationProcessor()
+        {
+            morph = new MorphAnalyzer(withLemmatization: true, withTrimAndLower: false);
+        }
+
+        public IEnumerable<string> Process(IEnumerable<string> words)
+        {
+            return morph.Parse(words).Select(morphInf => morphInf.Tags[0].Lemma);
+        }
+    }
+}
