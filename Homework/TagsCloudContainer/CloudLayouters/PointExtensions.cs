47aa60f4403ba2d1fb81ee17d453549a8a5bec5c
Dmitry_Murzin <dmitrymurzin91@gmail.com>
2021-12-13 23:46:54 +0500

    Move ClodLayouters to the current project

---
+++ Homework/TagsCloudContainer/CloudLayouters/PointExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+
+namespace TagsCloudContainer.CloudLayouters
+{
+    internal static class PointExtensions
+    {
+        public static double GetDistanceTo(this Point start, Point finish)
+        {
+            var xDist = finish.X - start.X;
+            var yDist = finish.Y - start.Y;
+            return Math.Sqrt(xDist * xDist + yDist * yDist);
+        }
+
+        public static Point GetShiftTo(this Point from, Point to)
+        {
+            return new Point(to.X - from.X, to.Y - from.Y);
+        }
+
+        public static Point AddShift(this Point point, Point shift)
+        {
+            return new Point(point.X + shift.X, point.Y + shift.Y);
+        }
+    }
+}
