36ffd250a80d86e4c45559e71ae00bf7812c0a1f
Dmitry_Murzin <dmitrymurzin91@gmail.com>
2021-12-14 22:43:38 +0500

    Add command line necessary arguments test

---
+++ Homework/TagsCloudContainerTests/CommandLineClientShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudContainer.Clients;
+
+namespace CloudContainerTests
+{
+    [TestFixture]
+    public class CommandLineClientShould
+    {
+        [Test]
+        public void ThrowExceptionWhen_AnyNecessaryArgumentIsAbsent()
+        {
+            var onlyInputFile = new string[] { "input" };
+            var emptyArgs = new string[] { };
+
+            ShouldThrowArgumentNullWithArgs(onlyInputFile);
+            ShouldThrowArgumentNullWithArgs(emptyArgs);
+        }
+
+        private static void ShouldThrowArgumentNullWithArgs(string[] onlyInputFile)
+        {
+            FluentActions
+                .Invoking(() => new CommandLineClient(onlyInputFile))
+                .Should()
+                .Throw<ArgumentNullException>();
+        }
+    }
+}
