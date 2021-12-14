5b1b5a67c5cfdd35b958b67903d4194f554b7156
Dmitry_Murzin <dmitrymurzin91@gmail.com>
2021-12-14 23:07:59 +0500

    Add several tests

--- Homework/TagsCloudContainerTests/CommandLineClientShould.cs
+++ Homework/TagsCloudContainerTests/CommandLineClientShould.cs
@@ -18,6 +18,51 @@ namespace CloudContainerTests
             ShouldThrowArgumentNullWithArgs(emptyArgs);
         }
 
+        [Test]
+        public void GetDefaultValuesWhen_OnlyNecessaryArgmentsAreGiven()
+        {
+            var onlyNecessaryArgs = new string[] { "input", "output" };
+            var defaultOptions = GetDefaultOptions(onlyNecessaryArgs);
+            var expectedUserConfig = new UserConfig(defaultOptions);
+
+            var client = new CommandLineClient(onlyNecessaryArgs);
+
+            client.UserConfig.Should().BeEquivalentTo(expectedUserConfig);
+        }
+
+        [Test]
+        public void ParseArgumentsCorrectly()
+        {
+            var args = new string[] { "input", "output", "-w", 
+                "2000", "-h", "1500", "-n", "TimesNewRoman", "-s", "50", "-c", "Blue"};
+            var parsedArgs = new Options();
+            parsedArgs.Input = args[0];
+            parsedArgs.Output = args[1];
+            parsedArgs.Width = int.Parse(args[3]);
+            parsedArgs.Height = int.Parse(args[5]);
+            parsedArgs.FontName = args[7];
+            parsedArgs.FontSize = int.Parse(args[9]);
+            parsedArgs.Color = args[11];
+            var expectedUserConfig = new UserConfig(parsedArgs);
+
+            var client = new CommandLineClient(args);
+
+            client.UserConfig.Should().BeEquivalentTo(expectedUserConfig);
+        }
+
+        private Options GetDefaultOptions(string[] onlyNecessaryArgs)
+        {
+            var defaultOptions = new Options();
+            defaultOptions.Input = onlyNecessaryArgs[0];
+            defaultOptions.Output = onlyNecessaryArgs[1];
+            defaultOptions.Height = 1000;
+            defaultOptions.Width = 1000;
+            defaultOptions.FontName = "Arial";
+            defaultOptions.FontSize = 20;
+            defaultOptions.Color = "DeepSkyBlue";
+            return defaultOptions;
+        }
+
         private static void ShouldThrowArgumentNullWithArgs(string[] args)
         {
             FluentActions
