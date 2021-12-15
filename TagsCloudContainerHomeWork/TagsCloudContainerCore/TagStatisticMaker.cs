520be464bfa223456fc558b84b7644c6f25e6831
ivanarray <i.maliamow@yandex.ru>
2021-12-15 18:29:48 +0500

    Выпилил рефы, добавил текст для примера

--- TagsCloudContainerHomeWork/TagsCloudContainerCore/TagStatisticMaker.cs
+++ TagsCloudContainerHomeWork/TagsCloudContainerCore/TagStatisticMaker.cs
@@ -3,74 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using TagsCloudContainerCore.InterfacesCore;
 
-namespace TagsCloudContainerCore
+namespace TagsCloudContainerCore;
+
+public class TagStatisticMaker : IStatisticMaker
 {
-    public class TagStatisticMaker : IStatisticMaker
+    private readonly IDictionary<string, int> tagsWithCount = new Dictionary<string, int>();
+
+    private IList<KeyValuePair<string, int>> orderedPairs;
+
+    private bool isChangeTags;
+
+    private readonly Func<string, bool> tagFilter;
+
+    public TagStatisticMaker(Func<string, bool> tagFilter = null)
     {
-        private readonly IDictionary<string, int> tagsWithCount = new Dictionary<string, int>();
+        tagFilter ??= _ => true;
+        this.tagFilter = tagFilter;
+    }
 
-        private IList<KeyValuePair<string, int>> orderedPairs;
+    public IEnumerable<KeyValuePair<string, int>> CountedTags => tagsWithCount;
 
-        private bool isChangeTags;
-
-        private readonly Func<string, bool> tagFilter;
-
-        public TagStatisticMaker(Func<string, bool> tagFilter = null)
+    public KeyValuePair<string, int> GetMaxTag()
+    {
+        // ReSharper disable once InvertIf
+        if (isChangeTags)
         {
-            tagFilter ??= _ => true;
-            this.tagFilter = tagFilter;
+            orderedPairs = tagsWithCount.OrderBy(x => x.Value).ToList();
+            isChangeTags = false;
         }
 
-        public IEnumerable<KeyValuePair<string, int>> CountedTags => tagsWithCount;
+        return orderedPairs[^1];
+    }
 
-        public KeyValuePair<string, int> GetMaxTag()
+    public KeyValuePair<string, int> GetMinTag()
+    {
+        // ReSharper disable once InvertIf
+        if (isChangeTags)
         {
-            // ReSharper disable once InvertIf
-            if (isChangeTags)
-            {
-                orderedPairs = tagsWithCount.OrderBy(x => x.Value).ToList();
-                isChangeTags = false;
-            }
-
-            return orderedPairs[^1];
+            orderedPairs = tagsWithCount.OrderBy(x => x.Value).ToList();
+            isChangeTags = false;
         }
 
-        public KeyValuePair<string, int> GetMinTag()
-        {
-            // ReSharper disable once InvertIf
-            if (isChangeTags)
-            {
-                orderedPairs = tagsWithCount.OrderBy(x => x.Value).ToList();
-                isChangeTags = false;
-            }
+        return orderedPairs[0];
+    }
 
-            return orderedPairs[0];
+    public void AddTags(IEnumerable<string> tags)
+    {
+        foreach (var tag in tags)
+        {
+            AddTag(tag);
+        }
+    }
+
+    private void AddTag(string tag)
+    {
+        tag = tag.ToLowerInvariant();
+        if (!tagFilter(tag))
+        {
+            return;
         }
 
-        public void AddTags(IEnumerable<string> tags)
+        isChangeTags = true;
+
+        if (!tagsWithCount.ContainsKey(tag))
         {
-            foreach (var tag in tags)
-            {
-                AddTag(tag);
-            }
+            tagsWithCount[tag] = 0;
         }
 
-        private void AddTag(string tag)
-        {
-            tag = tag.ToLowerInvariant();
-            if (!tagFilter(tag))
-            {
-                return;
-            }
-            
-            isChangeTags = true;
-            
-            if (!tagsWithCount.ContainsKey(tag))
-            {
-                tagsWithCount[tag] = 0;
-            }
-
-            tagsWithCount[tag]++;
-        }
+        tagsWithCount[tag]++;
     }
 }
--- TagsCloudContainerHomeWork/TagsCloudContainerCore/TagToRender.cs
+++ TagsCloudContainerHomeWork/TagsCloudContainerCore/TagToRender.cs
@@ -1,26 +1,25 @@
 ﻿using System.Drawing;
 
-namespace TagsCloudContainerCore
+namespace TagsCloudContainerCore;
+
+public class TagToRender
 {
-    public class TagToRender
+    public string Value { get; }
+
+    public int ColorHex { get; }
+
+    public Point Location { get; }
+
+    public float FontSize { get; }
+
+    public string FontName { get; }
+
+    public TagToRender(Point location, string value, int colorHex, float fontSize, string fontName)
     {
-        public  string Value { get; }
-
-        public int ColorHex { get; }
-
-        public Point Location { get; }
-
-        public float FontSize { get; }
-        
-        public string FontName { get; }
-
-        public TagToRender(Point location, string value, int colorHex, float fontSize, string fontName)
-        {
-            Location = location;
-            Value = value;
-            ColorHex = colorHex;
-            FontSize = fontSize;
-            FontName = fontName;
-        }
+        Location = location;
+        Value = value;
+        ColorHex = colorHex;
+        FontSize = fontSize;
+        FontName = fontName;
     }
 }
