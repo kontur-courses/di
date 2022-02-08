ed6d7872ec53b23eb48e3d546acfdf29eec6c0a9
George Drak <sonicgd@gmail.com>
2022-02-08 14:39:54 +0500

    refactor DragonPainter and DragonFractalAction

--- FractalPainter/App/Actions/DragonFractalAction.cs
+++ FractalPainter/App/Actions/DragonFractalAction.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using FractalPainting.App.Fractals;
 using FractalPainting.Infrastructure.Common;
-using FractalPainting.Infrastructure.Injection;
 using FractalPainting.Infrastructure.UiActions;
-using Ninject;
 
 namespace FractalPainting.App.Actions
 {
-    public class DragonFractalAction : IUiAction, INeed<IImageHolder>
+    public class DragonFractalAction : IUiAction
     {
         private IImageHolder imageHolder;
+        private IDragonPainterFactory dragonPainterFactory;
+        private Func<DragonSettingsGenerator> dragonSettingsGeneratorFactory;
 
-        public void SetDependency(IImageHolder dependency)
+        public DragonFractalAction(IImageHolder imageHolder, IDragonPainterFactory dragonPainterFactory, Func<DragonSettingsGenerator> dragonSettingsGeneratorFactory)
         {
-            imageHolder = dependency;
+            this.imageHolder = imageHolder;
+            this.dragonPainterFactory = dragonPainterFactory;
+            this.dragonSettingsGeneratorFactory = dragonSettingsGeneratorFactory;
         }
 
         public string Category => "Фракталы";
@@ -22,19 +24,11 @@ namespace FractalPainting.App.Actions
 
         public void Perform()
         {
-            var dragonSettings = CreateRandomSettings();
+            var dragonSettings = dragonSettingsGeneratorFactory().Generate();
             // редактируем настройки:
             SettingsForm.For(dragonSettings).ShowDialog();
             // создаём painter с такими настройками
-            var container = new StandardKernel();
-            container.Bind<IImageHolder>().ToConstant(imageHolder);
-            container.Bind<DragonSettings>().ToConstant(dragonSettings);
-            container.Get<DragonPainter>().Paint();
-        }
-
-        private static DragonSettings CreateRandomSettings()
-        {
-            return new DragonSettingsGenerator(new Random()).Generate();
+            dragonPainterFactory.Create(imageHolder, dragonSettings).Paint();
         }
     }
 }
