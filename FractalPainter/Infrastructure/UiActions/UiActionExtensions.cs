ce0fb69ff834ec90ef4f30803b0262af8196b552
George Drak <sonicgd@gmail.com>
2022-02-08 14:44:49 +0500

    order menu items

--- FractalPainter/Infrastructure/UiActions/UiActionExtensions.cs
+++ FractalPainter/Infrastructure/UiActions/UiActionExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
@@ -8,11 +9,26 @@ namespace FractalPainting.Infrastructure.UiActions
     {
         public static ToolStripItem[] ToMenuItems(this IUiAction[] actions)
         {
-            var items = actions.GroupBy(a => a.Category)
+            var items = actions.GroupBy(a => a.Category).OrderBy(grouping => GetGroupOrder(grouping.Key))
                 .Select(g => CreateTopLevelMenuItem(g.Key, g.ToList()))
                 .Cast<ToolStripItem>()
                 .ToArray();
             return items;
+        }
+
+        private static int GetGroupOrder(string category)
+        {
+            switch (category)
+            {
+                case "Файл":
+                    return 1;
+                case "Фракталы":
+                    return 2;
+                case "Настройки":
+                    return 3;
+                default:
+                    throw new InvalidOperationException($"Unknown menu item category {category}");
+            }
         }
 
         private static ToolStripMenuItem CreateTopLevelMenuItem(string name, IList<IUiAction> items)
