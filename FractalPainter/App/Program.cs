4ffe0d0428d584f35d96e042c3c39622bca0bdc3
George Drak <sonicgd@gmail.com>
2022-02-08 14:44:38 +0500

    configure DI container

--- FractalPainter/App/Program.cs
+++ FractalPainter/App/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Windows.Forms;
+using FractalPainting.App.Fractals;
+using FractalPainting.Infrastructure.Common;
+using FractalPainting.Infrastructure.UiActions;
 using Ninject;
+using Ninject.Extensions.Factory;
+using Ninject.Extensions.Conventions;
 
 namespace FractalPainting.App
 {
@@ -17,11 +22,25 @@ namespace FractalPainting.App
                 var container = new StandardKernel();
 
                 // start here
-                // container.Bind<TService>().To<TImplementation>();
+                container.Bind(x =>
+                    x.FromThisAssembly().SelectAllClasses().InheritedFrom<IUiAction>().BindAllInterfaces());
+                container.Bind<IObjectSerializer>().To<XmlObjectSerializer>();
+                container.Bind<IBlobStorage>().To<FileBlobStorage>();
+                container.Bind<SettingsManager>().ToSelf().InSingletonScope();
+                container.Bind<PictureBoxImageHolder>().ToSelf().InSingletonScope();
+                container.Bind<IImageHolder>().ToMethod(context => context.Kernel.Get<PictureBoxImageHolder>());
+                container.Bind<ImageSettings>()
+                    .ToMethod(context => context.Kernel.Get<SettingsManager>().Load().ImageSettings);
+                container.Bind<IImageDirectoryProvider>()
+                    .ToMethod(context => context.Kernel.Get<SettingsManager>().Load());
+                container.Bind<IDragonPainterFactory>().ToFactory();
+                container.Bind<DragonSettingsGenerator>()
+                    .ToMethod(context => new DragonSettingsGenerator(new Random()));
+                container.Bind<Palette>().ToSelf();
 
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new MainForm());
+                Application.Run(container.Get<MainForm>());
             }
             catch (Exception e)
             {
