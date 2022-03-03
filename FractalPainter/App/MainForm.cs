8255f2bcfd8c51b0ddd2332742ea002c3a98b5e6
Alexander <zubkov_95@mail.ru>
2022-03-03 23:29:40 +0500

    fix: use PictureBoxImageHolder singleton instance

--- FractalPainter/App/MainForm.cs
+++ FractalPainter/App/MainForm.cs
@@ -11,7 +11,7 @@ namespace FractalPainting.App
 {
     public class MainForm : Form
     {
-        public MainForm(IUiAction[] actions)
+        public MainForm(IUiAction[] actions, PictureBoxImageHolder pictureBoxImageHolder)
         {
             var imageSettings = CreateSettingsManager().Load().ImageSettings;
             ClientSize = new Size(imageSettings.Width, imageSettings.Height);
@@ -19,16 +19,13 @@ namespace FractalPainting.App
             var mainMenu = new MenuStrip();
             mainMenu.Items.AddRange(actions.ToMenuItems());
             Controls.Add(mainMenu);
+            
+            pictureBoxImageHolder.RecreateImage(imageSettings);
+            pictureBoxImageHolder.Dock = DockStyle.Fill;
+            Controls.Add(pictureBoxImageHolder);
 
-            var pictureBox = new PictureBoxImageHolder();
-            pictureBox.RecreateImage(imageSettings);
-            pictureBox.Dock = DockStyle.Fill;
-            Controls.Add(pictureBox);
-
-            DependencyInjector.Inject<IImageHolder>(actions, pictureBox);
             DependencyInjector.Inject<IImageDirectoryProvider>(actions, CreateSettingsManager().Load());
             DependencyInjector.Inject<IImageSettingsProvider>(actions, CreateSettingsManager().Load());
-            DependencyInjector.Inject(actions, new Palette());
         }
 
         private static SettingsManager CreateSettingsManager()
