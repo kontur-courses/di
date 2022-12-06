0303288a933215be6e700bca7a6fcad18a937a8b
KsamNole <atm.tsvetk@gmail.com>
2022-12-07 01:27:42 +0500

    Create file generator

--- TagsCloudVisualization/FileGenerator.cs
+++ TagsCloudVisualization/FileGenerator.cs
@@ -8,16 +8,18 @@ namespace TagsCloudVisualization
         private readonly Random random = new Random();
         private readonly string[] words = new[]
         {
-            "Apple", "Microsoft", "JetBrains", "Wiki", "Windows", "CopyPaste", "Linux", "Tim Cook", "Bill Gates",
+            "Apple", "Microsoft", "JetBrains", "Wiki", "Windows", "CopyPaste", "Linux", "Tim", "Bill", "Tsvetkov", 
+            "Andronov", "Mishurin", "Konovalov", "Davletbaev", "Fruit", "Jenga", "Kubernetes", "Samsung", "Tinkoff", "SKB",
+            "Mother", "Alex", "Max", "SQWOZBAB", "Command", "Lion", "Discord", "Teams", "Son", "Pen", "Java", "C#", "Python",
             "We", "They", "He", "Him", "All",
             "Or", "And", "A", "An", "In", "Out"
         };
 
-        public void Generate(string fileName, int amountWords)
+        public void Generate(string fileName, int amountLines)
         {
             using var writer = new StreamWriter(fileName);
             
-            for (var i = 0; i < amountWords; i++)
+            for (var i = 0; i < amountLines; i++)
                 writer.WriteLine(words[random.Next(0, words.Length)]);
         }
     }
