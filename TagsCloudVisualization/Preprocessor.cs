02308361dd4369ecc087265babed3ac5cb7c0ad5
KsamNole <atm.tsvetk@gmail.com>
2022-12-07 01:30:42 +0500

    Create preproccessor

---
+++ TagsCloudVisualization/Preprocessor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NHunspell;
+using WeCantSpell.Hunspell;
+
+namespace TagsCloudVisualization
+{
+    public class Preprocessor
+    {
+        public List<string> Process(string fileName)
+        {
+            var words = new List<string>();
+            // var dicPath = string.Concat(Program.ProjectDirectory, @"\en_US.dic");
+            // var affPath = string.Concat(Program.ProjectDirectory, @"\en_US.aff");
+
+            // using var hunspell = new Hunspell(affPath, dicPath);
+            using var reader = new StreamReader(fileName);
+            
+            while (!reader.EndOfStream)
+            {
+                var word = reader.ReadLine();
+
+                if (string.IsNullOrEmpty(word))
+                    continue;
+
+                words.Add(word.ToLower());
+            }
+            
+            return words;
+        }
+    }
+}
