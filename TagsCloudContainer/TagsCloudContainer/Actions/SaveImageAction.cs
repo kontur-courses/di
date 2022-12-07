3e3e640c5bea74e647b8732b0e585c7ef9982523
Anton Yaskov <yaskovanton333@gmail.com>
2022-12-07 19:33:46 +0500

    Add SaveImageAction implementation

--- TagsCloudContainer/TagsCloudContainer/Actions/SaveImageAction.cs
+++ TagsCloudContainer/TagsCloudContainer/Actions/SaveImageAction.cs
@@ -3,18 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TagsCloudContainer.Infrastructure;
 
 namespace TagsCloudContainer.Actions
 {
     public class SaveImageAction : IUiAction
     {
+        private ImageHolder imageHolder;
+        private FileSettings fileSettings;
+
+        public SaveImageAction(ImageHolder imageHolder, FileSettings fileSettings)
+        {
+            this.imageHolder = imageHolder;
+            this.fileSettings = fileSettings;
+        }
+
         public string Category => "Файл";
         public string Name => "Сохранить как...";
         public string Description => "Сохранить файл как";
 
         public void Perform()
         {
-            throw new NotImplementedException();
+            var dialog = new SaveFileDialog
+            {
+                CheckFileExists = false,
+                InitialDirectory = Path.GetFullPath(fileSettings.ResultImagePath),
+                DefaultExt = "bmp",
+                FileName = "image.bmp",
+                Filter = "Изображения (*.bmp)|*.bmp"
+            };
+            var res = dialog.ShowDialog();
+            if (res == DialogResult.OK)
+                imageHolder.SaveImage(dialog.FileName);
         }
     }
 }
