52ccc4439398dc336b8468564ed6ebd1b6589375
massshasch <massshasch@gmail.com>
2022-12-08 04:36:49 +0500

    Added test

---
+++ TagsCloudContainer/Tests/RectangleCoordinatesCalculatorTests.cs
@@ -0,0 +1,38 @@
+/*using System;
+using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudContainer.Tests
+{
+    public class RectangleCoordinatesCalculatorTests
+    {
+        [Test]
+        public void CalculateRectangleCoordinates_ShouldReturnCorrectCoordinates_OnCorrectInput()
+        {
+            var size = new Size(100, 100);
+            var center = new Point(-100, -100);
+            var coordinates = RectangleCoordinatesCalculator.CalculateRectangleCoordinates(center, size);
+            coordinates.Should().BeEquivalentTo(new Point(-150, -150));
+        }
+
+        [Test]
+        public void CalculateRectangleCoordinates_ShouldReturnCorrectCoordinates_OnZeroSize()
+        {
+            var size = new Size(0, 0);
+            var center = new Point(0, 0);
+            var coordinates = RectangleCoordinatesCalculator.CalculateRectangleCoordinates(center, size);
+            coordinates.Should().BeEquivalentTo(new Point(0, 0));
+        }
+
+        [Test]
+        public void CalculateRectangleCoordinates_ShouldThrowArgumentException_WhenSizeLessThanZero()
+        {
+            var size = new Size(-1, -1);
+            var center = new Point(0, 0);
+            Action act = () => RectangleCoordinatesCalculator.CalculateRectangleCoordinates(center, size);
+            act.Should().Throw<ArgumentException>().WithMessage("Incorrect size of rectangle");
+        }
+    }
+}*/
+
