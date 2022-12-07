51746eb7bdc19aece4bdee627afd63f12f82abda
Anton Yaskov <yaskovanton333@gmail.com>
2022-12-07 20:36:59 +0500

    add AlgorithmSettingsAction implementation

--- TagsCloudContainer/TagsCloudContainer/Actions/AlgorithmSettingsAction.cs
+++ TagsCloudContainer/TagsCloudContainer/Actions/AlgorithmSettingsAction.cs
@@ -1,19 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TagsCloudContainer.Infrastructure;
 
 namespace TagsCloudContainer.Actions
 {
     public class AlgorithmSettingsAction : IUiAction
     {
+        private AlgorithmSettings algoSettings;
+
+        public AlgorithmSettingsAction(AlgorithmSettings algoSettings)
+        {
+            this.algoSettings = algoSettings;
+        }
+
         public string Category => "Алгоритм";
         public string Name => "Настройки...";
         public string Description => "Изменить настройки алгоритма";
         public void Perform()
         {
-            throw new NotImplementedException();
+            SettingsForm.For(algoSettings).ShowDialog();
         }
     }
 }
