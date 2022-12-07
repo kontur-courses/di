2d42efde2bfae8a6ad49cddc16f219e4b4e6be01
angst-storm <serezha.kiprin@gmail.com>
2022-12-07 23:01:08 +0500

    base WordsLoader implementation

---
+++ TagCloud/FileWordsLoader.cs
@@ -0,0 +1,18 @@
+﻿using TagCloud.Abstractions;
+
+namespace TagCloud;
+
+public class FileWordsLoader : IWordsLoader
+{
+    private readonly string filename;
+
+    public FileWordsLoader(string filename)
+    {
+        this.filename = filename;
+    }
+    
+    public IEnumerable<string> Load()
+    {
+        return File.ReadAllLines(filename);
+    }
+}
