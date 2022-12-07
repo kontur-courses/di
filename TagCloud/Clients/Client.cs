d995e845182a6b6b129b95d0317d00a6040290fc
Seekard <maks_byx@mail.ru>
2022-12-07 14:16:06 +0300

    Added console client and CommandLineOptions

---
+++ TagCloud/Clients/Client.cs
@@ -0,0 +1,46 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using TagCloud.Extensions;
+using TagCloud.Files;
+using TagCloud.Savers;
+using TagCloud.Words;
+
+namespace TagCloud.Clients;
+
+public class Client
+{
+    private readonly CloudDrawer _drawer;
+    private readonly IFile _fileWithWords;
+    private readonly TextFormatter _formatter;
+    private readonly CloudLayouter _layouter;
+    private readonly IBitmapSaver _saver;
+
+    public Client(CloudLayouter layouter, CloudDrawer drawer, TextFormatter formatter, IFile fileWithWords,
+        IBitmapSaver saver)
+    {
+        _layouter = layouter;
+        _drawer = drawer;
+        _formatter = formatter;
+        _fileWithWords = fileWithWords;
+        _saver = saver;
+    }
+
+    public void Draw(string destinationPath, Font font)
+    {
+        var text = _fileWithWords.ReadAll();
+        var words = _formatter.Format(text);
+        var wordRectangles = new List<WordRectangle>();
+        var fontSize = font.Size;
+        foreach (var word in words.OrderByDescending(word => word.Amount))
+        {
+            font = font.ChangeSize(fontSize * word.Frequency);
+            var rectangleSize = word.MeasureWord(font);
+            var wordRectangle = new WordRectangle(word) { Rectangle = _layouter.PutRectangle(rectangleSize) };
+            wordRectangles.Add(wordRectangle);
+        }
+
+        font = font.ChangeSize(fontSize);
+        var image = _drawer.CreateImage(wordRectangles, font);
+        _saver.Save(image, destinationPath, ImageFormat.Png);
+    }
+}
---
+++ TagCloud/Clients/CommandLineOptions.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using CommandLine;
+
+namespace TagCloud.Clients;
+
+public class CommandLineOptions
+{
+    [Option('i', "input", Required = true, HelpText = "Input file with words")]
+    public string InputFile { get; set; }
+
+    [Option('o', "output", Required = false, HelpText = "Output image")]
+    public string OutputFile { get; set; }
+
+    [Option('c', "colors", Required = false, Default = null, HelpText = "Colors to be used in image")]
+    public IEnumerable<Color> Colors { get; set; }
+
+    [Option('h', "height", Required = false, Default = 1080, HelpText = "Height of image")]
+    public int Height { get; set; }
+
+    [Option('w', "width", Required = false, Default = 1920, HelpText = "Width of image")]
+    public int Width { get; set; }
+
+    [Option("fontName", Required = false, Default = "Times New Roman", HelpText = "Font name")]
+    public string FontName { get; set; }
+
+    [Option("fontSize", Required = false, Default = 50, HelpText = "Font size")]
+    public float FontSize { get; set; }
+
+    [Option('a', "algorithm", Required = false, Default = "Spiral", HelpText = "Algorithm for generating image")]
+    public string Curve { get; set; }
+}
