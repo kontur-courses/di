0e6a1fd0b2b3e3f29d2d3dca3bc0f9da59ca9c37
Ovchinnikov Nikita <ovchinnikov.poisk@gmail.com>
2022-12-07 23:48:32 +0500

    Implemented the main logic

---
+++ TagsCloudVisualization/ProcessingSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+    internal class ProcessingSettings
+    {
+        private const int defaultMinWordLength = 2;
+        private const int defaultMaxWordLength = 30;
+        public int MinWordLength { get; set; }
+        public int MaxWordLength { get; set; }
+        public string[] ExcludedWords { get; set; }
+        //public Speech.PartsOfSpeech[] ExcludedPartsOfSpeech { get; set; }
+
+        public ProcessingSettings(
+            //Speech.PartsOfSpeech[] excludedPartsOfSpeech = new,
+            string[] excludedWords = null,
+            int minLength = defaultMinWordLength,
+            int maxLength = defaultMaxWordLength)
+        {
+            //ExcludedPartsOfSpeech = excludedPartsOfSpeech;
+            ExcludedWords = excludedWords != null ? excludedWords : new string[] { };
+            MinWordLength = minLength;
+            MaxWordLength = maxLength;
+        }
+    }
+}
---
+++ TagsCloudVisualization/TextReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace TagsCloudVisualization
+{
+    internal static class TextReader
+    {
+        public static string[] Read(string path)
+        {
+            if (File.Exists(path))
+            {
+                return File.ReadAllLines(path);
+            }
+
+            throw new System.IO.FileNotFoundException("File at the specified path does not exist");
+        }
+    }
+}
