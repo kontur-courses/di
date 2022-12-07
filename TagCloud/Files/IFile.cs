81110fdce6884352abe19b6d0a1e9510ff0f06ea
Seekard <maks_byx@mail.ru>
2022-12-07 14:13:06 +0300

    Added IFile, TxtFile

---
+++ TagCloud/Files/IFile.cs
@@ -0,0 +1,17 @@
+﻿namespace TagCloud.Files;
+
+public interface IFile
+{
+    public string Path { get; }
+    public string ReadAll();
+
+    public static IFile GetByFileExtension(string filename)
+    {
+        var extension = filename.Split('.')[^1];
+        return extension.ToLower() switch
+        {
+            "txt" => new TxtFile(filename),
+            _ => throw new ArgumentException($"This extension {extension} are not supported!")
+        };
+    }
+}
---
+++ TagCloud/Files/TxtFile.cs
@@ -0,0 +1,18 @@
+﻿namespace TagCloud.Files;
+
+public class TxtFile : IFile
+{
+    public TxtFile(string path)
+    {
+        Path = path;
+    }
+
+    public string Path { get; }
+
+    public string ReadAll()
+    {
+        using var stream = new StreamReader(Path);
+        var text = stream.ReadToEnd();
+        return text;
+    }
+}
