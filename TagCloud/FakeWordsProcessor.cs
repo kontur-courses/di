f2e661e3dc0eadb028291b801848b975160b5700
angst-storm <serezha.kiprin@gmail.com>
2022-12-07 23:01:51 +0500

    fake WordsProcessor implementation

---
+++ TagCloud/FakeWordsProcessor.cs
@@ -0,0 +1,11 @@
+﻿using TagCloud.Abstractions;
+
+namespace TagCloud;
+
+public class FakeWordsProcessor : IWordsProcessor
+{
+    public IEnumerable<string> Process(IEnumerable<string> words)
+    {
+        return words;
+    }
+}
