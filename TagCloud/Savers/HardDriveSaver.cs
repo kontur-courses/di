cd7c721738d34f910747b8157fbca78171710835
Seekard <maks_byx@mail.ru>
2022-12-07 14:12:44 +0300

    Added savers

---
+++ TagCloud/Savers/HardDriveSaver.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace TagCloud.Savers;
+
+public class HardDriveSaver : IBitmapSaver
+{
+    public void Save(Bitmap bitmap, string filename, ImageFormat format)
+    {
+        filename = $"{filename}.{format}";
+        bitmap.Save(filename, format);
+    }
+}
---
+++ TagCloud/Savers/IBitmapSaver.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace TagCloud.Savers;
+
+public interface IBitmapSaver
+{
+    public void Save(Bitmap bitmap, string name, ImageFormat format);
+}
