178ea5cf365cf755c6aa96f4dfeed6f189302c64
massshasch <massshasch@gmail.com>
2022-12-08 04:39:45 +0500

    Added main, files etc

---
+++ TagsCloudContainer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Autofac;
+using CommandLine;
+using TagsCloudContainer.FileOpeners;
+using TagsCloudContainer.FileSavers;
+using TagsCloudContainer.LayouterAlgorithms;
+using TagsCloudContainer.WordsColoringAlgorithms;
+
+namespace TagsCloudContainer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var parsedArguments = Parser.Default.ParseArguments<ConsoleUi>(args).Value;
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleUi>().AsSelf();
+            builder.RegisterType<TxtOpener>().As<IFileOpener>();
+            builder.RegisterType<PngSaver>().As<IFileSaver>();
+            builder.RegisterType<InputFileHandler>().AsSelf();
+            builder.RegisterType<BoringWordsDeleter>().AsSelf();
+            builder.RegisterType<DefaultStainer>().As<IWordStainer>();
+            builder.RegisterType<Spiral>().AsSelf();
+            builder.RegisterInstance(new CustomSettings(parsedArguments.FontName,
+                parsedArguments.BackGroungColor,
+                parsedArguments.BrushColor,
+                parsedArguments.PathToSave,
+                parsedArguments.PathToOpen,
+                parsedArguments.CanvasWidth,
+                parsedArguments.CanvasHeight,
+                parsedArguments.CanvasBorder,
+                parsedArguments.RadiusOffset,
+                parsedArguments.AngleOffset)).As<CustomSettings>();
+
+            builder.RegisterType<BoringWordsDeleter>().AsSelf();
+            builder.RegisterType<CoefficientCalculator>().AsSelf();
+            builder.RegisterType<Spiral>().AsSelf();
+            builder.RegisterType<CircularCloudLayouter>().As<ICloudLayouterAlgorithm>();
+            builder.RegisterType<CircularCloudDrawer>().AsSelf();
+            var container = builder.Build();
+            container.Resolve<CircularCloudDrawer>().DrawWords();
+        }
+    }
+}
