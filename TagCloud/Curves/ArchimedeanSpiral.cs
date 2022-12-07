468bbbb522bd18fc1eecdddc5efb78c7fb607be8
Seekard <maks_byx@mail.ru>
2022-12-07 14:13:35 +0300

    Added ICurve, ArchimedianSpiral

---
+++ TagCloud/Curves/ArchimedeanSpiral.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+
+namespace TagCloud.Curves;
+
+public class ArchimedeanSpiral : ICurve
+{
+    public ArchimedeanSpiral(double startRadius = 0, double extendRatio = 0.25)
+    {
+        if (startRadius < 0 || extendRatio <= 0)
+            throw new ArgumentException("Parameters cannot be negative.");
+        StartRadius = startRadius;
+        ExtendRatio = extendRatio;
+    }
+
+    public double StartRadius { get; }
+    public double ExtendRatio { get; }
+
+    public Point GetPoint(double angle)
+    {
+        var radius = StartRadius + ExtendRatio * angle;
+        var x = Convert.ToInt32(radius * Math.Cos(angle));
+        var y = Convert.ToInt32(radius * Math.Sin(angle));
+        return new Point(x, y);
+    }
+}
---
+++ TagCloud/Curves/ICurve.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace TagCloud.Curves;
+
+public interface ICurve
+{
+    Point GetPoint(double t);
+
+    public static ICurve GetByName(string name)
+    {
+        return name.ToLower() switch
+        {
+            "spiral" => new ArchimedeanSpiral(),
+            _ => throw new ArgumentException("There is no such algorithm")
+        };
+    }
+}
