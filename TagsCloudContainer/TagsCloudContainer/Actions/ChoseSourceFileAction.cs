3f3c2de743ae5b5ff9d19777903fb258734d50e8
Anton Yaskov <yaskovanton333@gmail.com>
2022-12-07 19:40:29 +0500

    Add ChoseSourceFileAction implementation

--- TagsCloudContainer/TagsCloudContainer/Actions/ChoseSourceFileAction.cs
+++ TagsCloudContainer/TagsCloudContainer/Actions/ChoseSourceFileAction.cs
@@ -3,18 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TagsCloudContainer.Infrastructure;
 
 namespace TagsCloudContainer.Actions
 {
     public class ChoseSourceFileAction : IUiAction
     {
+        private FileSettings fileSettings;
+
+        public ChoseSourceFileAction(FileSettings fileSettings)
+        {
+            this.fileSettings = fileSettings;
+        }
+
         public string Category => "Файл";
         public string Name => "Источник данных...";
         public string Description => "Выбрать источник данных для алгоритма";
 
         public void Perform()
         {
-            throw new NotImplementedException();
+            var dialog = new OpenFileDialog()
+            {
+                CheckFileExists = true,
+                InitialDirectory = Path.GetFullPath(fileSettings.SourceFilePath),
+                DefaultExt = "txt",
+                FileName = "source.txt",
+                Filter = "Текстовые файлы (*.txt)|*.txt"
+            };
+            var res = dialog.ShowDialog();
+            if (res == DialogResult.OK)
+                fileSettings.SourceFilePath = dialog.FileName;
         }
     }
 }
