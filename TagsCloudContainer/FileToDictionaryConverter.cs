1fdfe0c83d5af5b5b20ea3eddb7ac5dab8673777
Art <bartalamu@list.ru>
2022-12-08 22:17:08 +0500

    Converter + Filter

---
+++ TagsCloudContainer/FileToDictionaryConverter.cs
@@ -0,0 +1,82 @@
+﻿using System.Diagnostics;
+
+namespace TagsCloudContainer
+{
+    public class FileToDictionaryConverter : IConverter
+    {
+        private readonly IMyConfiguration configs;
+        private readonly IWordsFilter filter;
+
+        public FileToDictionaryConverter(IMyConfiguration configs, IWordsFilter filter)
+        {
+            this.configs = configs;
+            this.filter = filter;
+        }
+
+        public Dictionary<string, int> GetWordsInFile()
+        {
+            var cmd = $"mystem.exe {Path.Combine(configs.TextsPath, configs.WordsFileName)} out.txt -nig";
+
+            var proc = new ProcessStartInfo
+            {
+                UseShellExecute = true,
+                WorkingDirectory = Path.Combine(configs.TextsPath),
+                FileName = @"C:\Windows\System32\cmd.exe",
+                Arguments = "/C" + cmd,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+
+            Process.Start(proc);
+
+            var boringWords = File.ReadAllLines(Path.Combine(configs.TextsPath, configs.BoringWordsName))
+                .Select(x => x.ToLower())
+                .ToList();
+
+            var taggedWordFilePath = Path.Combine(configs.TextsPath, "out.txt");
+
+            while (IsFileLocked(new FileInfo(taggedWordFilePath)))
+                Task.Delay(5);
+
+            var taggedWords = File.ReadAllLines(taggedWordFilePath)
+                .ToList();
+
+            File.Delete(taggedWordFilePath);
+
+            var filteredWords = filter.FilterWords(taggedWords, boringWords);
+
+            var result = new Dictionary<string, int>();
+            filteredWords.ForEach(x =>
+            {
+                if (result.ContainsKey(x))
+                    result[x] += 1;
+
+                else result.Add(x, 1);
+            });
+
+            return result
+                .ToList()
+                .OrderByDescending(x => x.Value)
+                .ToDictionary(x => x.Key, x => x.Value);
+        }
+
+        private static bool IsFileLocked(FileInfo file)
+        {
+            try
+            {
+                using var stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None);
+                stream.Close();
+            }
+            catch (IOException)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    public interface IConverter
+    {
+        Dictionary<string, int> GetWordsInFile();
+    }
+}
---
+++ TagsCloudContainer/WordsFilter.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TagsCloudContainer
+{
+    public class WordsFilter : IWordsFilter
+    {
+        private readonly IMyConfiguration configs;
+
+        public WordsFilter(IMyConfiguration configs)
+        {
+            this.configs = configs;
+        }
+
+        public List<string> FilterWords(List<string> taggedWords, List<string> boringWords)
+        {
+            var excludedParticalsArr = configs.ExcludedParticals.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            var excludedParts = string.Join('|', excludedParticalsArr
+                .Select(x => "=(,|=)"
+                    .Insert(1, x))
+                .ToArray());
+            excludedParts = excludedParts.Length == 0 ? "= (,|=)" : excludedParts;
+            var regexString = "^(\\w+){((?!).)*$".Insert(11, excludedParts); // something like that ^(\w+){((?!=SPRO(,|=)|=PR(,|=)|=PART(,|=)|=CONJ(,|=)).)*$
+            var regex = new Regex(regexString);
+
+            var inputWords = taggedWords
+                .Where(x => regex.IsMatch(x))
+                .Select(x =>
+                {
+                    var match = regex.Match(x);
+                    return match.Groups[1].Value.ToLower();
+                }).ToList();
+
+            return inputWords.Where(x => !boringWords.Contains(x)).ToList();
+        }
+    }
+
+    public interface IWordsFilter
+    {
+        public List<string> FilterWords(List<string> taggedWords, List<string> boringWords);
+    }
+}
