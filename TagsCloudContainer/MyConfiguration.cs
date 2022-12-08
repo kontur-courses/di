e921a8096bf45f67222f3fd56944cf322f1457b7
Art <bartalamu@list.ru>
2022-12-08 22:16:28 +0500

    Program.cs + Configuration + ConfigValidator + mystem

---
+++ TagsCloudContainer/MyConfiguration.cs
@@ -0,0 +1,28 @@
+﻿namespace TagsCloudContainer
+{
+    public class MyConfiguration : IMyConfiguration
+    {
+        public int PictureSize { get; set; }
+        public int FontSize { get; set; }
+        public string TextsPath { get; set; } = string.Empty;
+        public string WordsFileName { get; set; } = string.Empty;
+        public string BoringWordsName { get; set; } = string.Empty;
+        public string Font { get; set; } = string.Empty;
+        public string BackgroundColor { get; set; } = string.Empty;
+        public string FontColor { get; set; } = string.Empty;
+        public string ExcludedParticals { get; set; } = string.Empty;
+    }
+
+    public interface IMyConfiguration
+    {
+        int PictureSize { get; set; }
+        int FontSize { get; set; }
+        string TextsPath { get; set; }
+        string WordsFileName { get; set; }
+        string BoringWordsName { get; set; }
+        string Font { get; set; }
+        string BackgroundColor { get; set; }
+        string FontColor { get; set; }
+        string ExcludedParticals { get; set; }
+    }
+}
---
+++ TagsCloudContainer/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using CloudLayout;
+using System.Reflection;
+
+namespace TagsCloudContainer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("settings.json", optional: false)
+                .AddCommandLine(args)
+                .Build();
+
+            var myConfig = configuration.Get<MyConfiguration>();
+            MyConfigValidator.ValidateConfig(myConfig);
+
+            var container = new ServiceCollection()
+                .AddSingleton<IInputOptions>(new InputOptions(myConfig.PictureSize))
+                .AddSingleton<IMyConfiguration>(myConfig)
+                .AddSingleton<IConverter, FileToDictionaryConverter>()
+                .AddSingleton<IWordsFilter, WordsFilter>()
+                .AddSingleton<ISpiralDrawer, SpiralDrawer>()
+                .AddSingleton<ILayout, CircularCloudLayout>()
+                .AddSingleton<IWordSizeCalculator, WordSizeCalculator>()
+                .AddSingleton<CloudDrawer>()
+                .BuildServiceProvider();
+
+            var drawer = ActivatorUtilities.CreateInstance<CloudDrawer>(container);
+
+            drawer.DrawCloud();
+        }
+    }
+}
