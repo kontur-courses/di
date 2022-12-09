d871ccec7d32bd8edc748fde58f8b2decde070ba
VladislavSharonov <fydoooooo@gmail.com>
2022-12-09 15:32:57 +0500

    Added TxtFileLoader file check

--- TagCloud/TagCloud/TxtFileLoader.cs
+++ TagCloud/TagCloud/TxtFileLoader.cs
@@ -4,6 +4,12 @@ public class TxtFileLoader : IFileLoader
 {
     public string Load(string path)
     {
+        if (string.IsNullOrEmpty(path))
+            throw new ArgumentException("Path is null or empty");
+        if (!File.Exists(path))
+            throw new FileNotFoundException($"File with path \"{path}\" isn't found: ");
+        if (Path.GetExtension(path) != ".txt")
+            throw new ArgumentException("File format is not .txt");
         return File.ReadAllText(path);
     }
 }
