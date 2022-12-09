306c88609eeaebaea59cb52898d5336492f34be2
Igor Sotarev <igor@sotarev.ru>
2022-12-09 23:41:17 +0500

    Add mystem parsing config

---
+++ TagCloud/Parser/ParsingConfig/MyStemParsingConfig/BoringWordsMyStemParsingConfig.cs
@@ -0,0 +1,21 @@
+﻿namespace TagCloud.Parser.ParsingConfig.MyStemParsingConfig;
+
+public class BoringWordsMyStemParsingConfig : MyStemParsingConfig
+{
+    private static readonly HashSet<string> ExcludedTypes = new HashSet<string>
+    {
+        "ADV",
+        "ADVPRO",
+        "APRO",
+        "CONJ",
+        "INTJ",
+        "PART",
+        "PR",
+        "SPRO"
+    };
+
+    public override bool IsWordExcluded(string word)
+    {
+        return ExcludedTypes.Contains(GetWordType(word));
+    }
+}
---
+++ TagCloud/Parser/ParsingConfig/MyStemParsingConfig/MyStemParsingConfig.cs
@@ -0,0 +1,120 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO.Compression;
+
+namespace TagCloud.Parser.ParsingConfig.MyStemParsingConfig;
+
+public abstract class MyStemParsingConfig : IParsingConfig
+{
+    public abstract bool IsWordExcluded(string word);
+    
+    private const string MyStemUrl = "http://download.cdn.yandex.net/mystem/mystem-3.1-win-64bit.zip";
+    private static readonly string WorkingDirectory = Environment.CurrentDirectory;
+    private readonly Regex wordTypeRegex = new Regex("=([a-zA-Z]{1,6})[=,]");
+    private readonly Process myStem;
+    
+    protected MyStemParsingConfig()
+    {
+        if (TryFindMyStem(out var myStemPath))
+        {
+            myStem = StartMyStem(myStemPath!);
+        }
+        else if (TryFindArchive(out var archivePath))
+        {
+            myStemPath = Extract(archivePath!);
+            myStem = StartMyStem(myStemPath);
+        }
+        else
+        {
+            archivePath = DownloadMyStem().Result;
+            myStemPath = Extract(archivePath);
+            myStem = StartMyStem(myStemPath);
+        }
+    }
+
+    protected string GetWordType(string word)
+    {
+        myStem.StandardInput.WriteLine(word);
+        myStem.StandardInput.Flush();
+        
+        return wordTypeRegex.Match(ReadMyStemOutput(myStem.StandardOutput)).Groups[0].Value;
+    }
+
+    private string ReadMyStemOutput(StreamReader myStemStandardOutput)
+    {
+        var builder = new StringBuilder();
+        while (myStemStandardOutput.Peek() > 0)
+        {
+            builder.Append((char)myStemStandardOutput.Read());
+        }
+
+        return builder.ToString();
+    }
+
+    private Process StartMyStem(string filepath)
+    {
+        return Process.Start(new ProcessStartInfo
+        {
+            FileName = filepath,
+            Arguments = "-i",
+            UseShellExecute = false,
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true,
+            StandardInputEncoding = Encoding.UTF8,
+            StandardOutputEncoding = Encoding.UTF8
+        })!;
+    }
+
+    private static string Extract(string archivePath)
+    {
+        Console.WriteLine("Extracting Mystem...");
+        var destination = Path.Combine(WorkingDirectory, "mystem");
+        ZipFile.ExtractToDirectory(archivePath, destination);
+
+        File.Delete(archivePath);
+        Console.Clear();
+        return Path.Combine(destination, "mystem.exe");
+    }
+
+    private async Task<string> DownloadMyStem()
+    {
+        Console.Write("Downloading Mystem");
+        var myStemPath = Path.Combine(WorkingDirectory, "mystem.zip");
+        using var client = new HttpClient();
+        var response = client.GetAsync(MyStemUrl);
+
+        while (!response.IsCompleted)
+        {
+            Console.Write(".");
+            Thread.Sleep(750);
+        }   
+        
+        await using var fs = new FileStream(myStemPath, FileMode.CreateNew);
+        await response.Result.Content.CopyToAsync(fs);
+
+
+        Console.Clear();
+        return myStemPath;
+    }
+
+    private static bool TryFindArchive(out string? filename)
+    {
+        filename = Directory
+            .GetFiles(Environment.CurrentDirectory, "mystem*.zip", SearchOption.AllDirectories)
+            .FirstOrDefault();
+
+        return filename != null;
+    }
+
+    private static bool TryFindMyStem(out string? filename)
+    {
+        filename = Directory
+            .GetFiles(Environment.CurrentDirectory, "mystem.exe", SearchOption.AllDirectories)
+            .FirstOrDefault();
+
+        return filename != null;
+    }
+}
