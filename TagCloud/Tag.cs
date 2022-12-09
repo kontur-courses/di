5a63cc45a5cb54e64cf9835a5589041a3ad37b44
Igor Sotarev <igor@sotarev.ru>
2022-12-09 16:59:46 +0500

    Add Tag and TagCloudGenerator

---
+++ TagCloud/Tag.cs
@@ -0,0 +1,5 @@
+﻿using System.Drawing;
+
+namespace TagCloud;
+
+public record Tag(string Word, int Size);
---
+++ TagCloud/TagCloudGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using TagCloud.ImageGenerator;
+using TagCloud.Parser;
+using TagCloud.WordSizingAlgorithm;
+
+namespace TagCloud;
+
+public class TagCloudGenerator
+{
+    private readonly ITagParser parser;
+    private readonly IWordSizingAlgorithm wordSizingAlgorithm;
+    private readonly IImageGenerator imageGenerator;
+
+    public TagCloudGenerator(ITagParser parser, IWordSizingAlgorithm wordSizingAlgorithm, 
+        IImageGenerator imageGenerator)
+    {
+        this.parser = parser;
+        this.wordSizingAlgorithm = wordSizingAlgorithm;
+        this.imageGenerator = imageGenerator;
+    }
+
+    public Image GenerateCloud(string filepath)
+    {
+        var tagMap = parser.Parse(filepath);
+        var measuredTags = wordSizingAlgorithm.GetTagSizes(tagMap);
+        return imageGenerator.GenerateImage(measuredTags);
+    }
+}
