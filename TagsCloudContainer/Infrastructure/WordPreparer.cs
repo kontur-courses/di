a2eeab747efda48b3a892a4f13ebe32c1cbb2c39
Hrenaki <shikhovwtr@mail.ru>
2022-12-09 12:16:27 +0300

    Added WordPreparer

---
+++ TagsCloudContainer/Infrastructure/WordPreparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainer.Infrastructure
+{
+    internal class WordPreparer : IWordPreparer
+    {
+        public string[] Prepare(IEnumerable<string> words)
+        {
+            return words.Select(word => word.ToLower()).ToArray();       
+        }
+    }
+}
