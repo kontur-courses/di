501f622efc731ef867273d6adae5210f63340457
Defalt <ilyaudalov1@mail.ru>
2022-12-09 20:40:52 +0500

    Image savers added

---
+++ TagCloudCreatorExtensions/ImageSavers/EmfImageSaver.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using TagCloudCreator.Interfaces;
+using TagCloudCreator.Interfaces.Providers;
+
+namespace TagCloudCreatorExtensions.ImageSavers;
+
+public class EmfImageSaver : IImageSaver
+{
+    private readonly IImagePathSettingsProvider _pathSettingsProvider;
+
+    public EmfImageSaver(IImagePathSettingsProvider pathSettingsProvider)
+    {
+        _pathSettingsProvider = pathSettingsProvider;
+    }
+
+    public string SupportedExtension => ".emf";
+
+    public void SaveImage(Image image)
+    {
+        image.Save(_pathSettingsProvider.GetImagePathSettings().ImagePath, ImageFormat.Emf);
+    }
+}
---
+++ TagCloudCreatorExtensions/ImageSavers/GifImageSaver.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using TagCloudCreator.Interfaces;
+using TagCloudCreator.Interfaces.Providers;
+
+namespace TagCloudCreatorExtensions.ImageSavers;
+
+public class GifImageSaver : IImageSaver
+{
+    private readonly IImagePathSettingsProvider _pathSettingsProvider;
+
+    public GifImageSaver(IImagePathSettingsProvider pathSettingsProvider)
+    {
+        _pathSettingsProvider = pathSettingsProvider;
+    }
+
+    public string SupportedExtension => ".gif";
+
+    public void SaveImage(Image image)
+    {
+        image.Save(_pathSettingsProvider.GetImagePathSettings().ImagePath, ImageFormat.Gif);
+    }
+}
