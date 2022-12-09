ef5b7bbdb9b52c183e61a2b98a3ea044e959ba47
Everal <89090080110@mail.ru>
2022-12-09 06:55:36 +0500

    Replace Tag

---
+++ TagsCloud/TagsCloud.Core/TagContainersCreators/Tag.cs
@@ -0,0 +1,13 @@
+﻿namespace TagsCloud.Core.TagContainersCreators;
+
+public class Tag
+{
+    public Tag(string word, int count)
+    {
+        Word = word;
+        Count = count;
+    }
+
+    public string Word { get; }
+    public int Count { get; }
+}
