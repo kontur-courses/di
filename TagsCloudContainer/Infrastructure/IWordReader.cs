95e4ef9af5a21e87599f5f3e7b8c45b2328847cf
Hrenaki <shikhovwtr@mail.ru>
2022-12-09 12:13:10 +0300

    Added IWordReader with TextFileReader

---
+++ TagsCloudContainer/Infrastructure/IWordReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainer.Infrastructure
+{
+    public interface IWordReader
+    {
+        public bool TryReadWords(string filename, out string[] words);
+    }
+}
---
+++ TagsCloudContainer/Infrastructure/TextFileReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainer.Infrastructure
+{
+    internal class TextFileReader : IWordReader
+    {
+        private const string fileExtension = ".txt";
+
+        public bool TryReadWords(string filename, out string[] words)
+        {
+            words = Array.Empty<string>();
+
+            if (!File.Exists(filename) || Path.GetExtension(filename) != fileExtension)
+                return false;
+
+            var wordList = new List<string>();
+            using var stream = new StreamReader(filename);
+            while (!stream.EndOfStream)
+                wordList.Add(stream.ReadLine()!);
+
+            words = wordList.ToArray();
+            return true;
+        }
+    }
+}
