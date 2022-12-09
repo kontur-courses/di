07a6497c3b37d657acaa441d51dfc1a5d76695e5
Hrenaki <shikhovwtr@mail.ru>
2022-12-09 12:10:54 +0300

    Added missed WordRectangle.cs

---
+++ TagsCloudContainer/Infrastructure/WordRectangle.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace TagsCloudContainer.Infrastructure
+{
+    public class WordRectangle
+    {
+        public RectangleF Rectangle { get; set; }
+        public string Word { get; set; }
+    }
+}
