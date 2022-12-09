f322350b47ea9ed271e4f6df98a4b0e01d186274
Hrenaki <shikhovwtr@mail.ru>
2022-12-09 12:11:52 +0300

    Added WordFontSizeProvider

---
+++ TagsCloudContainer/Infrastructure/WordFontSizeProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainer.Infrastructure
+{
+    public class WordFontSizeProvider : IWordFontSizeProvider
+    {
+        public float GetFontSize(string word)
+        {
+            return 14F;
+        }
+    }
+}
---
+++ TagsCloudContainer/Infrastructure/WordFontSizeProviderFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainer.Infrastructure
+{
+    public static class WordFontSizeProviderFactory
+    {
+        public static IWordFontSizeProvider CreateDefault(Dictionary<string, int> wordFrequences, float minFrequencyFontSize, float maxFrequencyFontSize)
+        {
+            return new WordFontSizeProvider();
+        }
+    }
+}
