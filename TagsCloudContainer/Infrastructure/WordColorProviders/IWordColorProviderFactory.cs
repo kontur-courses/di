44b85ab2f4b7ba36204ab75da4203dd01d0e947a
Hrenaki <shikhovwtr@mail.ru>
2022-12-10 20:05:15 +0300

    Added WordColorProviderFactory and WordConstColorProvider

--- TagsCloudContainer/Infrastructure/WordColorProviders/IWordColorProviderFactory.cs
+++ TagsCloudContainer/Infrastructure/WordColorProviders/IWordColorProviderFactory.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TagsCloudContainer.Infrastructure.Settings;
 
 namespace TagsCloudContainer.Infrastructure
 {
-    public class WordColorProvider : IWordColorProvider
+    public interface IWordColorProviderFactory
     {
-        public Color GetColor(string word)
-        {
-            return Color.BlueViolet;
-        }
+        public IWordColorProvider CreateDefault(WordColorSettings settings);
     }
 }
