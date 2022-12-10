c63b7e18b6f683d9a61bb9e323b2022a9c325ee5
Hrenaki <shikhovwtr@mail.ru>
2022-12-10 20:09:08 +0300

    Added Options to TagsCloudContainer.ConsoleApp

---
+++ TagsCloudContainer.ConsoleApp/Options.cs
@@ -0,0 +1,21 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainer.ConsoleApp
+{
+    internal class Options
+    {
+        [Option('f', "inputFilename", Required = true, HelpText = "Set input file with words")]
+        public string InputWordFilename { get; set; }
+
+        [Option('o', "outputFilename", Required = true, HelpText = "Set image output file")]
+        public string OutputTagsCloudFilename { get; set; }
+
+        [Option("font", Required = false, Default = "Consolas", HelpText = "Set word font family")]
+        public string FontFamily { get; set; }
+    }
+}
