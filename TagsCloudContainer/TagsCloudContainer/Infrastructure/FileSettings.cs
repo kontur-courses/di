ce77f074b40e987de97ba2a1b347e0fe4b16e536
Anton Yaskov <yaskovanton333@gmail.com>
2022-12-10 17:54:17 +0500

    some rename in FileSettings

--- TagsCloudContainer/TagsCloudContainer/Infrastructure/FileSettings.cs
+++ TagsCloudContainer/TagsCloudContainer/Infrastructure/FileSettings.cs
@@ -8,10 +8,10 @@ namespace TagsCloudContainer.Infrastructure
 {
     public class FileSettings
     {
-        public static string PathToProj = string.Join("\\", Environment.CurrentDirectory.Split('\\').SkipLast(3))
+        public static string PathToProjResources = string.Join("\\", Environment.CurrentDirectory.Split('\\').SkipLast(3))
             + @"\Sources";
-        public string ResultImagePath { get; set; } = PathToProj;
-        public string SourceFilePath { get; set; } = PathToProj + @"\source.txt";
-        public string BoringWordsFilePath { get; set; } = PathToProj + @"\boring.txt";
+        public string ResultImagePath { get; set; } = PathToProjResources;
+        public string SourceFilePath { get; set; } = PathToProjResources + @"\source.txt";
+        public string CustomBoringWordsFilePath { get; set; } = PathToProjResources + @"\boring.txt";
     }
 }
