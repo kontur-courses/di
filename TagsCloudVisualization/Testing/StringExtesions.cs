dd91c5079bb83581a9bd8a2d74871e1b5c0b6d97
ShpakovDaniel <44341164+Reltig@users.noreply.github.com>
2022-12-10 20:35:43 +0500

    add string extension

---
+++ TagsCloudVisualization/Testing/StringExtesions.cs
@@ -0,0 +1,17 @@
+﻿namespace Testing;
+
+public static class StringExtesions
+{
+    public static int SubstringCount(this string str, string sub)
+    {
+        int count = 0;
+        int i = str.IndexOf(sub);
+        while (i != -1)
+        {
+            i = str.IndexOf(sub, i + 1);
+            count++;
+        }
+
+        return count;
+    }
+}
