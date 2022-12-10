7e48b1045e2539dc4d129b933a0330be899959fb
Ilya <ilyapol92@gmail.com>
2022-12-10 19:27:04 +0500

    add settings form

---
+++ TagCloudGui/Actions/ImageSettingsAction.cs
@@ -0,0 +1,29 @@
+﻿using TagCloud.TagCloudVisualizations;
+using TagCloudGui.Infrastructure;
+using TagCloudGui.Infrastructure.Common;
+
+namespace TagCloudGui.Actions
+{
+    public class ImageSettingsAction : IUiAction
+    {
+        private readonly IImageHolder imageHolder;
+        private readonly ITagCloudVisualizationSettings settings;
+
+        public ImageSettingsAction(IImageHolder imageHolder,
+            ITagCloudVisualizationSettings settings)
+        {
+            this.imageHolder = imageHolder;
+            this.settings = settings;
+        }
+
+        public MenuCategory Category => MenuCategory.Settings;
+        public string Name => "Настройки";
+        public string Description => "Настройки облака";
+
+        public void Perform()
+        {
+            SettingsForm.For(settings).ShowDialog();
+            imageHolder.RecreateImage(settings);
+        }
+    }
+}
