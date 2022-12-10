157ed8d8caf8ceed9688a908aafce3f2958a0c8c
Hrenaki <shikhovwtr@mail.ru>
2022-12-10 20:06:39 +0300

    Added IWordFontSizeFactory to TagsCloudGenerator

--- TagsCloudContainer/Infrastructure/ITagsCloudGenerator.cs
+++ TagsCloudContainer/Infrastructure/ITagsCloudGenerator.cs
@@ -4,11 +4,12 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TagsCloudContainer.Infrastructure.Settings;
 
 namespace TagsCloudContainer.Infrastructure
 {
     public interface ITagsCloudGenerator
     {
-        public WordPlate[] GeneratePlates(IEnumerable<string> words, string fontName, PointF center);
+        public WordPlate[] GeneratePlates(IEnumerable<string> words, string fontName, PointF center, WordFontSizeSettings fontSizeSettings);
     }
 }
--- TagsCloudContainer/Infrastructure/TagsCloudGenerator.cs
+++ TagsCloudContainer/Infrastructure/TagsCloudGenerator.cs
@@ -2,29 +2,34 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using TagsCloudContainer.Infrastructure.Settings;
 
 namespace TagsCloudContainer.Infrastructure
 {
     public class TagsCloudGenerator : ITagsCloudGenerator
     {
         private readonly IWordLayoutBuilder wordLayoutBuilder;
-        private readonly IWordFontSizeProvider fontSizeProvider;
+        private readonly IWordFontSizeProviderFactory fontSizeProviderFactory;
 
-        public TagsCloudGenerator(IWordLayoutBuilder wordLayoutBuilder, IWordFontSizeProvider fontSizeProvider)
+        public TagsCloudGenerator(IWordLayoutBuilder wordLayoutBuilder, IWordFontSizeProviderFactory fontSizeProviderFactory)
         {
             this.wordLayoutBuilder = wordLayoutBuilder;
-            this.fontSizeProvider = fontSizeProvider;
+            this.fontSizeProviderFactory = fontSizeProviderFactory;
         }
 
-        public WordPlate[] GeneratePlates(IEnumerable<string> words, string fontName, PointF center)
+        public WordPlate[] GeneratePlates(IEnumerable<string> words, string fontName, PointF center, WordFontSizeSettings fontSizeSettings)
         {
+            var fontSizeProvider = fontSizeProviderFactory.CreateDefault(fontSizeSettings);
+
             var graphics = Graphics.FromImage(new Bitmap(1, 1));
             foreach (var word in words)
             {
                 var font = new Font(fontName, fontSizeProvider.GetFontSize(word));
-                wordLayoutBuilder.AddWord(word, graphics.MeasureString(word, font));
+                var floatSize = graphics.MeasureString(word, font);
+                wordLayoutBuilder.AddWord(word, new Size((int)Math.Ceiling(floatSize.Width), (int)Math.Ceiling(floatSize.Height)));
             }
 
             var wordRectangles = wordLayoutBuilder.Build(center);
