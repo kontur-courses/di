5bfdfb03dee3fc60880b102a0eac5d89b81451c6
O-Evgeniy <64973576+O-Evgeniy@users.noreply.github.com>
2022-12-11 18:48:36 +0500

    add image saver tests

---
+++ TagsCloudVisualization.Tests/ImageSaverTests/JpegImageSaverTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudVisualization.ImageSavers;
+
+namespace TagsCloudVisualization.Tests.ImageSaverTests;
+
+public class JpegImageSaverTests
+{
+    private string filename = "test";
+    private string filepath;
+
+    public JpegImageSaverTests()
+    {
+        filepath = Path.Combine(Directory.GetCurrentDirectory(), filename);
+    }
+
+    [TearDown]
+    public void DeleteFile()
+    {
+        File.Delete($"{filepath}.jpeg");
+    }
+
+
+    [Test]
+    public void Save_ShouldSaveImage()
+    {
+        var imageSaver = new JpegImageSaver();
+
+        imageSaver.Save(filepath, new Bitmap(10, 10));
+
+        var files = Directory.GetFiles(Directory.GetCurrentDirectory()).Select(Path.GetFileName);
+        files.Should().Contain($"{filename}.jpeg");
+    }
+}
---
+++ TagsCloudVisualization.Tests/ImageSaverTests/PngImageSaverTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudVisualization.ImageSavers;
+
+namespace TagsCloudVisualization.Tests.ImageSaverTests;
+
+[TestFixture]
+public class PngImageSaverTests
+{
+    private string filename = "test";
+    private string filepath;
+
+    public PngImageSaverTests()
+    {
+        filepath = Path.Combine(Directory.GetCurrentDirectory(), filename);
+    }
+
+    [TearDown]
+    public void DeleteFile()
+    {
+        File.Delete($"{filepath}.png");
+    }
+
+
+    [Test]
+    public void Save_ShouldSaveImage()
+    {
+        var imageSaver = new PngImageSaver();
+
+        imageSaver.Save(filepath, new Bitmap(10, 10));
+
+        var files = Directory.GetFiles(Directory.GetCurrentDirectory()).Select(Path.GetFileName);
+        files.Should().Contain($"{filename}.png");
+    }
+}
