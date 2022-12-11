1483df1f9178c532824bbec439609f2e856b7f98
nina_prokopova <nina30600@gmail.com>
2022-12-11 21:18:17 +0500

    Ещё реализация интерфейсов

---
+++ TagsCloud2/TagsCloudMaker/BitmapMaker/BitmapMaker.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+
+namespace TagsCloud2;
+
+public class BitmapMaker : IBitmapMaker
+{
+    private ILayouter layouter;
+    public BitmapMaker(ILayouter layouter)
+    {
+        this.layouter = layouter;
+    }
+    public Bitmap MakeBitmap(Size size, Dictionary<string, TextOptions> stringSizeAndOrientation,
+        string fontFamilyName, Brush color)
+    {
+        var tagCloud = new Bitmap(size.Width, size.Height);
+        PaintBackground(size, tagCloud);
+        using var graphics = Graphics.FromImage(tagCloud);
+        foreach (var item in stringSizeAndOrientation)
+        {
+            var rectangle = layouter.PutNextRectangle(item.Value.Size);
+            var position = new PointF(rectangle.X, rectangle.Y);
+            using var font =  new Font(fontFamilyName, item.Value.FontSize);
+            if (item.Value.Orientation == Orientation.Vertical)
+            {
+                var stringFormat = new StringFormat();
+                stringFormat.FormatFlags = StringFormatFlags.DirectionVertical;
+                graphics.DrawString("firstText", font, color, position, stringFormat);
+            }
+            else
+                graphics.DrawString("firstText", font, color, position);
+        }
+        return tagCloud;
+    }
+
+    private static void PaintBackground(Size size, Bitmap tagCloud)
+    {
+        for (var x = 0; x < size.Width; x++)
+        {
+            for (var y = 0; y < size.Height; y++)
+            {
+                var whiteColor = Color.FromArgb(255, 255, 255);
+                tagCloud.SetPixel(x, y, whiteColor);
+            }
+        }
+    }
+
+}
---
+++ TagsCloud2/TagsCloudMaker/Layouter/CircularCloudLayouter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using TagsCloud2;
+
+namespace TagsCloudVisualization
+{
+    public class CircularCloudLayouter : ILayouter
+    {
+
+        private Point center;
+        private List<Rectangle> pastRectangles;
+        
+        public CircularCloudLayouter(Point center)
+        {
+            this.center = center;
+            pastRectangles = new List<Rectangle>();
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            var spiralPoints = new SpiralPoints(center, rectangleSize.Height);
+            while (true)
+            {
+                spiralPoints.MoveNext();
+                var nextCenterPoint = spiralPoints.Current;
+                var newRectangle = new Rectangle(
+                    nextCenterPoint.X - rectangleSize.Width/2,
+                    nextCenterPoint.Y - rectangleSize.Height/2,
+                    rectangleSize.Width,
+                    rectangleSize.Height
+                    );
+                var isPositionWithoutCrossing = true;
+                for (int i = 0; i < pastRectangles.Count; i++)
+                {
+                    var intersection = Rectangle.Intersect(pastRectangles[i], newRectangle);
+                    if (intersection != Rectangle.Empty)
+                    {
+                        isPositionWithoutCrossing = false;
+                        break;
+                    }
+                }
+
+                if (isPositionWithoutCrossing)
+                {
+                    pastRectangles.Add(newRectangle);
+                    return newRectangle;
+                }
+            }
+        }
+    }
+}
