812ddba245045877f0dfc3c25c00d33b1b66d6eb
Hrenaki <shikhovwtr@mail.ru>
2022-12-13 01:57:02 +0300

    Added TagsCloudContainer.Tests.TagsCloudGeneratorTests

---
+++ TagsCloudContainer.Tests/TagsCloudGeneratorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagsCloudContainer.Infrastructure;
+
+namespace TagsCloudContainer.Tests.TagsCloudGeneratorTests
+{
+    [TestFixture]
+    internal class TagsCloudGenerator_Should
+    {
+        private ITagsCloudGenerator tagsCloudGenerator;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            
+        }
+    }
+}
