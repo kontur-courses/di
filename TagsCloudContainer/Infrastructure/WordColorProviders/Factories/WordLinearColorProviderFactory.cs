5ff4095edaf5efee830ff6c5a62c8c39cc7da8e9
Hrenaki <shikhovwtr@mail.ru>
2022-12-12 23:03:05 +0300

    Added WordLinearColorProvider

---
+++ TagsCloudContainer/Infrastructure/WordColorProviders/Factories/WordLinearColorProviderFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagsCloudContainer.Infrastructure.Settings;
+
+namespace TagsCloudContainer.Infrastructure
+{
+    public class WordLinearColorProviderFactory : IWordColorProviderFactory
+    {
+        public IWordColorProvider CreateDefault(WordColorSettings settings) => new WordLinearColorProvider(settings);
+    }
+}
---
+++ TagsCloudContainer/Infrastructure/WordColorProviders/WordLinearColorProvider.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagsCloudContainer.Infrastructure.Settings;
+
+namespace TagsCloudContainer.Infrastructure
+{
+    public class WordLinearColorProvider : IWordColorProvider
+    {
+        private readonly WordColorSettings settings;
+
+        private readonly int minFrequency;
+        private readonly int maxFrequency;
+        private readonly float frequencyDelta;
+
+        public WordLinearColorProvider(WordColorSettings settings) 
+        {
+            this.settings = settings;
+
+            var frequencies = settings.WordFrequencies.Values;
+            minFrequency = frequencies.Min();
+            maxFrequency = frequencies.Max();
+            frequencyDelta = maxFrequency - minFrequency;
+        }
+
+        public Color GetColor(string word)
+        {
+            if (!settings.WordFrequencies.ContainsKey(word))
+                throw new ArgumentException($"Can't generate color for word '{word}'", new KeyNotFoundException(word));
+
+            return GenerateColor(GetNormalizedFrequency(settings.WordFrequencies[word]));
+        }
+
+        private float GetNormalizedFrequency(int frequency)
+        {
+            return (frequency - minFrequency) / frequencyDelta;
+        }
+
+        private Color GenerateColor(float normalizedFrequency)
+        {
+            var startColor = settings.MinFrequencyColor;
+            var endColor = settings.MaxFrequencyColor;
+
+            return Color.FromArgb(GenerateColorComponent(startColor.A, endColor.A, normalizedFrequency),
+                                  GenerateColorComponent(startColor.R, endColor.R, normalizedFrequency),
+                                  GenerateColorComponent(startColor.G, endColor.G, normalizedFrequency),
+                                  GenerateColorComponent(startColor.B, endColor.B, normalizedFrequency));
+        }
+
+        private static int GenerateColorComponent(int start, int end, float step)
+        {
+            return start + (int)Math.Round(step * (end - start));
+        }
+    }
+}
