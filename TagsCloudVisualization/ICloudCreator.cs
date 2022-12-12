beaa8295388064e56f25a9565f008bd5f1b1e1d7
KsamNole <atm.tsvetk@gmail.com>
2022-12-12 19:43:05 +0500

    Add di container

---
+++ TagsCloudVisualization/ICloudCreator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public interface ICloudCreator
+    {
+        IEnumerable<Bitmap> Create(IEnumerable<string> words, int amountWords = -1, int amountClouds = 1);
+    }
+}
--- TagsCloudVisualization/Program.cs
+++ TagsCloudVisualization/Program.cs
@@ -1,4 +1,10 @@
-﻿using CommandLine;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using CommandLine;
+using Microsoft.Extensions.DependencyInjection;
+using TagsCloudVisualization.Configurations;
 using TagsCloudVisualization.Enums;
 
 namespace TagsCloudVisualization
@@ -7,13 +13,27 @@ namespace TagsCloudVisualization
     {
         public static void Main(string[] args)
         {
-            Parser.Default.ParseArguments<Options>(args)
-                .WithParsed(Options.RunOptions);
-            
-            var words = Preprocessor.Process(Options.WordsFilePath, 
-                PartSpeech.Noun | PartSpeech.Adjective);
+            var options = Parser.Default.ParseArguments<Options>(args).Value;
+            options.RunOptions();
 
-            TagCloudCreator.Create(words, 100);
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddSingleton<CloudConfiguration>((x) => CloudConfiguration.Default);
+            serviceCollection.AddSingleton<DistributionConfiguration>((x) => DistributionConfiguration.Default);
+            serviceCollection.AddScoped<ICloudLayouter, CircularCloudLayouter>();
+            serviceCollection.AddScoped<ICloudCreator, TagCloudCreator>();
+
+            var provider = serviceCollection.BuildServiceProvider();
+
+            var lines = File.ReadAllLines(options.WordsFilePath);
+            
+            var words = Preprocessor.Process(lines, 
+                PartSpeech.Noun | PartSpeech.Adjective);
+            
+            var bitmaps = provider.GetService<ICloudCreator>()!.Create(words, 100).ToList();
+
+            for (var i = 0; i < bitmaps.Count; i++)
+                bitmaps[i].Save(options.SaveTagCloudImagePath + $"cloud_{i}.{ImageFormat.Png}");
         }
     }
 }
