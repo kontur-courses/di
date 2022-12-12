c1a02638346f7d7147e1617a89bbb329a9adb54f
Ovchinnikov Nikita <ovchinnikov.poisk@gmail.com>
2022-12-12 23:02:58 +0500

    Added ImageRenderingSettings and its interface

---
+++ TagsCloudVisualization/ImageRendering/IImageRenderingSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization.ImageRendering
+{
+    public interface IImageRenderingSettings
+    {
+        public int Height { get; }
+        public int Width { get; }
+    }
+}
---
+++ TagsCloudVisualization/ImageRendering/ImageRenderingSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization.ImageRendering
+{
+    internal class ImageRenderingSettings : IImageRenderingSettings
+    {
+        public int Height { get; }
+        public int Width { get; }
+
+        public ImageRenderingSettings(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+    }
+}
