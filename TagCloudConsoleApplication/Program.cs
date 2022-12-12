2a0152f3d25749096ef6c209ff9a73854fa3155c
Ruden <karimov.ruden@mail.ru>
2022-12-13 00:47:35 +0300

    add Console Application

---
+++ TagCloudConsoleApplication/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using Autofac;
+using Autofac.Core;
+using CommandLine;
+using TagCloudConsoleApplication;
+using TagCloudPainter;
+using TagCloudPainter.Common;
+using TagCloudPainter.Interfaces;
+using TagCloudPainter.Layouters;
+using TagCloudPainter.Parsers;
+using TagCloudPainter.Savers;
+using TagCloudPainter.Sizers;
+
+namespace TagCloudConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<TagCloudOptions>(args)
+                .WithParsed<TagCloudOptions>(o =>
+                {
+                    var imageSettings = BuildImageSettings(o);
+                    var builder = new ContainerBuilder();
+                    builder.RegisterType<ImageSettingsProvider>().As<IImageSettingsProvider>().WithProperty("ImageSettings",imageSettings);
+                    builder.RegisterType<CloudPainter>().As<ICloudPainter>();
+                    builder.RegisterType<ParseSettingsProvider>().As<IParseSettingsProvider>()
+                        .WithProperty("ParseSettings", new ParseSettings()).SingleInstance();
+                    builder.RegisterType<TxtTagParser>().As<ITagParser>().SingleInstance();
+                    builder.RegisterType<WordSizer>().As<IWordSizer>();
+                    builder.RegisterType<CircularCloudLayouter>().As<ICloudLayouter>().WithParameters(
+                        new List<Parameter>
+                        {
+                            new NamedParameter("center", new Point(o.XCenter, o.YCenter)),
+                            new NamedParameter("angleStep", o.Angle),
+                            new NamedParameter("radiusStep", o.Radius)
+                        });
+                    builder.RegisterType<CloudElementBuilder>().As<ITagCloudElementsBuilder>();
+                    builder.RegisterType<PngTagCloudSaver>().As<ITagCloudSaver>().SingleInstance();
+
+                    var container = builder.Build();
+
+                    container.Resolve<ITagCloudSaver>().SaveTagCloud(o.OutputPath, o.InputPath);
+                });
+        }
+
+        private static ImageSettings BuildImageSettings(TagCloudOptions options)
+        {
+            return new ImageSettings()
+            {
+                Font = new Font(options.FontFamily, options.FontSize),
+                Palette = new Palette()
+                {
+                    BackgroundColor = Color.FromName(options.BackgroundColor.ToString()),
+                    TagsColor = Color.FromName(options.ForegroundColor.ToString())
+                },
+                Size = new Size(options.ImageWidth, options.ImageHeight)
+            };
+        }
+    }
+}
---
+++ TagCloudConsoleApplication/TagCloudOptions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace TagCloudConsoleApplication
+{
+    public class TagCloudOptions
+    {
+        [Option('w',"width", Required = false, HelpText = "Set image width", Default = 500)]
+        public int ImageWidth { get; set; }
+        [Option('h', "height", Required = false, HelpText = "Set image height", Default = 500)]
+        public int ImageHeight { get; set; }
+        [Option('i', "inputPath", Required = true, HelpText = "Set path to input file")]
+        public string InputPath { get; set; } = String.Empty;
+        [Option('o', "outputPath", Required = true, HelpText = "Set path to output file")]
+        public string OutputPath { get; set; } = String.Empty;
+
+        [Option("fontFamily", Required = false, HelpText = "Set tags font name", Default = "Times New Roman")]
+        public string FontFamily { get; set; }
+        [Option("fontSize", Required = false, HelpText = "Set tags size to font", Default = 10)]
+        public int FontSize { get; set; }
+        [Option("backgroundColor",Required = false, HelpText = "Set background color to image",Default = ConsoleColor.Blue)]
+        public ConsoleColor BackgroundColor { get; set; }
+        [Option("foregroundColor", Required = false, HelpText = "Set foreground color to image", Default = ConsoleColor.DarkGray)]
+        public ConsoleColor ForegroundColor { get; set; } = ConsoleColor.DarkGray;
+        [Option('r',"radius",Required = false, HelpText = "Set radius step to layouter", Default = 0.25)]
+        public double Radius { get; set; }
+        [Option('a',"angle", Required = false, HelpText = "Set angle step to layouter", Default = Math.PI/12)]
+        public double Angle { get; set; }
+        [Option('x', "xCoord", Required = false, HelpText = "Set x coord to center point", Default = 250)]
+        public int XCenter { get; set; }
+        [Option('y', "yCoord", Required = false, HelpText = "Set y coord to center point", Default = 250)]
+        public int YCenter { get; set; }
+    }
+}
