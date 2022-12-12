fefbc34f707e86f221dee415e8c1205630ab3455
Ovchinnikov Nikita <ovchinnikov.poisk@gmail.com>
2022-12-12 22:55:42 +0500

    Added ProcessingSettings and its interface

---
+++ TagsCloudVisualization/WordProcessors/WordProcessingSettings/IProcessingSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace TagsCloudVisualization.WordProcessors.WordProcessingSettings
+{
+    public interface IProcessingSettings
+    {
+        private const int defaultMinWordLength = 2;
+        private const int defaultMaxWordLength = 30;
+        public int MinWordLength { get; set; }
+        public int MaxWordLength { get; set; }
+        public string[] ExcludedWords { get; set; }
+    }
+}
---
+++ TagsCloudVisualization/WordProcessors/WordProcessingSettings/ProcessingSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TagsCloudVisualization.WordProcessors.WordProcessingSettings
+{
+    public class ProcessingSettings : IProcessingSettings
+    {
+        private const int defaultMinWordLength = 3;
+        private const int defaultMaxWordLength = 30;
+        public int MinWordLength { get; set; }
+        public int MaxWordLength { get; set; }
+        public string[] ExcludedWords { get; set; }
+
+        public ProcessingSettings(
+            string excludedWords,
+            int minLength = defaultMinWordLength,
+            int maxLength = defaultMaxWordLength)
+        {
+
+            ExcludedWords = ParseWord(excludedWords);
+            MinWordLength = minLength;
+            MaxWordLength = maxLength;
+        }
+
+        private string[] ParseWord(string words)
+        {
+            return words.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+    }
+}
