0bacc01b344b3b9b62c6173e72dcc93d9ca90d69
Ovchinnikov Nikita <ovchinnikov.poisk@gmail.com>
2022-12-12 23:04:03 +0500

    Added ImageSaver and its interface

---
+++ TagsCloudVisualization/Saver/IImageSaver.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization.Saver
+{
+    internal interface IImageSaver
+    {
+        public string Path { get; set; }
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+
+        public void Save(Image image);
+    }
+}
---
+++ TagsCloudVisualization/Saver/ImageSaver.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization.Saver
+{
+    internal class ImageSaver : IImageSaver
+    {
+        public string Path { get; set; }
+        public string FileName { get; set; }
+        public string FileExtension{ get; set; }
+
+
+        public void Save(Image image)
+        {
+            image.Save($"{Path}{FileName}{FileExtension}");
+        }
+
+        public ImageSaver(string path, string fileName, string fileExtension)
+        {
+            Path = path;
+            FileName = fileName;
+            FileExtension = fileExtension;
+        }
+    }
+}
