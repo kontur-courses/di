bde26e4883475f5b9263d44ffef30441dd213440
Ovchinnikov Nikita <ovchinnikov.poisk@gmail.com>
2022-12-12 22:58:51 +0500

    Added WordProcessor and its interface

---
+++ TagsCloudVisualization/WordProcessors/IWordProcessor.cs
@@ -0,0 +1,11 @@
+﻿using TagsCloudVisualization.WordProcessors.WordProcessingSettings;
+
+namespace TagsCloudVisualization.WordProcessors
+{
+    public interface IWordProcessor
+    {
+        public IProcessingSettings Settings { get; }
+        public string[] Process(string[] words);
+        public bool WordIsAllowed(string word);
+    }
+}
---
+++ TagsCloudVisualization/WordProcessors/WordProcessor.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using TagsCloudVisualization.WordProcessors.WordProcessingSettings;
+
+namespace TagsCloudVisualization.WordProcessors
+{
+    public class WordProcessor : IWordProcessor 
+    {
+        public IProcessingSettings Settings { get; }
+
+        public bool WordIsAllowed(string word)
+        {
+            return word.Length >= Settings.MinWordLength &&
+                   word.Length <= Settings.MaxWordLength &&
+                   !Settings.ExcludedWords.Contains(word);
+        }
+
+        public string[] Process(string[] words)
+        {
+            return words.Where(WordIsAllowed)
+                 .Select(word => word.ToLower()).ToArray();
+        }
+
+        public WordProcessor(IProcessingSettings settings)
+        {
+            Settings = settings;
+        }
+    }
+}
