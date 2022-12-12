57c8c4d91986a8a1750297132708139c8202b6c0
Ovchinnikov Nikita <ovchinnikov.poisk@gmail.com>
2022-12-12 23:00:40 +0500

    Added Frequency and its interface

---
+++ TagsCloudVisualization/Frequency/FrequencyCounter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using TagsCloudVisualization.Storages;
+
+namespace TagsCloudVisualization.Frequency
+{
+    internal class FrequencyCounter : IFrequencyCounter
+    {
+        public IEnumerable<string> Elements { get; set; }
+
+        public FrequencyCounter(IWordStorage storage)
+        {
+            Elements = storage.Words;
+        }
+
+        public Dictionary<string, int> GetFrequency()
+        {
+            var frequency = new Dictionary<string, int>();
+            
+            foreach (var element in Elements)
+            {
+                if (!frequency.ContainsKey(element))
+                {
+                    frequency.Add(element, 1);
+                }
+                else
+                {
+                    frequency[element]++;
+                }
+            }
+
+            return frequency;
+        }
+    }
+}
---
+++ TagsCloudVisualization/Frequency/IFrequencyCounter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TagsCloudVisualization.Frequency
+{
+    public interface IFrequencyCounter
+    {
+        public IEnumerable<string> Elements { get; set; }
+        public Dictionary<string, int> GetFrequency();
+    }
+}
