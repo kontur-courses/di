4ec108357270d1790a1920ada4552e302e707734
Hrenaki <shikhovwtr@mail.ru>
2022-12-12 20:56:04 +0300

    Added WordDynamicFontSizeProvider with its Factory

---
+++ TagsCloudContainer/Infrastructure/WordFontSizeProviders/WordDynamicFontSizeProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagsCloudContainer.Infrastructure.Settings;
+
+namespace TagsCloudContainer.Infrastructure
+{
+    public class WordDynamicFontSizeProvider : IWordFontSizeProvider
+    {
+        private readonly WordFontSizeSettings settings;
+
+        private readonly int minFrequency;
+        private readonly int maxFrequency;
+        private readonly float frequencyDelta;
+
+        private readonly float fontSizeDelta;
+
+        public WordDynamicFontSizeProvider(WordFontSizeSettings settings) 
+        {
+            this.settings = settings;
+            fontSizeDelta = settings.MaxFrequencyFontSize - settings.MinFrequencyFontSize;
+
+            minFrequency = settings.WordFrequencies.Values.Min();
+            maxFrequency = settings.WordFrequencies.Values.Max();
+            frequencyDelta = maxFrequency - minFrequency;
+        }
+
+        public float GetFontSize(string word)
+        {
+            if (!settings.WordFrequencies.ContainsKey(word))
+                throw new ArgumentException($"Can't generate font size for { word }");
+            return GetFontSizeWithNormalizedFrequency(GetNormalizedFrequency(settings.WordFrequencies[word]));
+        }
+
+        private float GetNormalizedFrequency(int frequency)
+        {
+            return (frequency - minFrequency) / frequencyDelta;
+        }
+
+        private float GetFontSizeWithNormalizedFrequency(float normalizedFrequency)
+        {
+            return settings.MinFrequencyFontSize + fontSizeDelta * normalizedFrequency;
+        }
+    }
+}
---
+++ TagsCloudContainer/Infrastructure/WordFontSizeProviders/WordDynamicFontSizeProviderFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagsCloudContainer.Infrastructure.Settings;
+
+namespace TagsCloudContainer.Infrastructure
+{
+    public class WordDynamicFontSizeProviderFactory : IWordFontSizeProviderFactory
+    {
+        public IWordFontSizeProvider CreateDefault(WordFontSizeSettings settings) => new WordDynamicFontSizeProvider(settings);
+    }
+}
