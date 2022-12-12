54a8dbc8e9c3b35d1d90023b031ff9cb41ce88c4
Tupikov Ivan <i.tupikov12@gmail.com>
2022-12-12 05:07:20 +0500

    Added WordHandlerTest

---
+++ TagsCloudContainerTests/WordHandlerTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using TagsCloudContainer;
+
+namespace TagsCloudContainerTests
+{
+    public class WordHandlerTest
+    {
+        private Settings _settings;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _settings = new Settings()
+            {
+                WordFontName = "Arial",
+                WordFontSize = 16
+            };
+        }
+
+        [TestCase("OneCharacters.txt")]
+        [TestCase("AllWordsAreBoring.txt")]
+        public void ProcessWords_ShouldReturnEmptyArray(string fileName)
+        {
+            var handler = new WordHandler(fileName);
+            var words = handler.ProcessWords(_settings);
+            words.Should().BeEmpty();
+        }
+    }
+}
