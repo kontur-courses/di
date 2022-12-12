5b0b46435429b6fc159fa9f0e44be78714c2acae
Tupikov Ivan <i.tupikov12@gmail.com>
2022-12-12 06:17:55 +0500

    Refactor WordHandlerTest

--- TagsCloudContainerTests/WordHandlerTest.cs
+++ TagsCloudContainerTests/WordHandlerTest.cs
@@ -6,24 +6,33 @@ namespace TagsCloudContainerTests
 {
     public class WordHandlerTest
     {
-        private Settings _settings;
+        private TxtReader _txtReader;
         
         [SetUp]
         public void SetUp()
         {
-            _settings = new Settings()
-            {
-                WordFontName = "Arial",
-                WordFontSize = 16
-            };
+            _txtReader = new TxtReader();
         }
 
         [TestCase("OneCharacters.txt")]
         [TestCase("AllWordsAreBoring.txt")]
+        [TestCase("OneCharacters.docx")]
+        [TestCase("AllWordsAreBoring.docx")]
         public void ProcessWords_ShouldReturnEmptyArray(string fileName)
         {
-            var handler = new WordHandler(fileName);
-            var words = handler.ProcessWords(_settings);
+            Settings settings = new Settings()
+            {
+                WordFontName = "Arial",
+                WordFontSize = 16,
+                FileName = fileName
+            };
+            WordHandler handler = null;
+            if(fileName.Contains("docx"))
+               handler = new WordHandler(new WordReader(), settings);
+            if (fileName.Contains("txt"))
+                handler = new WordHandler(new TxtReader(), settings);
+            
+            var words = handler.ProcessWords();
             words.Should().BeEmpty();
         }
     }
