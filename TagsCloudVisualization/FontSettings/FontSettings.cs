fc4316d2e6c0b5a8677c9e9307d7686afad19f08
Ovchinnikov Nikita <ovchinnikov.poisk@gmail.com>
2022-12-12 22:53:58 +0500

    Added FontSettings and its interface

---
+++ TagsCloudVisualization/FontSettings/FontSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+
+namespace TagsCloudVisualization.FontSettings
+{
+    internal class FontSettings : IFontSettings
+    {
+        public FontFamily FontFamily { get; set; }
+        public string FontColor { get; set; }
+
+        private readonly string defaultFontFamily = "Arial";
+
+        public FontSettings(string fontFamily, string fontColor)
+        {
+            FontFamily = TryGetFontFamily(fontFamily);
+            FontColor = fontColor;
+        }
+
+        private FontFamily TryGetFontFamily(string fontFamily)
+        {
+            try
+            {
+                return new FontFamily(fontFamily);
+            }
+            catch (ArgumentException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Font {fontFamily} cannot be found. The default value has been set: {defaultFontFamily}");
+                Console.ResetColor();
+                return new FontFamily(defaultFontFamily);
+            }
+        }
+    }
+}
---
+++ TagsCloudVisualization/FontSettings/IFontSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization.FontSettings
+{
+    public interface IFontSettings
+    {
+        public FontFamily FontFamily { get; set; }
+        public string FontColor { get; set; }
+    }
+}
