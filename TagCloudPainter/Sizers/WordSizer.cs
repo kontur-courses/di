0a1ba62df36b72d12a65ff20f69c454fae808e74
Ruden <karimov.ruden@mail.ru>
2022-12-13 00:46:22 +0300

    add WordSizer and Saver Class

---
+++ TagCloudPainter/Sizers/WordSizer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagCloudPainter.Interfaces;
+
+namespace TagCloudPainter.Sizers
+{
+    public class WordSizer : IWordSizer
+    {
+        public Size GetTagSize(string word, int count)
+        {
+            var width = word.Length * 7;
+            var height = width / 3;
+            return new Size(width, height);
+        }
+    }
+}
