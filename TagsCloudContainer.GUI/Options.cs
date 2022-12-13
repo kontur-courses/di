cc1aac3ad839eb356208ba3c94977c72ae0974b8
Hrenaki <shikhovwtr@mail.ru>
2022-12-13 15:03:44 +0300

    Added OptionsViewModel

--- TagsCloudContainer.GUI/Options.cs
+++ TagsCloudContainer.GUI/Options.cs
@@ -1,130 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Printing;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TagsCloudContainer.GUI
 {
-    public class Options : INotifyPropertyChanged
+    public class Options
     {
-        private string inputWordFilename;
-        public string InputWordFilename 
-        {
-            get => inputWordFilename;
-            set
-            {
-                if (value == inputWordFilename) return;
-                inputWordFilename = value;
-                OnPropertyChanged(nameof(InputWordFilename));
-            }
-        }
+        public string InputWordFilename { get; set; }
 
-        private string outputWordFilename;
-        public string OutputImageFilename
-        {
-            get => outputWordFilename;
-            set
-            {
-                if (value == outputWordFilename) return;
-                outputWordFilename = value;
-                OnPropertyChanged(nameof(OutputImageFilename));
-            }
-        }
+        public string OutputImageFilename { get; set; }
 
-        private int outputImageWidth = 500;
-        public int OutputImageWidth
-        {
-            get => outputImageWidth;
-            set
-            {
-                if(value == outputImageWidth) return;
-                outputImageWidth = value;
-                OnPropertyChanged(nameof(OutputImageWidth));
-            }
-        }
+        public int OutputImageWidth { get; set; } = 500;
 
-        private int outputImageHeight = 500;
-        public int OutputImageHeight
-        {
-            get => outputImageHeight;
-            set
-            {
-                if(value == outputImageHeight) return;
-                outputImageHeight = value;
-                OnPropertyChanged(nameof(OutputImageHeight));
-            }
-        }
+        public int OutputImageHeight { get; set; } = 500;
 
-        private string fontFamily = "Consolas";
-        public string FontFamily
-        {
-            get => fontFamily;
-            set
-            {
-                if(value == fontFamily) return;
-                fontFamily = value;
-                OnPropertyChanged(nameof(FontFamily));
-            }
-        }
+        public string FontFamily { get; set; } = "Consolas";
 
-        private string minFrequencyColorString = "#FFFFAA00";
-        public string MinFrequencyColorString
-        {
-            get => minFrequencyColorString;
-            set
-            {
-                if(value == minFrequencyColorString) return;
-                minFrequencyColorString = value;
-                OnPropertyChanged(nameof(MinFrequencyColorString));
-            }
-        }
+        public string MinFrequencyColorString { get; set; } = "#FFFFAA00";
 
-        private string maxFrequencyColorString = "#FFFF0000";
-        public string MaxFrequencyColorString
-        {
-            get => maxFrequencyColorString;
-            set
-            {
-                if(value == maxFrequencyColorString) return;
-                maxFrequencyColorString = value;
-                OnPropertyChanged(nameof(MaxFrequencyColorString));
-            }
-        }
+        public string MaxFrequencyColorString { get; set; } = "#FFFF0000";
 
-        private float minFrequncyFontSize = 14F;
-        public float MinFrequncyFontSize
-        {
-            get => minFrequncyFontSize;
-            set 
-            {
-                if(value == minFrequncyFontSize) return;
-                minFrequncyFontSize = value;
-                OnPropertyChanged(nameof(MinFrequncyFontSize));
-            }
-        }
+        public float MinFrequncyFontSize { get; set; } = 14F;
 
-        private float maxFrequncyFontSize = 24F;
-        public float MaxFrequncyFontSize
-        {
-            get => maxFrequncyFontSize;
-            set
-            {
-                if(value != maxFrequncyFontSize) return;
-                maxFrequncyFontSize = value;
-                OnPropertyChanged(nameof(MaxFrequncyFontSize));
-            }
-        }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-        public void OnPropertyChanged([CallerMemberName] string prop = "")
-        {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(prop));
-        }
+        public float MaxFrequncyFontSize { get; set; } = 30F;
     }
 }
---
+++ TagsCloudContainer.GUI/OptionsViewModel.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Printing;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudContainer.GUI
+{
+    public class OptionsViewModel : INotifyPropertyChanged
+    {
+        private static readonly PropertyInfo[] properties = typeof(OptionsViewModel).GetProperties();
+
+        private Options currentOptions;
+        public Options CurrentOptions
+        {
+            get => currentOptions;
+            set
+            {
+                if(value == currentOptions) return;
+                currentOptions = value;
+
+                foreach (var property in properties)
+                    OnPropertyChanged(property.Name);
+            }
+        }
+
+        public string InputWordFilename 
+        {
+            get => currentOptions.InputWordFilename;
+            set
+            {
+                if (value == currentOptions.InputWordFilename) return;
+                currentOptions.InputWordFilename = value;
+                OnPropertyChanged(nameof(InputWordFilename));
+            }
+        }
+
+        public string OutputImageFilename
+        {
+            get => currentOptions.OutputImageFilename;
+            set
+            {
+                if (value == currentOptions.OutputImageFilename) return;
+                currentOptions.OutputImageFilename = value;
+                OnPropertyChanged(nameof(OutputImageFilename));
+            }
+        }
+
+        public int OutputImageWidth
+        {
+            get => currentOptions.OutputImageWidth;
+            set
+            {
+                if(value == currentOptions.OutputImageWidth) return;
+                currentOptions.OutputImageWidth = value;
+                OnPropertyChanged(nameof(OutputImageWidth));
+            }
+        }
+
+        public int OutputImageHeight
+        {
+            get => currentOptions.OutputImageHeight;
+            set
+            {
+                if(value == currentOptions.OutputImageHeight) return;
+                currentOptions.OutputImageHeight = value;
+                OnPropertyChanged(nameof(OutputImageHeight));
+            }
+        }
+
+        public string FontFamily
+        {
+            get => currentOptions.FontFamily;
+            set
+            {
+                if(value == currentOptions.FontFamily) return;
+                currentOptions.FontFamily = value;
+                OnPropertyChanged(nameof(FontFamily));
+            }
+        }
+
+        public string MinFrequencyColorString
+        {
+            get => currentOptions.MinFrequencyColorString;
+            set
+            {
+                if(value == currentOptions.MinFrequencyColorString) return;
+                currentOptions.MinFrequencyColorString = value;
+                OnPropertyChanged(nameof(MinFrequencyColorString));
+            }
+        }
+
+        public string MaxFrequencyColorString
+        {
+            get => currentOptions.MaxFrequencyColorString;
+            set
+            {
+                if(value == currentOptions.MaxFrequencyColorString) return;
+                currentOptions.MaxFrequencyColorString = value;
+                OnPropertyChanged(nameof(MaxFrequencyColorString));
+            }
+        }
+
+        public float MinFrequncyFontSize
+        {
+            get => currentOptions.MinFrequncyFontSize;
+            set 
+            {
+                if(value == currentOptions.MinFrequncyFontSize) return;
+                currentOptions.MinFrequncyFontSize = value;
+                OnPropertyChanged(nameof(MinFrequncyFontSize));
+            }
+        }
+
+        public float MaxFrequncyFontSize
+        {
+            get => currentOptions.MaxFrequncyFontSize;
+            set
+            {
+                if(value != currentOptions.MaxFrequncyFontSize) return;
+                currentOptions.MaxFrequncyFontSize = value;
+                OnPropertyChanged(nameof(MaxFrequncyFontSize));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
