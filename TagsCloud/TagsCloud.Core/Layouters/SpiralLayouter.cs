50a0cb3e0a23ec4bb84f95eafba33dfdb791ae2e
Everal <89090080110@mail.ru>
2022-12-13 07:08:56 +0500

    Add SpiralLayouter

---
+++ TagsCloud/TagsCloud.Core/Layouters/SpiralLayouter.cs
@@ -0,0 +1,44 @@
+﻿using System.Drawing;
+using TagsCloud.Core.Helpers;
+using TagsCloud.Core.Layouters.Spirals;
+
+namespace TagsCloud.Core.Layouters;
+
+public class SpiralCloudLayouter : ICloudLayouter
+{
+	private readonly List<Rectangle> placedRectangles;
+
+	private readonly ISpiral spiral;
+
+	public SpiralCloudLayouter(Point center, double spiralStepInPixels, double angleStepInDegrees)
+	{
+		spiral = new ArchimedeanSpiral(center, spiralStepInPixels, angleStepInDegrees);
+		placedRectangles = new List<Rectangle>();
+	}
+
+	public Rectangle PutNextRectangle(Size rectangleSize)
+	{
+		var rectangleOnSpiral = GetRectangleOnSpiral(rectangleSize);
+
+		placedRectangles.Add(rectangleOnSpiral);
+
+		return rectangleOnSpiral;
+	}
+
+	private Rectangle GetRectangleOnSpiral(Size rectangleSize)
+	{
+		Rectangle newRectangle;
+
+		do
+		{
+			newRectangle = RectangleCreator.GetRectangle(spiral.GetNextPoint(), rectangleSize);
+		} while (IntersectsWithPlacedRectangles(newRectangle));
+
+		return newRectangle;
+	}
+
+	private bool IntersectsWithPlacedRectangles(Rectangle rectangle)
+	{
+		return placedRectangles.Any(placedRectangle => placedRectangle.IntersectsWith(rectangle));
+	}
+}
