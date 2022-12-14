8f7e4ac6d32e700a1afb605c8532bc0746a6bbd0
angst-storm <serezha.kiprin@gmail.com>
2022-12-15 00:42:47 +0500

    expanded options

--- ConsoleClient/Options.cs
+++ ConsoleClient/Options.cs
@@ -4,9 +4,37 @@ namespace ConsoleClient;
 
 public class Options
 {
-    [Option('s', "source", Required = false, HelpText = "Source filepath")]
-    public string? Source { get; set; }
+    [Value(0, Required = true, HelpText = "Source filepath", MetaValue = nameof(String))]
+    public string Source { get; set; } = "";
 
-    [Option('r', "result", Required = false, HelpText = "Result filepath")]
-    public string? Result { get; set; }
+    [Value(1, Required = true, HelpText = "Result filepath", MetaValue = nameof(String))]
+    public string Result { get; set; } = "";
+
+    [Option('w', "width", Default = 800, Required = false,
+        HelpText = "Result image width", MetaValue = nameof(Int32))]
+    public int ImageWidth { get; set; }
+
+    [Option('h', "height", Default = 600, Required = false,
+        HelpText = "Result image height", MetaValue = nameof(Int32))]
+    public int ImageHeight { get; set; }
+
+    [Option('b', "background", Default = "White", Required = false,
+        HelpText = "Background color name", MetaValue = nameof(String))]
+    public string BackgroundColorName { get; set; } = "";
+
+    [Option('c', "color", Default = "Black", Required = false,
+        HelpText = "Background color name", MetaValue = nameof(String))]
+    public string TextColorName { get; set; } = "";
+
+    [Option('f', "font", Default = "Arial", Required = false,
+        HelpText = "Font family name", MetaValue = nameof(String))]
+    public string FontFamilyName { get; set; } = "";
+
+    [Option('M', "maxFontSize", Default = 50, Required = false,
+        HelpText = "Maximum font size", MetaValue = nameof(Int32))]
+    public int MaxFontSize { get; set; }
+
+    [Option('m', "minFontSize", Default = 10, Required = false,
+        HelpText = "Minimum font size", MetaValue = nameof(Int32))]
+    public int MinFontSize { get; set; }
 }
--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -5,57 +5,56 @@ using ConsoleClient;
 using TagCloud;
 using TagCloud.Abstractions;
 
-Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
-{
-    if (o.Source is null)
-    {
-        Console.Write("Source filepath: ");
-        o.Source = Console.ReadLine();
-    }
-
-    if (o.Result is null)
-    {
-        Console.Write("Result filepath: ");
-        o.Result = Console.ReadLine();
-    }
-
-    ConfigureContainer(o.Source).Resolve<Client>().Execute(o.Result);
-});
+Parser.Default.ParseArguments<Options>(args)
+    .WithParsed(o => ConfigureContainer(o).Resolve<Client>().Execute(o.Result));
 
 
-IContainer ConfigureContainer(string sourceFilepath)
+IContainer ConfigureContainer(Options options)
 {
     var builder = new ContainerBuilder();
 
-    builder.RegisterInstance(new FileWordsLoader(sourceFilepath))
-        .As<IWordsLoader>().SingleInstance();
+    builder.RegisterInstance(new FileWordsLoader(options.Source)).As<IWordsLoader>();
 
-    var trimToLowerProcessor = new FuncWordsProcessor(words => words.Select(w => w.Trim().ToLower()));
-    builder.RegisterInstance(trimToLowerProcessor)
-        .As<IWordsProcessor>().SingleInstance();
-    var bored = new[] { "мест", "предл", "союз", "част", "межд", "неизв" };
-    builder.RegisterInstance(new MorphWordsProcessor(bored))
-        .As<IWordsProcessor>().SingleInstance();
+    ConfigureProcessors(builder);
 
-    builder.RegisterType<CountWordsTagger>().As<IWordsTagger>();
+    builder.RegisterType<CountWordsTagger>().As<IWordsTagger>().SingleInstance();
 
-    var drawer = new BaseCloudDrawer(new Size(800, 600))
-    {
-        FontFamily = new FontFamily("Arial"),
-        MaxFontSize = 50,
-        MinFontSize = 10,
-        TextColor = Color.Black,
-        BackgroundColor = Color.White
-    };
-    builder.RegisterInstance(drawer)
-        .As<ICloudDrawer>();
-    var pointGenerator = new SpiralPointGenerator(0.1, 0.1);
-    builder.RegisterInstance(new BaseCloudLayouter(new Point(400, 300), pointGenerator))
-        .As<ICloudLayouter>();
-    builder.RegisterType<DrawingCloudCreator>()
-        .As<ICloudCreator>();
+    ConfigureDrawer(builder, options);
+
+    ConfigureLayouter(builder, options);
+
+    builder.RegisterType<DrawingCloudCreator>().As<ICloudCreator>();
 
     builder.RegisterType<Client>().AsSelf().SingleInstance();
 
     return builder.Build();
+}
+
+void ConfigureProcessors(ContainerBuilder builder)
+{
+    var trimToLowerProcessor = new FuncWordsProcessor(words => words.Select(w => w.Trim().ToLower()));
+    builder.RegisterInstance(trimToLowerProcessor).As<IWordsProcessor>();
+
+    var bored = new[] { "мест", "предл", "союз", "част", "межд", "неизв" };
+    builder.RegisterInstance(new MorphWordsProcessor(bored)).As<IWordsProcessor>();
+}
+
+void ConfigureDrawer(ContainerBuilder builder, Options options)
+{
+    var drawer = new BaseCloudDrawer(new Size(options.ImageWidth, options.ImageHeight))
+    {
+        FontFamily = new FontFamily(options.FontFamilyName),
+        MaxFontSize = options.MaxFontSize,
+        MinFontSize = options.MinFontSize,
+        TextColor = Color.FromName(options.TextColorName),
+        BackgroundColor = Color.FromName(options.BackgroundColorName)
+    };
+    builder.RegisterInstance(drawer).As<ICloudDrawer>();
+}
+
+void ConfigureLayouter(ContainerBuilder builder, Options options)
+{
+    var pointGenerator = new SpiralPointGenerator(0.1, 0.1);
+    var center = new Point(options.ImageWidth / 2, options.ImageHeight / 2);
+    builder.RegisterInstance(new BaseCloudLayouter(center, pointGenerator)).As<ICloudLayouter>();
 }
