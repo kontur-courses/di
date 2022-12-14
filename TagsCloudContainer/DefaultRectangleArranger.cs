b52256364033a5fa1193ec0694d4572c1f49da6b
CBist <105043288+CBists@users.noreply.github.com>
2022-12-14 20:55:16 +0500

    Fix

--- TagsCloudContainer/DefaultRectangleArranger.cs
+++ TagsCloudContainer/DefaultRectangleArranger.cs
@@ -6,19 +6,21 @@ namespace TagsCloudContainer
     public class DefaultRectangleArranger : IRectangleArranger
     {
         private readonly ICloudLayouter cloudLayouter;
+
         public DefaultRectangleArranger(ICloudLayouter cloudLayouter)
         {
             this.cloudLayouter = cloudLayouter;
         }
+
         public List<TextContainer> GetContainers(Dictionary<string, int> words, Settings settings)
         {
             var textContainers = new List<TextContainer>();
             var rectangles = new List<Rectangle>();
-            var defaultFont = settings.Font;
+            var baseFont = settings.Font;
             cloudLayouter.StepSize = settings.StepSize;
             foreach (var word in words.Keys)
             {
-                var font = new Font(defaultFont.FontFamily, defaultFont.Size + (words[word] - 1));
+                var font = new Font(baseFont.FontFamily, baseFont.Size + (words[word] - 1), baseFont.Style);
                 var rectSize = word.MeasureString(font);
                 var rect = cloudLayouter.GetNextRectangle(new Point(0, 0), rectangles, rectSize);
                 rectangles.Add(rect);
