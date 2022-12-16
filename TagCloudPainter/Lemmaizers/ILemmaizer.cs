468456ffddbf346582ac609480bdb78379d7f9e1
Ruden <karimov.ruden@mail.ru>
2022-12-16 12:46:20 +0300

    add Lemmaizers

---
+++ TagCloudPainter/Lemmaizers/ILemmaizer.cs
@@ -0,0 +1,7 @@
+﻿namespace TagCloudPainter.Lemmaizers;
+
+public interface ILemmaizer
+{
+    public string GetMorph(string word);
+    public string GetLemma(string word);
+}
---
+++ TagCloudPainter/Lemmaizers/Lemmaizer.cs
@@ -0,0 +1,32 @@
+﻿using MystemHandler;
+using String = System.String;
+
+namespace TagCloudPainter.Lemmaizers;
+
+public class Lemmaizer : ILemmaizer
+{
+    private readonly MystemMultiThread mt;
+    public Mystem.Net.Mystem mystem;
+
+    public Lemmaizer(string pathToMyStem)
+    {
+        mt = new MystemMultiThread(1, pathToMyStem);
+        mystem = new Mystem.Net.Mystem();
+    }
+
+    public string GetMorph(string word)
+    {
+        if (String.IsNullOrWhiteSpace(word))
+            throw new ArgumentNullException();
+
+        return mystem.Mystem.Analyze(word).Result[0].AnalysisResults[0].Grammeme.Split(',', '=')[0];
+    }
+
+    public string GetLemma(string word)
+    {
+        if(String.IsNullOrWhiteSpace(word))
+            throw new ArgumentNullException();
+
+        return mt.StemOneWord(word);
+    }
+}
