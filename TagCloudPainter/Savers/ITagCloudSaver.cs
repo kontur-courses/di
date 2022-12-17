9cf40c20f92f6caa1e1ad1caa047b9f0d60881b3
Ruden <karimov.ruden@mail.ru>
2022-12-17 17:41:44 +0300

    refactor Savers

--- TagCloudPainter/Savers/ITagCloudSaver.cs
+++ TagCloudPainter/Savers/ITagCloudSaver.cs
@@ -1,6 +1,9 @@
-﻿namespace TagCloudPainter.Savers;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace TagCloudPainter.Savers;
 
 public interface ITagCloudSaver
 {
-    void SaveTagCloud(string outputPath, string inputPath);
+    void SaveTagCloud(Bitmap bitmap,string outputPath, ImageFormat format);
 }
--- TagCloudPainter/Savers/TagCloudSaver.cs
+++ TagCloudPainter/Savers/TagCloudSaver.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing.Imaging;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
 using TagCloudPainter.Builders;
 using TagCloudPainter.FileReader;
 using TagCloudPainter.Painters;
@@ -8,28 +9,9 @@ namespace TagCloudPainter.Savers;
 
 public class TagCloudSaver : ITagCloudSaver
 {
-    public readonly ITagCloudElementsBuilder _builder;
-    public readonly ICloudPainter _painter;
-    public readonly IFileReader _reader;
-    public readonly IWordPreprocessor _wordPreprocessor;
 
-    public TagCloudSaver(ICloudPainter painter, ITagCloudElementsBuilder builder, IFileReader reader,
-        IWordPreprocessor wordPreprocessor)
+    public void SaveTagCloud(Bitmap bitmap,string outputPath, ImageFormat format)
     {
-        _painter = painter;
-        _builder = builder;
-        _reader = reader;
-        _wordPreprocessor = wordPreprocessor;
-    }
-
-    public ImageFormat Format { get; set; }
-
-    public void SaveTagCloud(string outputPath, string inputPath)
-    {
-        var words = _reader.ReadFile(inputPath);
-        var dictionary = _wordPreprocessor.GetWordsCountDictionary(words);
-        var tags = _builder.GetTags(dictionary);
-        var btm = _painter.PaintTagCloud(tags);
-        btm.Save(outputPath, Format);
+        bitmap.Save(outputPath, format);
     }
 }
