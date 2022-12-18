7f539041fb2064307367f8486b5ff705c56e3775
FoxDictator <mega.y1979@gmail.com>
2022-12-18 11:31:17 +0500

    Fixed multiple call to reader

--- TagsCloudWinformsApp/InputFilesReader.cs
+++ TagsCloudWinformsApp/InputFilesReader.cs
@@ -4,28 +4,59 @@ namespace TagsCloudWinformsApp;
 
 internal class InputFilesReader : IWordSequenceProvider, IWordFilterProvider
 {
-    public string? FilterPath = null;
+    public string? FilterPath{
+        get => filterPath;
+        set
+        {
+            filterPath = value;
+            readFilter = true;
+        }
+    }
 
-    public string? InputPath = null;
+    private string? filterPath = null;
+    private bool readFilter = true;
+
+    public string? InputPath
+    {
+        get => inputPath;
+        set
+        {
+            inputPath = value;
+            readInput = true;
+        }
+    }
+
+    private string? inputPath = null;
+    private bool readInput = true;
 
     public Result<IEnumerable<string>> WordFilter
     {
         get
         {
+            if (!readFilter) return wordFilter;
             if (FilterPath == null) return new(new List<string>());
-            var wordSeq = new List<string>();
-            foreach (var line in File.ReadAllLines(FilterPath)) wordSeq.AddRange(line.Split());
-            return new Result<IEnumerable<string>>(wordSeq);
+            var wordFilt = new List<string>();
+            foreach (var line in File.ReadAllLines(FilterPath)) wordFilt.AddRange(line.Split());
+            wordFilter = new(wordFilt);
+            readFilter = false;
+            return wordFilter;
         }
     }
+
+    private Result<IEnumerable<string>> wordFilter;
 
     public Result<IEnumerable<string>> WordSequence
     {
         get
         {
+            if (!readInput) return wordSequence;
             var wordSeq = new List<string>();
             foreach (var line in File.ReadAllLines(InputPath)) wordSeq.AddRange(line.Split());
-            return new Result<IEnumerable<string>>(wordSeq);
+            wordSequence = new(wordSeq);
+            readInput = false;
+            return wordSequence;
         }
     }
+
+    private Result<IEnumerable<string>> wordSequence;
 }
