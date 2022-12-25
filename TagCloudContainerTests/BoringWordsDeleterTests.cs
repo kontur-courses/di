d1344eafcad4e9b2d4d3a5fd9c8a161fc8513850
massshasch <massshasch@gmail.com>
2022-12-25 19:23:34 +0500

    Fixed tests

--- TagCloudContainerTests/BoringWordsDeleterTests.cs
+++ TagCloudContainerTests/BoringWordsDeleterTests.cs
@@ -5,22 +5,24 @@ using NUnit.Framework;
 using TagsCloudContainer;
 using TagsCloudContainer.UI;
 
-/*namespace TagCloudContainerTests
+namespace TagCloudContainerTests
 {
     public class BoringWordsDeleterTests
     {
         private IUi parsedArguments;
+
         [SetUp]
         public void SetUp()
         {
             var app = new ConsoleUiSettings();
-           parsedArguments = Parser.Default.ParseArguments<ConsoleUiSettings>(new string[] { }).Value;
+            parsedArguments = Parser.Default.ParseArguments<ConsoleUiSettings>(new string[] { }).Value;
         }
+
         [Test]
         public void BoringWordsDeleter_ShouldNotDeleteNotBoringWords()
         {
             parsedArguments.ExceptPartOfSpeech = "";
-            var notBoringWords = new[] {"котик", "котенок", "кошка", "кисуля", "котяра"};
+            var notBoringWords = new[] { "котик", "котенок", "кошка", "кисуля", "котяра" };
             var result = BoringWordsDeleter.DeleteBoringWords(notBoringWords, parsedArguments);
             result.Should().BeEquivalentTo(notBoringWords);
         }
@@ -28,20 +30,10 @@ using TagsCloudContainer.UI;
         [Test]
         public void BoringWordsDeleter_ShouldDeleteBoringWords()
         {
-            parsedArguments.ExceptPartOfSpeech = "";
-            var notBoringWords = new[] {"кто", "на", "где", "а", "и"};
+            parsedArguments.IncludePartOfSpeech = "";
+            var notBoringWords = new[] { "кто", "на", "где", "а", "и" };
             var result = BoringWordsDeleter.DeleteBoringWords(notBoringWords, parsedArguments);
             result.Should().BeEmpty();
         }
-
-        [Test]
-        public void BoringWordsDeleter_ShouldDeleteEmptyStrings()
-        {
-            var notBoringWords = new[] {"а", "и", "не", "спатеньки"};
-            var result = BoringWordsDeleter.DeleteBoringWords(notBoringWords, parsedArguments);
-            result.Should().BeEquivalentTo("спатеньки");
-        }
     }
-
 }
-    */
