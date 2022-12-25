acadc6f4f13dbb3d9a87dfd31717c11bcae42172
sasharumega <dredd88888888@gmail.com>
2022-12-25 15:07:18 +0500

    Рефакторинг

---
+++ TagCloudDI/TagCloudContainer/Interfaces/ICloudCreateSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace TagCloudContainer.Interfaces
+{
+    public interface ICloudCreateSettings
+    {
+        public IPointer PointFigure { get; }
+        public IRectangleBuilder RectangleBuilder { get; set; }
+    }
+}
