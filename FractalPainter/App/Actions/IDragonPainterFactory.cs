aa77c66fed1c511c598a566417861b79f781b288
Alex <96792931+SilversShade@users.noreply.github.com>
2024-01-22 21:30:09 +0500

    Delete FractalPainter directory

--- FractalPainter/App/Actions/IDragonPainterFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using FractalPainting.App.Fractals;
-using FractalPainting.Infrastructure.Common;
-
-namespace FractalPainting.App.Actions;
-
-public interface IDragonPainterFactory
-{
-    DragonPainter CreateDragonPainter(DragonSettings settings);
-}
--- FractalPainter/Solved/Step05/Infrastructure/Injection/INeed.cs
+++
@@ -1,7 +0,0 @@
-namespace FractalPainting.Solved.Step05.Infrastructure.Injection
-{
-    public interface INeed<in T>
-    {
-        void SetDependency(T dependency);
-    }
-}
