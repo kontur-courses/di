12149fbe73dc2bd5429daa5b85132a5c3a3eaa20
daHil <ssocmain@gmail.com>
2024-01-24 04:20:38 +0500

    added file reader

---
+++ TagsCloudPainter/FileReader/IFileReader.cs
@@ -0,0 +1,7 @@
+﻿namespace TagsCloudPainter.FileReader
+{
+    public interface IFileReader
+    {
+        public string ReadFile(string path);
+    }
+}
---
+++ TagsCloudPainter/FileReader/TextFileReader.cs
@@ -0,0 +1,35 @@
+﻿using Spire.Doc;
+
+namespace TagsCloudPainter.FileReader
+{
+    public class TextFileReader : IFileReader
+    {
+        public string ReadFile(string path)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException();
+
+            return Path.GetExtension(path) switch
+            {
+                ".txt" => ReadTxtFile(path),
+                ".doc" => ReadDocFile(path),
+                ".docx" => ReadDocFile(path),
+                _ => throw new ArgumentException("Incorrect file extension. Supported file extensions: txt, doc, docx"),
+            };
+        }
+
+        private static string ReadTxtFile(string path)
+        {
+            return File.ReadAllText(path).Trim();
+        }
+
+        private static string ReadDocFile(string path)
+        {
+            var doc = new Document();
+            doc.LoadFromFile(path);
+            var text = doc.GetText();
+            var lastIndexOfSpirePart = text.IndexOf(Environment.NewLine);
+            return text.Substring(lastIndexOfSpirePart + 2).Trim();
+        }
+    }
+}
