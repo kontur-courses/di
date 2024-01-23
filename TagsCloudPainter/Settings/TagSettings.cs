ec0dca0f453fc97cfca203f53e82f1fbaf3464ac
daHil <ssocmain@gmail.com>
2024-01-24 04:23:57 +0500

    added tag builder

---
+++ TagsCloudPainter/Settings/TagSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace TagsCloudPainter.Settings
+{
+    public class TagSettings
+    {
+        public int TagFontSize { get; set; }
+        public string TagFontName { get; set; } = "Arial";
+        public Color TagColor { get; set; }
+    }
+}
---
+++ TagsCloudPainter/Tags/ITagsBuilder.cs
@@ -0,0 +1,7 @@
+﻿namespace TagsCloudPainter.Tags
+{
+    public interface ITagsBuilder
+    {
+        public List<Tag> GetTags(List<string> words);
+    }
+}
