a03b6f79e690c933ffb2d44594957cce1cb4ffce
IlyaO <rikgogs@gmail.com>
2024-01-23 23:24:41 +0500

    add tests

---
+++ TagsCloudContainerTests/CircularCloudLayouter_Should.cs
@@ -0,0 +1,94 @@
+﻿using TagsCloudContainer.Algorithm;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudContainer.Infrastucture.Settings;
+using System.Drawing;
+using TagsCloudContainer.Infrastucture;
+
+namespace TagsCloudContainerTests
+{
+
+    public class CircularCloudLayouter_Should
+    {
+        private CircularCloudLayouter layouter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var algSettings = new AlgorithmSettings();
+            var imgSettings = new ImageSettings();
+            layouter = new CircularCloudLayouter(algSettings, imgSettings);
+        }
+
+        [Test]
+        public void CreateAnEmptyLayout_WhenFrequencyDictionaryIsEmpty()
+        {
+            var textRectangles = layouter.GetRectangles(new Dictionary<string, int>());
+
+            textRectangles.Should().BeEmpty();
+        }
+
+        [Test]
+        public void CreateLayoutWithoutIntersections_WhenNoIdenticalFrequencies()
+        {
+            var wordsFrequencies = new Dictionary<string, int>()
+        {
+            {"яблоко" , 1},
+            {"банан", 2},
+            {"груша", 6},
+            {"мандарин", 17},
+        };
+
+            var textRectangles = layouter.GetRectangles(wordsFrequencies);
+
+            IsIntersections(textRectangles).Should().BeFalse();
+        }
+
+        [Test]
+        public void CreateLayoutWithoutIntersections_WhenIdenticalFrequencies()
+        {
+            var wordsFrequencies = new Dictionary<string, int>()
+        {
+            {"яблоко" , 1},
+            {"банан", 6},
+            {"груша", 6},
+            {"мандарин", 17},
+            {"апельсин", 17},
+
+        };
+
+            var textRectangles = layouter.GetRectangles(wordsFrequencies);
+
+            IsIntersections(textRectangles).Should().BeFalse();
+        }
+
+        [Test]
+        public void CreateLayout_WhenFrequencyWordIncreasesTheSizeRectangleIncreases()
+        {
+            var wordsFrequencies = new Dictionary<string, int>()
+        {
+            {"купец" , 1},
+            {"упецк", 2},
+            {"пецку", 6},
+            {"ецкуп", 17},
+        };
+
+            var textRectangles = layouter.GetRectangles(wordsFrequencies.OrderBy(word => word.Value).ToDictionary());
+
+            textRectangles.Should().BeInAscendingOrder(textRectangle => textRectangle.Area);
+        }
+
+        public bool IsIntersections(List<TextRectangle> textRectangles)
+        {
+            var rectList = textRectangles.Select(x => x.Rectangle).ToList();
+
+            for (int i = 0; i < rectList.Count; i++)
+                for (int j = i + 1; j < rectList.Count; j++)
+                    if (rectList[i].IntersectsWith(rectList[j]))
+                        return true;
+
+            return false;
+        }
+
+    }
+}
---
+++ TagsCloudContainerTests/WordProcessor_Should.cs
@@ -0,0 +1,105 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using StopWord;
+using TagsCloudContainer.Algorithm;
+
+namespace TagsCloudContainerTests
+{
+    public class WordProcessor_Should
+    {
+        private WordProcessor wordProcessor;
+
+        [SetUp]
+        public void SetUp()
+        {
+            wordProcessor = new WordProcessor(new FileParser());
+        }
+
+        [Test]
+        public void GetInterestingWords_WhenFileWithBoringWordsIsEmpty()
+        {
+            var sourceFilePath = GetProjectDirectory() + @"\src\sourceData.txt";
+            var boringFilePath = GetProjectDirectory() + @"\src\emptyData.txt";
+            var stopWords = StopWords.GetStopWords("ru");
+
+            var interestingWords = wordProcessor.GetInterestingWords(sourceFilePath, boringFilePath);
+
+            interestingWords.Should().NotContain(stopWords);
+        }
+
+        [Test]
+        public void GetInterestingWords_WhenFileWithBoringWordsIsNotEmpty()
+        {
+            var sourceFilePath = GetProjectDirectory() + @"\src\sourceData.txt";
+            var boringFilePath = GetProjectDirectory() + @"\src\boringData.txt";
+            var stopWords = StopWords.GetStopWords("ru");
+
+            var interestingWords = wordProcessor.GetInterestingWords(sourceFilePath, boringFilePath);
+            var boringWords = new FileParser().ReadWordsInFile(boringFilePath);
+
+            interestingWords.Should().NotContain(stopWords);
+            interestingWords.Should().NotContain(boringWords);
+        }
+
+        [Test]
+        public void CorrectlyCalculatesFrequencyWords()
+        {
+            var expectedDictonary = new Dictionary<string, int>
+            {
+                { "js", 3 },
+                { "python", 4},
+                { "c#", 5 },
+                { "c++", 2 },
+                { "rust", 2 },
+                { "go", 4 },
+                { "1c", 1 }
+            };
+            var sourceFilePath = GetProjectDirectory() + @"\src\sourceData.txt";
+            var boringFilePath = GetProjectDirectory() + @"\src\emptyData.txt";
+
+            var wordsFrequency = wordProcessor.CalculateFrequencyInterestingWords(sourceFilePath, boringFilePath);
+
+            wordsFrequency.Should().Contain(expectedDictonary);
+        }
+
+        [Test]
+        public void GiveFrequencyWordsDescendingOrder()
+        {
+            var expectedDictonary = new Dictionary<string, int>
+            {
+                { "js", 3 },
+                { "python", 4},
+                { "c#", 5 },
+                { "c++", 2 },
+                { "rust", 2 },
+                { "go", 4 },
+                { "1c", 1 }
+            };
+            var sourceFilePath = GetProjectDirectory() + @"\src\sourceData.txt";
+            var boringFilePath = GetProjectDirectory() + @"\src\emptyData.txt";
+
+            var wordsFrequency = wordProcessor.CalculateFrequencyInterestingWords(sourceFilePath, boringFilePath);
+
+            wordsFrequency.Should().BeInDescendingOrder(word => word.Value);
+        }
+
+        [Test]
+        public void ReturnEmptyDictionary_WhenSourceWordFileIsEmpty()
+        {
+            var sourceFilePath = GetProjectDirectory() + @"\src\emptyData.txt";
+            var boringFilePath = GetProjectDirectory() + @"\src\emptyData.txt";
+
+            var wordsFrequency = wordProcessor.CalculateFrequencyInterestingWords(sourceFilePath, boringFilePath);
+
+            wordsFrequency.Should().BeEmpty();
+        }
+
+        public string GetProjectDirectory()
+        {
+            var binDirectory = AppContext.BaseDirectory;
+            var projectDirectory = Directory.GetParent(binDirectory).Parent.Parent.Parent.FullName;
+
+            return projectDirectory;
+        }
+    }
+}
