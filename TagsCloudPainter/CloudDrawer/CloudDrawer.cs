ee647198ed1e45fadaa70809edc9d49428944f64
daHil <ssocmain@gmail.com>
2024-01-24 04:28:30 +0500

    added cloud drawer

---
+++ TagsCloudPainter/CloudDrawer/CloudDrawer.cs
@@ -0,0 +1,56 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using TagsCloudPainter.Settings;
+
+namespace TagsCloudPainter.Drawer
+{
+    public class CloudDrawer
+    {
+        private readonly TagSettings tagSettings;
+        private readonly CloudSettings cloudSettings;
+
+        public CloudDrawer(TagSettings tagSettings, CloudSettings cloudSettings) 
+        {
+            this.tagSettings = tagSettings;
+            this.cloudSettings = cloudSettings;
+        }
+
+        public Bitmap DrawCloud(TagsCloud cloud, int imageWidth, int imageHeight)
+        {
+            if (cloud.Tags.Count == 0)
+                throw new ArgumentException("rectangles are empty");
+            if (imageWidth <= 0 || imageHeight <= 0)
+                throw new ArgumentException("either width or height of rectangle size is not positive");
+
+            var drawingScale = CalculateObjectDrawingScale(cloud.GetWidth(), cloud.GetHeight(), imageWidth, imageHeight);
+            var bitmap = new Bitmap(imageWidth, imageHeight);
+            var graphics = Graphics.FromImage(bitmap);
+            var pen = new Pen(tagSettings.TagColor);
+
+            graphics.TranslateTransform(-cloud.Center.X, -cloud.Center.Y);
+            graphics.ScaleTransform(drawingScale, drawingScale, MatrixOrder.Append);
+            graphics.TranslateTransform(cloud.Center.X, cloud.Center.Y, MatrixOrder.Append);
+            graphics.Clear(cloudSettings.BackgroundColor);
+            foreach(var tag in cloud.Tags)
+            {
+                var font = new Font(tagSettings.TagFontName, tag.Key.FontSize);
+                graphics.DrawString(tag.Key.Value, font, pen.Brush, tag.Value.Location);
+            }
+
+            return bitmap;
+        }
+
+        public static float CalculateObjectDrawingScale(float width, float height, float imageWidth, float imageHeight)
+        {
+            var scale = 1f;
+            var scaleAccuracy = 0.05f;
+            var widthScale = scale;
+            var heightScale = scale;
+            if (width * scale > imageWidth)
+                widthScale = imageWidth / width - scaleAccuracy;
+            if (height * scale > imageHeight)
+                heightScale = imageHeight / height - scaleAccuracy;
+            return Math.Min(widthScale, heightScale);
+        }
+    }
+}
