fe33b622e3573ea326a2a5d805f53c3f85a6a45f
Arman Mukanov <mukanovarman777@gmail.com>
2024-01-25 15:18:32 +0500

    test: добавил тесты.

---
+++ TagsCloudContainer.Tests/TagsCloudGeneratorTests.cs
@@ -0,0 +1,15 @@
+namespace TagsCloudContainer.Tests;
+
+public class TagsCloudGeneratorTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
