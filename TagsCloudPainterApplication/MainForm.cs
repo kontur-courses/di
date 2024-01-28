49959c5009398f35189ab35cdbd1515ab412708b
daHil <ssocmain@gmail.com>
2024-01-28 05:59:02 +0500

    refactored exceptions throwing

--- TagsCloudPainterApplication/MainForm.cs
+++ TagsCloudPainterApplication/MainForm.cs
@@ -7,8 +7,11 @@ public partial class MainForm : Form
 {
     public MainForm(IUiAction[] actions, ImageSettings imageSettings, PictureBoxImageHolder pictureBox)
     {
-        if (actions is null || actions.Length == 0 || imageSettings is null || pictureBox is null)
-            throw new ArgumentNullException("MainForm cann't be injected with nullable reference");
+        ArgumentNullException.ThrowIfNull(actions);
+        if (actions.Count() == 0)
+            throw new ArgumentException(nameof(actions));
+        ArgumentNullException.ThrowIfNull(imageSettings);
+        ArgumentNullException.ThrowIfNull(pictureBox);
 
         ClientSize = new Size(imageSettings.Width, imageSettings.Height);
 
